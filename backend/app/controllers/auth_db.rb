8fbb4ddbc4c99dd442364af9e6c000d6fab076c9
Mark Triggs <mark@dishevelled.net>
2012-07-10 10:02:02 +1000

    Added simple password authentication.

--- backend/app/controllers/auth_db.rb
+++ backend/app/controllers/auth_db.rb
@@ -1,26 +1,47 @@
+require 'bcrypt'
+
 class DBAuth
-  def initialize
+
+  include BCrypt
+
+  def add_user(username, password)
+    pwhash = Password.create(password)
+
+    DB.open do |db|
+      db[:auth_db].insert(:username => username,
+                          :pwhash => pwhash,
+                          :create_time => Time.now,
+                          :last_modified => Time.now)
+    end
+  rescue Sequel::DatabaseError => ex
+    if DB.is_integrity_violation(ex)
+      raise ConflictException.new("User '#{username}' already exists.")
+    end
   end
+
 
   def login(username, password)
     DB.open do |db|
-      puts "Do something clever with #{db.inspect} here"
-    end
+      pwhash = db[:auth_db].filter(:username => username).get(:pwhash)
 
-    return true
+      return (pwhash and (Password.new(pwhash) == password))
+    end
   end
 end
 
 
 class ArchivesSpaceService < Sinatra::Base
 
-  post '/auth/db/login' do
+  configure do
+    set :db_auth, DBAuth.new
+  end
+
+
+  post '/auth/db/user/:username/login' do
     ensure_params ["username" => {:doc => "Your username"},
                    "password" => {:doc => "Your password"}]
 
-    db_auth = DBAuth.new
-
-    if db_auth.login(params[:username], params[:password])
+    if settings.db_auth.login(params[:username], params[:password])
       session = Session.new
       session[:user] = params[:username]
       session[:login_time] = Time.now
@@ -28,9 +49,17 @@ class ArchivesSpaceService < Sinatra::Base
 
       json_response({:session => session.id})
     else
-      [403, {}, ["No dice"]]
+      json_response({:error => "Login failed"}, 403)
     end
 
+  end
+
+
+  post '/auth/db/user/:username' do
+    ensure_params ["username" => {:doc => "Username for new account"},
+                   "password" => {:doc => "Password for new account"}]
+
+    settings.db_auth.add_user(params[:username], params[:password])
   end
 
 
