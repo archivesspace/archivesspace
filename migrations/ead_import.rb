bacc670f4b60e04b1820ace05cb801adc203366e
Brian Hoffman <brianjhoffman@gmail.com>
2012-07-25 17:01:36 -0400

    added json schema hooks

--- migrations/ead_import.rb
+++ migrations/ead_import.rb
@@ -1,9 +1,9 @@
 # written for Ruby 1.9.3
 
-require_relative "lib/classes.rb"
+require_relative File.join("..", "common", "jsonmodel")
+require_relative File.join("lib", "classes")
 
 input_file = ARGV[0]
-
 
 if input_file != nil and File.exists? input_file
   puts input_file.inspect
