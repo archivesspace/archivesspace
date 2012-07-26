03bf0b27c726622bf592ce9771aa1f9964d0d308
Payten Giles <payten.giles@gmail.com>
2012-07-26 15:04:36 +1000

    enable controller based stylesheets; add demo tree to resource#new stub;

--- frontend/app/assets/javascripts/resources.js
+++
@@ -1,9 +0,0 @@
-//= require plugins/jstree
-
-
-$(function() {
-   // init the archives tree
-   $(".archives-tree").jstree({
-       "plugins": ["themes","json_data","ui","crrm","cookies","dnd","search","types","hotkeys","contextmenu"]       
-   })	
-});
--- frontend/app/helpers/application_helper.rb
+++ frontend/app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
   end
   
   def include_controller_js
-   javascript_include_tag "#{controller.controller_name}" if File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.js")
-  end  
+   javascript_include_tag "#{controller.controller_name}" if File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.js") ||  File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.js.erb")
+  end
+  
+  def include_controller_css
+   stylesheet_link_tag "#{controller.controller_name}" if File.exists?("#{Rails.root}/app/assets/stylesheets/#{controller_name}.css") ||  File.exists?("#{Rails.root}/app/assets/stylesheets/#{controller_name}.css.less")
+  end
+  
 end
