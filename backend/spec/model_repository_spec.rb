b07f6db1c09c61127c9e2d331659532a03f26fbf
Mark Triggs <mark@dishevelled.net>
2012-07-30 13:08:50 +1000

    Added test for accession ID uniqueness in the model

--- backend/spec/model_repository_spec.rb
+++ backend/spec/model_repository_spec.rb
@@ -68,6 +68,28 @@ describe 'Repository model' do
   end
 
 
+  it "Enforces ID uniqueness" do
+    repo = Repository.create(:repo_id => "TESTREPO",
+                             :description => "My new test repository")
+
+    lambda {
+      2.times do
+        repo.create_accession(JSONModel(:accession).
+                              from_hash({
+                                          "accession_id_0" => "1234",
+                                          "accession_id_1" => "5678",
+                                          "accession_id_2" => "9876",
+                                          "accession_id_3" => "5432",
+                                          "title" => "Papers of Mark Triggs",
+                                          "accession_date" => Time.now,
+                                          "content_description" => "Unintelligible letters written by Mark Triggs addressed to Santa Claus",
+                                          "condition_description" => "Most letters smeared with jam"
+                                        }))
+      end
+    }.should raise_error(Sequel::ValidationFailed)
+  end
+
+
   it "Allows resources to be created" do
     repo = Repository.create(:repo_id => "TESTREPO",
                              :description => "My new test repository")
