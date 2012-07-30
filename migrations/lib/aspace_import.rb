07ffd6e3bc16e39877de101606c8c150e2b0b548
Brian Hoffman <brianjhoffman@gmail.com>
2012-07-29 21:46:09 -0400

    checking this in because somehow an old version popped up in a merged branch

--- migrations/lib/aspace_import.rb
+++ migrations/lib/aspace_import.rb
@@ -1,67 +1,59 @@
 #!/usr/bin/env ruby
+require_relative File.join("..", "..", "common", "jsonmodel")
 
-puts "ASpace module"
-
-#method for accessing an eigenclass
-#class Object; def eigenclass; class << self; self end end end
-
-module ASpaceImporter
+class ASpaceImporter
+  include JSONModel
+  @@importers = { }
+  def self.register_importer name
+    @@importers[name] = self
+  end
+  def self.create_importer name
+    i = @@importers[name]
+    if i
+      i.new
+    else
+      puts "Bad importer type or importer not found for: #{name}"
+    end
+  end
+  def self.importer name, superclass=ASpaceImporter, &block
+    c = Class.new(superclass, &block)
+    c.register_importer(name)
+    Object.const_set("#{name.to_s.capitalize}ASpaceImporter", c)
+  end
   
-  class ASpaceImporterNotFound < NameError; end
-  class << self
-    def create type 
-      puts "Creating #{type}"
-      puts "Registered handler: ok"
-      puts self[type] 
-      (self[type] ||= const_get("#{type.to_s.capitalize}ASpaceImporter").new)
-    rescue NameError => e
-      raise ASpaceImporterNotFound, "Bad importer type or importer not found for: #{type}" if e.class == NameError && e.message =~ /[^: ]ASpaceImporter/
-      raise
-    end
-    
-    def []=(type, klass)
-      puts "Assiging #{type} to #{klass.name}"
-      @importers ||= {type => klass}
-      def []=(type, klass)
-        @importers[type] = klass
+  def import(type, hsh)
+    #Run the code in the importer
+    #Make sure the type is importable, i.e., that it has a class
+    puts type
+    puts ALLOWFAILURES
+    begin
+      if JSONModel(type) 
+        puts JSONModel(type).name
+      else
+        raise ArgumentError.new("Don't know how to import a #{type}")
       end
-      klass
-    end
-    
-    def [](type)
-      @importers ||= {}
-      def [](type)
-        @importers[type]
+      #make sure hsh is a hash
+      if hsh.is_a?(Hash)
+      else
+        raise ArgumentError.new("Expected a Hash got #{hsh}") #TODO rescue and continue if opted for
+      end    
+      puts "Importing #{hsh.to_json}"
+      jo = JSONModel(type).from_hash(hsh)
+      puts "Here it is: #{jo.to_json}"
+    rescue ArgumentError => e
+      if ALLOWFAILURES
+        puts "Warning: #{e.message}"
+      else
+        raise e
       end
-      nil
     end
+
     
-    def included klass
-      puts "Included method of the eigenclass"
-      self[klass.name[/[[:upper:]][[:lower]]*/].downcase.to_sym] = klass if klass.is_a? Class
-    end
-    
-    def test
-      puts "This is a mixed in method"
-    end
   end
-end
+  
 
-def ASpaceImporter type
-  puts "ASpaceImporter method #{type}"
-  ASpaceImporter[type] = self.name
-  ASpaceImporter
+
 end
 
 
 
-
-class ASpaceRecordPoster
-  def post_json(record_type, json_record)
-    #TODO - Work out the POST URL by the record type
-    #TODO - config for ASPACE host, port, repository, auth info
-    #TODO - get back an ID and confirmation from ASpace
-    puts "Posting new '#{record_type}' to ASpace:" 
-    puts json_record;
-  end
-end
