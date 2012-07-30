3a0de5e9878d574dffc32142f63b65c326573890
Brian Hoffman <brianjhoffman@gmail.com>
2012-07-29 21:52:41 -0400

    newer version

--- migrations/import.rb
+++ migrations/import.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require_relative File.join("lib", "bootstrap")
 
 options = {}
+
+=begin
+Possible params to add:
+  - flag for continuing if a single import fails
+  - padd -h to the importer so it can declare what params it wants
+=end
+
+::ALLOWFAILURES = false
 optparse = OptionParser.new do|opts|
   opts.banner = "Usage: ead_import.rb [options] ead_file"
   options[:dry_run] = false
@@ -16,15 +25,31 @@ optparse = OptionParser.new do|opts|
     puts opts
     exit
   end
+  opts.on( '-a', '--allow_failures', 'Do not stop because an import fails') do
+    ::ALLOWFAILURES = true
+  end
 end
 
 optparse.parse!
 
-input_file = ARGV[0]
-importer_file = File.join("importers", options[:importer])
-require_relative(importer_file)
 
-if File.exists? input_file
-  i = Importer.new # is it a bad idea to have a library of files that all define the 'Importer' class?
-  i.read(input_file)
+if options[:importer]
+  i = ASpaceImporter.create_importer(options[:importer].to_sym)
+  i.run
+  $types.each do |k, v|
+    puts "#{k} #{v}"
+  end
+  $models.each do |k|
+    puts "#{k}"
+  end
 end
+
+
+#input_file = ARGV[0]
+#importer_file = File.join("importers", options[:importer])
+#require_relative(importer_file)
+
+#if File.exists? input_file
+#  i = Importer.new # is it a bad idea to have a library of files that all define the 'Importer' class?
+#  i.read(input_file)
+#end
