c9ad92722ca4b66996982e734f888f7c5b0730c8
Brian Hoffman <brianjhoffman@gmail.com>
2012-07-31 21:35:10 -0400

    dummy importer for a chain of records

--- migrations/importers/foo.rb
+++ migrations/importers/foo.rb
@@ -4,35 +4,53 @@ ASpaceImporter.importer :foo do
     "A test importer that takes 0 arguments"
   end
   def run
-    test_accession = {  
-                      "title" => "Foo Accession", 
-                      "accession_id_0" => "abc123",
-                      "content_description" => "foooo",
-                      "condition_description" => "terrible",
-                      "accession_date" => "2012-01-01",
-                      "fake_field" => "A field the schema should filter out"
-                      }  
-    #Good import:
-    import :accession, test_accession
+    # Step 1: Create a Repo
+    test_repo = {  
+                "repo_id" => "#{(0...8).map{ ('a'..'z').to_a[rand(26)] }.join}", 
+                "description" => "Integalactic Employment Agency"
+                      }
 
-    #Bad Import - Good type, bad hash
-    import :accession, "flub"
+    res = import :repository, test_repo
+    repo_id = res.parsed_response['id']
+    puts "REPO ID: #{repo_id}"
+
+    # Step 2: Create a Collection (as it's called for now)
+    test_coll = {
+                "id_0" => "#{(0...4).map{ ('a'..'z').to_a[rand(26)] }.join}", 
+                "title" => "Resource #{('A'..'Z').to_a[rand(26)]} - #{('A'..'Z').to_a[rand(26)]}"
+              }
+    test_coll_params = {'repo_id' => repo_id}
+
+    res = import :collection, test_coll, test_coll_params
+
+    coll_id = res.parsed_response['id']
+    puts "COLLECTION ID: #{coll_id}"
     
-    #Bad Import - Bad type, good hash
-    import :dfsdfsdfs, test_accession
+    #Step 3: Create an Archival Object
+    test_ao = {
+              "id_0" => "#{(0...4).map{ ('a'..'z').to_a[rand(26)] }.join}",
+              "title" => "Thing or Things #{('A'..'Z').to_a[rand(26)]} - #{('A'..'Z').to_a[rand(26)]}"
+    }
+    test_ao_params = {'repo_id' => repo_id, 'collection' => coll_id}
     
+    res = import :archival_object, test_ao, test_ao_params
+
+    ao_id = res.parsed_response['id']
+    puts "OBJECT ID #{ao_id}"
+    
+    #Step 4: Create a nested Archival Object
+    test_ao2 = {
+              "id_0" => "#{(0...4).map{ ('a'..'z').to_a[rand(26)] }.join}",
+              "title" => "Thing or Things #{('A'..'Z').to_a[rand(26)]} - #{('A'..'Z').to_a[rand(26)]}"
+    }
+    test_ao2_params = {'repo_id' => repo_id, 'collection' => coll_id, 'parent' => ao_id}
+    
+    res = import :archival_object, test_ao, test_ao2_params
+
+    ao2_id = res.parsed_response['id']
+    puts "OBJECT ID #{ao2_id}"   
     
   end  
 end
 
-
-
-
-
-#class FooImporter < ASpaceImporter
-#  register_import_key :foo
-#  def display 
-#    puts "Here I am, Foo"
-#  end
-#end
 
