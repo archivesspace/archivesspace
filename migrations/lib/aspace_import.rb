049dbc4ffbaba19f9b63ab84b0e85df1579085f3
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-02 16:22:39 -0400

    refactored to pass tests

--- migrations/lib/aspace_import.rb
+++ migrations/lib/aspace_import.rb
@@ -1,57 +1,70 @@
-#!/usr/bin/env ruby
-#require 'net/http'
-require 'httparty'
-
-class ASpaceParty
-  include HTTParty
-  base_uri ASpaceImportConfig::ASPACE_BASE
-end
-
 class ASpaceImporter
   include JSONModel
   @@importers = { }
-  def self.register_importer name
-    @@importers[name] = self
+  def self.importer_count
+    @@importers.length
   end
-  def self.create_importer options
-    i = @@importers[options[:importer].to_sym]
-    if i
-      i.new options
-    else
-      puts "Bad importer type or importer not found for: #{name}"
-    end
-  end
-  def self.importer name, superclass=ASpaceImporter, &block
-    # TODO - Ensure the name / key hasn't been registered already
-    c = Class.new(superclass, &block)
-    c.register_importer(name)
-    Object.const_set("#{name.to_s.capitalize}ASpaceImporter", c)
-  end
-  
   def self.list
     puts "The following importers are available"
     @@importers.each do |i, klass|
       puts "#{i} -- #{klass.name} -- #{klass.profile}"
     end
   end
+  def self.create_importer options
+    i = @@importers[options[:importer].to_sym]
+    if i.usable
+      i.new options
+    else
+      raise StandardError.new("Unusable importer or importer not found for: #{name}")
+    end
+  end
+  def self.importer name, superclass=ASpaceImporter, &block
+    if @@importers.has_key? name
+      raise StandardError.new("Attempted to register #{name} a second time")
+    else
+      c = Class.new(superclass, &block)
+      Object.const_set("#{name.to_s.capitalize}ASpaceImporter", c)
+      @@importers[name] = c
+      true
+    end
+  end
   
-  def initialize opts
-    @relaxed, @verbose, @dry, @repo = opts[:relaxed], opts[:verbose], opts[:dry], opts[:repo]
+  def self.usable
+    if !defined? self.profile
+      return false
+    elsif !method_defined? :run
+      return false
+    else
+      return true
+    end
+  end
+  def initialize opts={ } 
+#    @relaxed, @verbose, @dry, @repo = opts[:relaxed], opts[:verbose], opts[:dry], opts[:repo]
+    opts.each do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
     @goodimports = 0
     @badimports = 0   
   end
   
   def report
-    puts "#{@goodimports} records successfully imported"
+    puts "#{@goodimports} records imported"
     puts "#{@badimports} records failed to import"
+  end
+  
+  def run
+    raise StandardError.new("Unexpected error: run method must be defined by a subclass")
   end
        
   def import(type, hsh, params = { })
     @response = nil
+    if params[:repo_id] == nil
+      params[:repo_id] = @repo
+    end
     begin
       if JSONModel(type) 
       else
-        raise ArgumentError.new("Don't know how to import a #{type}")
+        raise ArgumentError.new("Don't know how to import a #{type}, mate!")
       end
       if hsh.is_a?(Hash)
       else
@@ -80,11 +93,12 @@ class ASpaceImporter
           puts "Posting an Archival Object" if @verbose
           opts = {:body => params.merge('archival_object' => jo.to_json)}
           @response = ASpaceParty.post('/archival_object', opts)
-          puts @response.insepect if $DEBUG
+          puts @response.inspect if $DEBUG
         else
           puts "This error should never happen, type = #{type}"
         end 
-        if defined? @response.parsed_response['id']
+        if @response.parsed_response['id']
+          puts "RETURNED ID = #{@response.parsed_response['id']}"
           @goodimports += 1
           {:id => @response.parsed_response['id']}
         else
