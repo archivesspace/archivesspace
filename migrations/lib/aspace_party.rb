975c8c6d6c9490ba2be342242845149cd6b5ff1c
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-07 14:57:10 -0400

    commiting so I can rm

--- migrations/lib/aspace_party.rb
+++ migrations/lib/aspace_party.rb
@@ -3,5 +3,29 @@ require 'httparty'
 class ASpaceParty
   include HTTParty
   base_uri ASpaceImportConfig::ASPACE_BASE
+  default_timeout 5
+
+  def initialize(repo_key, dry = false)
+    @repo_key = repo_key
+    puts "REPO #{@repo_key}"
+    @dry = dry
+  end
+
+  def post(jo)
+    options = { :body => jo.to_json }
+    post_uri = jo.class.uri_for( nil,  :repo_id => @repo_key )
+    if @dry
+      puts "(Not) Posting to #{post_uri}: #{jo.to_json}"
+      { :key => 9999 }
+    else
+      begin
+      response = self.class.post( post_uri, options )
+      puts response.code
+      rescue Timeout::Error => e
+        puts "Timout: #{e.message}"
+      end        
+    end
+  end
+  
 end
 
