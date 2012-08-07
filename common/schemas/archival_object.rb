bdc4a5f9d36e895d6ab0bbc3efba51532aff2821
Mark Triggs <mark@dishevelled.net>
2012-08-07 14:02:42 +1000

    testing

--- common/schemas/archival_object.rb
+++ common/schemas/archival_object.rb
@@ -5,7 +5,7 @@
     "properties" => {
       "uri" => {"type" => "string", "required" => false},
 
-      "id_0" => {"type" => "string", "required" => true, "minLength" => 1, "pattern" => "^[a-zA-Z0-9]*$"},
+      "id_0" => {"type" => "string", "ifmissing" => "error", "minLength" => 1, "pattern" => "^[a-zA-Z0-9]*$"},
       "id_1" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
       "id_2" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
       "id_3" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
