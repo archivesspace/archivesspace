43d619dc1d2fbcadbb510c34e8d05ae8b2577976
Mark Triggs <mark@dishevelled.net>
2012-08-13 14:23:58 +1000

    Chopped out the ASModel code for converting to/from references.  No longer used.

--- backend/app/model/ASModel.rb
+++ backend/app/model/ASModel.rb
@@ -22,7 +22,7 @@ module ASModel
 
   def update_from_json(json)
     old = JSONModel(json.class.record_type).from_hash(self.values).to_hash
-    changes = self.class.references_to_ids(json)
+    changes = json.to_hash
 
     old.each do |k, v|
       if not changes.has_key?(k)
@@ -38,68 +38,9 @@ module ASModel
 
   module ClassMethods
 
-    def reference_to_id(ref)
-      if ref =~ /\/([0-9]+)$/
-        return $1
-      else
-        # This shouldn't happen: the JSON model should have already validated the
-        # syntax of each reference.
-        raise "Invalid reference: '#{ref}'"
-      end
-    end
-
-
-    def reference_to_id_map
-      {
-        "repository" => :repo_id,
-      }
-    end
-
-
-    def id_to_reference_map
-      {
-        :repo_id => {
-          :property => "repository",
-          :uri_format => "/repositories/%s",
-        }
-      }
-    end
-
-
-    def ids_to_references(hash)
-      if self.respond_to? :id_to_reference_map
-        self.id_to_reference_map.each do |column, reference|
-          if hash[column]
-            hash[reference[:property]] = sprintf(reference[:uri_format],
-                                                 hash[column])
-          end
-        end
-      end
-
-      hash
-    end
-
-
-    def references_to_ids(json)
-      row = json.to_hash
-
-      if self.respond_to? :reference_to_id_map
-        self.reference_to_id_map.each do |reference, column|
-          if column and row[reference]
-            row[column] = reference_to_id(row[reference])
-          end
-
-          row.delete(reference)
-        end
-      end
-
-      row
-    end
-
-
     def create_from_json(json, extra_values = {})
       self.strict_param_setting = false
-      self.create(references_to_ids(json).merge(extra_values))
+      self.create(json.to_hash.merge(extra_values))
     end
 
 
@@ -115,9 +56,7 @@ module ASModel
         obj = get_or_die(obj)
       end
 
-      mapped = ids_to_references(obj.values)
-
-      json = JSONModel(model).from_hash(mapped.reject {|k, v| v.nil? })
+      json = JSONModel(model).from_hash(obj.values.reject {|k, v| v.nil? })
 
       json.uri = json.class.uri_for(obj.id, {:repo_id => obj[:repo_id]})
 
