b94c9801434182418ae35295e50cf47c00b217e9
jambun <jambun@gmail.com>
2012-08-14 11:46:23 +1000

    added example DB_URL for MySQL

--- backend/config/config-example.rb
+++ backend/config/config-example.rb
@@ -2,4 +2,6 @@ require_relative 'config-distribution'
 
 class AppConfig
   # You can override the default configuration options by setting them here.
+  # For example, something like like this if you want to use MySQL:
+  DB_URL = "jdbc:mysql://localhost:3306/archivesspace?user=as&password=as123"
 end
