160e86d2155a5b40043d175ad15a0bb98d3d9551
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-14 16:01:03 -0400

    added schema uri and ref_id

--- common/schemas/vocabulary.rb
+++ common/schemas/vocabulary.rb
@@ -1,10 +1,11 @@
 {
   :schema => {
+    "$schema" => "http://www.archivesspace.org/archivesspace.json",
     "type" => "object",
     "uri" => "/vocabularies",
     "properties" => {
       "uri" => {"type" => "string", "required" => false},
-
+      "ref_id" => {"type" => "string", "minLength" => 1, "required" => true},
       "name" => {"type" => "string", "minLength" => 1, "required" => true}
     },
 
