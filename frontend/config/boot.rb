9d376ee2136f1f4aa60add24e61b39f1f1bbac47
Mark Triggs <mark@dishevelled.net>
2012-08-16 11:06:27 +1000

    Backed out patch causing Rails precompile syntax error:
      https://github.com/twitter/bootstrap/commit/7609fd4dc62dfdc83c08fe32cbcd2b8e5cac5500#comments

--- frontend/config/boot.rb
+++ frontend/config/boot.rb
@@ -4,20 +4,6 @@ require 'stringio'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-# Hackery to suppress spurious git error messages from twitter-bootstrap-rails
-old_stderr = $stderr
-$stderr = StringIO.new
-begin
-  require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
-ensure
-  errors = $stderr.string.split(/\r?\n/).reject {|s| s =~ /Not a git repository/}
-  $stderr = old_stderr
-
-  if not errors.empty?
-    errors.each do |line|
-      $stderr.puts(line)
-    end
-  end
-end
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 $:.unshift File.join(File.dirname(__FILE__), "..", "..", "common")
