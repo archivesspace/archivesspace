0d84e8866f3edb4e617dd78f0251faeb646d0a29
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-17 17:35:10 -0400

    initial tests for a crosswalk module

--- migrations/lib/aspace_walk.rb
+++ migrations/lib/aspace_walk.rb
@@ -1,7 +1,39 @@
+require 'psych'
+
 module ASpaceWalk
   include JSONModel
+
+  @@walks = {}
   
   def self.get_5 
     5
   end
+  
+  def self.load_walk(yaml)
+    # load in the YAML
+    # create a class represeting the crosswalk
+    cls = Class.new do
+      
+      @@source = Psych.load(yaml)
+      puts Psych.dump(@@source)
+
+      def self.to_s
+        "ASpaceWalk from #{@@source['source_format']}"
+      end
+
+      def self.causes_object?(node)
+        @@source['objects'].each do |type, obj|
+          xpath = obj['xpath']
+          if xpath.match(/(\/)*#{node}/)
+            return true
+          end
+        end  
+        return false
+      end
+    end
+    
+    cls
+  
+  end
+    
 end
--- migrations/spec/aspace_walk_spec.rb
+++ migrations/spec/aspace_walk_spec.rb
@@ -1,13 +1,35 @@
 require "../lib/bootstrap.rb"
+require 'psych'
 
 # TODO - Consider writing tests for import method
 
 describe ASpaceWalk do
+  before(:each) do
+    @yaml = Psych.dump({'source_format' => "TEST",
+                        'objects' => {
+                                      'obj1' => {'xpath' => '//hot'},
+                                      'obj2' => {'xpath' => '//damn'}
+                                      }
+                       })
+    
+  end
+  
+  
+  
   it "should give me 5 when I go to get 5" do
     ASpaceWalk.get_5.should == 5
   end
   
-  it "should be able to give a list if crosswalks"
+  it "should create a class for a YAML crosswalk" do
+    w = ASpaceWalk.load_walk(@yaml).new    
+    w.class.to_s.should eq('ASpaceWalk from TEST')
+  end
   
+  it "should say whether a source node begets an object" do
+    w = ASpaceWalk.load_walk(@yaml).new
+    w.class.causes_object?('hot').should be_true
+    w.class.causes_object?('cold').should be_false
+    
+  end
 end
 
