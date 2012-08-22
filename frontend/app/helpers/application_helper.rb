4dc72f73909e1a7ad67a19f4e9282a6cc33d23c9
Payten Giles <payten.giles@gmail.com>
2012-08-22 17:05:17 +1000

    refactor js file naming so one 'crud' javascript file can be pulled in for the 4 CRUD actions new/create/edit/update

--- frontend/app/helpers/application_helper.rb
+++ frontend/app/helpers/application_helper.rb
@@ -2,8 +2,13 @@ module ApplicationHelper
 
   def include_controller_js
    javascript_include_tag "#{controller.controller_name}" if File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.js") ||  File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.js.erb")
-   
+
    javascript_include_tag "#{controller.controller_name}.#{controller.action_name}" if File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.#{controller.action_name}.js") ||  File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.#{controller.action_name}.js.erb")
+
+   if ["new", "create", "edit", "update"].include?(controller.action_name)
+      javascript_include_tag "#{controller.controller_name}.crud" if File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.crud.js") ||  File.exists?("#{Rails.root}/app/assets/javascripts/#{controller_name}.crud.js.erb")
+   end
+
   end
 
 end
