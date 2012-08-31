dcfb4fab9286aeca5e499c79b8ebeed5d800a280
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-31 17:30:57 -0400

    yard

--- _yard/endpoint_object.rb
+++ _yard/endpoint_object.rb
@@ -15,31 +15,36 @@ module YARD::CodeObjects
 
     
     def describe
-      puts self.source
+
       e = eval("#{self.source}")
-      self[:method] = e[:method]
-      self[:description] = e[:description]
-      self[:params] = []
-      e[:params].each do |param|
-        
-    #     opts = (param[3] or {})
-    # 
-    #     vs = opts[:validation] ? " -- #{opts[:validation][0]}" : ""
-    # 
-    #     if opts[:body]
-    #       puts "    #{param[1]} <request body> -- #{param[2]}#{vs}"
-    #     else
-    #       puts "    #{param[1]} #{param[0]} -- #{param[2]}#{vs}"
-    #     end
-    #   end
-    # 
-    # #  puts "  Returns: #{e[:returns].inspect}"
-    #   puts "  Returns:"
-    #   e[:returns].each do |ret|
-    #     puts "    #{ret[0]} -- #{ret[1]}"
-    #   end    
       
+      self[:meth] = e[:method]
+      
+      self[:description] = e[:description]
+      
+      self[:params] = Array.new
+      puts "PARAMS #{e[:required_params].inspect}"
+      e[:required_params].each do |param|
+        
+        opts = (param[3] or {})
+
+        vs = opts[:validation] ? " -- #{opts[:validation][0]}" : ""
+
+        if opts[:body]
+          self[:params] << "#{param[1]} <request body> -- #{param[2]}#{vs}"
+        else
+          self[:params] << "#{param[1]} #{param[0]} -- #{param[2]}#{vs}"
+        end
+      end
+
+      self[:returns] = Array.new
+      
+      e[:returns].each do |ret|
+        self[:returns] << "#{ret[0]} -- #{ret[1]}"
+      end    
+      puts "ENDPOINT #{self[:params].inspect}"
       self
+      
     end
   end
 end
