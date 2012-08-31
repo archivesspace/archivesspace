3184afef804ab4171afa460a360d0c7bd50f23ae
jambun <jambun@gmail.com>
2012-08-31 15:58:31 +1000

    adding tests to improve coverage

--- backend/spec/common_jsonmodel_spec.rb
+++ backend/spec/common_jsonmodel_spec.rb
@@ -7,38 +7,53 @@ describe 'JSON model' do
     JSONModel.create_model_for("testschema",
                                {
                                  "type" => "object",
+                                 "uri" => "/testthings",
                                  "properties" => {
                                    "elt_0" => {"type" => "string", "required" => true, "minLength" => 1, "pattern" => "^[a-zA-Z0-9 ]*$"},
                                    "elt_1" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
                                    "elt_2" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
                                    "elt_3" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
+                                   "moos_if_missing" => {"type" => "string", "ifmissing" => "moo", "default" => ""},
+                                   "no_shorty" => {"type" => "string", "required" => false, "default" => "", "minLength" => 6},
+                                   "shorty" => {"type" => "string", "required" => false, "default" => "", "maxLength" => 2},
+                                   "wants_integer" => {"type" => "integer", "required" => false},
                                  },
 
                                  "additionalProperties" => false
                                })
+
   end
+
 
   after(:all) do
+
     JSONModel.destroy_model(:testschema)
     JSONModel.destroy_model(:strictschema)
+
   end
 
-  it "Accepts a simple record" do
+
+  it "accepts a simple record" do
+
     JSONModel(:testschema).from_hash({
                                        "elt_0" => "helloworld",
                                        "elt_1" => "thisisatest"
                                      })
+
   end
 
 
-  it "Flags errors on invalid values" do
+  it "flags errors on invalid values" do
+
     lambda {
       JSONModel(:testschema).from_hash({"elt_0" => "/!$"})
     }.should raise_error(ValidationException)
+
   end
 
 
-  it "Provides accessors for non-schema properties but doesn't serialise them" do
+  it "provides accessors for non-schema properties but doesn't serialise them" do
+
     obj = JSONModel(:testschema).from_hash({
                                              "elt_0" => "helloworld",
                                              "special" => "some string"
@@ -49,10 +64,12 @@ describe 'JSON model' do
 
     obj.to_hash.has_key?("special").should be_false
     JSON[obj.to_json].has_key?("special").should be_false
+
   end
 
 
-  it "Allows for updates" do
+  it "allows for updates" do
+
     obj = JSONModel(:testschema).from_hash({
                                              "elt_0" => "helloworld",
                                            })
@@ -60,10 +77,12 @@ describe 'JSON model' do
     obj.elt_0 = "a new string"
 
     JSON[obj.to_json]["elt_0"].should eq("a new string")
+
   end
 
 
-  it "Throws an exception with some useful accessors" do
+  it "throws an exception with some useful accessors" do
+
     exception = false
     begin
       JSONModel(:testschema).from_hash({"elt_0" => "/!$"})
@@ -78,19 +97,23 @@ describe 'JSON model' do
 
     # And you can get a list of its problems too
     exception.errors["elt_0"][0].should eq "Did not match regular expression: ^[a-zA-Z0-9 ]*$"
+
   end
 
 
-  it "Warns on missing properties instead of erroring" do
+  it "warns on missing properties instead of erroring" do
+
     JSONModel::strict_mode(false)
     model = JSONModel(:testschema).from_hash({})
 
     model._warnings.keys.should eq(["elt_0"])
     JSONModel::strict_mode(true)
+
   end
 
 
-  it "Supports the 'ifmissing' definition" do
+  it "supports the 'ifmissing' definition" do
+
     JSONModel.create_model_for("strictschema",
                                {
                                  "type" => "object",
@@ -111,6 +134,103 @@ describe 'JSON model' do
 
     model._exceptions[:errors].keys.should eq(["container/strict"])
     JSONModel::strict_mode(true)
+
   end
+
+
+  it "can have its validation disabled" do
+
+    ts = JSONModel(:testschema).new._always_valid!
+    ts._exceptions.should eq({})
+
+  end
+
+
+  it "returns false if you ask for a model that doesn't exist" do
+
+    JSONModel(:not_a_real_model).should eq false
+
+  end
+
+
+  it "can give a string representation of itself" do
+
+    JSONModel(:testschema).to_s.should eq "JSONModel(:testschema)"
+
+  end
+
+
+  it "can give a string representation of an instance" do
+
+    ts = JSONModel(:testschema).from_hash({
+                                            "elt_0" => "helloworld",
+                                            "elt_1" => "thisisatest"
+                                          })
+    ts.to_s.should match /\#<JSONModel\(:testschema\).*"elt_0"=>"helloworld".*>/
+
+  end
+
+
+  it "knows a bad uri when it sees one" do
+
+    expect { JSONModel(:testschema).id_for("/moo/moo") }.to raise_error
+
+  end
+
+
+  it "supports setting values for properties" do
+
+    ts = JSONModel(:testschema).from_hash({
+                                            "elt_0" => "helloworld",
+                                            "elt_1" => "thisisatest"
+                                          })
+    ts[:elt_2] = "a value has been set"
+    ts[:elt_2].should eq "a value has been set"
+
+  end
+
+
+  it "returns nil if you try to look it up in a bad hash" do
+
+    JSONModel(:testschema).lookup({}).should eq nil
+
+  end
+
+
+  it "enforces minimum length of property values" do
+
+    ts = JSONModel(:testschema).from_hash({
+                                            "elt_0" => "helloworld",
+                                            "elt_1" => "thisisatest"
+                                          })
+    ts[:no_shorty] = "meep"
+    ts._exceptions[:errors].keys.should eq (["no_shorty"])
+
+  end
+
+
+  it "enforces the type of property values" do
+
+    ts = JSONModel(:testschema).from_hash({
+                                            "elt_0" => "helloworld",
+                                            "elt_1" => "thisisatest"
+                                          })
+    ts[:wants_integer] = "meep"
+    ts._exceptions[:errors].keys.should eq (["wants_integer"])
+
+  end
+
+
+  it "copes with unexpected kinds of validation exception" do
+
+    ts = JSONModel(:testschema).from_hash({
+                                            "elt_0" => "helloworld",
+                                            "elt_1" => "thisisatest"
+                                          })
+    ts[:shorty] = "meep"
+    ts._exceptions[:errors].keys.should eq ([:unknown])
+
+  end
+
 
 end
