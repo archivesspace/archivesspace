6e32a57f3e84e3ea8548b2aef5305942c4034532
Payten Giles <payten.giles@gmail.com>
2012-09-13 18:13:54 +1000

    refactor agents controller in prep for other agent types

--- frontend/app/controllers/agents_controller.rb
+++ frontend/app/controllers/agents_controller.rb
@@ -1,45 +1,58 @@
 class AgentsController < ApplicationController
+  before_filter :assign_types
 
   def index
     @agents = JSONModel::all('/agents', :agent_type)
   end
 
   def show
-    @agent = JSONModel(:"#{params[:type]}").find(params[:id])
+    @agent = JSONModel(@agent_type).find(params[:id])
   end
 
   def new
-    @name_type = :name_person
-    @agent = JSONModel(:"#{params[:type]}").new({:agent_type => params[:type]})._always_valid!
+    @agent = JSONModel(@agent_type).new({:agent_type => @agent_type})._always_valid!
     @agent.names = [JSONModel(@name_type).new._always_valid!]
   end
 
   def edit
-    @name_type = :name_person
-    @agent = JSONModel(:"#{params[:type]}").find(params[:id])
+    @agent = JSONModel(@agent_type).find(params[:id])
   end
 
   def create
-    @name_type = :name_person
     handle_crud(:instance => :agent,
-                :model => JSONModel(:"#{params[:type]}"),
+                :model => JSONModel(@agent_type),
                 :on_invalid => ->(){
                   return render :action => :new
                 },
                 :on_valid => ->(id){
-                  redirect_to :controller => :agents, :action => :show, :id => id, :type => params[:type]
+                  redirect_to :controller => :agents, :action => :show, :id => id, :type => @agent_type
                 })
   end
 
   def update
-    @name_type = :name_person
     handle_crud(:instance => :agent,
-                :model => JSONModel(:"#{params[:type]}"),
+                :model => JSONModel(@agent_type),
                 :on_invalid => ->(){
                   return render :action => :edit
                 },
                 :on_valid => ->(id){
-                  redirect_to :controller => :agents, :action => :show, :id => id, :type => params[:type]
+                  redirect_to :controller => :agents, :action => :show, :id => id, :type => @agent_type
                 })
   end
+
+  private
+
+    def name_type_for_agent_type(agent_type)
+      agent_schema = JSONModel(agent_type).schema
+      name_type = agent_schema['properties']['names']['items']['type']
+      name_type =~ /JSONModel\(:([a-zA-Z_\-]+)\)/
+      :"#{$1}"
+    end
+
+    def assign_types
+      return if not params.has_key? 'type'
+
+      @agent_type = :"#{params[:type]}"
+      @name_type = name_type_for_agent_type(@agent_type)
+    end
 end
