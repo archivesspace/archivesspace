bd3d3c6e0e84e210cbc6b85d853d2a81db497b60
Mark Triggs <mark@dishevelled.net>
2012-09-27 19:39:26 +1000

    Don't pull in the whole application just for running migrations.

--- build/scripts/migrate_db.rb
+++ build/scripts/migrate_db.rb
@@ -11,7 +11,8 @@ if (AppConfig[:db_url] =~ /jdbc:derby:(.*?);.*aspacedemo=true$/)
   end
 end
 
-require_relative "../../backend/app/main"
+require_relative "../../backend/app/model/db_migrator"
+
 
 Sequel.connect(AppConfig[:db_url],
                :max_connections => AppConfig[:db_max_connections],
