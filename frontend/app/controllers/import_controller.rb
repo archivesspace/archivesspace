5b80fc3a220aebc7bd5ec34cb06cbba50750374c
Payten Giles <payten.giles@gmail.com>
2012-10-06 16:10:36 +1000

    Twitter-bootstrapify import form; And added some basic error handling;

--- frontend/app/controllers/import_controller.rb
+++ frontend/app/controllers/import_controller.rb
@@ -9,22 +9,35 @@ class ImportController < ApplicationController
   end
   
   def upload
-    
-    source_file = ImportFile.new(params[:upload])
 
-    # invoke a new importer and pass the file to it
-    # return the results of the import to the screen
-    # delete the uploaded file
-    
-    results = run_import(source_file)
-    
-    source_file.delete
-    
-    flash[:notice] = results
-    
+    if params[:upload].blank?
+      flash[:error] = "Please select an EAD file to upload"
+    else
+      source_file = ImportFile.new(params[:upload])
+
+      # invoke a new importer and pass the file to it
+      # return the results of the import to the screen
+      # delete the uploaded file
+
+      begin
+
+        results = run_import(source_file)
+
+        source_file.delete
+
+        flash[:success] = results
+
+      rescue ValidationException => e
+        errors_str = e.errors.collect.map{|attr, err| "#{e.invalid_object.class.record_type}/#{attr} #{err.join(', ')}"}.join("")
+        flash[:error] = "Error importing file: <br/> <div class='offset1'>#{errors_str}</div>".html_safe
+      rescue Exception => e
+        flash[:error] = "Error importing file: <br/> <div class='offset1'>#{e.class.name} #{e.inspect}</div>".html_safe
+      end
+
+    end
+
     redirect_to(:controller => :import,
                 :action => :index)
-    
   end
   
   
