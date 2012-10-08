c05c250c209a7a68ce50ab2c67d8c52b30f51599
Brian Hoffman <brianjhoffman@gmail.com>
2012-10-08 19:59:35 -0400

    changed backend port

--- migrations/config/config.rb
+++ migrations/config/config.rb
@@ -1,7 +1,7 @@
 class ASpaceImportConfig
   ASPACE_HOST = "127.0.0.1"
   ASPACE_PORT = "8089"
-  ASPACE_BASE = 'http://localhost:8089' # use 4567 for standalone devserver (backend:devserver)
+  ASPACE_BASE = 'http://localhost:4567' # use 4567 for standalone devserver (backend:devserver)
   DEFAULT_REPO_ID = "1"
   DEFAULT_VOCAB_ID = "1"
 
