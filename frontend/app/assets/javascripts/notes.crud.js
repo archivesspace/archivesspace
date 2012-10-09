d89bbb8a2b98de7e109cc842487e8e70662d435d
Payten Giles <payten.giles@gmail.com>
2012-10-09 22:29:01 +1100

    WIP Add basic workflow for adding multipart forms

--- frontend/app/assets/javascripts/notes.crud.js
+++ frontend/app/assets/javascripts/notes.crud.js
@@ -13,8 +13,58 @@ $(function() {
 
       var index = $(".subrecord-form-wrapper", $this).length;
 
-      $(".add-note-for-type-btn", $this).click(function(event) {
+      var initBibliographyNote = function($subform) {
+        $(".add-bibliography-item-btn", $subform).click(function() {
+
+          var $subsubform = $(AS.renderTemplate("note_bibliography_item_form_template", {
+            path: "foo",
+            index: index
+          }));
+
+          $(".subrecord-form-container", $subform).append($subsubform);
+
+          initRemoveActionForSubRecord($subsubform)
+        });
+      };
+
+
+      var initRemoveActionForSubRecord = function($subform) {
+        var removeBtn = $("<a href='javascript:void(0)' class='btn btn-mini pull-right subrecord-form-remove'><span class='icon-remove'></span></a>");
+        $subform.prepend(removeBtn);
+        removeBtn.on("click", function() {
+          AS.confirmSubFormDelete($(this), function() {
+            $subform.remove();
+            $this.parents("form:first").triggerHandler("form-changed");
+          });
+        });
+      };
+
+
+      var initMultipartNote = function($subform) {
+        $(".add-sub-note-btn", $subform).click(function() {
+          event.preventDefault();
+
+          var selected = $("option:selected", $(this).parents(".dropdown-menu"));
+          var $subsubform = $(AS.renderTemplate(selected.text()+"_form_template", {
+            path: "foo",
+            index: index,
+            type: selected.text()
+          }));
+
+          $(".subrecord-form-container", $subform).append($subsubform);
+
+          initRemoveActionForSubRecord($(".subrecord-form-heading", $subsubform));
+
+          if (selected.text() === "note_bibliography") {
+            initBibliographyNote($subsubform);
+          }
+        });
+      };
+
+
+      var initTopLevelNote = function(event) {
         event.preventDefault();
+
         var selected = $("option:selected", $(this).parents(".dropdown-menu"));
         var $subform = $(AS.renderTemplate(selected.val()+"_form_template", {
           path: "foo",
@@ -24,18 +74,16 @@ $(function() {
 
         $(".notes-container", $this).append($subform);
 
-        // add the remove button
-        var removeBtn = $("<a href='javascript:void(0)' class='btn btn-mini pull-right subrecord-form-remove'><span class='icon-remove'></span></a>");
-        $subform.prepend(removeBtn);
-        removeBtn.on("click", function() {
-          AS.confirmSubFormDelete($(this), function() {
-            $subform.remove();
-            $this.parents("form:first").triggerHandler("form-changed");
-          });
-        });
+        initRemoveActionForSubRecord($subform);
+
+        if (selected.val() === "note_multipart") {
+          initMultipartNote($subform);
+        }
 
         index++;
-      });
+      };
+
+      $(".add-note-for-type-btn", $this).click(initTopLevelNote);
 
     })
   };
