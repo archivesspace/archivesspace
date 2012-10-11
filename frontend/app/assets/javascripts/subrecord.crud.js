315ef637b7fe49c48b037223af9a1bd4f6ca0f74
Payten Giles <payten.giles@gmail.com>
2012-10-11 20:34:48 +1100

    Get Extents subform working with new form helper

--- frontend/app/assets/javascripts/subrecord.crud.js
+++ frontend/app/assets/javascripts/subrecord.crud.js
@@ -13,26 +13,17 @@ $(function() {
 
       $this.data("form_index", $("> .subrecord-form-container .subrecord-form-wrapper", $this).length);
 
-
       var init_subform = function() {
         var $subform = $(this);
 
-        if ($("> .subrecord-form-fields", $subform).data("allow-removal") === false) {
-          var removeBtn = $("<a href='javascript:void(0)' class='btn btn-mini pull-right subrecord-form-remove'><span class='icon-remove'></span></a>");
-          $subform.prepend(removeBtn);
-          removeBtn.on("click", function() {
-            AS.confirmSubFormDelete($(this), function() {
-              $subform.remove();
-              $this.parents("form:first").triggerHandler("form-changed");
-              if ($("> .subrecord-form-container .subrecord-form-wrapper", $this).length === 0) {
-                $("> .subrecord-form-container > .alert", $this).show();
-              }
-            });
+        var removeBtn = $("<a href='javascript:void(0)' class='btn btn-mini pull-right subrecord-form-remove'><span class='icon-remove'></span></a>");
+        $subform.prepend(removeBtn);
+        removeBtn.on("click", function() {
+          AS.confirmSubFormDelete($(this), function() {
+            $subform.remove();
+            $this.parents("form:first").triggerHandler("form-changed");
           });
-
-          // init any sub sub records!
-          $(".subrecord-form:not(.initialised)", $subform).init_subrecord_form();
-        }
+        });
 
         $(document).triggerHandler("subrecord.new", [$this.data("object-name"), $subform]);
       };
@@ -42,25 +33,31 @@ $(function() {
         // add binding for creation of subforms
         $("> .subrecord-form-heading > .btn", $this).on("click", function() {
 
+          var $target_subrecord_list = $(".subrecord-form-list:first", $(this).parents(".subrecord-form:first"));
+
           var index_data = {
-            "index": $this.data("form_index"),
-            "sub_index" : "${index}"
+            path: AS.quickTemplate($target_subrecord_list.data("name-path"), {index: $this.data("form_index")}),
+            id_path: AS.quickTemplate($target_subrecord_list.data("id-path"), {index: $this.data("form_index")}),
+            index: $this.data("form_index")
           };
 
           var formEl = $(AS.renderTemplate($this.data("template"), index_data));
           formEl.hide();
 
-          $("> .subrecord-form-container", $this).append(formEl);
+          $target_subrecord_list.append(formEl);
+
           formEl.fadeIn();
-          $("> .subrecord-form-container > .alert", $this).hide();
+
           $this.parents("form:first").triggerHandler("form-changed");
+
           $.proxy(init_subform, formEl)();
           $(":input:visible:first", formEl).focus();
+
           $this.data("form_index", $this.data("form_index")+1);
         });
 
         // init any existing subforms
-        $("> .subrecord-form-container > .subrecord-form-wrapper", $this).each(init_subform);
+        $("> .subrecord-form-container .subrecord-form-list > .subrecord-form-fields", $this).each(init_subform);
       };
 
       init();
