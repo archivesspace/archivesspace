8757fc5d9970b235c113440693c41f50af705667
Mark Triggs <mark@dishevelled.net>
2012-10-16 14:20:48 +1100

    Don't lock down access to the webhooks controller

--- frontend/app/controllers/webhook_controller.rb
+++ frontend/app/controllers/webhook_controller.rb
@@ -1,4 +1,6 @@
 class WebhookController < ApplicationController
+  skip_before_filter :unauthorised_access, :only => [:notify]
+
   def notify
     JSONModel::Webhooks::notify(JSONModel(:webhook_notification).from_hash(JSON(params[:notification])))
 
