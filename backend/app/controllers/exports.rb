29cd099e2d1bbfcfd91d245bf6206ccf9404d24c
Brian Hoffman <brianjhoffman@gmail.com>
2012-10-17 11:16:34 -0400

    changed endpoint for ead export

--- backend/app/controllers/exports.rb
+++ backend/app/controllers/exports.rb
@@ -2,7 +2,7 @@ class ArchivesSpaceService < Sinatra::Base
   
   include ExportHelpers
 
-  Endpoint.get('/repositories/:repo_id/resources/:resource_id/ead')
+  Endpoint.get('/repositories/:repo_id/resource_descriptions/:resource_id.ead')
     .description("Get an EAD representation of a Resource ")
     .params(["resource_id", Integer, "The ID of the resource to retrieve"],
             ["repo_id", :repo_id])
