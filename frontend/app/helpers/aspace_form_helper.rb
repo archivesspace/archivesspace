a95c9684061885bfb37f0f0177aa4107a5c767f6
Payten Giles <payten.giles@gmail.com>
2012-10-21 22:06:21 +1100

    Fix scrollTo from error message to field -- and then focus field

--- frontend/app/helpers/aspace_form_helper.rb
+++ frontend/app/helpers/aspace_form_helper.rb
@@ -139,12 +139,17 @@ module AspaceFormHelper
       [:errors, :warnings].each do |condition|
         if exceptions[condition]
           result[condition] = exceptions[condition].keys.map {|property|
-            "#{form_top}#{property.split("/").collect{|a| "[#{a}]"}.join}".gsub(/[\[\]\/]/, "_")
+            id_for_javascript(property)
           }
         end
       end
 
       result.to_json.html_safe
+    end
+
+
+    def id_for_javascript(name)
+      "#{form_top}#{name.split("/").collect{|a| "[#{a}]"}.join}".gsub(/[\[\]\/]/, "_")
     end
 
 
