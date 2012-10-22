5d4b78179829d032ccabf2b8a74fe9fbdab0321f
Payten Giles <payten.giles@gmail.com>
2012-10-23 10:40:39 +1100

    ensure date form is initialised with date.crud javascript

--- frontend/app/assets/javascripts/events.crud.js
+++ frontend/app/assets/javascripts/events.crud.js
@@ -1,1 +1,6 @@
+//= require dates.crud
 //= require subrecord.crud
+
+$(document).ready(function() {
+  $(document).triggerHandler("init.subrecord", ["date", $("#event_date .subrecord-form-fields")]);
+});
