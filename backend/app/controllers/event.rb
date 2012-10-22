0faf84fb69c787b5165625aabc3558069fe43f63
Mark Triggs <mark@dishevelled.net>
2012-10-23 10:18:49 +1100

    WIP on an endpoint for getting records that are linkable to events

--- backend/app/controllers/event.rb
+++ backend/app/controllers/event.rb
@@ -27,40 +27,13 @@ class ArchivesSpaceService < Sinatra::Base
   end
 
 
+  Endpoint.get('/repositories/:repo_id/events/linkable-records/list')
+    .description("Get a list of records matching some search criteria that can be linked to an event")
+    .params(["q", /[\w0-9 -.]/, "The record title prefix to match"])
+    .returns([200, "A list of matching records"]) \
+  do
+    json_response(Event.linkable_records_for(params[:q]))
+  end
 
-
-  # Endpoint.post('/repositories/:repo_id/archival_objects/:archival_object_id')
-  #   .description("Update an Archival Object")
-  #   .params(["archival_object_id", Integer, "The Archival Object ID to update"],
-  #           ["archival_object", JSONModel(:archival_object), "The Archival Object data to update", :body => true],
-  #           ["repo_id", :repo_id])
-  #   .returns([200, :updated],
-  #            [400, :error],
-  #            [409, '{"error":{"[:resource_id, :ref_id]":["An Archival Object Ref ID must be unique to its resource"]}}']) \
-  # do
-  #   handle_update(ArchivalObject, :archival_object_id, :archival_object)
-  # end
-
-
-  # Endpoint.get('/repositories/:repo_id/archival_objects/:archival_object_id/children')
-  #   .description("Get the children of an Archival Object")
-  #   .params(["archival_object_id", Integer, "The Archival Object ID"],
-  #           ["repo_id", :repo_id])
-  #   .returns([200, "[(:archival_object)]"],
-  #            [404, '{"error":"ArchivalObject not found"}']) \
-  # do
-  #   ao = ArchivalObject.get_or_die(params[:archival_object_id], params[:repo_id])
-  #   json_response(ao.children.map {|child|
-  #                   ArchivalObject.to_jsonmodel(child, :archival_object, params[:repo_id]).to_hash})
-  # end
-
-
-  # Endpoint.get('/repositories/:repo_id/archival_objects')
-  #   .description("Get a list of Archival Objects for a Repository")
-  #   .params(["repo_id", :repo_id])
-  #   .returns([200, "[(:archival_object)]"]) \
-  # do
-  #   handle_listing(ArchivalObject, :archival_object, :repo_id => params[:repo_id])
-  # end
 
 end
