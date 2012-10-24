d4641f2bda813ccdde1257488ebdc939b3cd25f0
Payten Giles <payten.giles@gmail.com>
2012-10-24 14:10:27 +1100

    Add stubs for digital object/component frontend controllers, templates and js

---
+++ frontend/app/controllers/digital_object_components_controller.rb
@@ -0,0 +1,47 @@
+class DigitalObjectComponentsController < ApplicationController
+  skip_before_filter :unauthorised_access, :only => [:index, :show, :new, :edit, :create, :update]
+  before_filter :user_needs_to_be_a_viewer, :only => [:index, :show]
+  before_filter :user_needs_to_be_an_archivist, :only => [:new, :edit, :create, :update]
+
+  def new
+    @digital_object_component = JSONModel(:digital_object_component).new._always_valid!
+    @digital_object_component.title = "New Digital Object Component"
+    @digital_object_component.parent = JSONModel(:digital_object_component).uri_for(params[:parent]) if params.has_key?(:parent)
+    @digital_object_component.digital_object = JSONModel(:digital_object).uri_for(params[:digital_object]) if params.has_key?(:digital_object)
+
+    return render :partial => "digital_object_components/new_inline" if inline?
+
+    # render the full AO form
+
+  end
+
+  def edit
+    @digital_object_component = JSONModel(:digital_object_component).find(params[:id], "resolve[]" => ["subjects"])
+    render :partial => "digital_object_components/edit_inline" if inline?
+  end
+
+
+  def create
+    handle_crud(:instance => :digital_object_component,
+                :on_invalid => ->(){ render :partial => "new_inline" },
+                :on_valid => ->(id){ render :partial => "digital_object_components/edit_inline" })
+  end
+
+
+  def update
+    handle_crud(:instance => :digital_object_component,
+                :obj => JSONModel(:digital_object_component).find(params[:id],
+                                                         "resolve[]" => ["subjects"]),
+                :on_invalid => ->(){ return render :partial => "edit_inline" },
+                :on_valid => ->(id){
+                  flash[:success] = "Digital Object Component Saved"
+                  render :partial => "edit_inline"
+                })
+  end
+
+
+  def show
+    @digital_object_component = JSONModel(:digital_object_component).find(params[:id], "resolve[]" => ["subjects"])
+    render :partial => "digital_object_components/show_inline" if inline?
+  end
+end
---
+++ frontend/app/controllers/digital_objects_controller.rb
@@ -0,0 +1,101 @@
+class DigitalObjectsController < ApplicationController
+  skip_before_filter :unauthorised_access, :only => [:index, :show, :tree, :new, :edit, :create, :update, :update_tree]
+  before_filter :user_needs_to_be_a_viewer, :only => [:index, :show, :tree]
+  before_filter :user_needs_to_be_an_archivist, :only => [:new, :edit, :create, :update, :update_tree]
+
+  def index
+    @digital_objects = JSONModel(:digital_object).all
+  end
+
+  def show
+    @digital_object = JSONModel(:digital_object).find(params[:id], "resolve[]" => ["subjects"])
+
+    if params[:inline]
+      return render :partial => "digital_objects/show_inline"
+    end
+
+    fetch_digital_object_tree(@digital_object)
+  end
+
+  def new
+    @digital_object = JSONModel(:digital_object).new({:title => "New Digital Object"})._always_valid!
+    @digital_object.extents = [JSONModel(:extent).new._always_valid!]
+  end
+
+  def edit
+    @digital_object = JSONModel(:digital_object).find(params[:id], "resolve[]" => ["subjects"])
+
+    if params[:inline]
+      return render :partial => "digital_objects/edit_inline"
+    end
+
+    fetch_digital_object_tree(@digital_object)
+  end
+
+
+  def create
+    handle_crud(:instance => :digital_object,
+                :on_invalid => ->(){ render action: "new" },
+                :on_valid => ->(id){ redirect_to(:controller => :digital_objects,
+                                                 :action => :edit,
+                                                 :id => id) })
+  end
+
+
+  def update
+    handle_crud(:instance => :digital_object,
+                :obj => JSONModel(:digital_object).find(params[:id],
+                                                  "resolve[]" => ["subjects"]),
+                :on_invalid => ->(){
+                  render :partial => "edit_inline"
+                },
+                :on_valid => ->(id){
+                  flash[:success] = "Resource Saved"
+                  render :partial => "edit_inline"
+                })
+  end
+
+
+  def destroy
+
+  end
+
+  def tree
+    fetch_digital_object_tree(JSONModel(:digital_object).find(params[:id]))
+    render :text => @digital_object_tree.to_json
+  end
+
+  def update_tree
+    begin
+      tree = JSONModel(:digital_object_tree).from_json(params[:tree])
+      tree.save(:digital_object_id => params[:id])
+      render :text => "Success"
+    rescue JSONModel::ValidationException => e
+      render :text => "Error"
+    end
+  end
+
+
+  private
+
+  def convert_refs_to_ids(tree)
+    tree["id"] = JSONModel(:digital_object_component).id_for(tree["digital_object_component"])
+
+    tree["children"].each do |child|
+      convert_refs_to_ids(child)
+    end
+
+    tree
+  end
+
+  def fetch_digital_object_tree(digital_object)
+    tree = JSONModel(:digital_object_tree).find(nil, :digital_object_id => digital_object.id)
+
+    @digital_object_tree = {
+      "digital_object_id" => digital_object.id,
+      "title" => digital_object.title,
+      "children" => tree ? [convert_refs_to_ids(tree.to_hash)] : []
+    }
+  end
+
+end
