93893d03b6d23a19a1baca27767b0b1db7c77990
Brian Hoffman <brianjhoffman@gmail.com>
2012-10-24 14:02:52 -0400

    cleaned up some comments

--- frontend/app/helpers/export_helper.rb
+++ frontend/app/helpers/export_helper.rb
@@ -19,15 +19,6 @@ module ExportHelper
         end
       end
     end
-    
-    # Net::HTTP.get_response(uri) do |res|
-    #   size, total = 0, res.header['Content-Length'].to_i
-    #   res.read_body do |chunk|
-    #     size += chunk.size
-    #     percent = (size * 100) / total
-    #     yield chunk, percent
-    #   end
-    # end
   end
 end
     
