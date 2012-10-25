9d8482e053cbcc3b8de89543729fdaef9fac92d0
Mark Triggs <mark@dishevelled.net>
2012-10-25 13:29:24 +1100

    New backend tests for digital objects and their components.

---
+++ backend/app/controllers/digital_object_component.rb
@@ -0,0 +1,62 @@
+class ArchivesSpaceService < Sinatra::Base
+
+  Endpoint.post('/repositories/:repo_id/digital_object_components')
+    .description("Create an Digital Object Component")
+    .params(["digital_object_component", JSONModel(:digital_object_component), "The Digital Object Component to create", :body => true],
+            ["repo_id", :repo_id])
+    .returns([200, :created],
+             [400, :error]) \
+  do
+    handle_create(DigitalObjectComponent, :digital_object_component)
+  end
+
+
+  Endpoint.post('/repositories/:repo_id/digital_object_components/:digital_object_component_id')
+    .description("Update an Digital Object Component")
+    .params(["digital_object_component_id", Integer, "The Digital Object Component ID to update"],
+            ["digital_object_component", JSONModel(:digital_object_component), "The Digital Object Component data to update", :body => true],
+            ["repo_id", :repo_id])
+    .returns([200, :updated],
+             [400, :error]) \
+  do
+    handle_update(DigitalObjectComponent, :digital_object_component_id, :digital_object_component)
+  end
+
+
+  Endpoint.get('/repositories/:repo_id/digital_object_components/:digital_object_component_id')
+    .description("Get an Digital Object Component by ID")
+    .params(["digital_object_component_id", Integer, "The Digital Object Component ID"],
+            ["repo_id", :repo_id],
+            ["resolve", [String], "A list of references to resolve and embed in the response",
+             :optional => true])
+    .returns([200, "(:digital_object_component)"],
+             [404, '{"error":"DigitalObjectComponent not found"}']) \
+  do
+    json = DigitalObjectComponent.to_jsonmodel(params[:digital_object_component_id], :digital_object_component, params[:repo_id])
+
+    json_response(resolve_references(json.to_hash, params[:resolve]))
+  end
+
+
+  Endpoint.get('/repositories/:repo_id/digital_object_components/:digital_object_component_id/children')
+    .description("Get the children of an Digital Object Component")
+    .params(["digital_object_component_id", Integer, "The Digital Object Component ID"],
+            ["repo_id", :repo_id])
+    .returns([200, "[(:digital_object_component)]"],
+             [404, '{"error":"DigitalObjectComponent not found"}']) \
+  do
+    digital_object = DigitalObjectComponent.get_or_die(params[:digital_object_component_id], params[:repo_id])
+    json_response(digital_object.children.map {|child|
+                    DigitalObjectComponent.to_jsonmodel(child, :digital_object_component, params[:repo_id]).to_hash})
+  end
+
+
+  Endpoint.get('/repositories/:repo_id/digital_object_components')
+    .description("Get a list of Digital Object Components for a Repository")
+    .params(["repo_id", :repo_id])
+    .returns([200, "[(:digital_object_component)]"]) \
+  do
+    handle_listing(DigitalObjectComponent, :digital_object_component, :repo_id => params[:repo_id])
+  end
+
+end
---
+++ backend/spec/controller_digital_object_component_spec.rb
@@ -0,0 +1,116 @@
+require 'spec_helper'
+
+describe 'Digital Object Component controller' do
+
+  before(:each) do
+    make_test_repo
+  end
+
+
+  def create_digital_object_component(opts = {})
+
+    doc = JSONModel(:digital_object_component).from_hash("component_id" => "abc123",
+                                                         "title" => "The digital object component title")
+    doc.update(opts)
+    doc.save
+  end
+
+
+
+  it "lets you create an digital object component and get it back" do
+    created = create_digital_object_component
+    JSONModel(:digital_object_component).find(created).title.should eq("The digital object component title")
+  end
+
+  it "lets you list all digital object components" do
+    id = create_digital_object_component
+    JSONModel(:digital_object_component).all.count.should eq(1)
+  end
+
+
+  it "lets you create an digital object component with a parent" do
+    digital_object = JSONModel(:digital_object).from_hash("title" => "a resource",
+                                                          "digital_object_id" => "abc123",
+                                                          "extents" => [{"portion" => "whole",
+                                                                          "number" => "5 or so",
+                                                                          "extent_type" => "reels"}])
+    digital_object.save
+
+    created = create_digital_object_component("digital_object" => digital_object.uri,
+                                              "component_id" => "parent123")
+
+    create_digital_object_component("component_id" => "child123",
+                                    "digital_object" => digital_object.uri,
+                                    "title" => "child digital object component",
+                                    "parent" => "#{@repo}/digital_object_components/#{created}")
+
+    get "#{@repo}/digital_object_components/#{created}/children"
+
+    children = JSON(last_response.body)
+    children[0]["title"].should eq("child digital object component")
+  end
+
+
+  it "handles updates for an existing digital object component" do
+    created = create_digital_object_component
+
+    doc = JSONModel(:digital_object_component).find(created)
+    doc.title = "A brand new title"
+    doc.save
+
+    JSONModel(:digital_object_component).find(created).title.should eq("A brand new title")
+  end
+
+
+  # it "treats updates as being replaces, not additions" do
+  #   created = create_digital_object_component
+
+  #   doc = JSONModel(:digital_object_component).find(created)
+  #   doc.level = "series"
+  #   doc.save
+
+  #   JSONModel(:digital_object_component).find(created).level.should eq("series")
+
+  # end
+
+
+  # it "lets you create an digital object component with a subject" do
+  #   vocab = JSONModel(:vocabulary).from_hash("name" => "Some Vocab",
+  #                                            "ref_id" => "abc"
+  #                                            )
+  #   vocab.save
+
+  #   subject = JSONModel(:subject).from_hash("terms" => [{"term" => "a test subject", "term_type" => "Cultural context", "vocabulary" => JSONModel(:vocabulary).uri_for(vocab.id)}],
+  #                                           "vocabulary" => JSONModel(:vocabulary).uri_for(vocab.id)
+  #                                           )
+  #   subject.save
+
+  #   created = create_digital_object_component("ref_id" => "4567",
+  #                                    "subjects" => [subject.uri],
+  #                                    "title" => "child digital object component")
+
+  #   JSONModel(:digital_object_component).find(created).subjects[0].should eq(subject.uri)
+  # end
+
+
+  # it "can resolve subjects for you" do
+  #   vocab = JSONModel(:vocabulary).from_hash("name" => "Some Vocab",
+  #                                            "ref_id" => "abc"
+  #                                            )
+  #   vocab.save
+
+  #   subject = JSONModel(:subject).from_hash("terms" => [{"term" => "a test subject", "term_type" => "Cultural context", "vocabulary" => JSONModel(:vocabulary).uri_for(vocab.id)}],
+  #                                           "vocabulary" => JSONModel(:vocabulary).uri_for(vocab.id)
+  #                                           )
+  #   subject.save
+
+  #   created = create_digital_object_component("ref_id" => "4567",
+  #                                    "subjects" => [subject.uri],
+  #                                    "title" => "child digital object component")
+
+
+  #   doc = JSONModel(:digital_object_component).find(created, "resolve[]" => "subjects")
+
+  #   doc['resolved']['subjects'][0]["terms"][0]["term"].should eq("a test subject")
+  # end
+end
