5b6dd60f280c8743f03b32b159a31e8f880d9bf9
Mark Triggs <mark@dishevelled.net>
2012-11-06 10:51:40 +1100

    Check that the configured database is one of the supported ones.
    Also print the URL of the database as we connect to it.

--- backend/app/model/db.rb
+++ backend/app/model/db.rb
@@ -1,8 +1,25 @@
 class DB
 
+  SUPPORTED_DATABASES = [
+                         {
+                           :pattern => /jdbc:mysql/,
+                           :name => "MySQL"
+                         },
+                         {
+                           :pattern => /jdbc:derby/,
+                           :name => "Apache Derby"
+                         }
+                        ]
+
   def self.connect
     if not @pool
+
+      if !AppConfig[:allow_unsupported_database]
+        check_supported(AppConfig[:db_url])
+      end
+
       begin
+        Log.info("Connecting to database: #{AppConfig[:db_url]}")
         pool = Sequel.connect(AppConfig[:db_url],
                               :max_connections => AppConfig[:db_max_connections],
                               :test => true,
@@ -14,7 +31,7 @@ class DB
 
         @pool = pool
       rescue
-        puts "DB connection failed: #{$!}"
+        Log.error("DB connection failed: #{$!}")
       end
     end
   end
@@ -77,5 +94,50 @@ class DB
 
   def self.disconnect
     @pool.disconnect
+  end
+
+
+  def self.check_supported(url)
+    if !SUPPORTED_DATABASES.any? {|db| url =~ db[:pattern]}
+
+      msg = <<eof
+
+=======================================================================
+UNSUPPORTED DATABASE
+=======================================================================
+
+The database listed in your configuration:
+
+  #{url}
+
+is not officially supported by ArchivesSpace.  Although the system may
+still work, there's no guarantee that future versions will continue to
+work, or that it will be possible to upgrade without losing your data.
+
+It is strongly recommended that you run ArchivesSpace against one of
+these supported databases:
+
+eof
+
+      SUPPORTED_DATABASES.each do |db|
+        msg += "  * #{db[:name]}"
+      end
+
+      msg += "\n"
+      msg += <<eof
+
+To ignore this (very good) advice, you can set the configuration option:
+
+  AppConfig[:allow_unsupported_database] = true
+
+
+=======================================================================
+
+eof
+
+      Log.error(msg)
+
+      raise "Database not supported"
+    end
   end
 end
