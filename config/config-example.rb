d314e481a6c7e90abbc380e04ae8509fe4b9a4cf
Mark Triggs <mark@dishevelled.net>
2012-11-08 09:54:44 +1100

    UTF-8 fixes
    There were two unrelated issues here:
    
      * Our sample MySQL JDBC URL didn't specify an encoding, so would choke on UTF-8 characters
    
      * We were using an old json-jruby gem on the backend, which had a UTF-8 handling bug.  Upgraded it to the latest version.

--- config/config-example.rb
+++ config/config-example.rb
@@ -1,3 +1,3 @@
 # You can override the default configuration options by setting them here.
 # For example, something like like this if you want to use MySQL:
-AppConfig[:db_url] = "jdbc:mysql://localhost:3306/archivesspace?user=as&password=as123"
+AppConfig[:db_url] = "jdbc:mysql://localhost:3306/archivesspace?useUnicode=true&characterEncoding=UTF-8&user=as&password=as123"
