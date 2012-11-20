875976f8bc0d9722c4e450e7b6fe5a30d1e10f4e
Mark Triggs <mark@dishevelled.net>
2012-11-20 17:38:52 +1100

    Removed the Rails modules we're not using

--- frontend/config/application.rb
+++ frontend/config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+require 'action_controller/railtie'
+require 'sprockets/railtie'
 
 require 'java'
 
@@ -56,7 +58,7 @@ module ArchivesSpace
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
