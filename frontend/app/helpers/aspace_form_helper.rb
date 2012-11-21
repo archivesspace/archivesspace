06ad7b73c93342ce399f0a4a2870e533c00e81c8
Payten Giles <payten.giles@gmail.com>
2012-11-21 15:57:25 +1100

    Add handling for digital_object and component URLs from results list; remove puts in helper too;

--- frontend/app/helpers/aspace_form_helper.rb
+++ frontend/app/helpers/aspace_form_helper.rb
@@ -467,7 +467,6 @@ module AspaceFormHelper
   end
 
   def jsonmodel_url_for(search_result_json, action)
-    puts search_result_json.inspect
     case search_result_json["type"]
       when "accession"
         {:controller => :accessions, :action => action, :id => JSONModel(:accession).id_for(search_result_json["id"])}
@@ -479,6 +478,15 @@ module AspaceFormHelper
           :action => action,
           :id => JSONModel(:resource).id_for(search_result_json["resource"]),
           :anchor => "tree::archival_object_#{JSONModel(:archival_object).id_for(search_result_json["id"])}"
+        }
+      when "digital_object"
+        {:controller => :digital_objects, :action => action, :id => JSONModel(:digital_object).id_for(search_result_json["id"])}
+      when "digital_object_component"
+        {
+          :controller => :digital_objects,
+          :action => action,
+          :id => JSONModel(:digital_object).id_for(search_result_json["resource"]),
+          :anchor => "tree::digital_object_component_#{JSONModel(:digital_object_component).id_for(search_result_json["id"])}"
         }
       else
         nil
