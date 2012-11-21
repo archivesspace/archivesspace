84ef471045ce63f63539e802b407ec83d03078b1
Payten Giles <payten.giles@gmail.com>
2012-11-21 15:13:39 +1100

    Render results; Add missing tempalte do_search

--- frontend/app/controllers/search_controller.rb
+++ frontend/app/controllers/search_controller.rb
@@ -3,7 +3,13 @@ class SearchController < ApplicationController
   before_filter :user_needs_to_be_a_viewer, :only => [:do_search]
 
   def do_search
-    @search_data = {}
+    @criteria = {
+      :q => params[:q],
+      :page => params[:page] || 1
+    }
+    @criteria[:type] = params[:type] if not params[:type].blank?
+
+    @search_data = JSONModel::HTTP::get_json("/repositories/#{session[:repo_id]}/search", @criteria)
   end
 
 end
--- frontend/app/helpers/aspace_form_helper.rb
+++ frontend/app/helpers/aspace_form_helper.rb
@@ -466,4 +466,31 @@ module AspaceFormHelper
     string.gsub(/\n/, '<br>')
   end
 
+  def jsonmodel_url_for(search_result_json, action)
+    puts search_result_json.inspect
+    case search_result_json["type"]
+      when "accession"
+        {:controller => :accessions, :action => action, :id => JSONModel(:accession).id_for(search_result_json["id"])}
+      when "resource"
+        {:controller => :resources, :action => action, :id => JSONModel(:resource).id_for(search_result_json["id"])}
+      when "archival_object"
+        {
+          :controller => :resources,
+          :action => action,
+          :id => JSONModel(:resource).id_for(search_result_json["resource_id"]),
+          :hash => "tree::archival_object_#{JSONModel(:archival_object).id_for(search_result_json["id"])}"
+        }
+      else
+        nil
+    end
+  end
+
+  def jsonmodel_edit_url_for(search_result_json)
+    jsonmodel_url_for(search_result_json, :show)
+  end
+
+  def jsonmodel_view_url_for(search_result_json)
+    jsonmodel_url_for(search_result_json, :edit)
+  end
+
 end
