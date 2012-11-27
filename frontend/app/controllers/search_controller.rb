7d3e713fa0c2b268b3c8c13ef1564e49db99e4c9
Payten Giles <payten.giles@gmail.com>
2012-11-27 12:18:21 +1100

    Store the 5 most recent searches on the session and display a return to search results button in the breadcrumb if a search_token is passed in the URL;

--- frontend/app/controllers/search_controller.rb
+++ frontend/app/controllers/search_controller.rb
@@ -10,6 +10,31 @@ class SearchController < ApplicationController
     @criteria[:type] = params[:type] if not params[:type].blank?
 
     @search_data = JSONModel::HTTP::get_json("/repositories/#{session[:repo_id]}/search", @criteria)
+
+    store_search
+  end
+
+  private
+
+  RECENT_SEARCH_LIMIT = 5
+
+  def store_search
+    session[:recent_searches] ||= {}
+    session[:recent_searches_index] ||= 0
+
+    token = params["search_token"] if params.has_key?("search_token")
+
+    if token.nil?
+      session[:recent_searches_index] = session[:recent_searches_index] + 1
+      token = session[:recent_searches_index]
+    end
+
+    session[:recent_searches][token] = params.clone.merge({ :timestamp => Time.now })
+
+    # expire old searches
+    session[:recent_searches].delete_if {|k, v| k < token - RECENT_SEARCH_LIMIT }
+
+    @search_data["search_token"] = token
   end
 
 end
--- frontend/app/helpers/aspace_form_helper.rb
+++ frontend/app/helpers/aspace_form_helper.rb
@@ -467,39 +467,4 @@ module AspaceFormHelper
     string.gsub(/\n/, '<br>')
   end
 
-  def jsonmodel_url_for(search_result_json, action)
-    case search_result_json["type"]
-      when "accession"
-        {:controller => :accessions, :action => action, :id => JSONModel(:accession).id_for(search_result_json["id"])}
-      when "resource"
-        {:controller => :resources, :action => action, :id => JSONModel(:resource).id_for(search_result_json["id"])}
-      when "archival_object"
-        {
-          :controller => :resources,
-          :action => action,
-          :id => JSONModel(:resource).id_for(search_result_json["resource"]),
-          :anchor => "tree::archival_object_#{JSONModel(:archival_object).id_for(search_result_json["id"])}"
-        }
-      when "digital_object"
-        {:controller => :digital_objects, :action => action, :id => JSONModel(:digital_object).id_for(search_result_json["id"])}
-      when "digital_object_component"
-        {
-          :controller => :digital_objects,
-          :action => action,
-          :id => JSONModel(:digital_object).id_for(search_result_json["digital_object"]),
-          :anchor => "tree::digital_object_component_#{JSONModel(:digital_object_component).id_for(search_result_json["id"])}"
-        }
-      else
-        nil
-    end
-  end
-
-  def jsonmodel_edit_url_for(search_result_json)
-    jsonmodel_url_for(search_result_json, :show)
-  end
-
-  def jsonmodel_view_url_for(search_result_json)
-    jsonmodel_url_for(search_result_json, :edit)
-  end
-
 end
