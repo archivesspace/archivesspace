f02b768be1ea9c4b38ea17f1df4500be10e6edbb
Payten Giles <payten.giles@gmail.com>
2012-12-10 13:41:00 +1100

    Fix :order for subjects -- value needs to be a symbol

--- backend/app/model/subjects.rb
+++ backend/app/model/subjects.rb
@@ -4,7 +4,7 @@ require_relative 'subject'
 module Subjects
 
   def self.included(base)
-    base.many_to_many :subject, :join_table => "subject_#{base.table_name}", :order => "subject_#{base.table_name}__id"
+    base.many_to_many :subject, :join_table => "subject_#{base.table_name}", :order => "subject_#{base.table_name}__id".intern
 
     base.jsonmodel_hint(:the_property => :subjects,
                         :contains_records_of_type => :subject,
