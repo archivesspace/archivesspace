d69fd538b1ef8aba63a4538f89b5255a20e8bb72
Mark Triggs <mark@dishevelled.net>
2012-12-11 16:51:47 +1100

    Fixed a bug in the collection management record JSON schema, and added a test to catch it.

--- backend/spec/controller_collection_management_record_spec.rb
+++ backend/spec/controller_collection_management_record_spec.rb
@@ -3,17 +3,12 @@ require 'spec_helper'
 
 describe 'Collection Management Record controller' do
 
-  it "paginates record listings" do
-    10.times { create(:json_collection_management) }
+  it "can create a collection management record" do
+    digital_object = create(:json_digital_object)
 
-    page1_ids = JSONModel(:collection_management).all(:page => 1, :page_size => 5)['results'].map {|obj| obj.id}
-    page2_ids = JSONModel(:collection_management).all(:page => 2, :page_size => 5)['results'].map {|obj| obj.id}
+    cm = create(:json_collection_management, :linked_records => [{:ref => digital_object.uri, :other_junk => "dropped"}])
 
-    page1_ids.length.should eq(5)
-    page2_ids.length.should eq(5)
-
-    # No overlaps between the contents of our two pages
-    (page1_ids - page2_ids).length.should eq(5)
+    cm.should_not be(nil)
   end
 
 end
