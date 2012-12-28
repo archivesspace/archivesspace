42742946a2597719822ef2d407024aa8b3924316
Brian Hoffman <brianjhoffman@gmail.com>
2012-12-28 09:08:00 -0500

    corrected source and authority_id validation per spec update

--- backend/app/model/name_mixin.rb
+++ backend/app/model/name_mixin.rb
@@ -1,10 +1,11 @@
 module NameMixin
 
   def validate
-    is_using_source = self[:source] || self[:authority_id]
+    is_using_source = self[:source]
+    is_using_authority_id = self[:authority_id]
 
     validates_presence([:rules]) if not is_using_source
-    validates_presence([:source, :authority_id]) if is_using_source
+    validates_presence([:source]) if is_using_authority_id
     super
   end
 
