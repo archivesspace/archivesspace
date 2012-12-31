a8e8f802f6b147fd8e229039590f2a409e23f1fc
Brian Hoffman <brianjhoffman@gmail.com>
2012-12-31 12:08:36 -0500

    removed redundant validations from model

--- backend/app/model/name_mixin.rb
+++ backend/app/model/name_mixin.rb
@@ -1,15 +1,5 @@
 module NameMixin
 
-  def validate
-    is_using_source = self[:source]
-    is_using_authority_id = self[:authority_id]
-
-    validates_presence([:rules]) if not is_using_source
-    validates_presence([:source]) if is_using_authority_id
-    super
-  end
-
-
   def self.included(base)
     base.set_model_scope :global
   end
