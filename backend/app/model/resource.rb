f16588349e834f2e19578115f2184297ad765846
jambun <jambun@gmail.com>
2013-01-11 11:22:18 +1100

    removed obselete methods from resource model

--- backend/app/model/resource.rb
+++ backend/app/model/resource.rb
@@ -24,17 +24,4 @@ class Resource < Sequel::Model(:resource)
                       :json_property => 'related_accessions',
                       :contains_references_to_types => proc {[Accession]})
 
-
-
-  def link(opts)
-    child = ArchivalObject.get_or_die(opts[:child])
-    child.resource_id = self.id
-    child.parent_id = opts[:parent]
-    child.save
-  end
-
-
-  def children
-    ArchivalObject.filter(:resource_id => self.id, :parent_id => nil).order(:position)
-  end
 end
