f6d12df3eb3206e987511d06f0b8948a17228e7d
Brian Hoffman <brianjhoffman@gmail.com>
2013-01-17 17:46:00 -0500

    apply generalized automatable checkbox to sort name

--- frontend/app/assets/javascripts/agents.crud.js
+++ frontend/app/assets/javascripts/agents.crud.js
@@ -5,21 +5,36 @@ $(function() {
   var init_name_form = function(subform) {
     var $checkbox = $(":checkbox[name$=\"[sort_name_auto_generate]\"]", subform);
     var $sortNameField = $(":input[name$=\"[sort_name]\"]", subform);
+
+		var disableSortName = function() {
+			$sortNameField.attr("readonly","readonly");
+			$sortNameField.prop('disabled', true);
+			$sortNameField.attr("readonly","readonly");
+			$userEnteredSortNameValue = $sortNameField[0].value;
+			$sortNameField[0].value = $checkbox.attr("display_text_when_checked");
+		}
+
+
     if ($checkbox.is(":checked")) {
-      $sortNameField.attr("readonly","readonly");
-      $sortNameField.closest(".control-group").hide();
+      disableSortName();
+      // $sortNameField.closest(".control-group").hide();
     }
 
     $checkbox.change(function() {
       if ($checkbox.is(":checked")) {
-        $sortNameField.attr("readonly","readonly");
-        $sortNameField.closest(".control-group").hide();
+				disableSortName();
+        // $sortNameField.attr("readonly","readonly");
+        // $sortNameField.closest(".control-group").hide();
       } else {
+				$sortNameField.prop('disabled', false);
         $sortNameField.removeAttr("readonly");
-        $sortNameField.closest(".control-group").show();
+				$sortNameField[0].value = $userEnteredSortNameValue;
+        // $sortNameField.closest(".control-group").show();
       }
     });
   };
+
+	
 
 
   $(document).bind("new.subrecord, init.subrecord", function(event, object_name, subform) {
