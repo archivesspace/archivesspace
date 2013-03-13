8e7712a79ae3d6ee18fc6c45f8bbbd7ec4edba8c
Mark Triggs <mark@dishevelled.net>
2013-03-13 11:33:30 +1100

    Move default config options into config-defaults.rb and load and eval them.
    This will let us get the defaults back later.

---
+++ common/config/config-defaults.rb
@@ -0,0 +1,46 @@
+AppConfig[:data_directory] = File.join(Dir.home, "ArchivesSpace")
+AppConfig[:backup_directory] = proc { File.join(AppConfig[:data_directory], "demo_db_backups") }
+AppConfig[:solr_index_directory] = proc { File.join(AppConfig[:data_directory], "solr_index") }
+AppConfig[:solr_home_directory] = proc { File.join(AppConfig[:data_directory], "solr_home") }
+AppConfig[:solr_indexing_frequency_seconds] = 30
+
+AppConfig[:max_page_size] = 250
+
+AppConfig[:allow_other_unmapped] = false
+
+AppConfig[:db_url] = proc { AppConfig.demo_db_url }
+AppConfig[:db_max_connections] = 10
+
+AppConfig[:allow_unsupported_database] = false
+
+AppConfig[:demo_db_backup_schedule] = "0 4 * * *"
+AppConfig[:demo_db_backup_number_to_keep] = 7
+
+AppConfig[:backend_url] = "http://localhost:4567"
+AppConfig[:frontend_url] = "http://localhost:3000"
+AppConfig[:solr_url] = "http://localhost:2999"
+AppConfig[:indexer_url] = "http://localhost:2998"
+AppConfig[:public_url] = "http://localhost:3001"
+
+# If you have multiple instances of the backend running behind a load
+# balancer, list the URL of each backend instance here.  This is used by the
+# real-time indexing, which needs to connect directly to each running
+# instance.
+#
+# By default we assume you're not using a load balancer, so we just connect
+# to the regular backend URL.
+#
+AppConfig[:backend_instance_urls] = proc { [AppConfig[:backend_url]] }
+
+AppConfig[:frontend_theme] = "default"
+AppConfig[:public_theme] = "default"
+
+AppConfig[:session_expire_after_seconds] = 3600
+
+AppConfig[:search_username] = "search_indexer"
+
+AppConfig[:public_username] = "public_anonymous"
+
+AppConfig[:authentication_sources] = []
+
+AppConfig[:realtime_index_backlog_ms] = 60000
