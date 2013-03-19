d3cfa09b42f29a85d735ddfc9073a561b0c7512a
Mark Triggs <mark@dishevelled.net>
2013-03-19 15:03:46 +1100

    Honour JAVA_OPTS from the launcher script

--- launcher/archivesspace.sh
+++ launcher/archivesspace.sh
@@ -26,7 +26,7 @@ ARCHIVESSPACE_BASE="$PWD"
 export GEM_HOME="$ARCHIVESSPACE_BASE/gems"
 export GEM_PATH=
 
-startup_cmd="java -Darchivesspace-daemon=yes \
+startup_cmd="java $JAVA_OPTS -Darchivesspace-daemon=yes \
         -Xss2m -XX:MaxPermSize=256m -Xmx256m -Dfile.encoding=UTF-8 \
         -cp \"$GEM_HOME/gems/jruby-jars-1.7.0/lib/*:$GEM_HOME/gems/jruby-rack-1.1.12/lib/*:lib/*:launcher/lib/*\" \
         org.jruby.Main --1.9 \"launcher/launcher.rb\""
