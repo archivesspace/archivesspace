81c312f33b0616eaf15c298c23fa89f43a123f71
Mark Triggs <mark@dishevelled.net>
2013-03-19 16:34:50 +1100

    Have the setup-database.sh and configure-tomcat.sh scripts honour JAVA_OPTS too

--- launcher/scripts/setup-database.bat
+++ launcher/scripts/setup-database.bat
@@ -4,4 +4,4 @@ cd /d %~dp0%
 set GEM_HOME=%~dp0%\..\gems
 set GEM_PATH=
 
-java -cp "..\gems\gems\jruby-jars-1.7.0\lib\*;..\lib\*" org.jruby.Main --1.9 ..\scripts\rb\migrate_db.rb
+java %JAVA_OPTS% -cp "..\gems\gems\jruby-jars-1.7.0\lib\*;..\lib\*" org.jruby.Main --1.9 ..\scripts\rb\migrate_db.rb
