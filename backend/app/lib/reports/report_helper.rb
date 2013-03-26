c183143116bde3437d9197d777011decb0999e9e
Payten Giles <payten.giles@gmail.com>
2013-03-26 14:01:35 +1100

    Remove comments from helper

--- backend/app/lib/reports/report_helper.rb
+++ backend/app/lib/reports/report_helper.rb
@@ -7,7 +7,6 @@ module ReportHelper
   module ResponseHelpers
     def report_response(report, format)
       @base_url = request.base_url
-      # if download - "Content-Disposition" => "attachment; filename=\"#{report.class.name}_#{Time.now.iso8601}.csv\""
 
       if format == "csv"
         [200, {"Content-Type" => "text/plain"}, CSVResponse.new(report)]
@@ -21,10 +20,6 @@ module ReportHelper
           {"Content-Type" => "application/pdf", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.pdf\""},
           PDFResponse.new(report, erb(:'reports/report', :locals => {:report => report}),  @base_url).generate
         ]
-        #stream do |out|
-        #  PDFResponse.new(report, erb(:'reports/report', :locals => {:report => report})).to_stream(out)
-        #end
-
       else
         # default to json
         [200, {"Content-Type" => "application/json"}, JSONResponse.new(report)]
