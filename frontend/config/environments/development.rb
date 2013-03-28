61b877d6a5c155ed1c31c012af96913aa485b35e
Payten Giles <payten.giles@gmail.com>
2013-03-29 07:57:09 +1100

    Disable threadsafe in development - so code auto-complile/hot code replacement works again

--- frontend/config/environments/development.rb
+++ frontend/config/environments/development.rb
@@ -29,7 +29,8 @@ ArchivesSpace::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.threadsafe!
+  # Disable multi-threaded mode for development, so "hot code replacement" works
+  # config.threadsafe!
 
   # Do not compress assets
   config.assets.compress = false
