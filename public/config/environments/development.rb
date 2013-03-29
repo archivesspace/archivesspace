893deb9b9fcf271804884e000692a53919d09115
Payten Giles <payten.giles@gmail.com>
2013-03-29 11:03:58 +1100

    Fix subject links in public resource/ao detail pages; Make public run not in threadsafe mode when in development;

--- public/config/environments/development.rb
+++ public/config/environments/development.rb
@@ -22,7 +22,7 @@ ArchivesSpacePublic::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.threadsafe!
+  # config.threadsafe!
 
   # Do not compress assets
   config.assets.compress = false
