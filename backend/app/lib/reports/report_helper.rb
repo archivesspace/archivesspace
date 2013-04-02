997dbac50b96d8ed674bc843dfd8f1826cfc5e35
Payten Giles <payten.giles@gmail.com>
2013-04-02 16:02:31 +1100

    WIP attempt to render unicode characters in PDF reports

--- backend/app/lib/reports/report_helper.rb
+++ backend/app/lib/reports/report_helper.rb
@@ -9,9 +9,9 @@ module ReportHelper
       @base_url = request.base_url
 
       if format == "csv"
-        [200, {"Content-Type" => "text/plain", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.csv\""}, CSVResponse.new(report)]
+        [200, {"Content-Type" => "text/plain; charset=UTF-8", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.csv\""}, CSVResponse.new(report)]
       elsif format == "xlsx"
-        [200, {"Content-Type" => "text/plain", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.xlsx\""}, XLSXResponse.new(report).to_stream]
+        [200, {"Content-Type" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.xlsx\""}, XLSXResponse.new(report).to_stream]
       elsif format == "html"
         [200, {"Content-Type" => "text/html", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.html\""}, erb(:'reports/report', :locals => {:report => report})]
       elsif format == "pdf"
@@ -22,7 +22,7 @@ module ReportHelper
         ]
       else
         # default to json
-        [200, {"Content-Type" => "application/json", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.json\""}, JSONResponse.new(report)]
+        [200, {"Content-Type" => "application/json; charset=UTF-8", "Content-Disposition" => "attachment; filename=\"#{report.class.name}.json\""}, JSONResponse.new(report)]
       end
     end
   end
