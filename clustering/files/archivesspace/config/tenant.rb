c0a7cbbbe15a0cdec76ffbcb4b4ce6a08d70b543
Mark Triggs <mark@dishevelled.net>
2013-04-02 19:12:21 +1100

    Added support for running in a clustered environment.
    Includes sample directory structures, configuration files and
    supporting scripts.
    
    See clustering/README.md for a tutorial guide on setting this up.

---
+++ clustering/files/archivesspace/config/tenant.rb
@@ -0,0 +1,21 @@
+# Boiler plate for loading the right files for a given tenant on a given server
+
+$hostname = `hostname`.strip
+load File.join(File.dirname(__FILE__), "config.rb")
+
+host_config = File.join($basedir, "instance_#{$hostname}.rb")
+if File.exists?(host_config)
+  config = eval(File.read(host_config))
+  config.each do |setting, value|
+    AppConfig[setting] = value
+  end
+end
+
+# Find the URL of each backend instance to make them available to the indexer.
+
+AppConfig[:backend_instance_urls] = proc {
+  Dir.glob(File.join($basedir, "instance_*.rb")).map do |f|
+    config = eval(File.read(f))
+    config[:backend_url]
+  end
+}
