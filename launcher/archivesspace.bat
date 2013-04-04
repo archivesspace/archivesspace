f6d414165d7ac61869fb78c3e7a421c948aca085
Mark Triggs <mark@dishevelled.net>
2013-04-04 15:58:22 +1100

    .bat file changes to avoid hard-coded Ruby version

--- launcher/archivesspace.bat
+++ launcher/archivesspace.bat
@@ -1,8 +1,15 @@
-echo off
+@echo off
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 cd /d %~dp0%
 
 set GEM_HOME=%~dp0%\gems
 set GEM_PATH=
 
+set JRUBY=
+FOR /D %%c IN (!GEM_HOME!\gems\jruby-*) DO (
+  set JRUBY=!JRUBY!;%%c\lib\*
+)
 
-java -Darchivesspace-daemon=yes %JAVA_OPTS% -Xss2m -XX:MaxPermSize=256m -Xmx512m -Dfile.encoding=UTF-8 -cp "%GEM_HOME%\gems\jruby-jars-1.7.0\lib\*;%GEM_HOME%\gems\jruby-rack-1.1.12\lib\*;lib\*;launcher\lib\*" org.jruby.Main --1.9 "launcher/launcher.rb" > "logs/archivesspace.out"
+java -Darchivesspace-daemon=yes %JAVA_OPTS% -Xss2m -XX:MaxPermSize=256m -Xmx512m -Dfile.encoding=UTF-8 -cp "%GEM_HOME%\gems\jruby-rack-1.1.12\lib\*;lib\*;launcher\lib\*!JRUBY!" org.jruby.Main --1.9 "launcher/launcher.rb" > "logs/archivesspace.out"
--- launcher/scripts/setup-database.bat
+++ launcher/scripts/setup-database.bat
@@ -1,7 +1,14 @@
-echo off
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 cd /d %~dp0%
 
 set GEM_HOME=%~dp0%\..\gems
 set GEM_PATH=
 
-java %JAVA_OPTS% -cp "..\gems\gems\jruby-jars-1.7.0\lib\*;..\lib\*" org.jruby.Main --1.9 ..\scripts\rb\migrate_db.rb
+set JRUBY=
+FOR /D %%c IN (..\gems\gems\jruby-*) DO (
+  set JRUBY=!JRUBY!;%%c\lib\*
+)
+
+java %JAVA_OPTS% -cp "..\lib\*!JRUBY!" org.jruby.Main --1.9 ..\scripts\rb\migrate_db.rb
