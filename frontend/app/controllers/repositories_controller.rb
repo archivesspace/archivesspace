b4109509730406306c362944c21965c60454f11a
Payten Giles <payten.giles@gmail.com>
2013-04-05 22:45:23 +1100

    Display the repo_code in the flash message after select

--- frontend/app/controllers/repositories_controller.rb
+++ frontend/app/controllers/repositories_controller.rb
@@ -60,7 +60,7 @@ class RepositoriesController < ApplicationController
     session[:repo] = selected.uri
     session[:repo_id] = selected.id
 
-    flash[:success] = I18n.t("repository._html.messages.changed")
+    flash[:success] = I18n.t("repository._html.messages.changed", :repo_code => selected.repo_code)
 
     redirect_to :root
   end
