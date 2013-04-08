b9bec72272fe24a727a34349a1df885b904a5bf8
Brian Hoffman <brianjhoffman@gmail.com>
2013-04-08 17:26:40 -0400

    move the children of anything transferred

--- backend/app/lib/component_transfer.rb
+++ backend/app/lib/component_transfer.rb
@@ -8,28 +8,35 @@ module ComponentTransfer
       begin
         ComponentTransfer.transfer(resource_uri, archival_object_uri)
         json_response({:component => archival_object_uri, :resource => resource_uri}, 200)
-        
-      # rescue
-      #   json_response({:error => "Something went wrong..."}, 501)
+  
       end
     end
   end
   
+  
   def self.transfer(resource_uri, archival_object_uri)
     
     id = JSONModel(:archival_object).id_for(archival_object_uri)
+  
+    obj = ArchivalObject[:id => id]
     
-    if !id
+    if obj.nil?
       raise NotFoundException.new("That which does not exist cannot be moved.")
     end
     
-    obj = ArchivalObject.to_jsonmodel(id)
-    
-    obj.resource['ref'] = resource_uri
-    
-    obj.save
+    deep_transfer(JSONModel(:resource).id_for(resource_uri), obj)
   end
     
+    
+  def self.deep_transfer(new_resource_id, obj)
+    
+    ArchivalObject.this_repo.filter(:root_record_id => obj.root_record_id, :parent_id => obj.id).each do |child|
+      deep_transfer(new_resource_id, child)
+    end
+
+    obj.root_record_id = new_resource_id
+    obj.save
+  end
 end
 
 
--- backend/spec/component_transfer_spec.rb
+++ backend/spec/component_transfer_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-describe "Resource Component Transfer Service" do
+describe "Resource Component Transfer Endpoint" do
 
-  before(:each) do
-    
+  before(:each) do    
+    @resource_alpha = create(:json_resource)
+    @resource_beta = create(:json_resource)
   end
   
   def transfer(resource, object)
@@ -13,9 +14,8 @@ describe "Resource Component Transfer Service" do
     request = Net::HTTP::Post.new(url.request_uri)
     request.set_form_data({"target_resource" => resource.uri, "component" => object.uri})
      
-    response = JSONModel::HTTP.do_http_request(url, request)
-    
-    puts "RESPONSE BODY #{response.body}"
+    response = JSONModel::HTTP.do_http_request(url, request)    
+    # puts "RESPONSE BODY #{response.body}"
     
     response
   end
@@ -23,16 +23,11 @@ describe "Resource Component Transfer Service" do
   
   it "can move an archival object from one resource tree to another" do
     
-    resource_alpha = create(:json_resource)
-    resource_beta = create(:json_resource)
-
-    object = create(:json_archival_object, :resource => {:ref => resource_alpha.uri})
-
-    puts object.inspect
+    object = create(:json_archival_object, :resource => {:ref => @resource_alpha.uri})
     
-    object.resource['ref'].should eq(resource_alpha.uri)
+    object.resource['ref'].should eq(@resource_alpha.uri)
 
-    response = transfer(resource_beta, object)
+    response = transfer(@resource_beta, object)
     
     puts response.body.inspect
     
@@ -40,36 +35,44 @@ describe "Resource Component Transfer Service" do
     
     refreshed_object = JSONModel(:archival_object).find(object.id)
     
-    refreshed_object.resource['ref'].should eq(resource_beta.uri)
+    refreshed_object.resource['ref'].should eq(@resource_beta.uri)
 
   end
+
 
   it "returns a 404 response code when asked to transfer a non-existent object" do
     
     fake_uri = JSONModel(:archival_object).uri_for(99*99)
-
-    resource = create(:json_resource)
     
-    response = transfer(resource, build(:json_archival_object, :uri => fake_uri))
+    response = transfer(@resource_alpha, build(:json_archival_object, :uri => fake_uri))
     
     response.code.should eq('404')
-    response.body.should match(/not found/)
+    response.body.should match(/That which does not exist cannot be moved/)
   end
-  
-  it "returns a 409 response code when asked to transfer an object to a resource containing a conflicting object" do
+
+
+  it "returns a 400 response code when asked to transfer an object to a resource containing a conflicting object" do
     
     conflicting_ref_id = generate(:alphanumstr)
     
-    resource_alpha = create(:json_resource)
-    resource_beta = create(:json_resource)
+    object_alpha = create(:json_archival_object, :resource => {:ref => @resource_alpha.uri}, :ref_id => conflicting_ref_id)
+    object_beta = create(:json_archival_object, :resource => {:ref => @resource_beta.uri}, :ref_id => conflicting_ref_id)
     
-    object_alpha = create(:json_archival_object, :resource => {:ref => resource_alpha.uri}, :ref_id => conflicting_ref_id)
-    object_beta = create(:json_archival_object, :resource => {:ref => resource_beta.uri}, :ref_id => conflicting_ref_id)
-    
-    response = transfer(resource_beta, object_alpha)
+    response = transfer(@resource_beta, object_alpha)
     
     response.code.should eq('400')
     response.body.should match (/unique to its resource/)
   end
+  
 
+  it "moves children of the component it's moving" do
+    
+    parent = create(:json_archival_object, :resource => {:ref => @resource_alpha.uri})
+    child = create(:json_archival_object, :parent => {:ref => parent.uri}, :resource => {:ref => @resource_alpha.uri})
+
+    transfer(@resource_beta, parent)
+    
+    JSONModel(:archival_object).find(child.id).resource['ref'].should eq(@resource_beta.uri)
+        
+  end  
 end
