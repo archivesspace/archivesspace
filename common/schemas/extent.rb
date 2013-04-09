da545fd8e163225bb6170a80470511c50c92d2b9
Brian Hoffman <brianjhoffman@gmail.com>
2013-04-09 12:13:11 -0400

    fix max length attributes on extent schema to match DB

--- common/schemas/extent.rb
+++ common/schemas/extent.rb
@@ -8,8 +8,8 @@
       "number" => {"type" => "string", "maxLength" => 255, "minLength" => 1, "ifmissing" => "error"},
       "extent_type" => {"type" => "string", "minLength" => 1, "ifmissing" => "error", "dynamic_enum" => "extent_extent_type"},
 
-      "container_summary" => {"type" => "string", "maxLength" => 255, "required" => false},
-      "physical_details" => {"type" => "string", "maxLength" => 255, "required" => false},
+      "container_summary" => {"type" => "string", "maxLength" => 32672, "required" => false},
+      "physical_details" => {"type" => "string", "maxLength" => 32672, "required" => false},
       "dimensions" => {"type" => "string", "maxLength" => 255, "required" => false},
     },
 
