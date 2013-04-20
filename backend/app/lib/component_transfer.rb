f9ad159bd0651ddd31d4d0681a86e9cea7bd3bd4
Payten Giles <payten.giles@gmail.com>
2013-04-20 17:16:01 +1000

    WIP Create an event upon completing a compontent transfer

--- backend/app/lib/component_transfer.rb
+++ backend/app/lib/component_transfer.rb
@@ -20,7 +20,7 @@ module ComponentTransfer
   end
   
   
-  def self.transfer(resource_uri, archival_object_uri)
+  def self.transfer(target_resource_uri, archival_object_uri)
     id = JSONModel(:archival_object).id_for(archival_object_uri)
 
     obj = ArchivalObject[:id => id]
@@ -30,17 +30,44 @@ module ComponentTransfer
     end
 
     # Move the children first
-    deep_transfer(JSONModel::JSONModel(:resource).id_for(resource_uri), obj)
+    deep_transfer(JSONModel::JSONModel(:resource).id_for(target_resource_uri), obj)
 
     # Now move the main object to the next 
     # available top-level slot in the target
     json = obj.class.to_jsonmodel(obj)
-    
+
     json.parent = nil
-    
-    json.resource['ref'] = resource_uri
+
+    source_resource_uri = json['resource'][:ref]
+
+    json.resource['ref'] = target_resource_uri
     
     obj.update_from_json(json, {}, false)
+
+    # generate an event to mark this component transfer
+    # first get the current user
+    user = User[:username => RequestContext.get(:current_username)]
+    # build the event
+    event = JSONModel(:event).from_hash({
+      "event_type" => "component_transfer",
+      "date" => {
+        "label" => "modified",
+        "date_type" => "single",
+        "begin" => Time.now.strftime("%Y-%m-%d"),
+        "begin_time" => Time.now.strftime("%H:%M:%S"),
+      },
+      "linked_records" => [
+        {"role" => "source", "ref" => source_resource_uri},
+        {"role" => "outcome", "ref" => target_resource_uri},
+      ],
+      "linked_agents" => [
+        {"role" => "implementer", "ref" => JSONModel(:agent_person).uri_for(user.agent_record_id)}
+      ]
+    })
+    # save the event to the DB in the global context
+    Event.create_from_json(event)
+
+    obj
   end
     
     
