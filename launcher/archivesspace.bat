df30630a967ffdd241a9780daee2dffc3462e3e0
Mark Triggs <mark@dishevelled.net>
2013-04-24 07:05:04 +1000

    Tweak archivesspace.bat to log everything to logs/archivesspace.out

--- launcher/archivesspace.bat
+++ launcher/archivesspace.bat
@@ -12,4 +12,5 @@ FOR /D %%c IN (!GEM_HOME!\gems\jruby-*) DO (
   set JRUBY=!JRUBY!;%%c\lib\*
 )
 
-java -Darchivesspace-daemon=yes %JAVA_OPTS% -Xss2m -XX:MaxPermSize=256m -Xmx512m -Dfile.encoding=UTF-8 -cp "%GEM_HOME%\gems\jruby-rack-1.1.12\lib\*;lib\*;launcher\lib\*!JRUBY!" org.jruby.Main --1.9 "launcher/launcher.rb" > "logs/archivesspace.out"
+echo Writing log file to logs\archivesspace.out
+java -Darchivesspace-daemon=yes %JAVA_OPTS% -Xss2m -XX:MaxPermSize=256m -Xmx512m -Dfile.encoding=UTF-8 -cp "%GEM_HOME%\gems\jruby-rack-1.1.12\lib\*;lib\*;launcher\lib\*!JRUBY!" org.jruby.Main --1.9 "launcher/launcher.rb" > "logs/archivesspace.out" 2>&1
