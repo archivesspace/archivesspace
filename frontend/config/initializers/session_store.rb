2e1b27b57ca0130c4f9cf5c2601ca84f464ad0a7
Mark Triggs <mark@dishevelled.net>
2013-04-28 14:45:24 +1000

    Tidy ups: remove obsolete README.md, fix typo in frontend cookie name

--- frontend/config/initializers/session_store.rb
+++ frontend/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ArchivesSpace::Application.config.session_store :cookie_store, key: '_archivespace_session'
+ArchivesSpace::Application.config.session_store :cookie_store, key: '_archivesspace_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
