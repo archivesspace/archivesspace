f1205b31f3aa45e36b7e4c1b8c385a220ecd0a2d
Brian Hoffman <brianjhoffman@gmail.com>
2013-05-02 12:03:19 -0400

    make sure the frontend import directory exists

--- frontend/app/models/import_file.rb
+++ frontend/app/models/import_file.rb
@@ -6,6 +6,8 @@ class ImportFile
   def initialize(upload)
     name = upload['import_file'].original_filename
     directory = Rails.root.join('tmp', 'import')
+
+    Dir::mkdir(directory) unless File.directory?(directory)
     
     path = File.join(directory, name)
     File.open(path, "wb") { |f| f.write(upload['import_file'].read) }  
