22072ce3eb128498a018eec5460818b0fdc78063
Mark Triggs <mark@dishevelled.net>
2013-05-16 07:15:32 +1000

    Shorten index names for MySQL

--- common/db/migrations/001_create_base_schema.rb
+++ common/db/migrations/001_create_base_schema.rb
@@ -423,7 +423,7 @@ Sequel.migration do
       add_foreign_key([:root_record_id], :resource, :key => :id)
       add_foreign_key([:parent_id], :archival_object, :key => :id)
 
-      add_index([:parent_name, :position], :unique => true)
+      add_index([:parent_name, :position], :unique => true, :name => "uniq_ao_pos")
 
       add_unique_constraint([:root_record_id, :ref_id], :name => "ao_unique_refid")
     end
@@ -497,7 +497,7 @@ Sequel.migration do
       add_foreign_key([:root_record_id], :digital_object, :key => :id)
       add_foreign_key([:parent_id], :digital_object_component, :key => :id)
 
-      add_index([:root_record_id, :parent_name, :position], :unique => true)
+      add_index([:parent_name, :position], :unique => true, :name => "uniq_do_pos")
     end
 
 
