a3dc544457c7892b7cb346a415b78b3d594e10f0
Brian Hoffman <brianjhoffman@gmail.com>
2013-05-19 19:00:48 -0400

    working import controller for XHR posts

--- frontend/app/controllers/import_controller.rb
+++ frontend/app/controllers/import_controller.rb
@@ -1,11 +1,11 @@
 class ImportController < ApplicationController
 
   skip_before_filter :unauthorised_access
+  skip_before_filter :verify_authenticity_token
 
   def index
-    
     @importer_key = params[:importer]
-    
+        
     unless session[:repo_id] and session[:repo_id] > 1
       flash.now[:notice] = I18n.t("import.messages.missing_repo")
     end
@@ -14,103 +14,58 @@ class ImportController < ApplicationController
    
    
   def upload_xhr
-    # ASpaceImport::Importer.destroy_importers
-    # load '../migrations/lib/importer.rb'
-    # ASpaceImport::init
-    # Rails.logger.debug(params.inspect)
+
     if params[:upload].blank?
       self.response_body = Enumerator.new do |y|
-        y << {'errors' => ["No file uploaded"]}
+        y << {'errors' => ["No file uploaded"]} + "---\n"
       end
     else
       
+      repo_id = params[:repo_id] ? params[:repo_id] : session[:repo_id]
+      
       source_file = ImportFile.new(params[:upload])
-      importer = get_importer(source_file, params[:importer])
+      importer = get_importer(source_file, params[:importer], repo_id)
       self.response_body = Enumerator.new do |y|
-        
-        finished = false
-        while !finished
-          begin
-            importer.run_safe do |message|
 
-              message.each do |k,v|
-
-                if k == 'finished'
-                  v.each do |k,j|
-                    cdata = j.is_a?(Array) ? j.join("<br />") : v
-                    y << "<div class='import-#{k}'>#{cdata}</div>\n"
-                  end
-                  v = "Finished"
-                end
-
-                cdata = v.is_a?(Array) ? v.join("<br />") : v
-                y << "<div data class='import-#{k}'>#{cdata}</div>\n"
-                finished = true if k =~ /finished/
-              end
+        begin
+          importer.run_safe do |message|
+      
+            progress = nil
+            
+            # message is expected to be a hash
+            if message.is_a?(String)
+              message = {'string' => message}
             end
-          ensure
-            finished = true
-          end
-        end
-      end
+            
+            if message.has_key?('status') && message['status'].is_a?(String)
+              message['status'] = [message['status']]
+            end
+            
+            if message.has_key?('saved')
+
+              message['saved'] = message['saved'].map {|k,v| v[0]}
+              # resources/43#tree::archival_object_89591
+            end
       
-      headers['Last-Modified'] = Time.now.to_s
-    end
-
-  end 
-  
-
-  # TODO -fix this up for non XHR2 browsers
-  def upload
-
-    if params[:upload].blank?
-      flash.now[:error] = I18n.t("import.messages.missing_file")
-    else
-      source_file = ImportFile.new(params[:upload])
-
-      begin
-
-        i = get_importer(source_file, params[:importer])
-        
-        i.run_safe do |status|
-          # send status to the browser
+            y << ASUtils.to_json(message) + "---\n"
+          end
           
-          
+        rescue Exception => e
+          Rails.logger.debug("Import Exception #{e.to_s}")
+          y << {"errors" => [e.to_s]} + "---\n"
+        ensure
         end
 
-        # [i.report_summary, i.report]    
-        
-
-        source_file.delete
-
-        if results[0].match /200/
-          flash.now[:success] = I18n.t("import.messages.complete")
-        else
-          flash.now[:error] = I18n.t("import.messages.error_prefix") 
-        end
-        
-        @import_results = results[1]
-
-      rescue ValidationException => e
-        errors_str = e.errors.collect.map{|attr, err| "#{e.invalid_object.class.record_type}/#{attr} #{err.join(', ')}"}.join("")
-        flash.now[:error] = "#{I18n.t("import.messages.error_prefix")}: <br/> <div class='offset1'>#{errors_str}</div>".html_safe
-      rescue Exception => e
-        Rails.logger.debug("Exception raised on file import: #{e.inspect}")
-        flash.now[:error] = "#{I18n.t("import.messages.error_prefix")}: <br/> <div class='offset1'>#{e.class.name} #{e.inspect}</div>".html_safe
       end
-
+      self.response.headers['Last-Modified'] = Time.now.to_s
     end
-
-    index
-    
-    render :index
 
   end
   
   
   protected
   
-  def get_importer(source_file, importer_key)
+  def get_importer(source_file, importer_key, repo_id)
     
     flags = []
     
@@ -129,15 +84,16 @@ class ImportController < ApplicationController
       importer = :marcxml
       flags << 'subjects_and_agents_only'
     end
-
-        
+    
+    
     options = {
-               :repo_id => session[:repo_id], 
+               :repo_id => repo_id, 
                :vocab_id => '1',
                :importer => importer,
                :importer_flags => flags,
                :quiet => true,
-               :input_file => source_file.path
+               :input_file => source_file.path,
+               :log => Rails.logger
                }
     ASpaceImport::Importer.create_importer(options)    
 
