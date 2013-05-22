8b6cd614902d42de53e249745b3ee7a95a5d2312
Mark Triggs <mark@dishevelled.net>
2013-05-22 14:51:14 +1000

    Remove unused transfer method from classification_terms

--- frontend/app/controllers/classification_terms_controller.rb
+++ frontend/app/controllers/classification_terms_controller.rb
@@ -1,8 +1,8 @@
 class ClassificationTermsController < ApplicationController
   skip_before_filter :unauthorised_access, :only => [:index, :show, :new, :edit, :create, :update, :parent, :transfer, :delete]
   before_filter(:only => [:index, :show]) {|c| user_must_have("view_repository")}
-  before_filter(:only => [:new, :edit, :create, :update, :parent, :transfer]) {|c| user_must_have("update_archival_record")}
-  before_filter(:only => [:delete]) {|c| user_must_have("delete_archival_record")}
+  before_filter(:only => [:new, :edit, :create, :update, :parent, :transfer]) {|c| user_must_have("update_classification_record")}
+  before_filter(:only => [:delete]) {|c| user_must_have("delete_classification_record")}
 
   FIND_OPTS = {
     "resolve[]" => ["creator"]
@@ -14,10 +14,8 @@ class ClassificationTermsController < ApplicationController
     @classification_term.classification = {'ref' => JSONModel(:classification).uri_for(params[:classification_id])} if params.has_key?(:classification_id)
 
     return render :partial => "classification_terms/new_inline" if inline?
-
-    # render the full AO form
-
   end
+
 
   def edit
     @classification_term = JSONModel(:classification_term).find(params[:id], FIND_OPTS)
@@ -93,31 +91,6 @@ class ClassificationTermsController < ApplicationController
       errors = ASUtils.json_parse(response.body)
 
       render :partial => "shared/quick_messages", :locals => {:exceptions => errors, :jsonmodel => "classification_term"}, :status => 500
-    end
-  end
-
-
-  def transfer
-    begin
-      post_data = {
-        :target_classification => params["transfer"]["ref"],
-        :component => JSONModel(:classification_term).uri_for(params[:id])
-      }
-
-      response = JSONModel::HTTP.post_form("/repositories/#{session[:repo_id]}/component_transfers", post_data)
-
-      if response.code == '200'
-        @classification_term = JSONModel(:classification_term).find(params[:id], FIND_OPTS)
-
-        flash[:success] = I18n.t("classification_term._frontend.messages.transfer_success", JSONModelI18nWrapper.new(:classification_term => @classification_term, :classification => @classification_term['classification']['_resolved']))
-        redirect_to :controller => :classifications, :action => :edit, :id => JSONModel(:classification).id_for(params["transfer"]["ref"]), :anchor => "tree::classification_term_#{params[:id]}"
-      else
-        raise ASUtils.json_parse(response.body)['error'].to_s
-      end
-
-    rescue Exception => e
-      flash[:error] = I18n.t("classification_term._frontend.messages.transfer_error", :exception => e)
-      redirect_to :controller => :classifications, :action => :edit, :id => params["transfer"]["current_classification_id"], :anchor => "tree::classification_term_#{params[:id]}"
     end
   end
 
