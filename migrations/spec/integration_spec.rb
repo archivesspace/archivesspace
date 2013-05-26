da78b93c78694ee9dc8c8352759a0aa66337c9b8
Mark Triggs <mark@dishevelled.net>
2013-05-26 19:46:23 +1000

    Switch tests to use run_safe with a block

--- migrations/spec/integration_spec.rb
+++ migrations/spec/integration_spec.rb
@@ -2,113 +2,98 @@ require_relative "spec_helper.rb"
 
 
 describe 'ASpaceImport' do
-  
-  
+
+
   before(:all) do
-    
+
     start_backend
-    @vocab_uri = make_test_vocab  
+    @vocab_uri = make_test_vocab
   end
-  
+
   before(:each) do
     @repo = create(:json_repo)
     @repo_id = @repo.class.id_for(@repo.uri)
-    
+
     @opts = {
       :repo_id => @repo_id,
       :vocab_uri => build(:json_vocab).class.uri_for(2)
     }
-    
+
   end
-  
+
   after(:each) do
     @opts = {}
   end
-  
+
   after(:all) do
     stop_backend
   end
 
-  # Turning off for now b/c of Derby length issues with this EAD.
-  # it "can import the file at examples/ead/archon-tracer.xml" do
-  # 
-  #   @opts.merge!({
-  #           :input_file => '../examples/ead/archon-tracer.xml', 
-  #           :importer => 'ead',
-  #           :quiet => true
-  #           })
-  # 
-  #   @i = ASpaceImport::Importer.create_importer(@opts)
-  #   
-  #   @i.run
-  #   result = @i.report.split(/\n/)
-  #   result.shift.should match(/Aspace Import Report/)
-  #   result.each do |r|
-  #     r.should match(/^Saved: .*[0-9]\]$/)
-  #   end
-  #   
-  # end
-  
+
   it "can import the file at examples/ead/ferris.xml" do
-  
+
     @opts.merge!({
-            :input_file => '../examples/ead/ferris.xml', 
+            :input_file => '../examples/ead/ferris.xml',
             :importer => 'ead',
             :quiet => true
             })
 
     @i = ASpaceImport::Importer.create_importer(@opts)
-    
-    @i.run
-    result = @i.report.split(/\n/)
-    result.shift.should match(/Aspace Import Report/)
-    result.each do |r|
-      r.should match(/^Saved: .*[0-9]\]$/)
+
+    count = 0
+
+    @i.run_safe do |msg|
+      if msg['saved']
+        count = msg['saved'].count
+      end
     end
-    
+
+    (count > 0).should be(true)
   end
-  
-  
+
+
   it "can import the file at examples/eac/feynman-richard-phillips-1918-1988-cr.xml" do
-  
+
     @opts.merge!({
-            :input_file => '../examples/eac/feynman-richard-phillips-1918-1988-cr.xml', 
+            :input_file => '../examples/eac/feynman-richard-phillips-1918-1988-cr.xml',
             :importer => 'eac',
             :quiet => true
             })
 
     @i = ASpaceImport::Importer.create_importer(@opts)
-    
-    @i.run
-    result = @i.report.split(/\n/)
-    result.shift.should match(/Aspace Import Report/)
-    result.each do |r|
-      r.should match(/^Saved: .*[0-9]\]$/)
+
+    count = 0
+
+    @i.run_safe do |msg|
+      if msg['saved']
+        count = msg['saved'].count
+      end
     end
-    
+
+    (count > 0).should be(true)
   end
-  
-  
+
+
   it "can import the file at examples/marc/american-communist.xml" do
-  
+
     @opts.merge!({
-            :input_file => '../examples/marc/american-communist.xml', 
+            :input_file => '../examples/marc/american-communist.xml',
             :importer => 'marcxml',
             :quiet => true
             })
 
     @i = ASpaceImport::Importer.create_importer(@opts)
-    
-    @i.run
-    result = @i.report.split(/\n/)
-    result.shift.should match(/Aspace Import Report/)
-    result.each do |r|
-      r.should match(/^Saved: .*[0-9]\]$/)
+
+    count = 0
+
+    @i.run_safe do |msg|
+      if msg['saved']
+        count = msg['saved'].count
+      end
     end
-    result.count.should eq(10)
-    
+
+    count.should eq(10)
   end
 
 
 end
-
