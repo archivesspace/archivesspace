6ab3a8936095cbb0601e5135e843d1dcd565b43c
Payten Giles <payten.giles@gmail.com>
2013-05-29 20:12:20 +1000

    WIP show a message in the sidebar and form_messages div when the update_monitor returns a not-ok status

--- frontend/app/assets/javascripts/update_monitor.js
+++ frontend/app/assets/javascripts/update_monitor.js
@@ -1,6 +1,8 @@
 $(function() {
 
   var INTERVAL_PERIOD = 10000; // check every 10 seconds
+  var STATUS_STALE = "stale";
+  var STATUS_OTHER_EDITORS = "opened_for_editing";
 
   var setupUpdateMonitor = function($form) {
 
@@ -17,11 +19,44 @@ $(function() {
     var handleStaleRecord = function(status_data) {
       console.log("STALE RECORD");
       console.log(status_data);
+      insertErrorAndHighlightSidebar(status_data);
     };
 
     var handleOpenedForEditing = function(status_data) {
       console.log("RECORD OPENED FOR EDITING");
       console.log(status_data);
+      insertErrorAndHighlightSidebar(status_data);
+    };
+
+    var insertErrorAndHighlightSidebar = function(status_data) {
+      // insert the error
+      $("#form_messages .update-monitor-error", $form).remove(); // remove any existing errors
+      if (status_data.status === STATUS_STALE) {
+        $("#form_messages", $form).prepend(AS.renderTemplate("update_monitor_stale_record_message_template"));
+        $(".btn-primary, .btn-toolbar .btn", $form).attr("disabled", "disabled").addClass("disabled");
+      } else if (status_data.status === STATUS_OTHER_EDITORS) {
+        $("#form_messages", $form).prepend(AS.renderTemplate("update_monitor_other_editors_message_template", {user_ids: "admin"}));
+      }
+
+      // highlight in the sidebar
+      if ($(".as-nav-list li.alert-error").length === 0) {
+        $(".as-nav-list").prepend("<li class='alert-error update-monitor-error'><a href='#form_messages'>Errors and Warnings</a></li>");
+      }
+      var $errorNavListItem = $(".as-nav-list li.alert-error");
+
+      if (!$errorNavListItem.hasClass("acknowledged")) {
+        var highlight_interval = setInterval(function() {
+          $errorNavListItem.toggleClass("active");
+        }, 3000)
+        $errorNavListItem.hover(function() {
+          clearInterval(highlight_interval);
+          $errorNavListItem.removeClass("active").addClass("acknowledged");
+        }, function() {});
+      }
+    };
+
+    var clearAnyMonitorErrors = function() {
+      $("li.update-monitor-error", $form).remove();
     };
 
     var poll = function() {
@@ -32,12 +67,13 @@ $(function() {
           uri: uri
         },
         function(json, textStatus, jqXHR) {
-          if (json.status === "stale") {
+          if (json.status === STATUS_STALE) {
             handleStaleRecord(json);
-          } else if (json.status === "opened_for_editing") {
+          } else if (json.status === STATUS_OTHER_EDITORS) {
             handleOpenedForEditing(json);
           } else {
             // nobody else editing and lock_version still current
+            clearAnyMonitorErrors()
           }
         },
         "json")
