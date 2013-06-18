fb11d957bdd1c31b2f30bb3cd81371ff9efd092d
Mark Triggs <mark@dishevelled.net>
2013-06-18 11:36:48 +1000

    Add a new text note type to multipart notes

---
+++ common/schemas/note_text.rb
@@ -0,0 +1,19 @@
+{
+  :schema => {
+    "$schema" => "http://www.archivesspace.org/archivesspace.json",
+    "version" => 1,
+    "type" => "object",
+
+    "properties" => {
+      "content" => {
+        "type" => "string",
+        "maxLength" => 65000,
+        "ifmissing" => "error",
+      },
+
+      "publish" => {"type" => "boolean", "default" => true},
+    },
+
+    "additionalProperties" => false,
+  },
+}
