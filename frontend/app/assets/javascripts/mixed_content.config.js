d771a5e4f10f9ec136a0fefc61f51f16c099c6e9
Payten Giles <payten.giles@gmail.com>
2013-06-18 12:06:12 +1000

    Make all mixed_content.config entries a String.. as 'function' would think it was a js func

--- frontend/app/assets/javascripts/mixed_content.config.js
+++ frontend/app/assets/javascripts/mixed_content.config.js
@@ -1,73 +1,73 @@
 $(function() {
   AS.mixedContentElements = {
-      blockquote: {
-        tag: "blockquote",
-        attributes: []
+      "blockquote": {
+        "tag": "blockquote",
+        "attributes": []
       },
-      date: {
-        tag: "date",
-        attributes: ["type", "normal", "calendar", "era"]
+      "date": {
+        "tag": "date",
+        "attributes": ["type", "normal", "calendar", "era"]
       },
-      function: {
-        tag: "function",
-        attributes: ["rules", "source"]
+      "function": {
+        "tag": "function",
+        "attributes": ["rules", "source"]
       },
-      occupation: {
-        tag: "occupation",
-        attributes: ["type", "normal", "calendar", "era"]
+      "occupation": {
+        "tag": "occupation",
+        "attributes": ["type", "normal", "calendar", "era"]
       },
-      subject: {
-        tag: "subject",
-        attributes: ["type", "normal", "calendar", "era"]
+      "subject": {
+        "tag": "subject",
+        "attributes": ["type", "normal", "calendar", "era"]
       },
-      emph: {
-        tag: "emph",
-        attributes: ["render"]
+      "emph": {
+        "tag": "emph",
+        "attributes": ["render"]
       },
-      corpname: {
-        tag: "corpname",
-        attributes: ["rules", "role", "source"]
+      "corpname": {
+        "tag": "corpname",
+        "attributes": ["rules", "role", "source"]
       },
-      p: {
-        tag: "p",
-        attributes: [],
-        elements: ["emph"]
+      "p": {
+        "tag": "p",
+        "attributes": [],
+        "elements": ["emph"]
       },
-      persname: {
-        tag: "persname",
-        attributes: ["rules", "role", "source"]
+      "persname": {
+        "tag": "persname",
+        "attributes": ["rules", "role", "source"]
       },
-      famname: {
-        tag: "famname",
-        attributes: ["rules", "role", "source"]
+      "famname": {
+        "tag": "famname",
+        "attributes": ["rules", "role", "source"]
       },
-      name: {
-        tag: "name",
-        attributes: ["rules", "role", "source"]
+      "name": {
+        "tag": "name",
+        "attributes": ["rules", "role", "source"]
       },
-      geogname: {
-        tag: "geogname",
-        attributes: ["rules", "role", "source"]
+      "geogname": {
+        "tag": "geogname",
+        "attributes": ["rules", "role", "source"]
       },
-      genreform: {
-        tag: "genreform",
-        attributes: ["rules", "role", "type"]
+      "genreform": {
+        "tag": "genreform",
+        "attributes": ["rules", "role", "type"]
       },
-      title: {
-        tag: "title",
-        attributes: ["render"]
+      "title": {
+        "tag": "title",
+        "attributes": ["render"]
       },
-      ref: {
-        tag: "ref",
-        attributes: ["target", "show", "title", "actuate"]
+      "ref": {
+        "tag": "ref",
+        "attributes": ["target", "show", "title", "actuate"]
       },
-      extref: {
-        tag: "extref",
-        attributes: ["target", "show", "title", "actuate"]
+      "extref": {
+        "tag": "extref",
+        "attributes": ["target", "show", "title", "actuate"]
       },
-      lb: {
-        tag: "lb",
-        attributes: []
+      "lb": {
+        "tag": "lb",
+        "attributes": []
       }
     };
 });
