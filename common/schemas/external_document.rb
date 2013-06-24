e8df8706f6f660cd842f96d5db9a5b6797e4e0c0
Mark A. Matienzo <mark@matienzo.org>
2013-06-24 16:52:24 -0400

    Make URI fields LongStrings; fixes #51654063

--- common/schemas/external_document.rb
+++ common/schemas/external_document.rb
@@ -5,7 +5,7 @@
     "type" => "object",
     "properties" => {
       "title" => {"type" => "string", "maxLength" => 16384, "ifmissing" => "error", "minLength" => 1},
-      "location" => {"type" => "string", "maxLength" => 255, "ifmissing" => "error", "default" => ""},
+      "location" => {"type" => "string", "maxLength" => 16384, "ifmissing" => "error", "default" => ""},
       "publish" => {"type" => "boolean", "default" => true},
     },
 
