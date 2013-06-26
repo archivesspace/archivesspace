955739e0a709a779c9c12c90c4aa1c363ab6dec6
jambun <jambun@gmail.com>
2013-06-26 10:33:20 +1000

    WIP added version file to build and show it in footer

--- common/asconstants.rb
+++ common/asconstants.rb
@@ -1,11 +1,20 @@
 module ASConstants
 
+  @VERSION
+
   module Group
 
     def self.GLOBAL
       '_archivesspace'
     end
 
+  end
+
+  def self.VERSION
+    return @VERSION if @VERSION
+
+    version_file = File.join("..", "common", "VERSION")
+    @VERSION = File.exists?(version_file) ? File.open(version_file).read : "NO VERSION"
   end
 
 end
