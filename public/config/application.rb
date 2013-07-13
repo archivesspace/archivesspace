a9aa958c53cffc35791eb9a873970f92380a9090
Mark Triggs <mark@dishevelled.net>
2013-07-13 10:37:43 +1000

    Add an extra hook to let plugins insert their own JavaScript, CSS, etc.

--- public/config/application.rb
+++ public/config/application.rb
@@ -26,6 +26,10 @@ module ArchivesSpacePublic
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    Array(AppConfig[:plugins]).each do |plugin|
+      config.paths["app/controllers"] << "../plugins/#{plugin}/public/controllers"
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
