46428895191a18353698a5d4534d8d0a2fdd7488
Payten Giles <payten.giles@gmail.com>
2013-12-11 15:31:59 +1100

    Allow public and frontend rails apps to be scoped with a URI path to support multiple aspace instances running on the same machine with different prefixes

--- public/config/routes.rb
+++ public/config/routes.rb
@@ -1,16 +1,22 @@
 ArchivesSpacePublic::Application.routes.draw do
-  match 'search' => 'search#search', :via => [:get]
-  match 'advanced_search' => 'search#advanced_search', :via => [:get]
-  match 'repositories/:repo_id/resources/:id' => 'records#resource', :via => [:get]
-  match 'repositories/:repo_id/digital_objects/:id' => 'records#digital_object', :via => [:get]
-  match 'repositories/:repo_id/archival_objects/:id' => 'records#archival_object', :via => [:get]
-  match 'repositories/:repo_id/digital_object_components/:id' => 'records#digital_object_component', :via => [:get]
-  match 'repositories/:repo_id' => 'search#repository', :via => [:get]
-  match 'repositories/:repo_id/classifications/:id' => 'records#classification', :via => [:get]
-  match 'repositories/:repo_id/accessions/:id' => 'records#accession', :via => [:get]
-  match 'agents/:id' => 'records#agent', :via => [:get]
 
-  match 'repositories' => 'search#repository', :via => [:get]
-  match 'subjects/:id' => 'search#subject', :via => [:get]
-  root :to => "site#index"
+  scope URI(AppConfig[:public_url]).path do
+
+    match 'search' => 'search#search', :via => [:get]
+    match 'advanced_search' => 'search#advanced_search', :via => [:get]
+    match 'repositories/:repo_id/resources/:id' => 'records#resource', :via => [:get]
+    match 'repositories/:repo_id/digital_objects/:id' => 'records#digital_object', :via => [:get]
+    match 'repositories/:repo_id/archival_objects/:id' => 'records#archival_object', :via => [:get]
+    match 'repositories/:repo_id/digital_object_components/:id' => 'records#digital_object_component', :via => [:get]
+    match 'repositories/:repo_id' => 'search#repository', :via => [:get]
+    match 'repositories/:repo_id/classifications/:id' => 'records#classification', :via => [:get]
+    match 'repositories/:repo_id/accessions/:id' => 'records#accession', :via => [:get]
+    match 'agents/:id' => 'records#agent', :via => [:get]
+
+    match 'repositories' => 'search#repository', :via => [:get]
+    match 'subjects/:id' => 'search#subject', :via => [:get]
+    root :to => "site#index"
+
+  end
+
 end
