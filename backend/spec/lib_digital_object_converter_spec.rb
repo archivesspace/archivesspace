3618190f987bbf4f6f9a1388e8604d1ddceb154b
Mark Triggs <mark@dishevelled.net>
2013-12-17 17:08:58 +1100

    Make the digital object test CSV a little richer

--- backend/spec/lib_digital_object_converter_spec.rb
+++ backend/spec/lib_digital_object_converter_spec.rb
@@ -4,15 +4,17 @@ require_relative '../app/converters/digital_object_converter'
 describe 'Digital Object converter' do
 
   def convert_test_file
-    converter = DigitalObjectConverter.new(File.expand_path("../app/migrations/examples/digital_object/test_digital_object.csv",
-                                                            File.dirname(__FILE__)))
+    test_file = File.expand_path("../app/migrations/examples/digital_object/test_digital_object.csv",
+                                 File.dirname(__FILE__))
+    converter = DigitalObjectConverter.instance_for('digital_object_csv', test_file)
     converter.run
     JSON(IO.read(converter.get_output_path))
   end
 
 
   it "did something" do
-    record = convert_test_file.first
+    record = convert_test_file.find {|rec| rec['jsonmodel_type'] == 'digital_object'}
+    record.should_not be(nil)
 
     record['jsonmodel_type'].should eq('digital_object')
     record['level'].should eq('image')
