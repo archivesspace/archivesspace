9b18c5654884d012edfc59b344c4b94384249b11
Brian Hoffman <brianjhoffman@gmail.com>
2014-01-13 17:30:15 -0500

    revise accession converter test and add test for processing date

--- backend/spec/lib_accession_converter_spec.rb
+++ backend/spec/lib_accession_converter_spec.rb
@@ -6,16 +6,26 @@ describe 'Accession converter' do
   def convert_test_file
     test_file = File.expand_path("../app/migrations/examples/accession/test_accession.csv",
                                  File.dirname(__FILE__))
+
     converter = AccessionConverter.instance_for('accession_csv', test_file)
     converter.run
     JSON(IO.read(converter.get_output_path))
   end
 
-
-  it "did something" do
-    record = convert_test_file.select {|rec| rec['jsonmodel_type'] == 'accession'}
-
-    record.count.should eq(25)
+  before(:all) do
+    @records = convert_test_file
+    @accessions = @records.select {|r| r['jsonmodel_type'] == 'accession' }
   end
+
+
+  it "created one Accession record for each row in the CSV file" do
+    @accessions.count.should eq(10)
+  end
+
+
+  it "maps accession_processing_started_date to collection_management.processing_started_date" do    
+    @accessions[1]['collection_management']['processing_started_date'].should match(/\d{4}-\d{2}-\d{2}/)
+  end
+    
 end
 
