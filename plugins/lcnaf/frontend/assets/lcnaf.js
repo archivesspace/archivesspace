eba020b08e7cd0e57504865a248cc099e28cda5f
Payten Giles <payten.giles@gmail.com>
2014-01-22 13:23:27 +1100

    Add ability to add/remove results

--- plugins/lcnaf/frontend/assets/lcnaf.js
+++ plugins/lcnaf/frontend/assets/lcnaf.js
@@ -1,16 +1,44 @@
 $(function() {
   var $form = $("#lcnaf_search");
   var $results = $("#results");
+  var $selected = $("#selected");
+
+  var selected_lccns = {};
 
   var renderResults = function(json) {
     $results.empty();
     $results.append(AS.renderTemplate("template_lcnaf_result_summary", json));
     $.each(json.records, function(i, record) {
-      $results.append(AS.renderTemplate("template_lcnaf_result", record));
+      var $result = $(AS.renderTemplate("template_lcnaf_result", {record: record, selected: selected_lccns}));
+      if (selected_lccns[record.lccn]) {
+        $(".alert-success", $result).removeClass("hide");
+      } else {
+        $("button", $result).removeClass("hide");
+      }
+      $results.append($result);
+
     });
     $results.append(AS.renderTemplate("template_lcnaf_pagination", json));
     $('pre code', $results).each(function(i, e) {hljs.highlightBlock(e)});
   };
+
+  var removeSelected = function(lccn) {
+    selected_lccns[lccn] = false;
+    $("[data-lccn="+lccn+"]", $selected).remove();
+    var $result = $("[data-lccn="+lccn+"]", $results);
+    if ($result.length > 0) {
+      $result.removeClass("hide");
+      $result.siblings(".alert").addClass("hide");
+    }
+  };
+
+  var addSelected = function(lccn, $result) {
+    selected_lccns[lccn] = true;
+    $selected.append(AS.renderTemplate("template_lcnaf_selected", {lccn: lccn}))
+
+    $(".alert-success", $result).removeClass("hide");
+    $("button", $result).addClass("hide");
+  }
 
   $form.ajaxForm({
     dataType: "json",
@@ -27,6 +55,18 @@ $(function() {
       $results.ScrollTo();
       renderResults(json);
     });
-  })
+  }).on("click", ".lcnaf-result button", function(event) {
+    var lccn = $(this).data("lccn");
+    if (selected_lccns[lccn]) {
+      removeSelected(lccn);
+    } else {
+      addSelected(lccn, $(this).closest(".lcnaf-result"));
+    }
+  });
+
+  $selected.on("click", ".remove-selected", function(event) {
+    var lccn = $(this).parent().data("lccn");
+    removeSelected(lccn);
+  });
 
 })
