ee663119b8cbc7bf30d8594fce3eecc458179308
Mark Triggs <mark@dishevelled.net>
2014-02-03 10:10:31 +1100

    Turn HTTPOnly cookies back on.  Turning them off was me being misguided, since HTTPOnly doesn't stop the cookie being sent through with AJAX requests.

--- frontend/config/initializers/session_store.rb
+++ frontend/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Use httponly because some of our AJAX handlers need access to the session too.
-ArchivesSpace::Application.config.session_store :cookie_store, key: "#{AppConfig[:cookie_prefix]}_session", :httponly => false
+ArchivesSpace::Application.config.session_store :cookie_store, key: "#{AppConfig[:cookie_prefix]}_session"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
