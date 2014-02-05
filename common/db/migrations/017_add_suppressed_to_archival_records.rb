5741e0b9810e5defa283d91501bdec84d1deded9
Mark Triggs <mark@dishevelled.net>
2014-02-05 13:28:36 +1100

    Add 'suppressed' column to all relationship tables.

--- common/db/migrations/017_add_suppressed_to_archival_records.rb
+++ common/db/migrations/017_add_suppressed_to_archival_records.rb
@@ -8,6 +8,18 @@ Sequel.migration do
         add_column(:suppressed, :integer, :null => false, :default => 0)
       end
     end
+
+
+    [:classification_creator_rlshp, :classification_rlshp,
+     :classification_term_creator_rlshp, :event_link_rlshp, :housed_at_rlshp,
+     :instance_do_link_rlshp, :linked_agents_rlshp, :related_agents_rlshp,
+     :spawned_rlshp, :subject_rlshp].each do |relationship_table|
+
+      alter_table(relationship_table) do
+        add_column(:suppressed, :integer, :null => false, :default => 0)
+      end
+    end
+
   end
 
 
