14ba84e999b02184a2bc4e512f734719e467926c
jambun <jambun@gmail.com>
2014-02-11 15:29:53 +1100

    tweaked frontend preferences controller to enforce editing only your own prefs

--- frontend/app/controllers/preferences_controller.rb
+++ frontend/app/controllers/preferences_controller.rb
@@ -3,26 +3,27 @@ class PreferencesController < ApplicationController
   set_access_control  "view_repository" => [:edit, :update]
 
   def edit
-    if params['id'] == "0"
-      @search_data = JSONModel(:preference).all(:user_id => JSONModel(:user).id_for(session[:user_uri]))
+    @search_data = JSONModel(:preference).all(:user_id => JSONModel(:user).id_for(session[:user_uri]))
       
-      if @search_data.length == 1
-        pref = @search_data.first
-      elsif @search_data.length == 0
-        # create a record
-        pref = JSONModel(:preference).new({
-                                :defaults => '{}',
-                                :user_id => JSONModel(:user).id_for(session[:user_uri])
-                              })
-        pref.save
-      else
-        # problem
-      end
-      redirect_to(:controller => :preferences, :action => :edit, :id => pref.id)
+    if @search_data.length == 1
+      pref = @search_data.first
+    elsif @search_data.length == 0
+      # create a record
+      pref = JSONModel(:preference).new({
+                                          :defaults => '{}',
+                                          :user_id => JSONModel(:user).id_for(session[:user_uri])
+                                        })
+      pref.save
     else
-      @preference = JSONModel(:preference).find(params['id'])
+      # TODO: maybe we should deal with this problem. there should be only one
+      pref = @search_data.first
     end
-    
+
+    if params['id'] == pref.id.to_s
+      @preference = pref
+    else
+      redirect_to(:controller => :preferences, :action => :edit, :id => pref.id)
+    end
   end
 
 
