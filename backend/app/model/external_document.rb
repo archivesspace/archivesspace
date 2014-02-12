9a4f47b55c0e6125a10aa6bc6f213755b250513b
Payten Giles <payten.giles@gmail.com>
2014-02-12 20:48:26 +1100

    Add migration to convert the external document many-to-many relationship to a one-to-one.
    Modify the external document mixin to define a basic one-to-many nested record.
    
    Clean up the unique location (within a record) validations and add a translation (it was without an error msg at all).

--- backend/app/model/external_document.rb
+++ backend/app/model/external_document.rb
@@ -3,4 +3,24 @@ class ExternalDocument < Sequel::Model(:external_document)
   corresponds_to JSONModel(:external_document)
 
   set_model_scope :global
+
+  def validate
+    [:accession, :archival_object,
+     :resource, :subject,
+     :agent_person,
+     :agent_family,
+     :agent_corporate_entity,
+     :agent_software,
+     :rights_statement,
+     :digital_object,
+     :digital_object_component].each do |record|
+
+      validates_unique([:location, "#{record}_id".intern],
+                        :message => "location must be unique within a record")
+
+      map_validation_to_json_property([:location, "#{record}_id".intern], :location)
+    end
+
+    super
+  end
 end
--- backend/app/model/mixins/external_documents.rb
+++ backend/app/model/mixins/external_documents.rb
@@ -10,16 +10,7 @@ module ExternalDocuments
   end
 
   def self.included(base)
-    base.many_to_many(:external_document,
-                      :before_add => proc { |obj, item_to_add|
-                        if obj.external_document.any?{|doc| doc.location == item_to_add.location}
-                          raise Sequel::ValidationFailed.new("Duplicate entry for location: #{item_to_add.location}")
-                        end
-
-                        true
-                      },
-                      :join_table => "#{base.table_name}_external_document".intern,
-                      :order => "#{base.table_name}_external_document__id".intern)
+    base.one_to_many(:external_document)
 
     base.def_nested_record(:the_property => :external_documents,
                            :contains_records_of_type => :external_document,
