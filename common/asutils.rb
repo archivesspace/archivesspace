0b635d91040c712f2eb725a005c5a6c3151090a0
Mark Triggs <mark@dishevelled.net>
2014-02-19 12:25:56 +1100

    Move plugin gem loading into ASUtils

--- common/asutils.rb
+++ common/asutils.rb
@@ -162,4 +162,22 @@ EOF
   end
 
 
+  def self.load_plugin_gems(context)
+    ASUtils.find_local_directories.each do |plugin|
+      gemfile = File.join(plugin, 'Gemfile')
+      gemdir = File.join(plugin, "gems")
+
+      if File.exists?(gemfile)
+        if Dir.exists?(gemdir)
+          Dir.glob(File.join(File.expand_path(gemdir), "gems", "*", "lib")).each do |gem|
+            $: << gem
+          end
+        end
+
+        context.instance_eval(File.read(gemfile))
+      end
+    end
+  end
+
+
 end
