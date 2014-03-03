384d95862cbd4cb8a0da0113b900d510aecded8a
Mark Triggs <mark@dishevelled.net>
2014-03-04 10:57:45 +1100

    Return suppression status with records

--- common/schemas/record_tree.rb
+++ common/schemas/record_tree.rb
@@ -9,6 +9,7 @@
       "record_uri" => {"type" => "string", "ifmissing" => "error"},
       "title" => {"type" => "string", "minLength" => 1, "required" => false, "maxLength" => 16384},
       "publish" => {"type" => "boolean", "default" => true},
+      "suppressed" => {"type" => "boolean", "default" => false},
       "has_children" => {"type" => "boolean", "readonly" => true},
       "node_type" => {"type" => "string", "maxLength" => 255},
     },
