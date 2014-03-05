3bbe076d124b98a8138e9ca26a8c66f17f54c553
Payten Giles <payten.giles@gmail.com>
2014-03-05 12:29:00 +1100

    Only render a image if the size is provided (to avoid big images in the page)

--- frontend/app/helpers/file_embed_helper.rb
+++ frontend/app/helpers/file_embed_helper.rb
@@ -4,7 +4,8 @@ module FileEmbedHelper
     uri = URI(file_version['file_uri'])
 
     if %w(jpeg gif).include?(file_version['file_format_name']) && 
-        uri.scheme =~ /http/ && 
+        uri.scheme =~ /http/ &&
+        file_version['file_size_bytes'].to_i > 0 &&
         file_version['file_size_bytes'].to_i < 512001
 
       true
