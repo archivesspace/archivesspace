04b902bee31fedcf2d341eed3c85fef840cabb1c
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2014-04-09 21:33:37 +0200

    tweak to fix sequel issue with MySQL

--- common/db/migrations/028_remove_bulk_date.rb
+++ common/db/migrations/028_remove_bulk_date.rb
@@ -7,7 +7,8 @@ Sequel.migration do
     bulk_dates = self[:enumeration_value].filter(:value => "bulk").select(:id)
     inclusive_dates = self[:enumeration_value].filter(:value => "inclusive" ).select(:id)
     self[:date].filter(:date_type_id => bulk_dates ).update(:date_type_id => inclusive_dates)
-    self[:enumeration_value].filter(:id => bulk_dates).delete 
+ 
+    self[:enumeration_value].filter(:value => "bulk").delete 
   end
 
 
