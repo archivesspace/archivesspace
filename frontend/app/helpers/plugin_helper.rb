61f62ee654bc98b84bbe7f6addb33d357f6875c5
Mark Triggs <mark@dishevelled.net>
2014-05-19 17:35:59 +1000

    Modify the plugin_helper to handle zero_to_one record types.
    Currently it assumes that plugin-added record types will always be
    arrays.  Allow it to be a single value (or nil) too.

--- frontend/app/helpers/plugin_helper.rb
+++ frontend/app/helpers/plugin_helper.rb
@@ -5,7 +5,7 @@ module PluginHelper
     jsonmodel_type = record['jsonmodel_type']
     Plugins.plugins_for(jsonmodel_type).each do |plugin|
       name = Plugins.parent_for(plugin, jsonmodel_type)['name']
-      if not controller.action_name === "show" or record.send(name).length > 0
+      if not controller.action_name === "show" or Array(record.send(name)).length > 0
         result << '<li>'
         result << "<a href='##{jsonmodel_type}_#{name}_'>"
         result << I18n.t("plugins.#{plugin}._plural")
@@ -21,7 +21,7 @@ module PluginHelper
     jsonmodel_type = record['jsonmodel_type']
     Plugins.plugins_for(jsonmodel_type).each do |plugin|
       name = Plugins.parent_for(plugin, jsonmodel_type)['name']
-      if record.send(name).length > 0
+      if Array(record.send(name)).length > 0
         result << render_aspace_partial(:partial => "#{name}/show",
                          :locals => { name.intern => record.send(name),
                            :context => context, :section_id => "#{jsonmodel_type}_#{name}_" })
