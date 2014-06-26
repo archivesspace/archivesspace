3c7dea4cd0c5e6b8602b6ab555ca31e23ec3eb2b
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2014-06-26 11:19:02 +0200

    fixing some of the wrap-n-tag hints for notes

--- frontend/app/assets/javascripts/mixed_content.config.js
+++ frontend/app/assets/javascripts/mixed_content.config.js
@@ -3,94 +3,94 @@ $(function() {
       "blockquote": {
         "tag": "blockquote",
         "attributes": [],
-        "exclude": ["abstract"]
+        "exclude": ["abstract", "dimensions", "legalstatus"]
       },
       "date": {
         "tag": "date",
         "attributes": ["type", "normal", "calendar", "era"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent", "separatedmaterial"]
       },
       "function": {
         "tag": "function",
         "attributes": ["rules", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent", "separatedmaterial" ]
       },
       "occupation": {
         "tag": "occupation",
         "attributes": ["type", "normal", "calendar", "era"], 
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
       },
       "subject": {
         "tag": "subject",
         "attributes": ["type", "normal", "calendar", "era"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
       },
       "emph": {
         "tag": "emph",
         "attributes": ["render"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
       },
       "corpname": {
         "tag": "corpname",
         "attributes": ["rules", "role", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent", "separatedmaterial" ]
       },
       "p": {
         "tag": "p",
         "attributes": [],
         "elements": ["emph"],
-        "exclude": ["abstract"]
+        "exclude": ["abstract", "dimensions", "legalstatus"]
       },
       "persname": {
         "tag": "persname",
         "attributes": ["rules", "role", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
 
       },
       "famname": {
         "tag": "famname",
         "attributes": ["rules", "role", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
 
       },
       "name": {
         "tag": "name",
         "attributes": ["rules", "role", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
 
       },
       "geogname": {
         "tag": "geogname",
         "attributes": ["rules", "role", "source"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
 
       },
       "genreform": {
         "tag": "genreform",
         "attributes": ["rules", "role", "type"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent" , "separatedmaterial"]
 
       },
       "title": {
         "tag": "title",
-        "attributes": ["render", "accurals"],
-        "exclude": ["appraisal", "arrangement", "bioghist", "accessrestrict"]
+        "attributes": ["render", "accruals"],
+        "exclude": ["appraisal", "arrangement", "bioghist", "accessrestrict", "userestrict", "custodhist", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "phystech", "prefercite", "processinfo", "scopecontent" ]
       },
       "ref": {
         "tag": "ref",
         "attributes": ["target", "show", "title", "actuate", "href"],
-        "exclude": [ "accurals", "appraisal", "arrangement", "bioghist", "accessrestrict"]
+        "exclude": [ "accruals", "appraisal", "arrangement", "bioghist", "accessrestrict", "userestrict", "custodhist", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "phystech", "prefercite", "processinfo", "scopecontent" ]
       },
       "extref": {
         "tag": "extref",
         "attributes": ["show", "title", "actuate", "href"],
-        "exclude": ["abstract", "accurals", "appraisal", "arrangement", "bioghist", "accessrestrict"]
+        "exclude": ["abstract", "accruals", "appraisal", "arrangement", "bioghist", "accessrestrict", "userestrict", "custodhist", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "phystech", "prefercite", "processinfo", "scopecontent" , "separatedmaterial"]
 
       },
       "lb": {
         "tag": "lb",
         "attributes": [],
-        "exclude": [ "accurals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict"]
+        "exclude": [ "accruals", "appraisal", "arrangement", "bibliography", "bioghist", "accessrestrict", "userestrict", "custodhist", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent", "separatedmaterial" ]
       }
     };
 });
