e2d0d5ddea8cd6465eec511fd431054a9e86f9e7
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2014-10-20 10:29:27 +0200

    adding Jasper test reports and unregistering the test SubjectList report

--- backend/app/model/reports/subject_list_report.rb
+++ backend/app/model/reports/subject_list_report.rb
@@ -2,10 +2,15 @@ require_relative 'jdbc_report'
 
 class SubjectListReport < JDBCReport 
 
-  register_report({
-                    :uri_suffix => "subject_list",
-                    :description => "A list of subject",
-                  })
+
+# This is a test report to show how to construct Jasper reports
+# Uncomment the lines below to use this report in the UI
+
+  
+#  register_report({
+#                    :uri_suffix => "subject_list",
+#                    :description => "A list of subject",
+#                  })
 
 end
 
