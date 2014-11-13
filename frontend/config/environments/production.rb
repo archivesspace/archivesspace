5f3896502666e7c844001b58340959244d21193e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2014-11-13 14:35:59 +0100

    removing mistake value added to frontend production config

--- frontend/config/environments/production.rb
+++ frontend/config/environments/production.rb
@@ -21,7 +21,7 @@ ArchivesSpace::Application.configure do
 
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false 
 
   # Generate digests for assets URLs
   config.assets.digest = true
