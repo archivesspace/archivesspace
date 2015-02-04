1d5760a81a3478feaf61dbaccdb9b733a42a4a91
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-02-04 02:22:36 +0100

    fixing endpoint docs for YARD

--- backend/scripts/endpoint_doc.rb
+++ backend/scripts/endpoint_doc.rb
@@ -12,6 +12,15 @@ RESTHelpers::Endpoint.all.keep_if { |e| e[:uri] =~ /#{fltr}/ }.each do |e|
   puts "\n#{e[:method]} '#{e[:uri]}':"
   puts "  Description: #{e[:description]}"
   puts "  Parameters: "
+  
+  if e[:paginated]
+    puts "This endpoint is paginated. :page, :id_set, or :all_ids is required"   
+    puts "    Integer page -- The page set to be returned"
+    puts "    Integer page_size -- The size of the set to be returned ( Optional. default set in AppConfig )"
+    puts "    Comma seperated list id_set -- A list of ids to request resolved objects ( Must be smaller than default page_size )"
+    puts "    Boolean all_ids -- Return a list of all object ids"
+  end 
+
 
   e[:params].each do |param|
     opts = (param[3] or {})
