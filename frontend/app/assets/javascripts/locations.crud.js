fb6c274e1cb2c750f4b4244285e1f87d1980030d
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-03-04 09:13:03 +0100

    AR-1067 #finish Adding default day of today to location start and auto popular end with today if transferred to previous

--- frontend/app/assets/javascripts/locations.crud.js
+++ frontend/app/assets/javascripts/locations.crud.js
@@ -1,1 +1,14 @@
 //= require form
+$(function() {
+  $(document).bind("subrecordcreated.aspace", function(event, object_name, subform) {
+    if ( object_name === 'container_location' ){
+      $("[id$=__status_]", subform ).bind("change", function() {
+        $this = $(this);
+        $endDate = $("[id$=__end_date_]", subform );
+        if ( $this.val() == 'previous' && $endDate.val().length == 0 ) {
+          $endDate.val( $endDate.data('date') );
+        }
+      });
+    }
+  });
+});
--- selenium/spec/selenium_spec.rb
+++ selenium/spec/selenium_spec.rb
@@ -2072,6 +2072,105 @@ describe "ArchivesSpace user interface" do
   end
 
 
+  describe "Resource intances and containers" do
+
+    before(:all) do
+     if !$test_repo
+       ($test_repo, $test_repo_uri) = create_test_repo("repo_#{Time.now.to_i}_#{$$}", "description")
+     end
+      
+      login("admin", "admin")
+    end
+
+
+    after(:all) do
+      logout
+    end
+
+    it "can attach instances to resources" do
+      $driver.find_element(:link, "Create").click
+      $driver.find_element(:link, "Resource").click
+
+      $driver.clear_and_send_keys([:id, "resource_title_"], "a resource with instances")
+      $driver.complete_4part_id("resource_id_%d_")
+      combo = $driver.find_element(:xpath => '//div[@class="combobox-container"][following-sibling::select/@id="resource_language_"]//input[@type="text"]');
+      combo.clear
+      combo.click
+      combo.send_keys("eng")
+      combo.send_keys(:tab)
+      $driver.find_element(:id, "resource_level_").select_option("collection")
+      $driver.clear_and_send_keys([:id, "resource_extents__0__number_"], "10")
+      $driver.find_element(:id => "resource_extents__0__extent_type_").select_option("files")
+      
+      $driver.find_element(:id => "resource_dates__0__date_type_").select_option("single")
+      $driver.clear_and_send_keys([:id, "resource_dates__0__begin_"], "1978")
+        
+      $driver.find_element(:css => '#resource_instances_ .subrecord-form-heading .btn:not(.show-all)').click
+      $driver.find_element(:css => '#resource_instances__0__instance_type_').select_option('text')
+      $driver.clear_and_send_keys([:id, "resource_instances__0__container__barcode_1_"], "123456")
+
+      $driver.find_element(:css => "form .record-pane button[type='submit']").click
+      $driver.find_element_with_text('//div[contains(@class, "alert-success")]', /Resource a resource with instances created/)
+
+    end
+
+    it "can add a location to the instance" do
+      $driver.find_element(:css => '#resource_instances_ .btn.collapse-subrecord-toggle').click
+      $driver.wait_for_ajax
+      $driver.find_element(:css => '#resource_instances__0__container__container_locations_ .subrecord-form-heading .btn:not(.show-all)').click
+      $driver.wait_for_ajax
+      assert(5) { 
+        $driver.find_element(:css => '#resource_instances__0__container__container_locations__0__start_date_').attribute('value').should eq(Time.now.strftime("%Y-%m-%d"))
+      }
+      
+      $driver.find_element(:css => '#resource_instances__0__container__container_locations_ .dropdown-toggle').click
+      $driver.wait_for_ajax
+      $driver.find_element(:css, "a.linker-create-btn").click
+      
+      
+      $driver.clear_and_send_keys([:id, "location_building_"], "1129 W. 81st St")
+      $driver.clear_and_send_keys([:id, "location_floor_"], "55")
+      $driver.clear_and_send_keys([:id, "location_room_"], "66 MOO")
+      $driver.clear_and_send_keys([:id, "location_coordinate_1_label_"], "Box XYZ")
+      $driver.clear_and_send_keys([:id, "location_coordinate_1_indicator_"], "XYZ0001")
+      $driver.click_and_wait_until_gone(:css => "#createAndLinkButton")
+     
+      $driver.find_element(:css => "form .record-pane button[type='submit']").click
+      $driver.find_element_with_text('//div[contains(@class, "alert-success")]', /Resource a resource with instances updated/)
+    
+    end
+
+    it "has the start_date and end_date for locations properly set" do
+      $driver.find_element(:css => '#resource_instances_ .btn.collapse-subrecord-toggle').click
+      assert(5) { 
+        $driver.find_element(:css => '#resource_instances__0__container__container_locations__0__start_date_').attribute('value').should eq(Time.now.strftime("%Y-%m-%d"))
+      }
+      
+      assert(5) { 
+        $driver.find_element(:css => '#resource_instances__0__container__container_locations__0__end_date_').attribute('value').should eq("")
+      }
+            
+      $driver.find_element(:id, "resource_instances__0__container__container_locations__0__status_").select_option_with_text('Previous')
+      
+      assert(5) { 
+        $driver.find_element(:css => '#resource_instances__0__container__container_locations__0__end_date_').attribute('value').should eq(Time.now.strftime("%Y-%m-%d"))
+      }
+     
+      # you can transfer to a temporary location
+      $driver.find_element(:css => "form .record-pane button[type='submit']").click
+      $driver.find_element_with_text('//div[contains(@class, "alert-danger")]', /Status - Cannot transfer to previous if the location is temporary/) 
+      
+      $driver.find_element(:id, "resource_instances__0__container__container_locations__0__status_").select_option_with_text('Current')
+      
+      $driver.find_element(:css => "form .record-pane button[type='submit']").click
+      $driver.find_element_with_text('//div[contains(@class, "alert-success")]', /Resource a resource with instances updated/)
+
+
+    end
+
+  end
+
+
   describe "Notes" do
 
     before(:all) do
