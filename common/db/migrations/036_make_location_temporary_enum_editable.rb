822659029c5796daa2bbbcd5bf73f5c009af8c1e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-03-20 10:07:42 +0100

    keep migrations from spitting out this annoying '1'

--- common/db/migrations/036_make_location_temporary_enum_editable.rb
+++ common/db/migrations/036_make_location_temporary_enum_editable.rb
@@ -4,8 +4,7 @@ Sequel.migration do
 
   up do
     $stderr.puts "Making location_temporary list editble"
-    $stderr.puts self[:enumeration].filter(:name => 'location_temporary').update( :editable => 1 )
-
+    self[:enumeration].filter(:name => 'location_temporary').update( :editable => 1 )
   end
 
 end
