30930cdf88248b729982022485074306716b15a6
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-04-09 21:30:45 +0200

    enable full stack traces on error in frontend

--- frontend/config/environments/production.rb
+++ frontend/config/environments/production.rb
@@ -8,7 +8,7 @@ ArchivesSpace::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true 
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
