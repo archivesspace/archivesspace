60431a7db0c69d0f8195ba15b16200e98f559538
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-05-08 13:22:09 +0200

    AR-843 #finish adding some bits to escape characters solr doesn't like in Enumeration search

--- frontend/app/helpers/enumeration_helper.rb
+++ frontend/app/helpers/enumeration_helper.rb
@@ -1,6 +1,8 @@
 module EnumerationHelper
 
   def enumeration_simple_filter_params( relationships, value ) 
+    pattern = %r{(\+|\-|\&\&|\|\||\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\*|\?|\:|\\)}
+    value.gsub!(pattern) { |match| '\\' + match } 
     relationships.map { |rel| "#{rel}_enum_s:#{value}"  }.join(" OR ") 
   end
 
