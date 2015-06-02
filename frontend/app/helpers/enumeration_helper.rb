2ff1ee0dc62fec1038b4f76c5b6e89b5958e7b9f
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-06-02 11:18:29 +0200

    Another fix for enumeration searches

--- frontend/app/helpers/enumeration_helper.rb
+++ frontend/app/helpers/enumeration_helper.rb
@@ -1,7 +1,7 @@
 module EnumerationHelper
 
   def enumeration_simple_filter_params( relationships, value ) 
-    pattern = %r{(\+|\-|\&\&|\|\||\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\*|\?|\:|\\)}
+    pattern = %r{(\+|\-|\&\&|\|\||\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\*|\?|\ |\:|\\)}
     value.gsub!(pattern) { |match| '\\' + match } 
     relationships.map { |rel| "#{rel}_enum_s:#{value}"  }.join(" OR ") 
   end
