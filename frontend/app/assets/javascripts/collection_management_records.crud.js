1812f1a397db060e39615847f7ba9a774811ea90
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-06-25 23:28:51 +0200

    AR-827 #finish changing another CM label and turning estimate into float

--- frontend/app/assets/javascripts/collection_management_records.crud.js
+++ frontend/app/assets/javascripts/collection_management_records.crud.js
@@ -4,11 +4,11 @@
 
 var calculate_total_processing_hours = function(form) { 
     var $form = $(form);
-    var phe = parseInt($("#resource_collection_management__processing_hours_per_foot_estimate_", $form).val(), 10);
-    var pte = parseInt($("#resource_collection_management__processing_total_extent_", $form).val(), 10);
+    var phe = parseFloat($("#resource_collection_management__processing_hours_per_foot_estimate_", $form).val(), 10);
+    var pte = parseFloat($("#resource_collection_management__processing_total_extent_", $form).val(), 10);
 
     if ( $.isNumeric(phe) && $.isNumeric(pte) ) {
-        var tph = phe * pte;
+        var tph = (phe * pte).toFixed(2);
         $("#resource_collection_management__processing_hours_total_", $form).val(tph);
     }
 }
