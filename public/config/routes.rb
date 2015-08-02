4c3dc79e8e47ea35611621bdc5a5f6b8a2ab7371
Brian Hoffman <brianjhoffman@gmail.com>
2015-08-02 10:55:30 -0400

    rework config options for prefixed installs

--- public/config/routes.rb
+++ public/config/routes.rb
@@ -1,27 +1,29 @@
 ArchivesSpacePublic::Application.routes.draw do
 
-  scope AppConfig[:public_prefix] do
+  [AppConfig[:public_proxy_prefix], AppConfig[:public_prefix]].uniq.each do |prefix|
 
-    match 'search' => 'search#search', :via => [:get]
-    match 'advanced_search' => 'search#advanced_search', :via => [:get]
-    match 'tree' => 'records#tree', :via => [:get]
-    match 'repositories/:repo_id/resources/:id' => 'records#resource', :via => [:get]
-    match 'repositories/:repo_id/digital_objects/:id' => 'records#digital_object', :via => [:get]
-    match 'repositories/:repo_id/archival_objects/:id' => 'records#archival_object', :via => [:get]
-    match 'repositories/:repo_id/digital_object_components/:id' => 'records#digital_object_component', :via => [:get]
-    match 'repositories/:repo_id' => 'search#repository', :via => [:get]
-    match 'repositories/:repo_id/classifications/:id' => 'records#classification', :via => [:get]
-    match 'repositories/:repo_id/accessions/:id' => 'records#accession', :via => [:get]
-    match 'agents/:id' => 'records#agent', :via => [:get]
+    scope prefix do
 
-    match 'repositories' => 'search#repository', :via => [:get]
-    match 'subjects/:id' => 'search#subject', :via => [:get]
-    root :to => "site#index"
+      match 'search' => 'search#search', :via => [:get]
+      match 'advanced_search' => 'search#advanced_search', :via => [:get]
+      match 'tree' => 'records#tree', :via => [:get]
+      match 'repositories/:repo_id/resources/:id' => 'records#resource', :via => [:get]
+      match 'repositories/:repo_id/digital_objects/:id' => 'records#digital_object', :via => [:get]
+      match 'repositories/:repo_id/archival_objects/:id' => 'records#archival_object', :via => [:get]
+      match 'repositories/:repo_id/digital_object_components/:id' => 'records#digital_object_component', :via => [:get]
+      match 'repositories/:repo_id' => 'search#repository', :via => [:get]
+      match 'repositories/:repo_id/classifications/:id' => 'records#classification', :via => [:get]
+      match 'repositories/:repo_id/accessions/:id' => 'records#accession', :via => [:get]
+      match 'agents/:id' => 'records#agent', :via => [:get]
 
-    get 'agents/people/:id', to: redirect('/agents/%{id}?agent_type=agent_person')
-    get 'agents/software/:id', to: redirect('/agents/%{id}?agent_type=agent_software')
-    get 'agents/families/:id', to: redirect('/agents/%{id}?agent_type=agent_family')
-    get 'agents/corporate_entities/:id', to: redirect('/agents/%{id}?agent_type=agent_corporate_entity')
+      match 'repositories' => 'search#repository', :via => [:get]
+      match 'subjects/:id' => 'search#subject', :via => [:get]
+      root :to => "site#index"
+
+      get 'agents/people/:id', to: redirect('/agents/%{id}?agent_type=agent_person')
+      get 'agents/software/:id', to: redirect('/agents/%{id}?agent_type=agent_software')
+      get 'agents/families/:id', to: redirect('/agents/%{id}?agent_type=agent_family')
+      get 'agents/corporate_entities/:id', to: redirect('/agents/%{id}?agent_type=agent_corporate_entity')
+    end
   end
-
 end
