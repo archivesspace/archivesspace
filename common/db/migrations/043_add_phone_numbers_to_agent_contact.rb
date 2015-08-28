757d11d3d9915981cebb8cd9fb4ea671f41e6612
Brian Hoffman <brianjhoffman@gmail.com>
2015-08-28 11:14:02 -0400

    move telephone_ext to telephone if the latter is nil

--- common/db/migrations/043_add_phone_numbers_to_agent_contact.rb
+++ common/db/migrations/043_add_phone_numbers_to_agent_contact.rb
@@ -4,33 +4,39 @@ Sequel.migration do
 
   up do
 
-      create_table(:telephone) do
-        primary_key :id
-        Integer :agent_contact_id 
-        TextField :number, :null => false
-        TextField :ext, :null => true
-        apply_mtime_columns
+    create_table(:telephone) do
+      primary_key :id
+      Integer :agent_contact_id
+      TextField :number, :null => false
+      TextField :ext, :null => true
+      apply_mtime_columns
+    end
+
+    alter_table(:telephone) do
+      add_foreign_key([:agent_contact_id], :agent_contact, :key => :id)
+    end
+
+    self[:agent_contact].filter( Sequel.~(:telephone => nil)).or( Sequel.~( :telephone_ext => nil )).each do |row|
+      number = row[:telephone]
+      ext = row[:telephone_ext]
+
+      if number.nil? && ext
+        number = ext
+        ext = nil
       end
 
-      alter_table(:telephone) do
-        add_foreign_key([:agent_contact_id], :agent_contact, :key => :id)
-      end
-    
-      self[:agent_contact].filter( Sequel.~(:telephone => nil)).or( Sequel.~( :telephone_ext => nil )).each do |row|
-        self[:telephone].insert( :agent_contact_id => row[:id], :number => row[:telephone], :ext => row[:telephone_ext],
-                                  :last_modified_by => row[:last_modified_by],
-                                  :create_time => row[:create_time],
-                                  :system_mtime => row[:system_mtime],
-                                  :user_mtime => row[:user_mtime]
+      self[:telephone].insert( :agent_contact_id => row[:id], :number => number, :ext => ext,
+                               :last_modified_by => row[:last_modified_by],
+                               :create_time => row[:create_time],
+                               :system_mtime => row[:system_mtime],
+                               :user_mtime => row[:user_mtime]
                                )
-      end
-      
-      alter_table(:agent_contact) do
-        drop_column(:telephone)
-        drop_column(:telephone_ext)
-      end
+    end
 
-
+    alter_table(:agent_contact) do
+      drop_column(:telephone)
+      drop_column(:telephone_ext)
+    end
 
   end
 
