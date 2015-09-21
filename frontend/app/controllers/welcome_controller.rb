4089a756cffe4592720ce3cb226310b5718e1b40
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-09-21 10:01:21 +0200

    AR-1327 #finish adding a warning on the staff ui that derby is being used

--- frontend/app/controllers/welcome_controller.rb
+++ frontend/app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   set_access_control  :public => [:index]
 
   def index
+   
+    info = JSONModel::HTTP::get_json('/')
+    view_context.database_warning( info ) 
+    
     if session[:user] && @repositories.length === 0
       if user_can?('create_repository')
         flash.now[:info] = I18n.t("repository._frontend.messages.create_first_repository")
