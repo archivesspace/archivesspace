7d943b881ada3ba40433e2d9b941ff03637798fd
Brian Hoffman <brianjhoffman@gmail.com>
2015-09-29 13:04:07 -0400

    exclude bundler install logs from dist

--- backend/config/warble.rb
+++ backend/config/warble.rb
@@ -20,7 +20,7 @@ Warbler::Config.new do |config|
 
   # Additional files/directories to exclude
   # config.excludes = FileList["lib/tasks/*"]
-  config.excludes = FileList["app/exporters/examples/**/*"]
+  config.excludes = FileList["app/exporters/examples/**/*", ".bundle/install.log"]
 
   # Additional Java .jar files to include.  Note that if .jar files are placed
   # in lib (and not otherwise excluded) then they need not be mentioned here.
