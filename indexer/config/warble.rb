426a4d52d3e8748545b83c36ee3c4a02ab9f894e
Brian Hoffman <brianjhoffman@gmail.com>
2015-09-29 09:57:26 -0400

    sync gems and add .bundler files to dist

--- indexer/config/warble.rb
+++ indexer/config/warble.rb
@@ -11,10 +11,10 @@ Warbler::Config.new do |config|
   config.features = []
 
   # Application directories to be included in the webapp.
-  config.dirs = %w(app)
+  config.dirs = %w(app .bundle)
 
   # Additional files/directories to include, above those in config.dirs
-  config.includes = FileList["Gemfile"]
+  config.includes = FileList["Gemfile", "Gemfile.lock"]
 
   # Additional files/directories to exclude
   # config.excludes = FileList["lib/tasks/*"]
