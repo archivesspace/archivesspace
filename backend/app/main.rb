785ad8a5b7f428f0cfe277bbcfd63b4344c78586
Brian Hoffman <brianjhoffman@gmail.com>
2015-10-01 13:40:29 -0400

    bump rack to latest and increase param thresholds

--- backend/app/main.rb
+++ backend/app/main.rb
@@ -1,6 +1,9 @@
 require 'bundler/setup'
 Bundler.require
 
+Rack::Utils.key_space_limit = 655360 # x10 Rack default
+Rack::Utils.param_depth_limit = 200 # x2 Rack default
+
 if ENV['COVERAGE_REPORTS'] && ENV["ASPACE_INTEGRATION"] == "true"
   require 'aspace_coverage'
   ASpaceCoverage.start('backend_integration')
