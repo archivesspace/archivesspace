56c308a09abd41c5dd10a5c45e711d388a9cc039
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2015-10-10 23:29:21 +0200

    AR-1341 #finish make the record forms have input values that do not depend on the linker to persist data

--- frontend/app/assets/javascripts/linker.js
+++ frontend/app/assets/javascripts/linker.js
@@ -11,6 +11,11 @@ $(function() {
       }
 
       $this.addClass("initialised");
+      
+      // this is a bit hacky, but we need to have some input fields present in
+      // the form so we don't have to rely on the linker to make sure data
+      // presists. we can remove those after the linker does its thing.
+      $(".prelinker", $linkerWrapper).remove();
 
       var config = {
         url: decodeURIComponent($this.data("url")),
@@ -379,7 +384,7 @@ $(function() {
 $(document).ready(function() {
   $(document).bind("loadedrecordsubforms.aspace", function(event, $container) {
     $(".linker-wrapper:visible > .linker:not(.initialised)", $container).linker();
-     // we can go ahead and init dropdowns ( such as those in the toolbars ) 
+    // we can go ahead and init dropdowns ( such as those in the toolbars ) 
     $("#archives_tree_toolbar .linker:not(.initialised)").linker();
   });
 
