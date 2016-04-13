50b9d4f9c69189918ec22f694891e24ef6878ce3
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2016-04-13 12:50:49 +0200

    AR-1457 #finish adding responseHeader size configuration as well

--- launcher/launcher.rb
+++ launcher/launcher.rb
@@ -38,8 +38,11 @@ def start_server(port, *webapps)
   connector = org.eclipse.jetty.server.nio.SelectChannelConnector.new
   connector.port = port
   
-  buffer_size_bytes =  AppConfig[:jetty_request_buffer_size_bytes] || 64 * 1024 
-  connector.setRequestHeaderSize(buffer_size_bytes)
+  req_buffer_size_bytes =  AppConfig[:jetty_request_buffer_size_bytes] || 64 * 1024 
+  res_buffer_size_bytes =  AppConfig[:jetty_response_buffer_size_bytes] || 64 * 1024 
+  
+  connector.setRequestHeaderSize(req_buffer_size_bytes)
+  connector.setResponseHeaderSize(res_buffer_size_bytes)
 
   contexts = webapps.map do |webapp|
     if webapp[:war]
