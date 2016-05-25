200ef7ccfd858493513c8df9495f933bca555d50
jambun <jambun@gmail.com>
2016-05-25 11:18:36 +1000

    fixed report tests by passing newly required job argument to new

--- backend/spec/model_repository_report_spec.rb
+++ backend/spec/model_repository_report_spec.rb
@@ -5,7 +5,10 @@ describe 'RepositoryReport model' do
     
     repo = Repository.create_from_json(JSONModel(:repository).from_hash(:repo_code => "TESTREPO",
                                                                         :name => "My new test repository"))
-    report = RepositoryReport.new({:repo_id => repo.id})
+    report = RepositoryReport.new({:repo_id => repo.id},
+                                  Job.create_from_json(build(:json_job),
+                                                       :repo_id => repo.id,
+                                                       :user => create_nobody_user))
     report.to_enum.first[:repo_code].should eq(repo.repo_code)
     report.to_enum.first[:name].should eq(repo.name)
      
