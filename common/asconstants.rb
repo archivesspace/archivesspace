b16fcf216fe0e5fcaa2554038ff10722e2ab65fe
Mark Cooper <markchristophercooper@gmail.com>
2016-06-22 12:02:25 -0700

    Updating to v1.5.0-RC3

--- common/asconstants.rb
+++ common/asconstants.rb
@@ -27,7 +27,7 @@ module ASConstants
       else # some servlet containers have a hard time finding the resource...
         # fallback_version variable gets changed in dist ant task . The a is
         # just a cue that we're using this..
-        fallback_version = "v1.5.0-RC2.a"
+        fallback_version = "v1.5.0-RC3.a"
         @VERSION = fallback_version 
       end
     rescue
