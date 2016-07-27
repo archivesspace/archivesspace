5afdfd1ba47b443813c926e01a2cb0de57fbaa3d
Bobbi Fox <bobbi_fox@harvard.edu>
2016-07-27 15:14:18 -0400

    Add INITIAL welcome page

--- public-new/app/archivesspace-public/config/routes.rb
+++ public-new/app/archivesspace-public/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'index#index'
-
+  get '/welcome', to: 'welcome#show'
+  get "repositories/:id" => 'repositories#show'
+  get '/repositories', to: 'repositories#index'
   get '/search', to: 'search#search'
 end
