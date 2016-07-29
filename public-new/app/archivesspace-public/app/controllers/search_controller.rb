064416c0478255524e0ff7dec6183f7434941a47
Bobbi Fox <bobbi_fox@harvard.edu>
2016-07-29 16:28:24 -0400

    add pagination to example search

--- public-new/app/archivesspace-public/app/controllers/search_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/search_controller.rb
@@ -1,17 +1,14 @@
 class SearchController < ApplicationController
 
-  PAGE_NUMBERS_TO_SHOW = 10
-
   def search
     @query = params.require(:q)
     page = Integer(params.fetch(:page, "1"))
 
     @results = archivesspace.search(@query, page)
-
-    lower_page = [(page - PAGE_NUMBERS_TO_SHOW / 2), 1].max
-    upper_page = [(lower_page + PAGE_NUMBERS_TO_SHOW), @results['last_page'] + 1].min
-
-    @pages = Range.new(lower_page, upper_page, true)
+    @pager = Pager.new("/search?q=#{@query}",@results['this_page'],@results['last_page']) 
+#    Rails.logger.debug(@pager)
+#    Rails.logger.debug("\n\n#{@results}\n")
+    @page_title = "#{I18n.t('search_results.head_prefix')} #{@results['total_hits']} #{I18n.t('search_results.head_suffix')}"
   end
 
 end
