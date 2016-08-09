c7d4b07c37d3e0da00058474ae3a5f12ff536c36
Bobbi Fox <bobbi_fox@harvard.edu>
2016-08-09 17:40:46 -0400

    change 'front page'

--- public-new/app/archivesspace-public/config/routes.rb
+++ public-new/app/archivesspace-public/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'index#index'
+  get '/', to: 'welcome#show' #'index#index'
   get '/welcome', to: 'welcome#show'
   get "repositories/:id" => 'repositories#show'
   get '/repositories', to: 'repositories#index'
