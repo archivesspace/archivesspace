f36f4b363db3a2a2ec64da4dbd2c009a23745fd5
Bobbi Fox <bobbi_fox@harvard.edu>
2016-08-12 15:46:07 -0400

    improve results displays

---
+++ public-new/app/archivesspace-public/app/controllers/resources_controller.rb
@@ -0,0 +1,27 @@
+class ResourcesController <  ApplicationController
+  include HandleFaceting
+  include ProcessResults
+  skip_before_filter  :verify_authenticity_token
+  def index
+    @repo_name = params[:repo] || ""
+    @repo_id = "/repositories/#{params[:rid]}"
+    @criteria = {}
+    @criteria['sort'] = 'title asc'
+    @criteria['resolve[]']  = ['repository:id']
+    query = "repository:\"#{@repo_id}\" AND publish:true AND types:resource"
+    page_size =  params['page_size'].to_i if !params.blank?
+    page_size = AppConfig[:search_results_page_size] if page_size == 0
+    page = params['page'] || 1 if !params.blank?
+    @criteria[:page_size] = page_size
+    # might as well get the facets here
+    @results =  archivesspace.search(query, page, @criteria) || {}
+    @results = handle_results(@results)
+    @pager =  Pager.new("/repositories/#{params[:rid]}/resources?repo=#{@repo_name}", @results['this_page'],@results['last_page'])
+    @page_title = (@repo_name != '' ? "#{@repo_name}: " : '') +(@results['results'].length > 1 ? I18n.t('resource._plural') : I18n.t('resource._singular')) +  " " + I18n.t('listing')
+    Rails.logger.debug("Page title #{@page_title}")
+  end
+
+  def show
+
+  end
+end
