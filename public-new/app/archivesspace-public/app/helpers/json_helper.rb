0f2cb052825116fb4cd30bb01948c2e2a1e7c832
Bobbi Fox <bobbi_fox@harvard.edu>
2016-08-12 15:44:38 -0400

    improve results presentation

--- public-new/app/archivesspace-public/app/helpers/json_helper.rb
+++ public-new/app/archivesspace-public/app/helpers/json_helper.rb
@@ -3,19 +3,21 @@ module JsonHelper
   def get_note(json, type)
     note_txt = ''
     if !json['notes'].blank?
-      json['notes'].each do |note|
-        if note.has_key?('type') && note['type'] == type 
-          note_txt = "<span class='inline-label'>#{note['label']}:</span>" if note_txt.blank? && !note['label'].blank? 
-          if note['jsonmodel_type'] == 'note_multipart'
-            note['subnotes'].each do |sub|
-              if sub['publish']
-                note_txt = add_contents(sub['content'], note_txt)
+      if json['notes'].kind_of?(Array)
+        json['notes'].each do |note|
+          if note.has_key?('type') && note['type'] == type 
+            note_txt = "<span class='inline-label'>#{note['label']}:</span>" if note_txt.blank? && !note['label'].blank? 
+            if note['jsonmodel_type'] == 'note_multipart'
+              note['subnotes'].each do |sub|
+                if sub['publish']
+                  note_txt = add_contents(sub['content'], note_txt)
+                end
               end
-            end
-          else 
-            if note['publish']
-              note['content'].each do |txt|
-                note_txt = add_contents(txt, note_txt)
+            else 
+              if note['publish']
+                note['content'].each do |txt|
+                  note_txt = add_contents(txt, note_txt)
+                end
               end
             end
           end
