277bcb723cd8367dcf755b32eada1517efb03ea6
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2016-08-19 22:56:06 +0200

    catch the right exception for optimistic locking

--- backend/spec/model_db_spec.rb
+++ backend/spec/model_db_spec.rb
@@ -14,5 +14,33 @@ describe 'DB Model' do
 
     attempt.should eq(5)
   end
+  
+  it "Retries transactions on NoExistingObject/OptimisticLocking exception if told to retry on optimistic locking fail" do
+
+    attempt = 0
+    
+    expect {
+      supports_mvcc = true
+      DB.open( supports_mvcc,  :retry_delay => 0 ) do
+        attempt += 1
+        raise Sequel::Plugins::OptimisticLocking::Error.new("Couldn't create version of blah")
+      end
+    }.to raise_error(Sequel::NoExistingObject)
+
+    # the default it 10
+    attempt.should eq(1)
+    attempt = 0
+
+    expect {
+      supports_mvcc = true
+      DB.open( supports_mvcc, :retry_on_optimistic_locking_fail => true, :retry_delay => 0 ) do
+        attempt += 1
+        raise Sequel::Plugins::OptimisticLocking::Error.new("Couldn't create version of blah")
+      end
+    }.to raise_error(Sequel::NoExistingObject)
+
+    # the default it 10
+    attempt.should eq(10)
+  end
 
 end
