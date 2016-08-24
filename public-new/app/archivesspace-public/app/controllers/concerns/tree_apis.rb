39cddac67f9cbbf08adfe16cb2fe9e06975c900d
Bobbi Fox <bobbi_fox@harvard.edu>
2016-08-24 14:41:37 -0400

    add breadcrumb support

---
+++ public-new/app/archivesspace-public/app/controllers/concerns/tree_apis.rb
@@ -0,0 +1,26 @@
+module TreeApis
+  extend ActiveSupport::Concern
+  # fetch the tree to be manipulated by other methods in this controller
+  # TODO: try/catch for non 200 responses
+  def fetch_tree(node_uri)
+    tree = archivesspace.get_tree(node_uri)
+    if !tree.blank?
+      Pry::ColorPrinter.pp(tree)
+    end
+    tree
+  end
+  # create the contents for breadcrumbs
+  def get_path(tree)
+    path_to_root = {}
+    Rails.logger.debug("PATH_TO_ROUTE: #{tree['path_to_root']}")
+    if !tree['path_to_root'].blank?
+      path_to_root = tree['path_to_root'].map {|node|
+        {
+          'crumb' => process_mixed_content(node['title'] || ''),
+          'uri' => node['record_uri'] || ''
+        }
+      }
+    end
+    path_to_root
+  end
+end
