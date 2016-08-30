ea15cb4ac3787d77d5afc9f5d2f52c170b1555c3
Bobbi Fox <bobbi_fox@harvard.edu>
2016-08-30 15:53:45 -0400

    support searching within repo or resource

--- public-new/app/archivesspace-public/app/controllers/search_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/search_controller.rb
@@ -5,14 +5,25 @@ class SearchController < ApplicationController
     @criteria = {}
     @criteria['sort'] = 'title asc'
     @criteria['resolve[]']  = ['repository:id', 'resource:id@compact_resource']
-  
-   @query = params.require(:q)
+    @query = params.require(:q)
+    page_search = "/search?q=#{@query}" 
+
     @query = "#{@query} AND publish:true"
     page = Integer(params.fetch(:page, "1"))
+    res_id = params.fetch(:res_id, '')
+    repo_id = params.fetch(:repo_id, '')
+    if !res_id.blank?
+      @query = "resource:\"#{res_id}\" AND #{@query}"
+      page_search = "#{page_search}&res_id=#{res_id.gsub('/','%2f')}"
+    elsif !repo_id.blank?
+      @query =  "repository:\"#{repo_id}\" AND #{@query}""
+      page_search = "#{page_search}&repo_id=#{repo_id.gsub('/','%2f')}"
+    end
+    Rails.logger.debug("page search: #{page_search}")
 
     @results = archivesspace.search(@query, page, @criteria)
     @results = handle_results(@results)
-    @pager = Pager.new("/search?q=#{@query}",@results['this_page'],@results['last_page']) 
+    @pager = Pager.new(page_search,@results['this_page'],@results['last_page']) 
 #    Rails.logger.debug(@pager)
 #    Rails.logger.debug("\n\n#{@results}\n")
     @page_title = "#{I18n.t('search_results.head_prefix')} #{@results['total_hits']} #{I18n.t('search_results.head_suffix')}"
