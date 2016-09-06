134a1e20608684bc02f90555465c398be5a5a0e5
Bobbi Fox <bobbi_fox@harvard.edu>
2016-09-06 14:05:48 -0400

    Customize the branding image

--- public-new/app/archivesspace-public/app/lib/config_defaults.rb
+++ public-new/app/archivesspace-public/app/lib/config_defaults.rb
@@ -4,7 +4,8 @@ unless $CONFIG_DEFAULTS_LOADED
   AppConfig[:archivesspace_password] = 'admin'
 
   AppConfig[:search_results_page_size] = 10
-   AppConfig[:public_url] = '/'
+  AppConfig[:public_url] = '/'
+  AppConfig[:branding_img] = '/img/Aspace-logo.png'
 
   $CONFIG_DEFAULTS_LOADED = true
 end
