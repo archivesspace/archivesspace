3028c8ee55eea44f6903be85bbac980ae8fd739f
Bobbi Fox <bobbi_fox@harvard.edu>
2016-09-09 16:11:45 -0400

    I18n some page titles, strip html code from others

--- public-new/app/archivesspace-public/app/controllers/subjects_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/subjects_controller.rb
@@ -12,12 +12,12 @@ class SubjectsController <  ApplicationController
     @results =  handle_results(@results)
     if !@results['results'].blank? && @results['results'].length > 0
       @result = @results['results'][0]
-      Pry::ColorPrinter.pp(@result)
-      @page_title = "#{@result['json']['title']}" || "Agent #{uri}"
-      Rails.logger.debug("subject title: #{@page_title}")
+#      Pry::ColorPrinter.pp(@result)
+      @page_title = strip_mixed_content(@result['json']['title']) || "#{I18n.t('subject._singular')} #{uri}"
+#      Rails.logger.debug("subject title: #{@page_title}")
       @context = []
     else
-      @page_title = "NOT FOUND"
+      @page_title = I18n.t 'errors.error_404'
       @uri = uri
       @back_url = request.referer || ''
       render  'shared/not_found'
