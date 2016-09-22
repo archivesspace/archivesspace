2ded4d8bb7f787aedd06da4ef20e8531c2c1168b
Bobbi Fox <bobbi_fox@harvard.edu>
2016-09-22 15:43:21 -0400

    Add information, faceting, to agents

--- public-new/app/archivesspace-public/app/controllers/agents_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/agents_controller.rb
@@ -5,16 +5,31 @@ class AgentsController <  ApplicationController
 
   skip_before_filter  :verify_authenticity_token
 
+  DEFAULT_AG_TYPES = %w{repository resource archival_object digital_object}
+  DEFAULT_AG_FACET_TYPES = %w{primary_type subjects}
+  DEFAULT_AG_SEARCH_OPTS = {
+    'sort' => 'title_sort asc',
+    'facet.mincount' => 1
+  }
+
+
+
   def show
     uri = "/agents/#{params[:eid]}/#{params[:id]}"
     @criteria = {}
     @criteria['resolve[]']  = ['repository:id', 'resource:id@compact_resource']
-    @results =  archivesspace.search_records([uri],1,@criteria)
-    @results =  handle_results(@results)
-    if !@results['results'].blank? && @results['results'].length > 0
-      @result = @results['results'][0]
-      Pry::ColorPrinter.pp(@result)
-      @page_title = strip_mixed_content(@result['json']['title']) || "#{I18n.t('an_agent')}: #{uri}"
+    results =  archivesspace.search_records([uri],1,@criteria)
+    results =  handle_results(results)
+    if !results['results'].blank? && results['results'].length > 0
+      @result = results['results'][0]
+#      Pry::ColorPrinter.pp(@result)
+      @results = fetch_agent_results(@result['title'],uri, params)
+      if !@results.blank?
+        @pager =  Pager.new("#{uri}?q=#{params.fetch(:q,'*')}", @results['this_page'],@results['last_page'])
+      else
+        @pager = nil
+      end
+     @page_title = strip_mixed_content(@result['json']['title']) || "#{I18n.t('an_agent')}: #{uri}"
       Rails.logger.debug("Agent title: #{@page_title}")
       @context = []
     else
@@ -23,5 +38,21 @@ class AgentsController <  ApplicationController
       @back_url = request.referer || ''
       render  'shared/not_found'
     end
+  end
+
+  private
+  def fetch_agent_results(title, uri, params)
+    @results = []
+    set_up_search(DEFAULT_AG_TYPES, DEFAULT_AG_FACET_TYPES, DEFAULT_AG_SEARCH_OPTS, params)
+    q = params.fetch(:q,'*')
+    page = Integer(params.fetch(:page, "1"))
+    qry = "#{q} AND agents:\"#{title}\""
+    @results =  archivesspace.search(qry,page, @criteria)
+    if @results['total_hits'] > 0
+      process_search_results("#{uri}?q=#{q}")
+    else
+      @results = []
+    end
+    @results
   end
 end
