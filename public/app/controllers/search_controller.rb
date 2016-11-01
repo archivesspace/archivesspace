bfbe45343a02e1e7cdda040f5e474f86d5cf4e53
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2016-11-01 10:47:53 +0100

    Adds a line to sanitize filter_term param in the old old public ui
    fixes #536

--- public/app/controllers/search_controller.rb
+++ public/app/controllers/search_controller.rb
@@ -55,16 +55,17 @@ class SearchController < ApplicationController
   private
 
   def set_search_criteria
+    
     @criteria = params.select{|k,v|
       ["page", "q", "type", "sort",
        "filter_term", "root_record", "format"].include?(k) and not v.blank?
     }
-
+    
     @criteria["page"] ||= 1
     @criteria["sort"] = "title_sort asc" unless @criteria["sort"] or @criteria["q"] or params["advanced"].present?
 
     if @criteria["filter_term"]
-      @criteria["filter_term[]"] = Array(@criteria["filter_term"]).reject{|v| v.blank?}
+      @criteria["filter_term[]"] = Array(@criteria["filter_term"]).reject{|v| v.blank?}.map { |ft|  ActionController::Base.helpers.sanitize(ft) }
       @criteria.delete("filter_term")
     end
 
