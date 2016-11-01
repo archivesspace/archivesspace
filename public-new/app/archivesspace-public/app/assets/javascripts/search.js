5be55e15d3028db7d9d52b8cc62bd4d6ba1e5e04
Bobbi Fox <bobbi_fox@harvard.edu>
2016-11-01 17:51:35 -0400

    allow multi-line search specification (javascript)

---
+++ public-new/app/archivesspace-public/app/assets/javascripts/search.js
@@ -0,0 +1,64 @@
+/* search form javascript support */
+
+var $template, $as;
+var plusText = 'Add a search row';
+var minusText = 'Remove the row below';
+var fn_plusminus = function(e){
+    e.stopPropagation;
+    if ($(this).text() === '+') {
+	$(this).text('-');
+	$(this).attr('title', minusText);
+	var $row = new_row_from_template();
+	$(this).parents('.search_row').after($row);
+	$row.find(".bool select").focus();
+    }
+    else {
+	$(this).parents(".search_row").next(".search_row").remove();
+	$(this).text('+');
+	$(this).attr('title', plusText);
+    }
+    return false;
+}
+
+function initialize_search() {
+    $as = $("#advanced_search");
+    /* create the + button, attach click event */
+    new_button($($as.find("#search_row_0"))); 
+    /* then save the first_row, so we don't always have to find it */
+    var $first = $($as.find("#search_row_0"));
+    $template = $first.clone();
+    $template.find(".norepeat").each(function() { $(this).empty(); });
+    $template.find(".hidden").each(function() {$(this).removeClass("hidden"); });
+    $template.find("#op0").removeProp("disabled"); /* the disabled boolean operator */
+    $template.find("#op_").remove(); 
+    return true;
+}       
+
+function new_button($row) {
+    var $plus = $row.find(".plusminus");
+    $plus.html("<button title='" + plusText + "'>+</button>");
+    $plus.find("button").click(fn_plusminus);
+    return true;
+}
+
+function new_row_from_template() {
+    var num = $as.find(".search_row").size();
+    var $row = $template.clone();
+    replace_id_ref($row, 'label', 'for', num);
+    replace_id_ref($row, 'input', 'id', num);
+    replace_id_ref($row, 'select', 'id', num);
+    $row.attr("id", "search_row_" + num);
+    new_button($row);
+    return $row;
+}
+
+function add_search_line() {
+    $row = new_row_from_template();
+
+}
+
+function replace_id_ref($row, selector, type, num) {
+    $row.find(selector).each(function() {
+            $(this).attr(type, $(this).attr(type).replace('0', num));
+	});
+}
