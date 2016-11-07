f7d876d448c6b79d20a7a592ac58b5c11a5bd43b
Bobbi Fox <bobbi_fox@harvard.edu>
2016-11-07 14:30:32 -0500

    refactor repo info, prepare for citation

--- public-new/app/archivesspace-public/app/controllers/objects_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/objects_controller.rb
@@ -1,5 +1,7 @@
 class ObjectsController <  ApplicationController
   include TreeApis
+  include RepoInfo
+  helper_method :process_repo_info
 
 
   skip_before_filter  :verify_authenticity_token
@@ -12,16 +14,37 @@ class ObjectsController <  ApplicationController
     @results =  handle_results(@results)
     if !@results['results'].blank? && @results['results'].length > 0
       @result = @results['results'][0]
-      Pry::ColorPrinter.pp(@result)
+      @repo_info =  process_repo_info(@result)
       @page_title = strip_mixed_content(@result['json']['display_string'] || @result['json']['title'])
       @tree = fetch_tree(uri)
       @context = get_path(@tree)
       # TODO: This is a monkey patch for digital objects
       if @context.blank?
         @context = []
+        unless @result['_resolved_resource'].blank? || @result['_resolved_resource']['json'].blank?
+          @context.unshift({:uri => @result['_resolved_resource']['json']['uri'],
+                             :crumb => strip_mixed_content(@result['_resolved_resource']['json']['title'])})
+        end
       end
       @context.unshift({:uri => @result['_resolved_repository']['json']['uri'], :crumb =>  @result['_resolved_repository']['json']['name']})
       @context.push({:uri => '', :crumb => strip_mixed_content(@result['json']['display_string'] || @result['json']['title']) })
+      @prefer_cite = ''
+      cite = get_note(@result['json'], 'prefercite')
+      unless cite.blank?
+        @prefer_cite = strip_mixed_content(cite['note_text'])
+      else
+        @prefer_cite = "#{@result['json']['title']}." 
+        unless @result['_resolved_resource'].blank? || @result['_resolved_resource']['json'].blank?
+          @prefer_cite += " #{strip_mixed_content(@result['_resolved_resource']['json']['title'])}."
+        end
+         unless @repo_info['top']['name'].blank?
+           @prefer_cite += " #{ @repo_info['top']['name']}."
+        end
+      end
+      @prefer_cite += "   #{archivesspace.get_full_url(uri)}  #{I18n.t('accessed')} " +  Time.now.strftime("%B %d, %Y") + "."
+      Rails.logger.debug("cite: #{@prefer_cite}")
+
+
     else
       @page_title = I18n.t 'errors.error_404'
       @uri = uri
