57dde5687eba62b6725e2a87b4eac9d55dc38ad8
Bobbi Fox <bobbi_fox@harvard.edu>
2016-11-08 13:11:45 -0500

    Refactor common methods

--- public-new/app/archivesspace-public/app/controllers/concerns/result_info.rb
+++ public-new/app/archivesspace-public/app/controllers/concerns/result_info.rb
@@ -60,70 +60,7 @@ module ResultInfo
     end
   end
 
-# look for a representative instance
 
-  def get_rep_image(instances)
-    rep = {}
-    if instances && instances.kind_of?(Array)
-      instances.each do |instance|
-        unless instance['digital_object'].blank? || instance['digital_object']['_resolved'].blank? 
-          it =  instance['digital_object']['_resolved']
-          unless !it['publish'] || it['file_versions'].blank?
-            it['file_versions'].each do |ver|
-              if ver['is_representative'] && ver['xlink_show_attribute'] == 'embed' && ver['publish']
-                rep['title'] = strip_mixed_content(it['title'])
-                rep['uri'] = ver['file_uri']
-              end
-            end
-          end
-        end
-      end
-    end
-    rep
-  end
 
-# digital object processing 
-  def process_digital(json)
-    dig = {}
-    unless json['digital_object_id'].blank? ||  !json['digital_object_id'].start_with?('http')
-      dig['out'] = json['digital_object_id']
-    end
-    dig = process_file_versions(json, dig)
-  end
 
-# representative digital object for an archival object
-  def process_digital_instance(instances)
-    dig = {}
-    if instances && instances.kind_of?(Array)
-      instances.each do |instance|
-        unless instance['digital_object'].blank? || instance['digital_object']['_resolved'].blank?
-          it =  instance['digital_object']['_resolved']
-           unless !it['publish'] || it['file_versions'].blank?
-             dig = process_file_versions(it, dig)
-           end
-        end
-        break if !dig.blank?
-      end
-    end
-    dig
-  end
-
-  def process_file_versions(json, dig)
-    unless json['file_versions'].blank?
-      json['file_versions'].each do |version|
-        if version['publish'] && version['file_uri'].start_with?('http')
-          if !version['xlink_show_attribute'].blank? && (version['xlink_show_attribute']||'') == 'embed'
-            dig['thumb'] = (dig['thumb']? dig['thumb'].push(version['file_uri']) : [version['file_uri']])
-            unless json['html'].blank? || json['html']['note'].blank?
-              dig['caption'] =  json['html']['note']['note_text']
-              dig['caption'] = dig['caption'].html_safe if dig['caption']
-            end
-          elsif (version['xlink_show_attribute']||'') == 'new'
-            dig['out'] = version['file_uri'] if version['file_uri'] != (dig['out'] || '')
-          end
-        end
-      end
-    end
-    dig
-  end
 end
