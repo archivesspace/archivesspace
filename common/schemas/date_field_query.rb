16a43d6edc234c0dd67e5cc84d0e82d486ed1e44
Payten Giles <payten.giles@gmail.com>
2016-11-10 10:47:52 +1100

    AR-1582 update date and boolean advanced queries to support NOT / 'negated'

--- common/schemas/date_field_query.rb
+++ common/schemas/date_field_query.rb
@@ -8,6 +8,7 @@
       "comparator" => {"type" => "string", "enum" => ["greater_than", "lesser_than", "equal"]},
       "field" => {"type" => "string", "ifmissing" => "error"},
       "value" => {"type" => "date", "ifmissing" => "error"},
+      "negated" => {"type" => "boolean", "default" => false},
 
     },
   },
