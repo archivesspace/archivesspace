7f582bfc3e4dd78e08e172e7678f8176d5add24a
Bobbi Fox <bobbi_fox@harvard.edu>
2016-11-21 08:37:12 -0500

    Put admin back as u/p

--- public-new/app/archivesspace-public/app/lib/config_defaults.rb
+++ public-new/app/archivesspace-public/app/lib/config_defaults.rb
@@ -1,7 +1,7 @@
 unless $CONFIG_DEFAULTS_LOADED
   AppConfig[:archivesspace_url] = 'http://localhost:4567'
-  AppConfig[:archivesspace_user] = 'CHANGETHIS' 
-  AppConfig[:archivesspace_password] = 'CHANGETHIS'
+  AppConfig[:archivesspace_user] = 'admin' 
+  AppConfig[:archivesspace_password] = 'admin'
   AppConfig[:search_results_page_size] = 10
   AppConfig[:public_url] = '/'
   AppConfig[:branding_img] = '/img/Aspace-logo.png'
