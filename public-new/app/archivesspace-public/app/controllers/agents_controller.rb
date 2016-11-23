f2864509a7fb8cf123b14d51098b45e4ac57ad8c
Bobbi Fox <bobbi_fox@harvard.edu>
2016-11-23 15:51:16 -0500

    Support multi-line search, sorting, filtering by searching within results
    Needs more DRY-ing, presentation of search terms for multi-line searches

--- public-new/app/archivesspace-public/app/controllers/agents_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/agents_controller.rb
@@ -4,34 +4,52 @@ class AgentsController <  ApplicationController
 
   skip_before_filter  :verify_authenticity_token
 
-  DEFAULT_AG_TYPES = %w{pui_agent pui_person}
+  DEFAULT_AG_TYPES = %w{repository resource archival_object digital_object}
   DEFAULT_AG_FACET_TYPES = %w{primary_type subjects used_within_repository}
   DEFAULT_AG_SEARCH_OPTS = {
     'sort' => 'title_sort asc',
     'facet.mincount' => 1
   }
 
+  DEFAULT_AG_SEARCH_PARAMS = {
+    :q => ['*'],
+    :limit => 'pui_agent',
+    :op => ['OR'],
+    :field => ['title']
+  }
   def index
-#    repo_id = params.fetch(:repo_id, nil)
-    if params.fetch(:q, nil)
-        pass_params = params
-    else
-      pass_params = {}
-      pass_params[:q] = ['*']
-      pass_params[:limit] = 'pui_agent'
-      pass_params[:op] = ['OR']
-      pass_params[:field] = ['title']
+    repo_id = params.fetch(:rid, nil)
+    Rails.logger.debug("repo_id: #{repo_id}")
+    if !params.fetch(:q, nil)
+      DEFAULT_AG_SEARCH_PARAMS.each do |k, v|
+        params[k] = v unless params.fetch(k,nil)
+      end
     end
-    @base_search  =  '/agents?'
+    search_opts = default_search_opts(DEFAULT_AG_SEARCH_OPTS)
+    search_opts['fq'] = ["used_within_repository:\"/repositories/#{repo_id}\""] if repo_id
+    @base_search  =  repo_id ? "/repositories/#{repo_id}/agents?" : '/agents?'
     page = Integer(params.fetch(:page, "1"))
     begin
-      set_up_and_run_search( DEFAULT_AG_TYPES, DEFAULT_AG_FACET_TYPES,  DEFAULT_AG_SEARCH_OPTS, pass_params)
+      set_up_and_run_search( DEFAULT_AG_TYPES, DEFAULT_AG_FACET_TYPES,  search_opts, params)
     rescue Exception => error
       flash[:error] = error
       redirect_back(fallback_location: '/') and return
     end
+
+    @context = repo_context(repo_id, 'agent')
+    @search[:dates_within] = false
+    @search[:text_within] = @pager.last_page > 1
+
     @page_title = I18n.t('agent._plural')
     @results_type = @page_title
+    all_sorts = Search.get_sort_opts
+    @sort_opts = []
+    %w(title_sort_asc title_sort_desc).each do |type|
+      @sort_opts.push(all_sorts[type])
+    end
+    if params[:q].size > 1 || params[:q][0] != '*'
+      @sort_opts.unshift(all_sorts['relevance'])
+    end
     render 'search/search_results'
   end
 
@@ -60,9 +78,11 @@ class AgentsController <  ApplicationController
     results =  handle_results(results)
     if !results['results'].blank? && results['results'].length > 0
       @result = results['results'][0]
-#      Pry::ColoPrinter.pp(@result)
+      Pry::ColorPrinter.pp(@result)
       @results = fetch_agent_results(@result['title'],uri, params)
       if !@results.blank?
+        params[:q] = '*'
+#Pry::ColorPrinter.pp(@results['results'])
         @pager =  Pager.new("#{uri}?q=#{params.fetch(:q,'*')}", @results['this_page'],@results['last_page'])
       else
         @pager = nil
@@ -81,7 +101,7 @@ class AgentsController <  ApplicationController
   private
   def fetch_agent_results(title, uri, params)
     @results = []
-    qry = "#{params.fetch(:q,'*')} AND  agents:\"#{title}\""
+    qry = "agents:\"#{title}\""
     @base_search = "#{uri}?"
     set_up_search(DEFAULT_AG_TYPES, DEFAULT_AG_FACET_TYPES, DEFAULT_AG_SEARCH_OPTS, params,qry)
     page = Integer(params.fetch(:page, "1"))
