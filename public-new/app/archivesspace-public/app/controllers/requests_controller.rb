afd7059624dff0563bf78616c808b0f5c9186f40
Bobbi Fox <bobbi_fox@harvard.edu>
2016-12-19 16:14:16 -0500

    handle flash message arrays

--- public-new/app/archivesspace-public/app/controllers/requests_controller.rb
+++ public-new/app/archivesspace-public/app/controllers/requests_controller.rb
@@ -5,10 +5,10 @@ class RequestsController < ApplicationController
     @request = RequestItem.new(params)
     errs = @request.validate
     if errs.blank?
-      flash[:notice] = (@request.to_text_array.join("\n"))
+      flash[:notice] = (@request.to_text_array)
       redirect_to params.fetch('base_url', request[:request_uri])
     else
-      flash[:error] = errs.join("\n")
+      flash[:error] = errs
       redirect_back(fallback_location: request[:request_uri]) and return
     end
   end
