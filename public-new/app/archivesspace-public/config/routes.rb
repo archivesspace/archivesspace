fa92adf385879f02ee9a0de849ab3123424e32b5
Bobbi Fox <bobbi_fox@harvard.edu>
2016-12-19 09:20:58 -0500

    handle request bugs (no @request, route problem)

--- public-new/app/archivesspace-public/config/routes.rb
+++ public-new/app/archivesspace-public/config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   get 'repositories/resources' => 'resources#index'
   get  "repositories/:rid/accessions/:id" => 'accessions#show'
   post "repositories/:rid/archival_objects/:id/request" => 'objects#request_showing'
+  get "repositories/:rid/archival_objects/:id/request" => 'objects#request_showing'
   get  "repositories/:rid/classifications/:id" => 'classifications#show'
   get  "repositories/:repo_id/resources/:id/search"  => 'resources#search'
   get "repositories/:rid/resources/:id"  => 'resources#show'
