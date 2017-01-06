f8ea2f62171769d910d50035b7f515ae0d118a32
Bobbi Fox <bobbi_fox@harvard.edu>
2017-01-06 16:28:34 -0500

    move request submission button

--- public-new/app/archivesspace-public/app/assets/javascripts/request.js
+++ public-new/app/archivesspace-public/app/assets/javascripts/request.js
@@ -1,8 +1,26 @@
-function setupRequest(modalId) {
+function setupRequest(modalId, text) {
+    $(".noscript").hide();
     $('#request_sub').submit(function(e) {
             request_form();
             return false;
         });
+    var $modal = $('#' + modalId);
+    $modal.find('div.modal-body').attr('id', 'requestThis');
+    var x = $modal.find('.action-btn');
+    var btn;
+    if (x.length == 1) {
+	btn = x[0];
+    }
+    else {
+	btn = x;
+    }
+    $(btn).attr('id', "request_btn");
+    $(btn).html(text);
+    $(btn).click(function(e) { 
+	    /*	    alert("you clicked me!");
+		    console.log($("#request_form")) */
+    $("#request_form").submit();
+	});
 }
 
 
