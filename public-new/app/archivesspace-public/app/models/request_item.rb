0e8e106e291f38b956bc02af310c71fe980adc94
Bobbi Fox <bobbi_fox@harvard.edu>
2017-01-09 11:18:00 -0500

    fix barcode and machine restrictions bug in requests

--- public-new/app/archivesspace-public/app/models/request_item.rb
+++ public-new/app/archivesspace-public/app/models/request_item.rb
@@ -29,19 +29,18 @@ class RequestItem < Struct.new(:user_name, :user_email, :date, :note, :hier, :re
     errs.push(I18n.t('request.errors.name')) if self[:user_name].blank?
     errs.push(I18n.t('request.errors.email')) if self[:user_email].blank? ||  !self[:user_email].match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
     errs
-         
-              
-              
   end
+
   def to_text(skip_empty = false)
     to_text_array(skip_empty).join("\n")
   end
 
   def to_text_array(skip_empty = false)
     arr = []
-    %i(user_name user_email date note title identifier cite request_uri resource_name resource_id repo_name hier restrict machine).each do |sym|
+    %i(user_name user_email date note title identifier cite request_uri resource_name resource_id repo_name hier restrict).each do |sym|
       arr.push("#{sym.to_s}: #{self[sym]}") unless skip_empty && self[sym].blank?
     end
+    arr.push("machine: #{self[:machine].blank? ? '' : self[:machine].join(', ')}")
     if !self[:top_container_name].blank? &&  !self[:top_container_name].empty?
        self[:top_container_name].each_with_index do |v, i|
         arr.push("#{:top_container_name.to_s}: #{v}")
@@ -52,6 +51,7 @@ class RequestItem < Struct.new(:user_name, :user_email, :date, :note, :hier, :re
     elsif !skip_empty
       %i(top_container_name top_container_url barcode location_title location_url).each {|sym| arr.push("#{sym.to_s}:") }
     end
+
     arr
   end
 end
