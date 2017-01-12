4f59bdb6e836ef0f8364f8e9506cc23aca68a4be
Payten Giles <payten.giles@gmail.com>
2017-01-13 08:54:38 +1100

    Fix listing reports and CSS includes (I broke them goooood)

--- backend/app/lib/reports/report_response.rb
+++ backend/app/lib/reports/report_response.rb
@@ -44,4 +44,42 @@ class ReportErbRenderer
   def render(file)
     ERB.new( File.read(file) ).result(binding)
   end
+
+  def transform_text(s)
+    # The HTML to PDF library doesn't currently support the "break-word" CSS
+    # property that would let us force a linebreak for long strings and URIs.
+    # Without that, we end up having our tables chopped off, which makes them
+    # not-especially-useful.
+    #
+    # Newer versions of the library might fix this issue, but it appears that the
+    # licence of the newer version is incompatible with the current ArchivesSpace
+    # licence.
+    #
+    # So, we wrap runs of characters in their own span tags to give the renderer
+    # a hint on where to place the line breaks.  Pretty terrible, but it works.
+    #
+    if @report.format === 'pdf'
+      escaped = CGI.escapeHTML(s)
+
+      # Exciting regexp time!  We break our string into "tokens", which are either:
+      #
+      #   - A single whitespace character
+      #   - A HTML-escaped character (like '&amp;')
+      #   - A run of between 1 and 5 letters
+      #
+      # Each token is then wrapped in a span, ensuring that we don't go too
+      # long without having a spot to break a word if needed.
+      #
+      escaped.scan(/[\s]|&.*;|[^\s]{1,5}/).map {|token|
+        if token.start_with?("&") || token =~ /\A[\s]\Z/
+          # Don't mess with &amp; and friends, nor whitespace
+          token
+        else
+          "<span>#{token}</span>"
+        end
+      }.join("")
+    else
+      CGI.escapeHTML(s)
+    end
+  end
 end
