6decfa774b15e2ff11979a8f11b01edebf1073f1
Payten Giles <payten.giles@gmail.com>
2017-01-14 10:33:14 +1100

    Add the Name Records List report

---
+++ backend/app/model/reports/agent_list_report.rb
@@ -0,0 +1,52 @@
+class AgentListReport < AbstractReport
+
+  register_report({
+                    :uri_suffix => "agent_list_report",
+                    :description => "Displays selected name record(s). Report lists name, name type, and name source.",
+                  })
+
+  def title
+    "Name Records List"
+  end
+
+  def template
+    'generic_listing.erb'
+  end
+
+  def headers
+    ['sortName', 'nameType', 'nameSource']
+  end
+
+  def query
+    people = db[:name_person]
+              .filter(:name_person__is_display_name => 1)
+              .filter(Sequel.~(:name_person__source_id => nil))
+              .filter(db[:user]
+                        .filter(:user__agent_record_id => :name_person__agent_person_id)
+                        .select(:agent_record_id) => nil)
+              .select(Sequel.as(:agent_person_id, :agentId),
+                      Sequel.as(:sort_name, :sortName),
+                      Sequel.as('Person', :nameType),
+                      Sequel.as(Sequel.lit('GetEnumValueUF(name_person.source_id)'), :nameSource))
+
+    families = db[:name_family]
+                .filter(:name_family__is_display_name => 1)
+                .filter(Sequel.~(:name_family__source_id => nil))
+                .select(Sequel.as(:agent_family_id, :agentId),
+                        Sequel.as(:sort_name, :sortName),
+                        Sequel.as('Family', :nameType),
+                        Sequel.as(Sequel.lit('GetEnumValueUF(name_family.source_id)'), :nameSource))
+
+    corporate = db[:name_corporate_entity]
+                 .filter(:name_corporate_entity__is_display_name => 1)
+                 .filter(Sequel.~(:name_corporate_entity__source_id => nil))
+                 .select(Sequel.as(:agent_corporate_entity_id, :agentId),
+                         Sequel.as(:sort_name, :sortName),
+                         Sequel.as('Corporate', :nameType),
+                         Sequel.as(Sequel.lit('GetEnumValueUF(name_corporate_entity.source_id)'), :nameSource))
+
+    people
+      .union(families)
+      .union(corporate)
+  end
+end
