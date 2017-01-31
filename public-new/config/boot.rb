653b4ed700116042ad848e19fe884ae63549cf46
Mark Triggs <mark@dishevelled.net>
2017-01-31 20:07:17 +1100

    Import PUI project

--- public-new/config/boot.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'stringio'
-
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'aspace_gems'
-ASpaceGems.setup
-
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
