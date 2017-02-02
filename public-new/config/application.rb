a4d31c0365619c495ade0b44673e730c7e446d9c
Payten Giles <payten.giles@gmail.com>
2017-02-02 13:33:03 +1100

    Add plugin hooks for public-new and a test 'pug'in

--- public-new/config/application.rb
+++ public-new/config/application.rb
@@ -5,6 +5,9 @@ require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
 
+require 'asutils'
+require_relative 'initializers/plugins'
+
 # Maybe we won't need these?
 
 # DISABLED BY MST # require 'active_record/railtie'
@@ -23,8 +26,37 @@ module ArchivesspacePublic
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # we require the app_config module in order to access the directory value from the :custom key
-    require "#{Rails.root}/app/lib/app_config.rb"
-    config.i18n.load_path += Dir[File.join(Rails.root, AppConfig[:custom], 'locales', '**' , '*.{rb,yml}')]
+    # Add plugin controllers and models
+    config.paths["app/controllers"].concat(ASUtils.find_local_directories("public/controllers"))
+    config.paths["app/models"].concat(ASUtils.find_local_directories("public/models"))
+
+    # Load the shared 'locales'
+    ASUtils.find_locales_directories.map{|locales_directory| File.join(locales_directory)}.reject { |dir| !Dir.exist?(dir) }.each do |locales_directory|
+      config.i18n.load_path += Dir[File.join(locales_directory, '**' , '*.{rb,yml}')]
+    end
+
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    # Allow overriding of the i18n locales via the 'local' folder(s)
+    if not ASUtils.find_local_directories.blank?
+      ASUtils.find_local_directories.map{|local_dir| File.join(local_dir, 'public', 'locales')}.reject { |dir| !Dir.exist?(dir) }.each do |locales_override_directory|
+        I18n.load_path += Dir[File.join(locales_override_directory, '**' , '*.{rb,yml}')]
+      end
+    end
+
+    # Add template static assets to the path
+    if not ASUtils.find_local_directories.blank?
+      ASUtils.find_local_directories.map{|local_dir| File.join(local_dir, 'public', 'assets')}.reject { |dir| !Dir.exist?(dir) }.each do |static_directory|
+        config.assets.paths.unshift(static_directory)
+      end
+    end
+  end
+end
+
+# Load plugin init.rb files (if present)
+ASUtils.find_local_directories('public').each do |dir|
+  init_file = File.join(dir, "plugin_init.rb")
+  if File.exist?(init_file)
+    load init_file
   end
 end
