c91e48ebb5926c14f03a40919d6e645e7996e084
Payten Giles <payten.giles@gmail.com>
2017-02-06 13:24:46 +1100

    Raise an exception if aspace-public-formats plugin is enabled for the public-new app (it isn't currently supported)

--- plugins/aspace-public-formats/public/plugin_init.rb
+++ plugins/aspace-public-formats/public/plugin_init.rb
@@ -1,3 +1,7 @@
+if ENV['ASPACE_PUBLIC_NEW'] == 'true'
+  raise 'The aspace-public-formats plugin is only compatible with the original public user interface'
+end
+
 require "net/http"
 require "uri"
 
