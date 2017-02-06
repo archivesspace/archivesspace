31407e240921137e82dd10e89caf5e1d169e816f
Payten Giles <payten.giles@gmail.com>
2017-02-06 12:09:07 +1100

    Tinker the public-new configuration to allow for dist, including remove bower, disable debugging module, remove jstree and fix fonts in production dist

--- public-new/app/controllers/concerns/debugging.rb
+++ public-new/app/controllers/concerns/debugging.rb
@@ -1,12 +1,13 @@
-module Debugging
-  extend ActiveSupport::Concern
-  require 'pry'
-
-  # Pretty Print
-  def dbgpp(text, obj = nil)
-    if %w|development test|.include? Rails.env
-      Rails.logger.debug(text)
-      Pry::ColorPrinter.pp(obj) if obj
-    end
-  end
-end
+# FIXME doesn't work with dist
+# module Debugging
+#   extend ActiveSupport::Concern
+#   require 'pry'
+# 
+#   # Pretty Print
+#   def dbgpp(text, obj = nil)
+#     if %w|development test|.include? Rails.env
+#       Rails.logger.debug(text)
+#       Pry::ColorPrinter.pp(obj) if obj
+#     end
+#   end
+# end
--- public-new/config/boot.rb
+++ public-new/config/boot.rb
@@ -1,3 +1,10 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+require 'rubygems'
+require 'stringio'
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'aspace_gems'
+ASpaceGems.setup
+
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
