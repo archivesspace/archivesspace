1a7b4922fbfa5fa4fe77188c9132804d7904c955
Payten Giles <payten.giles@gmail.com>
2017-02-13 16:58:27 +1100

    WIP add child / add sibling behaviours

--- frontend/app/assets/javascripts/tree_ids_helpers.js
+++ frontend/app/assets/javascripts/tree_ids_helpers.js
@@ -2,13 +2,21 @@ var TreeIds = {}
 
 /* FIXME: Figure out how to get rid of plural stuff and hopefully all of this... */
 TreeIds.uri_to_tree_id = function (uri) {
+    var parts = TreeIds.uri_to_parts(uri);
+    return parts.type + '_' + parts.id;
+}
+
+TreeIds.uri_to_parts = function (uri) {
     var last_part = uri.replace(/\/repositories\/[0-9]+\//,"");
     var bits = last_part.match(/([a-z_]+)\/([0-9]+)/);
     var type_plural = bits[1].replace(/\//g,'_');
     var id = bits[2];
     var type = type_plural.replace(/s$/, '');
 
-    return [type, id].join('_');
+    return {
+        type: type,
+        id: id
+    };
 }
 
 TreeIds.parse_tree_id = function (tree_id) {
