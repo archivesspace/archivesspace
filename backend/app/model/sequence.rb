57ffa47001d95414aff78d59b42636f884ecb0bc
Mark Triggs <mark@dishevelled.net>
2017-02-16 19:21:02 +1100

    Run sequence generation in a pool of threads

--- backend/app/model/sequence.rb
+++ backend/app/model/sequence.rb
@@ -1,22 +1,35 @@
 class Sequence
 
+  # We run sequence generation in separate threads to allow us to commit
+  # transactions independently of the caller.  Otherwise the nested transaction
+  # is subsumed by the parent one, which doesn't work.
+  @@pool = java.util.concurrent.ThreadPoolExecutor.new(10,
+                                                       10,
+                                                       0,
+                                                       java.util.concurrent.TimeUnit::MILLISECONDS,
+                                                       java.util.concurrent.ArrayBlockingQueue.new(1024))
+
+  @@submit = @@pool.java_method(:submit, [java.util.concurrent.Callable])
+
+
   def self.init(sequence, value)
     DB.open(true) do |db|
       db[:sequence].insert(:sequence_name => sequence.to_s, :value => value)
     end
   end
 
-
   def self.get(sequence)
     Thread.current[:initialised_sequences] ||= {}
 
     needs_sequence_init = !Thread.current[:initialised_sequences][sequence]
 
-    future = Thread.new do
-      handle_get(sequence, needs_sequence_init)
-    end
+    future = @@submit.call(proc { handle_get(sequence, needs_sequence_init)})
 
-    future.value
+    begin
+      future.get
+    ensure
+      Thread.current[:initialised_sequences][sequence] = true
+    end
   end
 
   def self.handle_get(sequence, needs_sequence_init)
@@ -33,21 +46,21 @@ class Sequence
 
     # If we make it to here, the sequence already exists and needs to be incremented
     1000.times do
-      old_value = DB.open do |db|
-        db[:sequence].filter(:sequence_name => sequence.to_s).get(:value)
-      end
+      new_value = nil
 
       updated_count = DB.open do |db|
+        old_value = db[:sequence].filter(:sequence_name => sequence.to_s).get(:value)
+        new_value = old_value + 1
+
         db[:sequence].filter(:sequence_name => sequence.to_s, :value => old_value).
-          update(:value => old_value + 1)
+          update(:value => new_value)
       end
 
       if updated_count == 0
         # Need to retry
-        sleep(0.01)
+        sleep(0.001)
       elsif updated_count == 1
-        puts "Got sequence number: #{old_value + 1} for #{sequence.to_s}"
-        return old_value + 1
+        return new_value
       else
         raise SequenceError.new("Unrecognised response from SQL update when generating next element in sequence '#{sequence}': #{updated_count}")
       end
