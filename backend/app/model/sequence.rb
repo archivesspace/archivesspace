38db7c2c458e78086f474eaa624d16be3fc6a6aa
Mark Triggs <mark@dishevelled.net>
2017-02-16 14:55:43 +1100

    Generate sequences in individual transactions
    Otherwise we might just repeatedly read the same stale value and
    eventually give up.

--- backend/app/model/sequence.rb
+++ backend/app/model/sequence.rb
@@ -8,13 +8,12 @@ class Sequence
 
 
   def self.get(sequence)
-    DB.open(true) do |db|
+    Thread.current[:initialised_sequences] ||= {}
 
-      Thread.current[:initialised_sequences] ||= {}
+    if !Thread.current[:initialised_sequences][sequence]
+      Thread.current[:initialised_sequences][sequence] = true
 
-      if !Thread.current[:initialised_sequences][sequence]
-        Thread.current[:initialised_sequences][sequence] = true
-
+      DB.open(true) do |db|
         DB.attempt {
           init(sequence, 0)
           return 0
@@ -22,25 +21,30 @@ class Sequence
           # Sequence is already defined, which is fine
         }
       end
+    end
 
-      # If we make it to here, the sequence already exists and needs to be incremented
-      1000.times do
-        old_value = db[:sequence].filter(:sequence_name => sequence.to_s).get(:value)
-        updated_count = db[:sequence].filter(:sequence_name => sequence.to_s, :value => old_value).
-                                      update(:value => old_value + 1)
-
-        if updated_count == 0
-          # Need to retry
-          sleep(0.01)
-        elsif updated_count == 1
-          return old_value + 1
-        else
-          raise SequenceError.new("Unrecognised response from SQL update when generating next element in sequence '#{sequence}': #{updated_count}")
-        end
+    # If we make it to here, the sequence already exists and needs to be incremented
+    1000.times do
+      old_value = DB.open do |db|
+        db[:sequence].filter(:sequence_name => sequence.to_s).get(:value)
       end
 
-      raise SequenceError.new("Gave up trying to generate a sequence number for: '#{sequence}'")
-    end
-  end
+      updated_count = DB.open do |db|
+        db[:sequence].filter(:sequence_name => sequence.to_s, :value => old_value).
+          update(:value => old_value + 1)
+      end
 
+      if updated_count == 0
+        # Need to retry
+        sleep(0.01)
+      elsif updated_count == 1
+        puts "Got sequence number: #{old_value + 1} for #{sequence.to_s}"
+        return old_value + 1
+      else
+        raise SequenceError.new("Unrecognised response from SQL update when generating next element in sequence '#{sequence}': #{updated_count}")
+      end
+    end
+
+    raise SequenceError.new("Gave up trying to generate a sequence number for: '#{sequence}'")
+  end
 end
