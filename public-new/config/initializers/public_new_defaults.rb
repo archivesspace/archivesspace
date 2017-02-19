2f7d11b07c1f7e1ace04a6c740b4d94b4f956478
Payten Giles <payten.giles@gmail.com>
2017-02-20 09:54:29 +1100

    Allow record page actions to be plugged

--- public-new/config/initializers/public_new_defaults.rb
+++ public-new/config/initializers/public_new_defaults.rb
@@ -39,4 +39,38 @@ module PublicNewDefaults
 #  MemoryLeak::Resources.define(:repository, proc { ArchivesSpaceClient.new.list_repositories }, 60)
 #pp MemoryLeak::Resources.get(:repository)
 
+  # determining the record page actions menu
+  $RECORD_PAGE_ACTIONS = {}
+
+  # Add a page action for a particular jsonmodel record type
+  # - record_type: the type e.g, resource, archival_object etc
+  # - label: I18n path or string for the label
+  # - icon_css: CSS classes for the Font Awesome icon e.g. 'fa-book'
+  # - build_url_proc: a proc passed the record upon render which must return a string
+  #   (the record is passed as a param to the proc)  def self.add_record_page_action_javascript(record_type, label, icon_css, build_url_proc = nil, onclick_javascript = nil)
+  def self.add_record_page_action_proc(record_type, label, icon_css, build_url_proc)
+    $RECORD_PAGE_ACTIONS[record_type] ||= []
+    $RECORD_PAGE_ACTIONS[record_type] << {
+      'label' => label,
+      'icon_css' => icon_css,
+      'build_url_proc' => build_url_proc
+    }
+  end
+
+  # Add a page action for a particular jsonmodel record type
+  # - record_type: the type e.g, resource, archival_object etc
+  # - label: I18n path or string for the label
+  # - icon_css: CSS classes for the Font Awesome icon e.g. 'fa fa-book fa-3x'
+  # - onclick_javascript: a javascript expression to run when the action is clicked
+  #   (the record uri and title are available as data attributes on the button element)
+  def self.add_record_page_action_js(record_type, label, icon_css, onclick_javascript)
+    $RECORD_PAGE_ACTIONS[record_type] ||= []
+    $RECORD_PAGE_ACTIONS[record_type] << {
+      'label' => label,
+      'icon_css' => icon_css,
+      'onclick_javascript' => onclick_javascript
+    }
+  end
+
+
 end
