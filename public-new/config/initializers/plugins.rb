f2444bfe35d8e902197e4002ae9687ea96417090
Payten Giles <payten.giles@gmail.com>
2017-02-20 09:54:29 +1100

    Allow record page actions to be plugged

--- public-new/config/initializers/plugins.rb
+++ public-new/config/initializers/plugins.rb
@@ -9,4 +9,16 @@ module Plugins
       PublicNewDefaults::add_menu_item(path, label, position)
     end
   end
+
+  def self.add_record_page_action_proc(record_type, label, icon_css, build_url_proc)
+    ArchivesSpacePublic::Application.config.after_initialize do
+      PublicNewDefaults::add_record_page_action_proc(record_type, label, icon_css, build_url_proc)
+    end
+  end
+
+  def self.add_record_page_action_js(record_type, label, icon_css, onclick_javascript)
+    ArchivesSpacePublic::Application.config.after_initialize do
+      PublicNewDefaults::add_record_page_action_js(record_type, label, icon_css, onclick_javascript)
+    end
+  end
 end
