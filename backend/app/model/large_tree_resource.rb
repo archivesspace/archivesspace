a645168e29c327477c6ddadcf90007a49370cfc0
Payten Giles <payten.giles@gmail.com>
2017-02-24 12:38:16 +1100

    I18n the tree summary text and pull back all containers for these summaries

--- backend/app/model/large_tree_resource.rb
+++ backend/app/model/large_tree_resource.rb
@@ -1,10 +1,38 @@
 class LargeTreeResource
 
   def root(response, root_record)
-    response['level'] = root_record.level
+    response['level'] = root_record.other_level || root_record.level
 
-    unless root_record.instance.empty?
-      response['type'] = root_record.instance[0].instance_type
+    # Collect all container data
+    Instance
+      .join(:sub_container, :sub_container__instance_id => :instance__id)
+      .join(:top_container_link_rlshp, :sub_container_id => :sub_container__id)
+      .join(:top_container, :id => :top_container_link_rlshp__top_container_id)
+      .left_join(Sequel.as(:enumeration_value, :top_container_type), :id => :top_container__type_id)
+      .left_join(Sequel.as(:enumeration_value, :type_2), :id => :sub_container__type_2_id)
+      .left_join(Sequel.as(:enumeration_value, :type_3), :id => :sub_container__type_3_id)
+      .left_join(Sequel.as(:enumeration_value, :instance_type), :id => :instance__instance_type_id)
+      .filter(:resource_id => root_record.id)
+      .select(Sequel.as(:instance_type__value, :instance_type),
+              Sequel.as(:top_container_type__value, :top_container_type),
+              Sequel.as(:top_container__indicator, :top_container_indicator),
+              Sequel.as(:type_2__value, :type_2),
+              Sequel.as(:sub_container__indicator_2, :indicator_2),
+              Sequel.as(:type_3__value, :type_3),
+              Sequel.as(:sub_container__indicator_3, :indicator_3))
+    .each do |row|
+        response['containers'] ||= []
+
+        container_data = {}
+        container_data['instance_type'] = row[:instance_type] if row[:instance_type]
+        container_data['top_container_type'] = row[:top_container_type] if row[:top_container_type]
+        container_data['top_container_indicator'] = row[:top_container_indicator] if row[:top_container_indicator]
+        container_data['type_2'] = row[:type_2] if row[:type_2]
+        container_data['indicator_2'] = row[:indicator_2] if row[:indicator_2]
+        container_data['type_3'] = row[:type_3] if row[:type_3]
+        container_data['indicator_3'] = row[:indicator_3] if row[:indicator_3]
+
+        response['containers'] << container_data
     end
 
     response
@@ -17,19 +45,16 @@ class LargeTreeResource
   def waypoint(response, record_ids)
     # Load the instance type and record level
     ArchivalObject
-      .left_join(Instance, :archival_object_id => :id)
-      .left_join(Sequel.as(:enumeration_value, :type_enum), :id => :instance__instance_type_id)
       .left_join(Sequel.as(:enumeration_value, :level_enum), :id => :archival_object__level_id)
       .filter(:archival_object__id => record_ids)
       .select(Sequel.as(:archival_object__id, :id),
-              Sequel.as(:type_enum__value, :type),
-              Sequel.as(:level_enum__value, :level))
+              Sequel.as(:level_enum__value, :level),
+              Sequel.as(:archival_object__other_level, :other_level))
       .each do |row|
       id = row[:id]
       result_for_record = response.fetch(record_ids.index(id))
 
-      result_for_record['type'] = row[:type] if row[:type]
-      result_for_record['level'] = row[:level]
+      result_for_record['level'] = row[:other_level] || row[:level]
     end
 
     # Display container information
@@ -37,11 +62,13 @@ class LargeTreeResource
       .join(:sub_container, :sub_container__instance_id => :instance__id)
       .join(:top_container_link_rlshp, :sub_container_id => :sub_container__id)
       .join(:top_container, :id => :top_container_link_rlshp__top_container_id)
-      .join(Sequel.as(:enumeration_value, :top_container_type), :id => :top_container__type_id)
-      .join(Sequel.as(:enumeration_value, :type_2), :id => :sub_container__type_2_id)
-      .join(Sequel.as(:enumeration_value, :type_3), :id => :sub_container__type_3_id)
+      .left_join(Sequel.as(:enumeration_value, :top_container_type), :id => :top_container__type_id)
+      .left_join(Sequel.as(:enumeration_value, :type_2), :id => :sub_container__type_2_id)
+      .left_join(Sequel.as(:enumeration_value, :type_3), :id => :sub_container__type_3_id)
+      .left_join(Sequel.as(:enumeration_value, :instance_type), :id => :instance__instance_type_id)
       .filter(:archival_object_id => record_ids)
       .select(:archival_object_id,
+              Sequel.as(:instance_type__value, :instance_type),
               Sequel.as(:top_container_type__value, :top_container_type),
               Sequel.as(:top_container__indicator, :top_container_indicator),
               Sequel.as(:type_2__value, :type_2),
@@ -52,11 +79,18 @@ class LargeTreeResource
       id = row[:archival_object_id]
 
       result_for_record = response.fetch(record_ids.index(id))
-      result_for_record['container'] = [
-                                        [row[:top_container_type], row[:top_container_indicator]].join(": "),
-                                        [row[:type_2], row[:indicator_2]].compact.join(": "),
-                                        [row[:type_3], row[:indicator_3]].compact.join(": "),
-                                       ].reject(&:empty?).join(", ")
+      result_for_record['containers'] ||= []
+
+      container_data = {}
+      container_data['instance_type'] = row[:instance_type] if row[:instance_type]
+      container_data['top_container_type'] = row[:top_container_type] if row[:top_container_type]
+      container_data['top_container_indicator'] = row[:top_container_indicator] if row[:top_container_indicator]
+      container_data['type_2'] = row[:type_2] if row[:type_2]
+      container_data['indicator_2'] = row[:indicator_2] if row[:indicator_2]
+      container_data['type_3'] = row[:type_3] if row[:type_3]
+      container_data['indicator_3'] = row[:indicator_3] if row[:indicator_3]
+
+      result_for_record['containers'] << container_data
     end
 
     response
