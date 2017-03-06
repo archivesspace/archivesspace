a8ec7d840142ce64e865f54d7a1251ba4fb08e50
Mark Triggs <mark@dishevelled.net>
2017-03-06 18:47:46 +1100

    Roll back multi DB pool stuff in favor of using READ_COMMITTED for POST requests

--- backend/app/model/sequence.rb
+++ backend/app/model/sequence.rb
@@ -1,33 +1,21 @@
-# As things stand, this class isn't used by the core code anymore.  However, we
-# keep it around for the sake of plugins that do use it for things like ID
-# generation.
-#
 class Sequence
 
-  # Run a separate DB connection pool to allow sequences to commit independently
-  # to the caller.  The separate pool also prevents deadlocks that would occur
-  # if caller threads tried to take a second connection from the main DB
-  # connection pool.
-  db_connections = [(AppConfig[:db_max_connections] / 5.0).floor,
-                    5].min
-
-  @@pool = DB::DBPool.new(db_connections, :skip_utf8_check => true).connect
-
-
   def self.init(sequence, value)
-    @@pool.open(true) do |db|
+    DB.open(true) do |db|
       db[:sequence].insert(:sequence_name => sequence.to_s, :value => value)
     end
   end
 
+
   def self.get(sequence)
-    Thread.current[:initialised_sequences] ||= {}
+    DB.open(true) do |db|
 
-    needs_sequence_init = !Thread.current[:initialised_sequences][sequence]
+      Thread.current[:initialised_sequences] ||= {}
 
-    if needs_sequence_init
-      @@pool.open(true) do |db|
-        @@pool.attempt {
+      if !Thread.current[:initialised_sequences][sequence]
+        Thread.current[:initialised_sequences][sequence] = true
+
+        DB.attempt {
           init(sequence, 0)
           return 0
         }.and_if_constraint_fails {
@@ -35,32 +23,24 @@ class Sequence
         }
       end
 
-      Thread.current[:initialised_sequences][sequence] = true
-    end
-
-    # If we make it to here, the sequence already exists and needs to be incremented
-    1000.times do
-      new_value = nil
-
-      updated_count = @@pool.open do |db|
+      # If we make it to here, the sequence already exists and needs to be incremented
+      1000.times do
         old_value = db[:sequence].filter(:sequence_name => sequence.to_s).get(:value)
-        new_value = old_value + 1
+        updated_count = db[:sequence].filter(:sequence_name => sequence.to_s, :value => old_value).
+                                      update(:value => old_value + 1)
 
-        db[:sequence].filter(:sequence_name => sequence.to_s, :value => old_value).
-          update(:value => new_value)
+        if updated_count == 0
+          # Need to retry
+          sleep(0.01)
+        elsif updated_count == 1
+          return old_value + 1
+        else
+          raise SequenceError.new("Unrecognised response from SQL update when generating next element in sequence '#{sequence}': #{updated_count}")
+        end
       end
 
-      if updated_count == 0
-        # Need to retry
-        sleep(0.001)
-      elsif updated_count == 1
-        return new_value
-      else
-        raise SequenceError.new("Unrecognised response from SQL update when generating next element in sequence '#{sequence}': #{updated_count}")
-      end
+      raise SequenceError.new("Gave up trying to generate a sequence number for: '#{sequence}'")
     end
-
-    raise SequenceError.new("Gave up trying to generate a sequence number for: '#{sequence}'")
   end
 
 end
