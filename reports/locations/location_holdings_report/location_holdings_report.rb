0515adbf2510a2cb397890dfd872d474fac0a90a
Payten Giles <payten.giles@gmail.com>
2017-03-14 10:41:56 +1100

    Auto indent location_holdings_report.rb

--- reports/locations/location_holdings_report/location_holdings_report.rb
+++ reports/locations/location_holdings_report/location_holdings_report.rb
@@ -25,7 +25,7 @@ class LocationHoldingsReport < AbstractReport
       @start_location = Location.get_or_die(JSONModel(:location).id_for(params['location_start']['ref']))
 
       if ASUtils.present?(params['location_end'])
-	@end_location = Location.get_or_die(JSONModel(:location).id_for(params['location_end']['ref']))
+        @end_location = Location.get_or_die(JSONModel(:location).id_for(params['location_end']['ref']))
       end
     end
   end
@@ -50,26 +50,26 @@ class LocationHoldingsReport < AbstractReport
 
   def query
     dataset = if building
-		building_query
+                building_query
               elsif repository_uri
                 repository_query
-	      elsif start_location && end_location
-		range_query
-	      else
-		single_query
-	      end
+              elsif start_location && end_location
+                range_query
+              else
+                single_query
+              end
 
     # Join location to top containers, repository and (optionally) location and container profiles
     dataset = dataset
-	      .left_outer_join(:location_profile_rlshp, :location_id => :location__id)
-	      .left_outer_join(:location_profile, :id => :location_profile_rlshp__location_profile_id)
-	      .join(:top_container_housed_at_rlshp,
-		    :location_id => :location__id,
-		    :top_container_housed_at_rlshp__status => 'current')
-	      .join(:top_container, :id => :top_container_housed_at_rlshp__top_container_id)
-	      .join(:repository, :id => :top_container__repo_id)
-	      .left_outer_join(:top_container_profile_rlshp, :top_container_id => :top_container_housed_at_rlshp__top_container_id)
-	      .left_outer_join(:container_profile, :id => :top_container_profile_rlshp__container_profile_id)
+                .left_outer_join(:location_profile_rlshp, :location_id => :location__id)
+                .left_outer_join(:location_profile, :id => :location_profile_rlshp__location_profile_id)
+                .join(:top_container_housed_at_rlshp,
+                      :location_id => :location__id,
+                      :top_container_housed_at_rlshp__status => 'current')
+                .join(:top_container, :id => :top_container_housed_at_rlshp__top_container_id)
+                .join(:repository, :id => :top_container__repo_id)
+                .left_outer_join(:top_container_profile_rlshp, :top_container_id => :top_container_housed_at_rlshp__top_container_id)
+                .left_outer_join(:container_profile, :id => :top_container_profile_rlshp__container_profile_id)
 
     # A top container can be linked (via subcontainer) to an instance attached
     # to an archival object, resource or accession.  We'd like to report on the
@@ -78,52 +78,52 @@ class LocationHoldingsReport < AbstractReport
     #
     # So, here comes more joins...
     dataset = dataset
-	      .left_outer_join(:top_container_link_rlshp, :top_container_id => :top_container__id)
-	      .left_outer_join(:sub_container, :id => :top_container_link_rlshp__sub_container_id)
-	      .left_outer_join(:instance, :id => :sub_container__instance_id)
-	      .left_outer_join(:archival_object, :id => :instance__archival_object_id)
-	      .left_outer_join(:resource, :id => :instance__resource_id)
-	      .left_outer_join(:accession, :id => :instance__accession_id)
-	      .left_outer_join(:resource___resource_via_ao, :id => :archival_object__root_record_id)
+                .left_outer_join(:top_container_link_rlshp, :top_container_id => :top_container__id)
+                .left_outer_join(:sub_container, :id => :top_container_link_rlshp__sub_container_id)
+                .left_outer_join(:instance, :id => :sub_container__instance_id)
+                .left_outer_join(:archival_object, :id => :instance__archival_object_id)
+                .left_outer_join(:resource, :id => :instance__resource_id)
+                .left_outer_join(:accession, :id => :instance__accession_id)
+                .left_outer_join(:resource___resource_via_ao, :id => :archival_object__root_record_id)
 
     # Used so we can combine adjacent rows for accession/resources linkages
     # (i.e. one top container linked to multiple collections)
     dataset = dataset.order(:top_container_id)
 
     dataset = dataset.select(Sequel.as(:location__building, :building),
-			     Sequel.as(:location__floor, :floor),
-			     Sequel.as(:location__room, :room),
-			     Sequel.as(:location__area, :area),
-			     Sequel.as(:location__id, :location_id),
+                             Sequel.as(:location__floor, :floor),
+                             Sequel.as(:location__room, :room),
+                             Sequel.as(:location__area, :area),
+                             Sequel.as(:location__id, :location_id),
 
-			     Sequel.as(:location__coordinate_1_label, :coordinate_1_label),
-			     Sequel.as(:location__coordinate_1_indicator, :coordinate_1_indicator),
-			     Sequel.as(:location__coordinate_2_label, :coordinate_2_label),
-			     Sequel.as(:location__coordinate_2_indicator, :coordinate_2_indicator),
-			     Sequel.as(:location__coordinate_3_label, :coordinate_3_label),
-			     Sequel.as(:location__coordinate_3_indicator, :coordinate_3_indicator),
+                             Sequel.as(:location__coordinate_1_label, :coordinate_1_label),
+                             Sequel.as(:location__coordinate_1_indicator, :coordinate_1_indicator),
+                             Sequel.as(:location__coordinate_2_label, :coordinate_2_label),
+                             Sequel.as(:location__coordinate_2_indicator, :coordinate_2_indicator),
+                             Sequel.as(:location__coordinate_3_label, :coordinate_3_label),
+                             Sequel.as(:location__coordinate_3_indicator, :coordinate_3_indicator),
 
-			     Sequel.as(:location_profile__name, :location_profile),
-			     Sequel.as(:location__barcode, :location_barcode),
-			     Sequel.as(:top_container__indicator, :top_container_indicator),
-			     Sequel.as(:top_container__barcode, :top_container_barcode),
-			     Sequel.as(:container_profile__name, :container_profile),
-			     Sequel.as(:top_container__id, :top_container_id),
-			     Sequel.as(:top_container__ils_item_id, :ils_item_id),
-			     Sequel.as(:top_container__ils_holding_id, :ils_holding_id),
-			     Sequel.as(:repository__name, :repository),
+                             Sequel.as(:location_profile__name, :location_profile),
+                             Sequel.as(:location__barcode, :location_barcode),
+                             Sequel.as(:top_container__indicator, :top_container_indicator),
+                             Sequel.as(:top_container__barcode, :top_container_barcode),
+                             Sequel.as(:container_profile__name, :container_profile),
+                             Sequel.as(:top_container__id, :top_container_id),
+                             Sequel.as(:top_container__ils_item_id, :ils_item_id),
+                             Sequel.as(:top_container__ils_holding_id, :ils_holding_id),
+                             Sequel.as(:repository__name, :repository),
 
-			     Sequel.as(:resource__title, :resource_title),
-			     Sequel.as(:resource_via_ao__title, :resource_via_ao_title),
-			     Sequel.as(:accession__title, :accession_title),
+                             Sequel.as(:resource__title, :resource_title),
+                             Sequel.as(:resource_via_ao__title, :resource_via_ao_title),
+                             Sequel.as(:accession__title, :accession_title),
 
-			     Sequel.as(:resource__identifier, :resource_identifier),
-			     Sequel.as(:resource_via_ao__identifier, :resource_via_ao_identifier),
-			     Sequel.as(:accession__identifier, :accession_identifier),
-			    )
+                             Sequel.as(:resource__identifier, :resource_identifier),
+                             Sequel.as(:resource_via_ao__identifier, :resource_via_ao_identifier),
+                             Sequel.as(:accession__identifier, :accession_identifier),
+    )
 
     dataset
-end
+  end
 
   def building_query
     db[:location].filter(:location__building => building)
@@ -133,10 +133,10 @@ end
     repo_id = JSONModel.parse_reference(repository_uri)[:id]
 
     location_ids = db[:location]
-		   .join(:top_container_housed_at_rlshp, :location_id => :location__id)
-		   .join(:top_container, :top_container__id => :top_container_housed_at_rlshp__top_container_id)
-		   .filter(:top_container__repo_id => repo_id)
-		   .select(:location__id)
+                     .join(:top_container_housed_at_rlshp, :location_id => :location__id)
+                     .join(:top_container, :top_container__id => :top_container_housed_at_rlshp__top_container_id)
+                     .filter(:top_container__repo_id => repo_id)
+                     .select(:location__id)
 
     ds = db[:location].filter(:location__id => location_ids)
 
@@ -159,9 +159,9 @@ end
     [1, 2, 3].each do |coordinate|
       label = "coordinate_#{coordinate}_label"
       if !start_location[label].nil? && start_location[label] == end_location[label]
-	properties_to_compare << "coordinate_#{coordinate}_indicator".intern
+        properties_to_compare << "coordinate_#{coordinate}_indicator".intern
       else
-	break
+        break
       end
     end
 
@@ -172,22 +172,22 @@ end
 
       if start_location[property] && end_location[property]
 
-	if start_location[property] == end_location[property]
-	  # If both locations have the same value for this property, we'll skip it for the purposes of our range calculation
-	  matching_properties << property
-	else
-	  # But if they have different values, that's what we'll use for the basis of our range
-	  determinant_property = property
-	  break
-	end
+        if start_location[property] == end_location[property]
+          # If both locations have the same value for this property, we'll skip it for the purposes of our range calculation
+          matching_properties << property
+        else
+          # But if they have different values, that's what we'll use for the basis of our range
+          determinant_property = property
+          break
+        end
 
       elsif !start_location[property] && !end_location[property]
-	# If neither location has a value for this property, skip it
-	next
+        # If neither location has a value for this property, skip it
+        next
 
       else
-	# If we hit a property that only one location has a value for, we can't use it for a range calculation
-	break
+        # If we hit a property that only one location has a value for, we can't use it for a range calculation
+        break
       end
 
     end
@@ -206,8 +206,8 @@ end
     if determinant_property
       range_start, range_end = [start_location[determinant_property], end_location[determinant_property]].sort
       dataset = dataset
-		.filter("#{determinant_property} >= ?", range_start)
-		.filter("#{determinant_property} <= ?", range_end)
+                  .filter("#{determinant_property} >= ?", range_start)
+                  .filter("#{determinant_property} <= ?", range_end)
     end
 
     dataset
@@ -217,48 +217,48 @@ end
     collection_identifier_fields = [:resource_identifier, :resource_via_ao_identifier, :accession_identifier]
     collection_title_fields = [:resource_title, :resource_via_ao_title, :accession_title]
 
-      dataset = query
+    dataset = query
 
-      current_entry = nil
-      enum = dataset.to_enum
+    current_entry = nil
+    enum = dataset.to_enum
 
-      while true
-        row = next_row(enum)
+    while true
+      row = next_row(enum)
 
-	if row && current_entry && current_entry[:_top_container_id] == row[:top_container_id]
-	  # This row can be combined with the previous entry
-	  collection_identifier_fields.each do |field|
-	    current_entry['resource_or_accession_id'] << row[field]
-	  end
+      if row && current_entry && current_entry[:_top_container_id] == row[:top_container_id]
+        # This row can be combined with the previous entry
+        collection_identifier_fields.each do |field|
+          current_entry['resource_or_accession_id'] << row[field]
+        end
 
-	  collection_title_fields.each do |field|
-	    current_entry['resource_or_accession_title'] << row[field]
-	  end
-	else
-	  if current_entry
-	    # Yield the old value
-	    current_entry.delete(:_top_container_id)
-	    current_entry['resource_or_accession_id'] = current_entry['resource_or_accession_id'].compact.uniq.map {|s| format_identifier(s)}.join('; ')
-	    current_entry['resource_or_accession_title'] = current_entry['resource_or_accession_title'].compact.uniq.join('; ')
-	    yield current_entry
-	  end
+        collection_title_fields.each do |field|
+          current_entry['resource_or_accession_title'] << row[field]
+        end
+      else
+        if current_entry
+          # Yield the old value
+          current_entry.delete(:_top_container_id)
+          current_entry['resource_or_accession_id'] = current_entry['resource_or_accession_id'].compact.uniq.map {|s| format_identifier(s)}.join('; ')
+          current_entry['resource_or_accession_title'] = current_entry['resource_or_accession_title'].compact.uniq.join('; ')
+          yield current_entry
+        end
 
-          # If we hit the end of our rows, we're all done
-          break unless row
+        # If we hit the end of our rows, we're all done
+        break unless row
 
-	  # Otherwise, start a new entry for the next row
-          current_entry = Hash[headers.map { |h|
-				 val = (processor.has_key?(h)) ? processor[h].call(row) : row[h.intern]
-				 [h, val]
-			       }]
+        # Otherwise, start a new entry for the next row
+        current_entry = Hash[headers.map { |h|
+          val = (processor.has_key?(h)) ? processor[h].call(row) : row[h.intern]
+          [h, val]
+        }]
 
-	  current_entry['resource_or_accession_id'] = collection_identifier_fields.map {|field| row[field]}
-	  current_entry['resource_or_accession_title'] = collection_title_fields.map {|field| row[field]}
+        current_entry['resource_or_accession_id'] = collection_identifier_fields.map {|field| row[field]}
+        current_entry['resource_or_accession_title'] = collection_title_fields.map {|field| row[field]}
 
-	  # Use the top container ID to combine adjacent rows
-	  current_entry[:_top_container_id] = row[:top_container_id]
-	end
+        # Use the top container ID to combine adjacent rows
+        current_entry[:_top_container_id] = row[:top_container_id]
       end
+    end
   end
 
   private
@@ -286,8 +286,8 @@ end
 
     [1, 2, 3].each do |coordinate|
       if row["coordinate_#{coordinate}_label".intern]
-	fields << ("%s: %s" % [row["coordinate_#{coordinate}_label".intern],
-			       row["coordinate_#{coordinate}_indicator".intern]])
+        fields << ("%s: %s" % [row["coordinate_#{coordinate}_label".intern],
+                               row["coordinate_#{coordinate}_indicator".intern]])
       end
     end
 
@@ -295,7 +295,7 @@ end
   end
 
   def location_url(row)
-      JSONModel(:location).uri_for(row[:location_id])
+    JSONModel(:location).uri_for(row[:location_id])
   end
 
 end
