f985f1d4862a14a466c813646acd0f0d64b60017
Payten Giles <payten.giles@gmail.com>
2017-03-14 13:46:20 +1100

    Remove Net::ReadTimeout retries

--- selenium/common.rb
+++ selenium/common.rb
@@ -129,9 +129,3 @@ module SeleniumTest
     end
   end
 end
-
-RSpec.configure do |config|
-  config.verbose_retry = true
-  config.default_retry_count = 2
-  config.exceptions_to_retry = [Net::ReadTimeout]
-end
