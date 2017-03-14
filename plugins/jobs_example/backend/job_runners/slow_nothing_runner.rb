9b245c6ea38a7bcab900f190139356547adef920
jambun <jambun@gmail.com>
2017-03-14 16:13:34 +1100

    AR-1619 let the nothing job do something

--- plugins/jobs_example/backend/job_runners/slow_nothing_runner.rb
+++ plugins/jobs_example/backend/job_runners/slow_nothing_runner.rb
@@ -1,3 +1,6 @@
+require 'java'
+import java.lang.management.ManagementFactory
+
 class SlowNothingRunner < JobRunner
 
   register_for_job_type('slow_nothing_job', :run_concurrently => true)
@@ -8,7 +11,14 @@ class SlowNothingRunner < JobRunner
         log("Oh gee, I've been canceled")
         break
       end
-      log("I did something!!! #{i+1}")
+
+      memory = ManagementFactory.memory_mx_bean
+      log("#{i+1} of #{@json.job['times']}")
+      log(Time.now)
+      log("Heap:            #{memory.heap_memory_usage}")
+      log("Non-heap:        #{memory.non_heap_memory_usage}")
+      log("Finalize count:  #{memory.object_pending_finalization_count}")
+      log("===")
       sleep 10
     end
     log("Phew, I'm done doing nothing!")
