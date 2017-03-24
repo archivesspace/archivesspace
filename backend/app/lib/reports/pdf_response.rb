1b674c5fe4920d33e7c5cfef22cd77dbc1f7556e
Mark Triggs <mark@dishevelled.net>
2017-03-25 07:36:21 +1100

    Strip invalid XML characters before sending to iText

--- backend/app/lib/reports/pdf_response.rb
+++ backend/app/lib/reports/pdf_response.rb
@@ -14,15 +14,15 @@ class PDFResponse
 
     def each
       while true
-	chunk = @fh.read(4096)
+        chunk = @fh.read(4096)
 
-	if chunk
-	  yield chunk
-	else
-	  break
-	  @fh.close
-	  File.unlink(@path)
-	end
+        if chunk
+          yield chunk
+        else
+          break
+          @fh.close
+          File.unlink(@path)
+        end
       end
     end
   end
@@ -33,13 +33,26 @@ class PDFResponse
     @base_url = params[:base_url] || "/"
   end
 
+  CONTROL_CHARS = (0...32).map(&:chr)
+  VALID_CONTROL_CHARS = [0x9, 0xA, 0xD].map(&:chr)
+
+  def clean_invalid_xml_chars(s)
+    s.gsub!(/[#{CONTROL_CHARS.join("")}]/) do |ch|
+      if VALID_CONTROL_CHARS.include?(ch)
+        ch
+      else
+        ""
+      end
+    end
+  end
+
   def generate
     # PDFs can be large, so return a file handle instead of loading everything into memory
     output_pdf_file = java.io.File.createTempFile("pdf_response", "pdf")
     output_stream = java.io.FileOutputStream.new(output_pdf_file)
 
     html_file = ASUtils.tempfile("pdf_response_html")
-    html_file.write(@html_report)
+    html_file.write(clean_invalid_xml_chars(@html_report))
     html_file.close
 
     # No longer needed - can be GC'd
