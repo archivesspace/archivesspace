75347ba2eb48833ceda804a74ef9caba7f97eeae
jambun <jambun@gmail.com>
2017-05-17 09:08:55 +1000

    reintroduce instance cleaning

--- frontend/app/models/archival_record_children.rb
+++ frontend/app/models/archival_record_children.rb
@@ -11,4 +11,25 @@ class ArchivalRecordChildren < JSONModel(:archival_record_children)
     nil
   end
 
+
+  def self.clean(child)
+    super
+    clean_instances(child)
+  end
+
+
+  def self.clean_instances(child)
+    return if !child["instances"] || child["instances"].empty?
+
+    child["instances"] = [child["instances"].first]
+
+    if child["instances"][0]["sub_container"].reject{|k,v| v.blank?}.empty?
+      child["instances"][0].delete("sub_container")
+    end
+
+    if !child["instances"][0].has_key?("sub_container") && child["instances"][0]["instance_type"].blank?
+      child["instances"] = []
+    end
+  end
+
 end
