3a6de5364920624ebe10ae90b0e5f8de1486f6db
Payten Giles <payten.giles@gmail.com>
2017-05-18 12:13:35 +1000

    AR-1752 only show container inventory series facet if the series is published

--- common/schemas/top_container.rb
+++ common/schemas/top_container.rb
@@ -67,6 +67,7 @@
             "display_string" => {"type" => "string"},
             "identifier" => {"type" => "string"},
             "level_display_string" => {"type" => "string"},
+            "publish" => {"type" => "boolean"},
             "_resolved" => {
               "type" => "object",
               "readonly" => "true"
