2f663b0dd117ef8fb8d4f6316a2e06ca91bc8936
Mark Triggs <mark@dishevelled.net>
2017-06-08 13:24:58 +1000

    AR-1471 Switch PDF to use a download instead of an email

--- public-new/app/assets/javascripts/print.js
+++ public-new/app/assets/javascripts/print.js
@@ -1,24 +1,45 @@
-function setupPrint(modalId) {
-    $(".noscript").hide();
+$(function () {
+    /* The PDF controller sets a cookie containing a `token` nonce when the
+       download starts.  Poll until we see it. */
+    var whenDownloadBegins = function(token, callback) {
+        var attempt = 0;
+        var max_attempts = 60;
+        var delay = 1000;
+
+        var next = function () {
+            if (document.cookie.indexOf("pdf_generated_" + token) >= 0 || attempt >= max_attempts) {
+                callback();
+            } else {
+                setTimeout(next, delay);
+            }
+        }
+
+        setTimeout(next, delay);
+    };
 
     $('#print_button').on('click', function (e) {
-        e.preventDefault();
+        var self = $(this);
 
-        print_form();
+        var form = self.closest('form');
+        var base_token = form.find("input[name='base_token']").attr('value');
+
+        var token = (base_token + new Date().getTime());
+        form.find("input[name='token']").attr('value', token);
+
+        self.find('.print-label').hide();
+        self.find('.generating-label').show();
+        self.attr('disabled', 'disabled');
+
+        whenDownloadBegins(token, function () {
+            self.find('.print-label').show();
+            self.find('.generating-label').hide();
+            self.attr('disabled', null);
+        });
+
+        setTimeout(function () {
+            form.submit();
+        }, 0);
 
         return false;
     });
-}
-
-function print_form() {
-    var modal = $("#print_modal");
-    var submit = modal.find('.action-btn');
-
-    submit.text(modal.find('input[name=submit_text]').val());
-
-    submit.on('click', function () {
-        $('#print_modal').find('form').submit();
-    });
-
-    modal.modal('show');
-}
+});
