c9b76ee4a3aa11e2016a78dab7ad6091afa175e1
jambun <jambun@gmail.com>
2017-06-08 08:13:51 +0100

    AR-1484 fix affiliation by checking the correct relator value

--- public-new/app/models/agent_person.rb
+++ public-new/app/models/agent_person.rb
@@ -65,7 +65,7 @@ class AgentPerson < Record
     end
 
     md['affiliation'] = json['related_agents'].select{|ra|
-      ra['relator'] == ra['is_associative_with'] && ra['_resolved']['jsonmodel_type'] != json['jsonmodel_type']}.map do |ag|
+      ra['relator'] == 'is_associative_with' && ra['_resolved']['jsonmodel_type'] != json['jsonmodel_type']}.map do |ag|
       res = ag['_resolved']
       out = {}
       out['@id'] = res['display_name']['authority_id'] if res['display_name']['authority_id']
