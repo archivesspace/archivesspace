20df3a0e37955b1f0879271f5eeb349e9c5b9e97
Laney McGlohon <lmcglohon@gmail.com>
2017-06-16 07:47:40 -0400

    Change csv report to parse HTML until reports can be refactored Change json report to parse HTML until reports can be refactored Add reports test files

--- backend/spec/model_accession_deaccessions_list_report_spec.rb
+++ backend/spec/model_accession_deaccessions_list_report_spec.rb
@@ -13,7 +13,6 @@ describe AccessionDeaccessionsListReport do
                                 deacc_job,
                                 datab) }
   it 'returns the correct fields for the Accessions Acquired and Linked Deaccession Records report' do
-    puts "Laney #{report.inspect}"
     expect(report.query.first.keys.length).to eq(8)
     expect(report.query.first).to have_key(:accessionId)
     expect(report.query.first).to have_key(:repo_id)
@@ -23,12 +22,6 @@ describe AccessionDeaccessionsListReport do
     expect(report.query.first).to have_key(:extentNumber)
     expect(report.query.first).to have_key(:extentType)
     expect(report.query.first).to have_key(:containerSummary)
-    # expect(report.query.first).to have_key(:deaccessionId)
-    # expect(report.query.first).to have_key(:description)
-    # expect(report.query.first).to have_key(:notification)
-    # expect(report.query.first).to have_key(:deaccessionDate)
-    # expect(report.query.first).to have_key(:extentNumber)
-    # expect(report.query.first).to have_key(:extentType)
   end
   it 'has the correct template name' do
     expect(report.template).to eq('accession_deaccessions_list_report.erb')
