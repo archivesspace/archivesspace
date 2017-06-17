61bfbd461a820585b8b14695e813b71bc15c0a43
Steve Majewski <sdm7g@virginia.edu>
2017-06-17 10:57:10 -0400

    add prefix to query param, not scheme

--- plugins/lcnaf/frontend/models/opensearcher.rb
+++ plugins/lcnaf/frontend/models/opensearcher.rb
@@ -14,7 +14,7 @@ class OpenSearcher
 
   def initialize(base_url, scheme )
     @base_url = base_url
-    @scheme = "cs:" + scheme
+    @scheme = scheme
   end
 
 
@@ -37,6 +37,7 @@ class OpenSearcher
     lccns.each do |lccn|
       lccn.sub!( 'info:lc/authorities/subjects/', '')
       uri = URI("#{@scheme}/#{lccn}.marcxml.xml")
+      p uri
 
       HTTPRequest.new.get(uri) do |response|
         if response.code != '200'
@@ -66,7 +67,7 @@ class OpenSearcher
   def search(query, page, records_per_page)
     uri = URI(@base_url)
     start_record = calculate_start_record(page, records_per_page)
-    params = default_params.merge('q' => [query.to_s, @scheme],
+    params = default_params.merge('q' => [query.to_s, 'cs:' + @scheme],
                                   'count' => records_per_page,
                                   'start' => start_record)
 
