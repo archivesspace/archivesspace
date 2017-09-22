2bc04f860d82a03bf23475cd53bed9f970e77a18
Laney McGlohon <lmcglohon@gmail.com>
2017-09-22 13:07:06 -0400

    Revert "Use and prefer repo defined email for requests"

--- public/app/helpers/application_helper.rb
+++ public/app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 module ApplicationHelper
-  include PrefixHelper
-  include RequestsHelper
 
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
