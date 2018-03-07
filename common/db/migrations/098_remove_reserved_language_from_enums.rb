5c7c6684a3ca5099a47ba739c0dfc571f187084e
Lora Woodford <lorawoodford@gmail.com>
2018-03-07 13:27:55 -0500

    Update 098_remove_reserved_language_from_enums.rb
    Remove triggering full re-index.

--- common/db/migrations/098_remove_reserved_language_from_enums.rb
+++ common/db/migrations/098_remove_reserved_language_from_enums.rb
@@ -11,12 +11,6 @@ Sequel.migration do
     $stderr.puts("Deleting enumeration_id #{qaa.to_s}")
     self[:enumeration_value].where(qaa.all).delete
 
-    # trigger reindex of record types that may have this language_iso639_2 field populated
-    [:resource, :archival_object, :digital_object, :digital_object_component ].each do |klass|
-      $stderr.puts("Triggering reindex of #{klass.to_s}")
-      self[klass ].update(:system_mtime => Time.now)
-    end
-
   end
 
 end
