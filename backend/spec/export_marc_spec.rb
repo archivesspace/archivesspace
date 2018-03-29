f2483987ca783a62c7d6c52c9746489ea10030d8
Manny Rodriguez <manny@theavatar.org>
2018-03-29 15:04:13 -0600

    MARCXML Export: In 245 title tag, $f or $g date subfields should be prececeded by a comma (in $a)

--- backend/spec/export_marc_spec.rb
+++ backend/spec/export_marc_spec.rb
@@ -127,6 +127,10 @@ describe 'MARC Export' do
       end
     end
 
+    it "adds a comma after $a if a date is defined" do
+      expect(@marc.at("datafield[@tag='245']/subfield[@code='a']/text()").to_s[-1]).to eq(",")
+    end
+
 
     it "maps the first bulk date to subfield 'g'" do
       date = @dates.find{|d| d.date_type == 'bulk'}
@@ -338,7 +342,8 @@ describe 'MARC Export' do
     end
 
     it "should strip out the mixed content in title" do
-      @marc.should have_tag "datafield[@tag='245']/subfield[@code='a']" => "Foo  BAR  Jones"
+      @marc.should have_tag "datafield[@tag='245']/subfield[@code='a']"
+      expect(@marc.at("datafield[@tag='245']/subfield[@code='a']/text()").to_s).to match(/Foo  BAR  Jones/)
     end
   end
 
