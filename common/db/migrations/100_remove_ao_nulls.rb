0455c1cd94351adc44a6f57f16a1a3748ac35498
Lora Woodford <lorawoodford@gmail.com>
2018-04-05 11:22:01 -0400

    ANW-238 Migrate and disallow nulls in archival objects publish

---
+++ common/db/migrations/100_remove_ao_nulls.rb
@@ -0,0 +1,27 @@
+require_relative 'utils'
+
+Sequel.migration do
+
+  up do
+
+    # Find null values
+    nulls = self[:archival_object].where(:publish => nil)
+
+    # If null update to unpublished/false/0
+    if nulls
+      nulls.update(:publish => 0)
+    end
+
+    # Change the column to not allow null for all future archival_objects and update the default to unpublished/false/0
+    alter_table(:archival_object) do
+      set_column_default :publish, 0
+      set_column_not_null :publish
+    end
+
+    [:archival_object].each do |table|
+      self[table].update(:system_mtime => Time.now)
+    end
+
+  end
+
+end
