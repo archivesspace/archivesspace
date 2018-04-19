b2624cb5dfba4ee67cbd13c966b22175a049cf45
Manny Rodriguez <manny@theavatar.org>
2018-04-19 17:32:48 -0600

    simplifying ark_identifier table, model and unit tests for ark ids.

--- common/db/migrations/101_create_ark_identifier_table.rb
+++ common/db/migrations/101_create_ark_identifier_table.rb
@@ -8,16 +8,12 @@ Sequel.migration do
       Integer :resource_id
       Integer :accession_id
       Integer :digital_object_id
-      Integer :resource_type_id,  :null => false
-      Integer :repo_id,           :null => false
     end
 
     alter_table(:ark_identifier) do
       add_foreign_key([:resource_id], :resource, :key => :id)
       add_foreign_key([:accession_id], :accession, :key => :id)
       add_foreign_key([:digital_object_id], :digital_object, :key => :id)
-      add_foreign_key([:resource_type_id], :enumeration_value, :key => :id)
-      add_foreign_key([:repo_id], :repository, :key => :id)
     end
   end
 
@@ -26,8 +22,6 @@ Sequel.migration do
       drop_foreign_key([:resource_id])
       drop_foreign_key([:accession_id])
       drop_foreign_key([:digital_object_id])
-      drop_foreign_key([:resource_type_id])
-      drop_foreign_key([:repo_id])
     end 
 
     drop_table(:ark_identifier)
