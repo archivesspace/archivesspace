9ac3ec843b9d64ec77a9b60d467dbe35d48cc9d3
Manny Rodriguez <manny@theavatar.org>
2018-04-24 16:37:11 -0600

    database migration to populate ark_identifier table with records for existing resources, accessions, and digital objects

--- common/db/migrations/103_create_arks_for_resources.rb
+++ common/db/migrations/103_create_arks_for_resources.rb
@@ -1,10 +1,37 @@
 require_relative 'utils'
-require "ASModel"
 
+# populates the ark_identifier table with records for existing 
+# resources, accessions, and digital objects
 Sequel.migration do
   up do
-    Resource.any_repo.all.each do |r|
-      puts r[:id]
+    # resources
+    self[:resource].select(:id).each do |r|
+      self[:ark_identifier].insert(:resource_id      => r[:id],
+                                   :created_by       => 'admin',
+                                   :last_modified_by => 'admin',
+                                   :create_time      => Time.now,
+                                   :system_mtime     => Time.now,
+                                   :user_mtime       => Time.now)
+    end
+
+    # accessions
+    self[:accession].select(:id).each do |r|
+      self[:ark_identifier].insert(:accession_id      => r[:id],
+                                   :created_by       => 'admin',
+                                   :last_modified_by => 'admin',
+                                   :create_time      => Time.now,
+                                   :system_mtime     => Time.now,
+                                   :user_mtime       => Time.now)
+    end
+
+    # digital objects
+    self[:digital_object].select(:id).each do |r|
+      self[:ark_identifier].insert(:digital_object_id => r[:id],
+                                   :created_by        => 'admin',
+                                   :last_modified_by  => 'admin',
+                                   :create_time       => Time.now,
+                                   :system_mtime      => Time.now,
+                                   :user_mtime        => Time.now)
     end
   end
 end
