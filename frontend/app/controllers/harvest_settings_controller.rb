2428667dd88e8d0f239456a600f6186e4a9b68f9
Manny Rodriguez <manny@theavatar.org>
2018-05-22 17:57:11 -0600

    UI page for harvest settings loads correct option values from backend
    Option values for harvest setting toggles come from the hardcoded system
    enumeration "archival_record_level".
    
    Added a query by string to ASModel_crud to support pulling the correct
    values from the db through the backend to the frontend.

--- frontend/app/controllers/harvest_settings_controller.rb
+++ frontend/app/controllers/harvest_settings_controller.rb
@@ -2,9 +2,7 @@ class HarvestSettingsController < ApplicationController
   set_access_control  "manage_repository" => [:edit, :update]
 
   def edit
-    j = JSONModel(:enumeration_value).all
-    puts "++++++++++++++++++++++++++++++"
-    puts j.inspect
+    @enum = JSONModel(:enumeration).find("/names/archival_record_level")
   end
 
   def update
