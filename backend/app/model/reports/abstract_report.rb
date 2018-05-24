e6d5383d3e94de456f5f70d6dae08b4a226d100d
Sarah Morrissey <s.morrissey@gatech.edu>
2018-05-24 10:09:59 -0400

    worked on refactoring reports

--- backend/app/model/reports/abstract_report.rb
+++ backend/app/model/reports/abstract_report.rb
@@ -1,4 +1,5 @@
 require_relative 'report_manager'
+require 'erb'
 
 class AbstractReport
   include ReportManager::Mixin
@@ -7,24 +8,22 @@ class AbstractReport
   attr_accessor :format
   attr_accessor :params
   attr_accessor :db
-  attr_reader :job
+  attr_accessor :job
+  attr_accessor :info
+  attr_accessor :template
 
   def initialize(params, job, db)
-    # sanity check, please.
-    params = params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    @repo_id = params[:repo_id] if params.has_key?(:repo_id) && params[:repo_id] != ""
-    @format = params[:format] if params.has_key?(:format) && params[:format] != ""
+    @repo_id = params[:repo_id] if params.has_key?(:repo_id) && params[:repo_id] != ''
+    @format = params[:format] if params.has_key?(:format) && params[:format] != ''
     @params = params
-    @job = job
     @db = db
+    @job = job
+    @info = hash.new
+    @template = 'generic_listing.erb'
   end
 
   def title
     I18n.t("reports.#{code}.title", :default => code)
-  end
-
-  def new_subreport(subreport_model, params)
-    subreport_model.new(params.merge(:format => 'html'), job, db)
   end
 
   def get_binding
@@ -35,14 +34,6 @@ class AbstractReport
     self
   end
 
-  def headers
-    query.columns.map(&:to_s)
-  end
-
-  def template
-    'generic_listing.erb'
-  end
-
   def layout
     AppConfig[:report_page_layout]
   end
@@ -51,41 +42,49 @@ class AbstractReport
     "portrait"
   end
 
-  def processor
-    {}
-  end
-
   def current_user
     @job.owner
   end
 
   def query(db = @db)
-    raise "Please specify a query to return your reportable results"
+    raise 'Please specify a query to return your reportable results'
   end
 
-  def each(db = @db)
-    dataset = query
-    dataset.where(:repo_id => @repo_id) if @repo_id
-
-    dataset.each do |row|
-      yield(Hash[(headers + processor.keys).uniq.map { |h|
-        val = (processor.has_key?(h))?processor[h].call(row):row[h.intern]
-        [h, val]
-      }])
+  def generate(file)
+    if format == 'json'
+      generate_json(file)
+    elsif format == 'html'
+      generate_html(file)
+    else
+      generate_json(file)
     end
   end
 
-  def code
-    self.class.code
+  def generate_json(file)
+    json = ASUtils.to_json(query)
+    file.write(json)
+  end
+
+  def generate_html(file)
+    renderer = ERB.new(File.read(template))
+    file.write(renderer.result)
+  end
+
+  def format_sub_report(name, contents)
+    ''
+  end
+
+  def each
+    results = query
+    results.each
+  end
+
+  def identifier(record)
+    nil
   end
 
   def self.code
     self.name.gsub(/(.)([A-Z])/,'\1_\2').downcase
-  end
-
-  # Number of Records
-  def total_count
-    @total_count ||= self.query.count
   end
 
 end
