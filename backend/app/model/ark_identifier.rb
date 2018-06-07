c6d186076963c0a647193ad5ad08f3ebc6ebf510
Manny Rodriguez <manny@theavatar.org>
2018-06-07 15:22:36 -0600

    Move external ARK URL from ark_identifiers table to entity tables
    Added migration to add columns to add external_ark_url columns to
    accession, digital_object, and resource tables.
    
    Removed ARKIdentifier.external_id column.
    
    Updated UI to allow viewing and editing of new external_ark_url columns
    in frontend.
    
    Backend ARK controller redirects to external URL as defined in entity
    table if appropriate.
    
    Fixed and updated tests where appropriate.

--- backend/app/model/ark_identifier.rb
+++ backend/app/model/ark_identifier.rb
@@ -30,51 +30,32 @@ class ARKIdentifier < Sequel::Model(:ark_identifier)
   end
 
   def self.create_from_resource(resource)
-    # check if we have an external ID we need to handle
-    # self.identifier is a String representation of an array, like:
-    # "[\"https://n2t.net/ark:/00001/f1mw5e\",null,null,null]"
-    # We need to remove superflous charaters so we can turn it into an actual array.
-
-    id_ary = resource.identifier.gsub('[', "").gsub(']', "").gsub('"', '').split(",")
-    id = nil
-    id_ary.each {|i| id = i if i =~ /ark:\//}
-
     self.insert(:resource_id      => resource.id,
                 :created_by       => 'admin',
                 :last_modified_by => 'admin',
                 :create_time      => Time.now,
                 :system_mtime     => Time.now,
                 :user_mtime       => Time.now,
-                :external_id      => id,
                 :lock_version     => 0)
   end
 
   def self.create_from_accession(accession)
-    # same deal as above
-    id_ary = accession.identifier.gsub('[', "").gsub(']', "").gsub('"', '').split(",")
-    id = nil
-    id_ary.each {|i| id = i if i =~ /ark:\//}
-
     self.insert(:accession_id     => accession.id,
                 :created_by       => 'admin',
                 :last_modified_by => 'admin',
                 :create_time      => Time.now,
                 :system_mtime     => Time.now,
                 :user_mtime       => Time.now,
-                :external_id      => id,
                 :lock_version     => 0)
   end
 
   def self.create_from_digital_object(digital_object)
-    id = digital_object.digital_object_id =~ /ark:\// ? digital_object.digital_object_id : nil
-
     self.insert(:digital_object_id => digital_object.id,
                 :created_by        => 'admin',
                 :last_modified_by  => 'admin',
                 :create_time       => Time.now,
                 :system_mtime      => Time.now,
                 :user_mtime        => Time.now,
-                :external_id      => id,
                 :lock_version      => 0)
   end
 end
