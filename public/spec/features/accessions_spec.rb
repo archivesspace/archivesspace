e3cb4e2167674cc0bbe34e9f67fed4fa04a69e05
Laney McGlohon <lmcglohon@gmail.com>
2018-07-24 11:24:34 -0700

    Fix broken PUI tests

--- public/spec/features/accessions_spec.rb
+++ public/spec/features/accessions_spec.rb
@@ -8,7 +8,7 @@ describe 'Accessions', js: true do
     expect(current_path).to eq ('/accessions')
     finished_all_ajax_requests?
     within all('.col-sm-12')[0] do
-      expect(page).to have_content("Showing Unprocessed Materials: 1 - 3 of 3")
+      expect(page).to have_content("Showing Unprocessed Materials: 1 - 4 of 4")
     end
     within all('.col-sm-12')[1] do
       expect(page.all("a[class='record-title']", text: 'Published Accession').length).to eq 2
