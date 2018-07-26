e7af8ce5674f01c92ef7f71af9f2b2e1f298139a
Sarah Morrissey <s.morrissey@gatech.edu>
2018-07-26 12:40:50 -0400

    removed useless name option

--- reports/custom/date_subreport.rb
+++ reports/custom/date_subreport.rb
@@ -2,8 +2,7 @@ class DateSubreport < AbstractSubreport
 
 	register_subreport('date', ['accession', 'deaccession',
 		'archival_object', 'resource', 'event', 'digital_object',
-		'digital_object_component', 'agent',
-		'name_software'])
+		'digital_object_component', 'agent'])
 
 	def initialize(parent_custom_report, id)
 		super(parent_custom_report)
