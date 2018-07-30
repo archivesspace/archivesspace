98c72295c38093a4b4a4ef69cc32b36e6670d86b
Manny Rodriguez <manny@theavatar.org>
2018-07-30 16:08:30 -0600

    model validations & tests

--- backend/app/model/oai_config.rb
+++ backend/app/model/oai_config.rb
@@ -14,14 +14,26 @@ class OAIConfig < Sequel::Model(:oai_config)
   	validates_presence :oai_admin_email
   	validates_presence :oai_repository_name
 
-  	# TODO: check for valid email in oai_admin_email
+    validate_oai_admin_email_is_email
   end
 
   def validate_single_record
   	record_count = OAIConfig.all.count
-  	unless record_count == 0 || record_count == 1
-      errors.add(:base, 'Cannot have more than one record in oai_config table.') 
+
+  	unless record_count == 0
+      # if we have an existing record, we'd better be updating that one record
+      first_record = OAIConfig.first
+
+      if self.id.nil? || first_record.id != self.id
+        errors.add(:base, 'Cannot have more than one record in oai_config table.') 
+      end
   	end
+  end
+
+  def validate_oai_admin_email_is_email
+    unless self.oai_admin_email =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+      errors.add(:oai_admin_email, 'must be a valid email address.') 
+    end
   end
 
 end
