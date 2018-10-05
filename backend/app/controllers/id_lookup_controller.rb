ce874b10bf1cb096d7f405b97db5585b3d8eab46
Lora Woodford <lorawoodford@gmail.com>
2018-10-05 14:58:04 -0400

    Refactor idlookup model and improve endpoint documentation

--- backend/app/controllers/id_lookup_controller.rb
+++ backend/app/controllers/id_lookup_controller.rb
@@ -3,7 +3,7 @@ class ArchivesSpaceService < Sinatra::Base
   Endpoint.get('/repositories/:repo_id/find_by_id/resources')
     .description("Find Resources by their identifiers")
     .params(["repo_id", :repo_id],
-            ["identifier", [String], "A 4-part identifier expressed as JSON array (of up to 4 strings)", :optional => true],
+            ["identifier", [String], "A 4-part identifier expressed as a JSON array (of up to 4 strings) comprised of the id_0 to id_3 fields (though empty fields will be handled if not provided)", :optional => true],
             ["resolve", :resolve])
     .permissions([:view_repository])
     .returns([200, "JSON array of refs"]) \
@@ -15,8 +15,8 @@ class ArchivesSpaceService < Sinatra::Base
   Endpoint.get('/repositories/:repo_id/find_by_id/archival_objects')
     .description("Find Archival Objects by ref_id or component_id")
     .params(["repo_id", :repo_id],
-            ["ref_id", [String], "A set of record Ref IDs", :optional => true],
-            ["component_id", [String], "A set of record component IDs", :optional => true],
+            ["ref_id", [String], "An archival object's Ref ID (param may be repeated)", :optional => true],
+            ["component_id", [String], "An archival object's component ID (param may be repeated)", :optional => true],
             ["resolve", :resolve])
     .permissions([:view_repository])
     .returns([200, "JSON array of refs"]) \
@@ -25,11 +25,10 @@ class ArchivesSpaceService < Sinatra::Base
     json_response(resolve_references({'archival_objects' => refs}, params[:resolve]))
   end
 
-
   Endpoint.get('/repositories/:repo_id/find_by_id/digital_object_components')
     .description("Find Digital Object Components by component_id")
     .params(["repo_id", :repo_id],
-            ["component_id", [String], "A set of record component IDs", :optional => true],
+            ["component_id", [String], "A digital object component's component ID (param may be repeated)", :optional => true],
             ["resolve", :resolve])
     .permissions([:view_repository])
     .returns([200, "JSON array of refs"]) \
@@ -41,7 +40,7 @@ class ArchivesSpaceService < Sinatra::Base
   Endpoint.get('/repositories/:repo_id/find_by_id/digital_objects')
     .description("Find Digital Objects by digital_object_id")
     .params(["repo_id", :repo_id],
-            ["digital_object_id", [String], "A set of digital object IDs", :optional => true],
+            ["digital_object_id", [String], "A digital object's digital object ID (param may be repeated)", :optional => true],
             ["resolve", :resolve])
     .permissions([:view_repository])
     .returns([200, "JSON array of refs"]) \
--- backend/app/model/idlookup.rb
+++ backend/app/model/idlookup.rb
@@ -4,23 +4,20 @@ class IDLookup
     filters = {:repo_id => params[:repo_id]}
 
     params.each do |key, values|
-      unless key == :repo_id
+      unless key == :repo_id || key == :resolve
         # The identifier for a resource needs to be massaged to match the db
         if !Array(values).empty? && key == :identifier
-            identifiers = Array(values).map {|identifier|
-              parsed = ASUtils.json_parse(identifier)
-              if parsed.is_a?(Array) && parsed.length > 0 && parsed.length <= 4
-                padded = (parsed + ([nil] * 3)).take(4)
-                ASUtils.to_json(padded)
-              end
-            }.compact
-            filters[key] = Array(identifiers)
-        # For cases other than resources
-        elsif !Array(values).empty?
           identifiers = Array(values).map {|identifier|
-            ASUtils.json_parse(identifier)
+            parsed = ASUtils.json_parse(identifier)
+            if parsed.is_a?(Array) && parsed.length > 0 && parsed.length <= 4
+              padded = (parsed + ([nil] * 3)).take(4)
+              ASUtils.to_json(padded)
+            end
           }.compact
           filters[key] = Array(identifiers)
+        # For cases other than resources
+        elsif !Array(values).empty?
+          filters[key] = Array(values)
         end
       end
     end
