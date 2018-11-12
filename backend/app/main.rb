42da4ac6d9828671ffc8411e59cd12d1d936c2e5
Mark Triggs <mark@dishevelled.net>
2018-11-13 08:58:16 +1100

    Log a warning if a configured plugin can't be found
    Current behavior silently ignores this case, and makes config typos
    harder to spot than they should be.
    
    (cherry picked from commit b73aaed0fc319109094c3e31d8f0d2be70e7d275)

--- backend/app/main.rb
+++ backend/app/main.rb
@@ -203,6 +203,13 @@ class ArchivesSpaceService < Sinatra::Base
         @archivesspace_loaded = true
 
 
+        # Warn if any referenced plugins aren't present
+        ASUtils.find_local_directories.each do |plugin_dir|
+          unless Dir.exist?(plugin_dir)
+            Log.warn("Plugin referenced in AppConfig[:plugins] could not be found: #{File.absolute_path(plugin_dir)}")
+          end
+        end
+
         # Load plugin init.rb files (if present)
         ASUtils.find_local_directories('backend').each do |dir|
           init_file = File.join(dir, "plugin_init.rb")
