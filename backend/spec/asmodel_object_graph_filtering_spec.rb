829e0cfe9ad5421ee7a6436a8009686304a44762
James Bullen <jambun@gmail.com>
2018-11-27 14:04:46 +1100

    More tests for object graph filtering

--- backend/spec/asmodel_object_graph_filtering_spec.rb
+++ backend/spec/asmodel_object_graph_filtering_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe 'Object graph filtering' do
 
   let!(:resource) {
-    create(:json_resource, :publish => true)
+    create(:json_resource,
+           :publish => true,
+           :extents => [{"portion" => "whole", "number" => "BILLIONS", "extent_type" => "reels"}])
   }
 
   let!(:series_1) {
-    create(:json_archival_object, :resource => {:ref => resource.uri}, :publish => true, :title => "Series 1")
+    create(:json_archival_object, :resource => {:ref => resource.uri}, :publish => false, :title => "Series 1")
   }
 
   let!(:series_2) {
@@ -19,15 +21,16 @@ describe 'Object graph filtering' do
     create(:json_archival_object,
            :resource => {:ref => resource.uri},
            :parent => {:ref => series_1.uri},
-           :publish => true,
-           :title => "Series 1 Child 1")
+           :publish => false,
+           :title => "Series 1 Child 1",
+           :extents => [{"portion" => "whole", "number" => "ONE", "extent_type" => "reels"}])
   }
 
   let!(:series_1_child_2) {
     create(:json_archival_object,
            :resource => {:ref => resource.uri},
            :parent => {:ref => series_1.uri},
-           :publish => true,
+           :publish => false,
            :title => "Series 1 Child 2")
   }
 
@@ -48,15 +51,45 @@ describe 'Object graph filtering' do
            :title => "Series 2 Child 2")
   }
 
-  it "supports excluding items from the graph" do
-
+  it "supports excluding items" do
     filters = {}
     filters[ArchivalObject] = ArchivalObject.exclude(:ref_id => 'EXCLUDE')
 
     og = Resource[resource.id].object_graph(filters)
 
-    og.ids_for(ArchivalObject).should eq ([1,2,3,4,5])
-
+    og.ids_for(ArchivalObject).length.should eq (5)
   end
 
+
+  it "supports excluding items based on multiple criteria" do
+    filters = {}
+    filters[ArchivalObject] = ArchivalObject.exclude(:ref_id => 'EXCLUDE')
+                                            .filter(:publish => 1)
+
+    og = Resource[resource.id].object_graph(filters)
+
+    og.ids_for(ArchivalObject).length.should eq (2)
+  end
+
+
+  it "supports excluding nested records" do
+    filters = {}
+    filters[Extent] = Extent.exclude(:number => 'BILLIONS')
+
+    og = Resource[resource.id].object_graph(filters)
+
+    og.ids_for(Extent).length.should eq (1)
+  end
+
+
+  it "supports multiple model filters" do
+    filters = {}
+    filters[Extent] = Extent.filter(:number => 'ONE')
+    filters[ArchivalObject] = ArchivalObject.filter(:publish => 0)
+
+    og = Resource[resource.id].object_graph(filters)
+
+    og.ids_for(ArchivalObject).length.should eq (3)
+    og.ids_for(Extent).length.should eq (1)
+  end
 end
