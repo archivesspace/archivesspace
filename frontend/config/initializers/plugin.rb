46f9e9b677af9f61ff13d88b228f535d88c7868d
Payten Giles <payten.giles@gmail.com>
2018-11-28 14:49:12 +1100

    Introduce AbstractPluginSection (and examples) to allow arbitrary content to be appended to record's form and readonly views

--- frontend/config/initializers/plugin.rb
+++ frontend/config/initializers/plugin.rb
@@ -29,6 +29,8 @@ module Plugins
     if @config[:repository_menu_items].length > 0
       puts "Found repository menu items for plug-ins: #{repository_menu_items.inspect}"
     end
+
+    @sections = []
   end
 
 
@@ -66,6 +68,136 @@ module Plugins
     @config[:parents][parent] || []
   end
 
+
+  def self.register_plugin_section(plugin_section)
+    @sections << plugin_section
+  end
+
+  def self.sections_for(record, mode)
+    @sections.select{|plugin_section| plugin_section.supports?(record, mode)}
+  end
+
+
+  class AbstractPluginSection
+    def initialize(plugin, name, jsonmodel_types, opts = {})
+      @plugin = plugin
+      @name = name
+      @jsonmodel_types = ASUtils.wrap(jsonmodel_types)
+
+      parse_opts(opts)
+    end
+
+    def render_edit(view_context, record, form_context)
+      raise "IMPLEMENT ME"
+    end
+
+    def render_readonly(view_context, record, form_context)
+      raise "IMPLEMENT ME"
+    end
+
+    def render_sidebar(view_context, record, mode)
+      "<li>" +
+        "  <a href='##{build_section_id(record['jsonmodel_type'])}'>" +
+        "    #{@sidebar_label}" +
+        "    <span class='glyphicon glyphicon-chevron-right'></span>" +
+        "  </a>" +
+        "</li>".html_safe
+    end
+
+    def supports?(record, mode)
+      @jsonmodel_types.include?(record['jsonmodel_type']) &&
+        (mode == :edit && @show_on_edit ||
+          mode == :readonly && @show_on_readonly)
+    end
+
+    private
+
+    def parse_opts(opts)
+      @show_on_edit = opts.fetch(:show_on_edit, true)
+      @show_on_readonly = opts.fetch(:show_on_readonly, true)
+      @section_id = opts.fetch(:section_id, nil)
+      @sidebar_label = opts.fetch(:sidebar_label, I18n.t("plugins.#{@plugin}.#{@name}.section"))
+    end
+
+    def build_section_id(jsonmodel_type)
+      @section_id ? @section_id : "#{jsonmodel_type}_#{@name}"
+    end
+  end
+
+
+  class PluginSubRecord < AbstractPluginSection
+
+    def render_edit(view_context, record, form_context)
+      view_context.render_aspace_partial(
+        :partial => "shared/subrecord_form",
+        :locals => {
+          :form => form_context,
+          :name => @jsonmodel_field,
+          :cardinality => @cardinality,
+          :template => @template_name,
+          :template_erb => @erb_edit_template_path,
+          :js_template_name => @js_edit_template_name,
+          :section_id => build_section_id(form_context.obj['jsonmodel_type']),
+        })
+    end
+
+    def render_readonly(view_context, record, form_context)
+      view_context.render_aspace_partial(
+        :partial => @erb_readonly_template_path,
+        :locals => { @jsonmodel_field.intern => record.send(@jsonmodel_field.intern),
+                     :context => form_context,
+                     :section_id => build_section_id(form_context.obj['jsonmodel_type']) })
+    end
+
+    def supports?(record, mode)
+      result = super
+      if result && mode == :readonly
+        Array(record.send(@jsonmodel_field.intern)).length > 0
+      else
+        result
+      end
+    end
+
+    private
+
+    def parse_opts(opts)
+      super
+
+      @jsonmodel_field = opts.fetch(:jsonmodel_field, @name)
+      @cardinality = opts.fetch(:cardinality, :zero_to_many)
+      @template_name = opts.fetch(:template_name, @name)
+      @erb_edit_template_path = opts.fetch(:erb_edit_template_path, "#{@name}/template")
+      @js_edit_template_name = opts.fetch(:js_edit_template_name, "template_#{@name}")
+      @erb_readonly_template_path = opts.fetch(:erb_readonly_template_path, "#{@name}/show_as_subrecords")
+    end
+  end
+
+
+  class PluginReadonlySearch < AbstractPluginSection
+
+    def render_readonly(view_context, record, form_context)
+      view_context.render_aspace_partial(
+        :partial => "search/embedded",
+        :locals => {
+          :record => record,
+          :filter_term => @filter_term_proc.call(record),
+          :heading_text => @heading_text,
+          :section_id => @section_id ? @section_id : build_section_id(form_context.obj['jsonmodel_type']),
+        }
+      )
+    end
+
+    private
+
+    def parse_opts(opts)
+      super
+
+      @show_on_edit = false
+      @filter_term_proc = opts.fetch(:filter_term_proc)
+      @heading_text = opts.fetch(:heading_text)
+    end
+
+  end
 end
 
 Plugins::init
