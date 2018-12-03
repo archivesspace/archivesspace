e2ca25c7ceb6c134fc652d141117e4ecf486b61f
Laney McGlohon <lmcglohon@gmail.com>
2018-12-03 09:54:17 -0800

    Update aspace_form_helper.rb
    Remove extra space

--- frontend/app/helpers/aspace_form_helper.rb
+++ frontend/app/helpers/aspace_form_helper.rb
@@ -426,7 +426,7 @@ module AspaceFormHelper
       field_html =  textfield("id_0", obj["id_0"], :class => "id_0 form-control", :size => 10)
       field_html << textfield("id_1", obj["id_1"], :class => "id_1 form-control", :size => 10, :disabled => obj["id_0"].blank? && obj["id_1"].blank?, :'aria-label' => "id_1")
       field_html << textfield("id_2", obj["id_2"], :class => "id_2 form-control", :size => 10, :disabled => obj["id_1"].blank? && obj["id_2"].blank?, :'aria-label' => "id_2")
-      field_html << textfield("id_3", obj["id_3"], :class => "id_3 form-control", :size => 10, :disabled => obj["id_2"].blank? && obj["id_3"].blank?,  :'aria-label' => "id_3")
+      field_html << textfield("id_3", obj["id_3"], :class => "id_3 form-control", :size => 10, :disabled => obj["id_2"].blank? && obj["id_3"].blank?, :'aria-label' => "id_3")
       @forms.content_tag(:div, (I18n.t(i18n_for("id_0")) + field_html).html_safe, :class=> "identifier-fields")
       label_with_field("id_0", field_html, :control_class => "identifier-fields")
     end
