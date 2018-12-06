f2c98ef39f4c093e77895f7be004950d996b9edf
Manny Rodriguez <manny@theavatar.org>
2018-12-06 16:38:50 -0700

    migration for generating a slug for repositories
    This is needed to avoid a situation where a repo_scoped slugged URLs are enabled,
    but there is a repo without a slug defined

---
+++ common/db/migrations/118_create_slugs_for_repositories.rb
@@ -0,0 +1,20 @@
+require_relative 'utils'
+
+Sequel.migration do
+  up do
+    self[:repository].all.each do |r|
+
+    	# repo_codes are already unique, so no need to de-dupe.
+
+    	# remove URL characters from slug	
+    	slug = r[:repo_code].gsub(" ", "_").gsub(/[&;?$<>#%{}|\\^~\[\]`\/@=:+,!.]/, "")
+
+    	if slug.match(/^(\d)+$/) 
+      	slug = slug.prepend("_")
+    	end									
+
+      self[:repository].where(:id => r[:id]).update(:slug => slug)
+    end
+  end
+end
+
