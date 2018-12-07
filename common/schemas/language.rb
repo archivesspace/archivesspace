61dc3f4d7cde1cd0493d888999c8fb4b3ef32e75
Lora Woodford <lorawoodford@gmail.com>
2018-12-07 15:30:48 -0500

    Shows, templates, and schemas complete

--- common/schemas/language.rb
+++ common/schemas/language.rb
@@ -5,9 +5,9 @@
     "type" => "object",
 
     "properties" => {
-      "language" => {"type" => "string", "dynamic_enum" => "language_iso639_2"},
+      "language" => {"type" => "string", "dynamic_enum" => "language_iso639_2", "ifmissing" => "error"},
       "script" => {"type" => "string", "dynamic_enum" => "language_script"},
-      "note" => {"type" => "string", "minLength" => 1},
+      "note" => {"type" => "string", "minLength" => 1, "maxLength" => 65000, "required" => false},
     },
   },
 }
