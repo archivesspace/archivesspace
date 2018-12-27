ea8495f70bb833914204a59d5ce992a11336d1a7
Manny Rodriguez <manny@theavatar.org>
2018-12-27 18:42:02 -0500

    working unit tests for search box and selected icons

--- frontend/spec/selenium/spec/resources_spec.rb
+++ frontend/spec/selenium/spec/resources_spec.rb
@@ -29,12 +29,239 @@ describe "Resources Form" do
   end
 
 
-  it "displays icons for the different subject types" do
-    # click on Add Subject button
-    @driver.find_element(:css, "#resource_subjects_ button").click
-    # select input box and type "a" to bring up a list of subjects
-    @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "Test")
-  	# sleep 60
+  describe "search dropdown" do
+    it "displays correct icon for cultural_context term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  cultural_context")
+  
+      @driver.find_element(:css, ".subject_type_cultural_context")
+    end
+  
+    it "displays correct icon for function term_type in search dropdown"   do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  function")
+  
+      @driver.find_element(:css, ".subject_type_function")
+    end
+  
+    it "displays correct icon for genre_form term_type in search dropdown  " do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  genre_form")
+  
+      @driver.find_element(:css, ".subject_type_genre_form")
+    end
+  
+    it "displays correct icon for technique term_type in search dropdown  " do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "technique")
+
+      @driver.find_element(:css, ".subject_type_technique")
+    end
+  
+    it "displays correct icon for occupation term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "occupation")
+  
+      @driver.find_element(:css, ".subject_type_occupation")
+    end
+  
+    it "displays correct icon for style_period term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "style_period")
+  
+      @driver.find_element(:css, ".subject_type_style_period")
+    end
+  
+    it "displays correct icon for technique term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "technique")
+  
+      @driver.find_element(:css, ".subject_type_technique")
+    end
+  
+    it "displays correct icon for temporal term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "temporal")
+  
+      @driver.find_element(:css, ".subject_type_temporal")
+    end
+  
+    it "displays correct icon for topical term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "topical")
+  
+      @driver.find_element(:css, ".subject_type_topical")
+    end
+  
+    it "displays correct icon for uniform_title term_type in search dropdown" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "uniform_title")
+  
+      @driver.find_element(:css, ".subject_type_uniform_title")
+    end
   end
+
+  describe "subject selection" do
+    it "displays correct icon for cultural_context term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  cultural_context")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_cultural_context").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_cultural_context")
+    end
+
+    it "displays correct icon for function term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  function")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_function").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_function")
+    end
+
+    it "displays correct icon for genre_form term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  genre_form")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_genre_form").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_genre_form")
+    end
+  
+    it "displays correct icon for technique term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  technique")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_technique").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_technique")
+    end
+
+    it "displays correct icon for occupation term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  occupation")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_occupation").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_occupation")
+    end
+
+    it "displays correct icon for style_period term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  style_period")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_style_period").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_style_period")
+    end
+
+    it "displays correct icon for technique term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  technique")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_technique").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_technique")
+    end
+  
+    it "displays correct icon for temporal term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  temporal")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_temporal").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_temporal")
+    end
+
+    it "displays correct icon for topical term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  topical")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_topical").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_topical")
+    end
+
+    it "displays correct icon for uniform_title term_type in subject selection" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  uniform_title")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_uniform_title").click
+
+      # find now selected token
+      @driver.find_element(:css, ".icon-token.subject_type_uniform_title")
+    end
+  end
+
 end
 
