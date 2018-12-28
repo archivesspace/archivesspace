a59d0265d368516f92cea3428cfe4fd7fd90e8a0
Manny Rodriguez <manny@theavatar.org>
2018-12-28 14:02:56 -0500

    unit tests for correct icon showing for subjects when loading resource with saved subjects

--- frontend/spec/selenium/spec/resources_spec.rb
+++ frontend/spec/selenium/spec/resources_spec.rb
@@ -263,5 +263,168 @@ describe "Resources Form" do
     end
   end
 
+  describe "resource with previously saved subject" do
+    it "displays correct icon (cultural_context) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  cultural_context")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_cultural_context").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_cultural_context")
+    end
+
+    it "displays correct icon (function) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  function")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_function").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_function")
+    end
+     
+    it "displays correct icon (genre_form) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  genre_form")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_genre_form").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_genre_form")
+    end
+  
+    it "displays correct icon (technique) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  technique")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_technique").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_technique")
+    end
+
+    it "displays correct icon (occupation) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  occupation")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_occupation").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_occupation")
+    end
+
+    it "displays correct icon (style_period) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  style_period")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_style_period").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_style_period")
+    end
+  
+    it "displays correct icon (temporal) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  temporal")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_temporal").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_temporal")
+    end
+
+    it "displays correct icon (topical) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  topical")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_topical").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_topical")
+    end
+  
+    it "displays correct icon (uniform_title) for subjects when loading a saved one" do
+      # click on Add Subject button
+      @driver.find_element(:css, "#resource_subjects_ button").click
+
+      # select input box and type "a" to bring up a list of subjects
+      @driver.clear_and_send_keys([:css, "#resource_subjects_ input"], "  uniform_title")
+
+      # click on option in search bar to select it
+      @driver.find_element(:css, ".subject_type_uniform_title").click
+
+      # save form & reload page
+      @driver.find_element(:css, "button.btn-primary").click
+      @driver.navigate.refresh
+
+      # look for icon class after form loads again
+      @driver.find_element(:css, ".icon-token.subject_type_uniform_title")
+    end
+  end
 end
 
