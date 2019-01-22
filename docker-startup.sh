51dd1e9b225831ff38c5551a96eb53198dea2242
Austin Schaffer <aschaffer@atlas-sys.com>
2019-01-22 12:16:41 -0500

    Added a check for error codes from the db setup script

--- docker-startup.sh
+++ docker-startup.sh
@@ -27,5 +27,13 @@ fi
 
 # clear out tmp pre-startup as it can build up if persisted
 rm -rf $DATA_TMP_DIR/*
+
 /archivesspace/scripts/setup-database.sh
+
+if [[ "$?" != 0 ]]; then
+  echo "Error running the database setup script."
+  exit 1
+fi
+
 exec /archivesspace/archivesspace.sh
+
