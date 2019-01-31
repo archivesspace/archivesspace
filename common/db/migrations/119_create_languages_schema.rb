2dd9901360fad9e82b277f4bdac5b68104d5bf97
Lora Woodford <lorawoodford@gmail.com>
2019-01-31 14:44:05 -0500

    Update migration changing languages to lang_material

--- common/db/migrations/119_create_languages_schema.rb
+++ common/db/migrations/119_create_languages_schema.rb
@@ -7,10 +7,18 @@ def create_language_record_from_language_id(record_type, dataset, language_id)
     linked_record_id = self[record_type].filter(:id => row[:id]).get(:id)
     language_value = self[record_type].filter(:language_id => row[:language_id]).get(:language_id)
 
-    self[:language].insert(
+    language_record = self[:lang_material].insert(
+                          :json_schema_version => row[:json_schema_version],
+                          "#{record_type}_id" => linked_record_id,
+                          :create_time => row[:create_time],
+                          :system_mtime => row[:system_mtime],
+                          :user_mtime => row[:user_mtime]
+                        )
+
+    self[:language_and_script].insert(
                           :json_schema_version => row[:json_schema_version],
                           :language_id => language_value,
-                          "#{record_type}_id" => linked_record_id,
+                          :lang_material_id => language_record,
                           :create_time => row[:create_time],
                           :system_mtime => row[:system_mtime],
                           :user_mtime => row[:user_mtime]
@@ -22,7 +30,7 @@ end
 Sequel.migration do
   up do
 
-    create_table(:language) do
+    create_table(:lang_material) do
       primary_key :id
 
       Integer :lock_version, :default => 0, :null => false
@@ -33,22 +41,40 @@ Sequel.migration do
       Integer :digital_object_id, :null => true
       Integer :digital_object_component_id, :null => true
 
-      DynamicEnum :language_id, :null => true
-      DynamicEnum :script_id, :null => true
-
-      TextField :note, :null => true
-
       apply_mtime_columns
     end
 
-    alter_table(:language) do
+    alter_table(:lang_material) do
       add_foreign_key([:archival_object_id], :archival_object, :key => :id)
       add_foreign_key([:resource_id], :resource, :key => :id)
       add_foreign_key([:digital_object_id], :digital_object, :key => :id)
       add_foreign_key([:digital_object_component_id], :digital_object_component, :key => :id)
     end
 
+    create_table(:language_and_script) do
+      primary_key :id
+
+      Integer :lock_version, :default => 0, :null => false
+      Integer :json_schema_version, :null => false
+
+      Integer :lang_material_id
+
+      DynamicEnum :language_id, :null => true
+      DynamicEnum :script_id, :null => true
+
+      apply_mtime_columns
+    end
+
+    alter_table(:language_and_script) do
+      add_foreign_key([:lang_material_id], :lang_material, :key => :id)
+    end
+
     create_editable_enum('script_iso15924', ["Adlm", "Afak", "Aghb", "Ahom", "Arab", "Aran", "Armi", "Armn", "Avst", "Bali", "Bamu", "Bass", "Batk", "Beng", "Bhks", "Blis", "Bopo", "Brah", "Brai", "Bugi", "Buhd", "Cakm", "Cans", "Cari", "Cham", "Cher", "Cirt", "Copt", "Cpmn", "Cprt", "Cyrl", "Cyrs", "Deva", "Dogr", "Dsrt", "Dupl", "Egyd", "Egyh", "Egyp", "Elba", "Elym", "Ethi", "Geok", "Geor", "Glag", "Gong", "Gonm", "Goth", "Gran", "Grek", "Gujr", "Guru", "Hanb", "Hang", "Hani", "Hano", "Hans", "Hant", "Hatr", "Hebr", "Hira", "Hluw", "Hmng", "Hmnp", "Hrkt", "Hung", "Inds", "Ital", "Jamo", "Java", "Jpan", "Jurc", "Kali", "Kana", "Khar", "Khmr", "Khoj", "Kitl", "Kits", "Knda", "Kore", "Kpel", "Kthi", "Lana", "Laoo", "Latf", "Latg", "Latn", "Leke", "Lepc", "Limb", "Lina", "Linb", "Lisu", "Loma", "Lyci", "Lydi", "Mahj", "Maka", "Mand", "Mani", "Marc", "Maya", "Medf", "Mend", "Merc", "Mero", "Mlym", "Modi", "Mong", "Moon", "Mroo", "Mtei", "Mult", "Mymr", "Nand", "Narb", "Nbat", "Newa", "Nkdb", "Nkgb", "Nkoo", "Nshu", "Ogam", "Olck", "Orkh", "Orya", "Osge", "Osma", "Palm", "Pauc", "Perm", "Phag", "Phli", "Phlp", "Phlv", "Phnx", "Plrd", "Piqd", "Prti", "Qaaa", "Qabx", "Rjng", "Rohg", "Roro", "Runr", "Samr", "Sara", "Sarb", "Saur", "Sgnw", "Shaw", "Shrd", "Shui", "Sidd", "Sind", "Sinh", "Sogd", "Sogo", "Sora", "Soyo", "Sund", "Sylo", "Syrc", "Syre", "Syrj", "Syrn", "Tagb", "Takr", "Tale", "Talu", "Taml", "Tang", "Tavt", "Telu", "Teng", "Tfng", "Tglg", "Thaa", "Thai", "Tibt", "Tirh", "Ugar", "Vaii", "Visp", "Wara", "Wcho", "Wole", "Xpeo", "Xsux", "Yiii", "Zanb", "Zinh", "Zmth", "Zsye", "Zsym", "Zxxx", "Zyyy", "Zzzz"])
+
+    alter_table(:note) do
+      add_column(:lang_material_id, Integer,  :null => true)
+      add_foreign_key([:lang_material_id], :lang_material, :key => :id)
+    end
 
     create_enum("note_langmaterial_type", ["langmaterial"])
 
