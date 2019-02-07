7881c37d699fd558c999d34c639a2d3e72b81f15
Laney McGlohon <lmcglohon@gmail.com>
2019-02-07 13:54:56 -0800

    Adjust for params that are Strings and not Arrays

--- backend/spec/documentation_spec.rb
+++ backend/spec/documentation_spec.rb
@@ -2,33 +2,34 @@ require 'spec_helper'
 
 describe "Generate REST Documentation" do
 
+  it "gets all the endpoints and makes something can write documentation for" do
+    endpoints = ArchivesSpaceService::Endpoint.all.sort{|a,b| a[:uri] <=> b[:uri]}
+    output = {}
+    problems = []
 
- it "gets all the endpoints and makes something can write documentation for" do
- 
- 
-endpoints = ArchivesSpaceService::Endpoint.all.sort{|a,b| a[:uri] <=> b[:uri]}                                                                                                      
-output = {}
-problems = []
-
-models = {}
-    JSONModel.models.each_pair do |type, klass| 
+    models = {}
+    JSONModel.models.each_pair do |type, klass|
       begin
         models[type] = JSON.parse( build("json_#{type}".to_sym).to_json )
       rescue => err
-        # if you want a verbose output of the issues, you can set an ENV  
+        # if you want a verbose output of the issues, you can set an ENV
         $stderr.puts "Model problem with #{klass} : #{err.message}" if ENV["BUILD_DOCUMENTATION"]
       end
     end
 
-endpoints.each do |e|
-
+    endpoints.each do |e|
       output[e[:uri]] ||= {}
       output[e[:uri]][e[:method]] = {}
       e[:params].each do |p|
-        begin       
-          klass = p[1]
-          klass = klass.first if klass.is_a?(Array) 
-          
+        begin
+          if p.is_a? String
+            klass = p.to_sym
+          else
+            klass = p[1]
+          end
+
+          klass = klass.first if klass.is_a?(Array)
+
           if klass.is_a?(Symbol)
             record = klass.to_s
           elsif klass.respond_to?(:record_type)
@@ -41,24 +42,23 @@ endpoints.each do |e|
           else
             record = generate(klass.name.downcase.to_sym)
           end
-          output[e[:uri]][e[:method]][p[0]] = record
+          if p.is_a? String
+            output[e[:uri]][e[:method]][p] = record
+          else
+            output[e[:uri]][e[:method]][p[0]] = record
+          end
         rescue => err
-          # $stderr.puts "problem with #{e[:uri]} #{e[:method]}"
           problems << { :class => klass, :backtrace => err.backtrace,  :message => err.message }
-          next 
-          # raise err 
+          next
         end
       end
 
-end
-    
+    end
+
     file = File.join(File.dirname(__FILE__), '../../', "endpoint_examples.json")
     file_problems = File.join(File.dirname(__FILE__), '../../', "endpoint_examples_problems.json")
     File.open(file, "w") {  |f| f << output.to_json }
     File.open(file_problems, "w") {  |f| f << JSON.pretty_generate(problems) }
-    $stderr.puts "example file put at #{file}. Problems logged in #{file_problems}" 
-    
-
-
+    $stderr.puts "example file put at #{file}. Problems logged in #{file_problems}"
+  end
 end
-end 
