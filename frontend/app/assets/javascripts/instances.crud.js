807802880efa6ff4d9708f52663df8441a7d087e
Payten Giles <payten.giles@gmail.com>
2019-03-06 14:16:04 +1100

    Refactor instance form actions so templates are derrived directly from the button to add the instance item

--- frontend/app/assets/javascripts/instances.crud.js
+++ frontend/app/assets/javascripts/instances.crud.js
@@ -23,13 +23,8 @@ $(function() {
 
   $(document).bind("subrecordcreaterequest.aspace", function(event, object_name, add_button_data, index_data, $target_subrecord_list, callback) {
     if (object_name === "instance") {
-      var formEl;
-      if (add_button_data.instanceType === "digital-instance") {
-        formEl = $(AS.renderTemplate("template_instance_digital_object", index_data));
-      } else {
-        formEl = $(AS.renderTemplate("template_instance_container", index_data));
-        formEl.data("collapsed", false);
-      }
+      var formEl = $(AS.renderTemplate(add_button_data.template, index_data));
+      formEl.data("collapsed", false);
 
       callback(formEl, $target_subrecord_list);
     }
