34b04980c27437a8ee21d20248ff940a3f87fb35
Sarah Morrissey <s.morrissey@gatech.edu>
2019-03-11 08:28:22 -0400

    got accessions working working on assessments assessments working agents working most types working linker working links working and location and container profiles working on simplifying adding columns more work on search title sort issues make clearer added plugin to demonstrate search/browse column customization fixed something get rid of print working on adding everything to prefs working on adding to prefs add option for how many columns to show progress more work on prefs prefs trimming down code handle facets/sorting correctly when filtering/unfiltering record type some tinkering only show preferences in UI for certain types add dates and extents update fields that are shown english translations result changed to record Remove duplicate setting in config-defaults get rid of duplicate Copy preference defaults file over to config directory for inclusion in release

--- common/schemas/defaults.rb
+++ common/schemas/defaults.rb
@@ -1,16 +1,83 @@
-accession_browse_column_enum = [
-                      "identifier", "accession_date", "acquisition_type", "resource_type",
-                      "restrictions_apply", "access_restrictions", "use_restrictions",
-                      "publish", 'no_value'
-                     ]
-resource_browse_column_enum = [
-                      "identifier", "resource_type", "level", "restrictions",
-                      "ead_id", "finding_aid_status", "publish", 'no_value'
-                     ]
-digital_object_browse_column_enum = [
-                      "digital_object_id", "digital_object_type", "level", "restrictions",
-                      "publish", 'no_value'
-                     ]
+browse_column_enums = {
+  'accession' => [
+    'title', 'identifier', 'accession_date', 'acquisition_type', 'resource_type',
+    'restrictions_apply', 'publish', 'access_restrictions', 'use_restrictions',
+    'dates', 'extents', 'processing_priority', 'processors'
+  ],
+  'resource' => [
+    'title', 'identifier', 'level', 'resource_type', 'language', 'publish',
+    'restrictions', 'dates', 'extents', 'ead_id', 'finding_aid_status',
+    'processing_priority', 'processors'
+  ],
+  'archival_object' => [
+    "title", "publish", "context", "component_id", "ref_id", "level", 'dates',
+    'extents', 'language'
+  ],
+  'digital_object' => [
+    'title', 'digital_object_id', 'publish', 'level', 'digital_object_type', 'language',
+    'restrictions', 'dates', 'extents'
+  ],
+  'digital_object_component' => [
+    "title", "publish", "context", 'dates', 'extents', 'language'
+  ],
+  'subjects' => [
+    "title", "publish", "source", "first_term_type"
+  ],
+  'agent' => [
+    "title", "primary_type", "publish", "authority_id", "source", "rules", "is_user"
+  ],
+  'location' => [
+    "title", "publish", "building", "floor", "room", "area", "location_holdings",
+    "location_profile_display_string_u_ssort", 'temporary'
+  ],
+  'event' => [
+    "agents", "event_type", "outcome", "linked_records"
+  ],
+  'collection_management' => [
+    "parent_title", "parent_type", "processing_priority", "processing_status", "processing_hours_total",
+    "processing_funding_source", "processors"
+  ],
+  'classification' => [
+    "title", "publish", "has_classification_terms"
+  ],
+  'top_container' => [
+    "title", "publish", "container_profile_display_string_u_sstr", "location_display_string_u_sstr", "type",
+    "indicator", "barcode", "context"
+  ],
+  'assessment' => [
+    "assessment_id", "assessment_records", "assessment_record_types", "assessment_surveyors",
+    "assessment_survey_begin", "assessment_review_required", "assessment_sensitive_material",
+    "assessment_inactive", "assessment_survey_year", "assessment_collections",
+    "assessment_completed", "assessment_formats", "assessment_ratings",
+    "assessment_conservation_issues"
+  ],
+  'repositories' => [
+    "title", "publish"
+  ],
+  'container_profile' => [
+    "title", "publish", "container_profile_width_u_sstr", "container_profile_height_u_sstr",
+    "container_profile_depth_u_sstr", "container_profile_dimension_units_u_sstr"
+  ],
+  'location_profile' => [
+    "title", "publish", "location_profile_width_u_sstr", "location_profile_height_u_sstr",
+    "location_profile_depth_u_sstr", "location_profile_dimension_units_u_sstr"
+  ],
+  'multi' => [
+    "primary_type", "title", "context", "identifier", 'dates', 'extents'
+  ]
+}
+
+browse_columns = {}
+browse_column_enums.keys.each do |type|
+  Array(1..AppConfig[:max_search_columns]).each do |i|
+    browse_columns["#{type}_browse_column_#{i}"] = {
+      "type" => "string",
+      "enum" => browse_column_enums[type] + ['audit_info', 'no_value'],
+      "required" => false
+    }
+  end
+end
+
 {
   :schema => {
     "$schema" => "http://www.archivesspace.org/archivesspace.json",
@@ -20,84 +87,6 @@ digital_object_browse_column_enum = [
 
       "show_suppressed" =>  {"type" => "boolean", "required" => false},
       "publish" =>  {"type" => "boolean", "required" => false},
-
-      "accession_browse_column_1" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_2" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_3" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_4" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_5" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-
-      "resource_browse_column_1" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_2" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_3" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_4" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_5" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-
-      "digital_object_browse_column_1" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_2" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_3" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_4" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_5" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
 
       "default_values" => {
         "type" => "boolean",
@@ -110,6 +99,6 @@ digital_object_browse_column_enum = [
         "items" => {"type" => "string"}
       }
 
-    },
+    }.merge(browse_columns),
   },
 }
