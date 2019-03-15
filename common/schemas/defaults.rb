1938ffa89c61d668ee7de6b4f388a5d1ea3e5a98
Sarah Morrissey <s.morrissey@gatech.edu>
2019-03-15 15:07:42 -0400

    working on adding everything to prefs

--- common/schemas/defaults.rb
+++ common/schemas/defaults.rb
@@ -1,16 +1,35 @@
-accession_browse_column_enum = [
-                      "identifier", "accession_date", "acquisition_type", "resource_type",
-                      "restrictions_apply", "access_restrictions", "use_restrictions",
-                      "publish", 'no_value'
-                     ]
-resource_browse_column_enum = [
-                      "identifier", "resource_type", "level", "restrictions",
-                      "ead_id", "finding_aid_status", "publish", 'no_value'
-                     ]
-digital_object_browse_column_enum = [
-                      "digital_object_id", "digital_object_type", "level", "restrictions",
-                      "publish", 'no_value'
-                     ]
+browse_column_enums = {
+  'accession' => [
+    "title", "suppressed", "publish", "system_generated", "subjects","agents", "identifier",
+    "acquisition_type", "accession_date", "resource_type", "restrictions_apply", "access_restrictions",
+    "use_restrictions", "is_slug_auto"
+  ],
+  'resource' => [
+    "title", "suppressed", "publish", "system_generated", "notes", "level", "finding_aid_title",
+    "finding_aid_filing_title", "identifier", "resource_type", "language", "restrictions", "ead_id",
+    "finding_aid_status", "is_slug_auto"
+  ],
+  'digital_object' => [
+    "title", "suppressed", "publish", "system_generated", "subjects", "agents", "notes", "level",
+    "digital_object_type", "digital_object_id", "restrictions", "is_slug_auto"
+  ],
+  'subject' => [
+    "title", "suppressed", "publish", "system_generated", "source", "first_term_type",
+    "is_slug_auto", "used_within_repository", "used_within_published_repository"
+  ]
+}
+
+browse_columns = {}
+browse_column_enums.keys.each do |type|
+  Array(1..5).each do |i|
+    browse_columns["#{type}_browse_column_#{i}"] = {
+      "type" => "string",
+      "enum" => browse_column_enums[type] + ['audit_info', 'no_value'],
+      "required" => false
+    }
+  end
+end
+
 {
   :schema => {
     "$schema" => "http://www.archivesspace.org/archivesspace.json",
@@ -20,84 +39,6 @@ digital_object_browse_column_enum = [
 
       "show_suppressed" =>  {"type" => "boolean", "required" => false},
       "publish" =>  {"type" => "boolean", "required" => false},
-
-      "accession_browse_column_1" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_2" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_3" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_4" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-      "accession_browse_column_5" => {
-        "type" => "string",
-        "enum" => accession_browse_column_enum,
-        "required" => false
-      },
-
-      "resource_browse_column_1" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_2" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_3" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_4" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-      "resource_browse_column_5" => {
-        "type" => "string",
-        "enum" => resource_browse_column_enum,
-        "required" => false
-      },
-
-      "digital_object_browse_column_1" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_2" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_3" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_4" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
-      "digital_object_browse_column_5" => {
-        "type" => "string",
-        "enum" => digital_object_browse_column_enum,
-        "required" => false
-      },
 
       "default_values" => {
         "type" => "boolean",
@@ -110,6 +51,6 @@ digital_object_browse_column_enum = [
         "items" => {"type" => "string"}
       }
 
-    },
+    }.merge(browse_columns),
   },
 }
