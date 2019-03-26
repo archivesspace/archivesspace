54e38a065ecfd7c5ac870eddf70b1922a3cea460
Laney McGlohon <lmcglohon@gmail.com>
2019-03-26 13:29:14 -0700

    Add messages to new migrations

--- common/db/migrations/112_add_slug_fields_to_digital_object.rb
+++ common/db/migrations/112_add_slug_fields_to_digital_object.rb
@@ -2,6 +2,7 @@ require_relative 'utils'
 
 Sequel.migration do
   up do
+    $stderr.puts("Add slug fields to digital objects")
     alter_table(:digital_object) do
     	add_column(:slug, String)
       add_column(:is_slug_auto, Integer, :default => 0)
