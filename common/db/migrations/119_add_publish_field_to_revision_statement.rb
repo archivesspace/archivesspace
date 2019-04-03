9b0c9c0763451f91ca3d09c1ef7a1500d38bbf4c
Sarah Morrissey <s.morrissey@gatech.edu>
2019-04-03 09:26:06 -0400

    make more like other publish fields (no default)

--- common/db/migrations/119_add_publish_field_to_revision_statement.rb
+++ common/db/migrations/119_add_publish_field_to_revision_statement.rb
@@ -4,7 +4,10 @@ Sequel.migration do
   up do
     $stderr.puts("Add publish field to revision statement")
     alter_table(:revision_statement) do
-      add_column(:publish, Integer, :default => 0)
+      add_column(:publish, Integer)
     end
+
+    self[:revision_statement].update(:publish => 0)
+    self[:revision_statement].update(:system_mtime => Time.now)
   end
 end
