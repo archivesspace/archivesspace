b7a9ff32bf6efc00ab67a612c92b3e36f3927c78
Sarah Morrissey <s.morrissey@gatech.edu>
2019-04-10 13:57:39 -0400

    add background job to trim title whitespace

---
+++ backend/app/lib/job_runners/trim_whitespace_runner.rb
@@ -0,0 +1,44 @@
+class TrimWhitespaceRunner < JobRunner
+  register_for_job_type('trim_whitespace_job')
+
+  def run
+    begin
+
+      modified_records = []
+
+      RequestContext.open(:repo_id => @job.repo_id) do
+
+        [Resource, ArchivalObject, Accession, DigitalObject, DigitalObjectComponent].each do |record_class|
+          @job.write_output("Trimming whitespace from #{record_class} titles")
+
+
+          count = 0
+          record_class.this_repo.each do |r|
+            if r[:title] && (trimmed = r[:title].strip) != r[:title]
+              count += 1
+              r.update(:title => trimmed)
+              modified_records << r.uri
+            end
+          end
+          @job.write_output("#{count} records modified.")
+          @job.write_output("================================")
+        end
+      end
+
+      if modified_records.empty?
+        @job.write_output("All done, no records modified.")
+      else
+        @job.write_output("All done, logging modified records.")
+      end
+
+      self.success!
+
+      # just reuse JobCreated api for now...
+      @job.record_created_uris(modified_records)
+    rescue Exception => e
+      @job.write_output(e.message)
+      @job.write_output(e.backtrace)
+      raise e
+    end
+  end
+end
