c8d6b5f1e390df0c81787cf3b62aad9046d806f6
Dave Mayo <pobocks@gmail.com>
2019-04-11 12:28:14 -0400

    Bugfix in API.erb, used non-printing eval for shell example
    Also Documented an endpoint known to have incorrect/misleading docs

--- backend/app/controllers/container.rb
+++ backend/app/controllers/container.rb
@@ -114,6 +114,28 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.post('/repositories/:repo_id/top_containers/batch/location')
     .description("Update location for a batch of top containers")
+    .example('shell') do
+      <<-SHELL
+ curl -H "X-ArchivesSpace-Session: $SESSION" \\
+   -d 'ids[]=[1,2,3,4,5]' \\
+   -d 'location_uri=locations/1234' \\
+   "http://localhost:8089/repositories/2/top_containers/batch/location"
+      SHELL
+    end
+    .example('python') do
+      <<-PYTHON
+client = ASnakeClient()
+client.post('repositories/2/top_containers/batch/location',
+            params={ 'ids': [1,2,3,4,5],
+                     'location_uri': 'locations/1234' })
+      PYTHON
+    end
+    .documentation do
+      <<-DOCS
+This route takes the `ids` of one or more containers, and associates the containers
+with the location referenced by `location_uri`.
+      DOCS
+    end
     .params(["ids", [Integer]],
             ["location_uri", String, "The uri of the location"],
             ["repo_id", :repo_id])
