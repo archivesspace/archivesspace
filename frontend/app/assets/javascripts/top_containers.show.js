87e3171778da69caa5058ba31b6232b1188def09
jambun <jambun@gmail.com>
2019-06-18 14:49:55 +1000

    Trigger on form load to give js a hope

--- frontend/app/assets/javascripts/top_containers.show.js
+++ frontend/app/assets/javascripts/top_containers.show.js
@@ -1,7 +1,4 @@
-//= require space_calculator
-
 $(document).ready(function() {
-    $(".linker:not(.initialised)").linker();
     $(document).triggerHandler("loadedrecordform.aspace", [$("#new_top_container_form")]);
 });
 
