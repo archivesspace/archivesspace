1afcc862a40d5b84437390d70ec89b8089a6453c
Mark Triggs <mark@dishevelled.net>
2019-08-29 13:15:08 +1000

    Add a new hook to run once all plugins have been loaded

--- backend/app/main.rb
+++ backend/app/main.rb
@@ -58,6 +58,18 @@ class ArchivesSpaceService < Sinatra::Base
     end
   end
 
+  @plugins_loaded_hooks = []
+  @archivesspace_plugins_loaded = false
+
+  def self.plugins_loaded_hook(&block)
+    if @archivesspace_plugins_loaded
+      block.call
+    else
+      @plugins_loaded_hooks << block
+    end
+  end
+
+
 
   configure :development do |config|
     require 'sinatra/reloader'
@@ -220,6 +232,11 @@ class ArchivesSpaceService < Sinatra::Base
         end
 
         BackgroundJobQueue.init if ASpaceEnvironment.environment != :unit_test
+
+        @plugins_loaded_hooks.each do |hook|
+          hook.call
+        end
+        @archivesspace_plugins_loaded = true
 
         Notifications.notify("BACKEND_STARTED")
         Log.noisiness "Logger::#{AppConfig[:backend_log_level].upcase}".constantize
