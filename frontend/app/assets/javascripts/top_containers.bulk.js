28ec3ef520e70b94f67709b69c95408538311154
Lora Woodford <lorawoodford@gmail.com>
2019-11-14 14:36:08 -0500

    Bold selection, reword instructions, start with button disabled

--- frontend/app/assets/javascripts/top_containers.bulk.js
+++ frontend/app/assets/javascripts/top_containers.bulk.js
@@ -519,16 +519,13 @@ BulkActionBarcodeRapidEntry.prototype.setup_form_submission = function($modal) {
  *
  */
 
-function changeCheckbox(event) {
-   let item = document.getElementById('chkPref');
-   switch(item.getAttribute('aria-checked')) {
-       case "true":
-           item.setAttribute('aria-checked', "false");
-           break;
-       case "false":
-           item.setAttribute('aria-checked', "true");
-           break;
-   }
+function activateBtn(event) {
+  var merge_btn = document.getElementsByClassName("merge-button")[0];
+  if ($('input:checked').length > 0) {
+    merge_btn.removeAttribute("disabled");
+  } else {
+    merge_btn.attr("disabled", "disabled");
+  };
  };
 
 function BulkActionMerge(bulkContainerSearch) {
