e0a636d11f3be5a78bb60a1cd7c6c1c16c587499
Bobbi Fox <bobbi@bobbifox.net>
2020-01-09 17:58:26 -0500

    fix typos, rearrange classes

--- backend/app/controllers/bulk_import.rb
+++ backend/app/controllers/bulk_import.rb
@@ -1,42 +1,41 @@
 class ArchivesSpaceService < Sinatra::Base
-    require 'pp'
-    require 'nokogiri'
-    require 'rubyXL'
-    #Supports bulk import of spreadsheets
-    Endpoint.post('/bulkimport/ssload')
-        .description("Bulk Import an Excel Spreadsheet")
-        .params(["repo_id", :repo_id],
-                ["rid", :id],
-                ["ref_id", String, "Ref ID"],
-                ["position", String, "Position in tree"],
-                ["type", String, "resource or archival_object"],
-                ["aoid", String, "archival object ID"],
-                ["filename", String, "the original file name"],
-                ["filepath", String, "the spreadsheet temp path"])
-        .permissions([:update_resource_record])
-        .returns([200,"HTML"],
-                [400, :error]) \
-    do
-        #right now, just return something!
-        @rows = initialize_info(params)
-        Log.error("Number of rows: #{@row.count}")
-        erb :'bulk/bulk_import_response'
-    end
-# set up all the @ variables (except for @header)
-    def initialize_info(params)
-        dispatched_file_path = params[:filepath]
-        @orig_filename = params[:filename]
+  require "pp"
+  require "nokogiri"
+  require "rubyXL"
+  #Supports bulk import of spreadsheets
+  Endpoint.post("/bulkimport/ssload")
+    .description("Bulk Import an Excel Spreadsheet")
+    .params(["repo_id", :repo_id],
+            ["rid", :id],
+            ["ref_id", String, "Ref ID"],
+            ["position", String, "Position in tree"],
+            ["type", String, "resource or archival_object"],
+            ["aoid", String, "archival object ID"],
+            ["filename", String, "the original file name"],
+            ["filepath", String, "the spreadsheet temp path"])
+    .permissions([:update_resource_record])
+    .returns([200, "HTML"],
+             [400, :error]) do
+    #right now, just return something!
+    @rows = initialize_info(params)
+    Log.error("Number of rows: #{@rows.count}")
+    erb :'bulk/bulk_import_response'
+  end
+  # set up all the @ variables (except for @header)
+  def initialize_info(params)
+    dispatched_file_path = params[:filepath]
+    @orig_filename = params[:filename]
     # @report.set_file_name(@orig_filename)
     # initialize_handler_enums
     # @resource = Resource.find(params[:rid])
     # @repository = @resource['repository']['ref']
     # @hier = 1
-        # ingest archival objects needs this
+    # ingest archival objects needs this
     # unless @digital_load
     #   @note_types =  note_types_for(:archival_object)
     #   tree = JSONModel(:resource_tree).find(nil, :resource_id => params[:rid]).to_hash
-        #  @ao = nil
-    #   aoid = params[:aoid] 
+    #  @ao = nil
+    #   aoid = params[:aoid]
     #   @resource_level = aoid.blank?
     #   @first_one = false  # to determine whether we need to worry about positioning
     #   if @resource_level
@@ -45,22 +44,22 @@ class ArchivesSpaceService < Sinatra::Base
     #   else
     #     @ao = JSONModel(:archival_object).find(aoid, find_opts )
     #     @start_position = @ao.position
-    #     parent = @ao.parent # we need this for sibling/child disabiguation later on 
+    #     parent = @ao.parent # we need this for sibling/child disabiguation later on
     #     @parents.set_uri(0, (parent ? ASUtils.jsonmodels_to_hashes(parent)['ref'] : nil))
     #     @parents.set_uri(1, @ao.uri)
     #     @first_one = true
     #   end
     # end
 
-        @input_file = dispatched_file_path
-        @counter = 0
-        @rows_processed = 0
-        @error_rows = 0
-        Log.error("About to open #{@input_file}")
-        workbook = RubyXL::Parser.parse(@input_file)
-        Log.error("Got workbook ")
-        sheet = workbook[0]
-        Log.error("Got sheet: ")
-        rows = sheet.enum_for(:each)
-    end
+    @input_file = dispatched_file_path
+    @counter = 0
+    @rows_processed = 0
+    @error_rows = 0
+    Log.error("About to open #{@input_file}")
+    workbook = RubyXL::Parser.parse(@input_file)
+    Log.error("Got workbook ")
+    sheet = workbook[0]
+    Log.error("Got sheet: ")
+    rows = sheet.enum_for(:each)
+  end
 end
--- backend/app/converters/bulk_import_converter.rb
+++ backend/app/converters/bulk_import_converter.rb
@@ -1,22 +1,21 @@
-require_relative 'converter'
-require_relative '/lib/bulk_import/bulk_import_tracker'
-require 'nokogiri'
-require 'pp'
-require 'rubyXL'
-require 'asutils'
+require_relative "converter"
+require_relative "lib/bulk_import/bulk_import_tracker"
+require "nokogiri"
+require "pp"
+require "rubyXL"
+require "asutils"
 
 START_MARKER = /ArchivesSpace field code/
 DO_START_MARKER = /ArchivesSpace digital object import field codes/
-set_access_control "update_resource_record" => [:new, :edit, :create, :update, :rde, :add_children, :publish, :accept_children, :load_ss,  :load_dos]
-
+#set_access_control "update_resource_record" => [:new, :edit, :create, :update, :rde, :add_children, :publish, :accept_children, :load_ss, :load_dos]
 
 class BulkImportConverter < Converter
   def self.import_types(show_hidden = false)
     [
-     {
-       :name => "bulk_import_excel",
-       :description => "Import Import Archival and Digital Objects from an Excel Spreadsheet"
-     }
+      {
+        :name => "bulk_import_excel",
+        :description => "Import Import Archival and Digital Objects from an Excel Spreadsheet",
+      },
     ]
   end
   def self.instance_for(type, input_file)
@@ -28,49 +27,49 @@ class BulkImportConverter < Converter
   end
 
   def run
-
+    Log.error("RUN")
   end
+
   def initialize(input_file, opts = {})
     @input_file = input_file
     @batch = ASpaceImport::RecordBatch.new
     @opts = opts
   end
 
-private
-# set up all the @ variables (except for @header)
-def initialize_info
+  private
+
+  # set up all the @ variables (except for @header)
+  def initialize_info
     @orig_filename = @opts[:filename]
-    
     @report_out = []
     @report = BulkImportTracker.new
     @headers
-    @digital_load  = @opts(:digital_load,'') == 'true'
+    @digital_load = @opts[:digital_load] == "true"
     @report.set_file_name(@orig_filename)
-# initialize_handler_enums
+    # initialize_handler_enums
     @resource = Resource.find(@opts[:rid])
-    @repository = @resource['repository']['ref']
+    @repository = @resource["repository"]["ref"]
     @hier = 1
     # ingest archival objects needs this
-     unless @digital_load
-#   @note_types =  note_types_for(:archival_object)
-    tree = JSONModel(:resource_tree).find(nil, :resource_id => @opts[:rid]).to_hash
-    @ao = nil
-    aoid = @opts[:aoid] || nil
-    @resource_level = aoid.nil?
-    @first_one = false  # to determine whether we need to worry about positioning
-#   if @resource_level
-#     @parents.set_uri(0, nil)
-#     @hier = 0
-#   else
-#     @ao = JSONModel(:archival_object).find(aoid, find_opts )
-#     @start_position = @ao.position
-#     parent = @ao.parent # we need this for sibling/child disabiguation later on 
-#     @parents.set_uri(0, (parent ? ASUtils.jsonmodels_to_hashes(parent)['ref'] : nil))
-#     @parents.set_uri(1, @ao.uri)
-#     @first_one = true
-#   end
-# end
-
+    unless @digital_load
+      @note_types = note_types_for(:archival_object)
+      tree = JSONModel(:resource_tree).find(nil, :resource_id => @opts[:rid]).to_hash
+      @ao = nil
+      aoid = @opts[:aoid] || nil
+      @resource_level = aoid.nil?
+      @first_one = false  # to determine whether we need to worry about positioning
+      if @resource_level
+        @parents.set_uri(0, nil)
+        @hier = 0
+      else
+        @ao = JSONModel(:archival_object).find(aoid, find_opts)
+        @start_position = @ao.position
+        parent = @ao.parent # we need this for sibling/child disabiguation later on
+        @parents.set_uri(0, (parent ? ASUtils.jsonmodels_to_hashes(parent)["ref"] : nil))
+        @parents.set_uri(1, @ao.uri)
+        @first_one = true
+      end
+    end
     @input_file = dispatched_file_path
     @counter = 0
     @rows_processed = 0
@@ -81,6 +80,5 @@ def initialize_info
     sheet = workbook[0]
     Log.error("Got sheet: ")
     rows = sheet.enum_for(:each)
-end
-
+  end
 end
