d75f84a901f9404e8bf9e2b5bf9d129862f33f9a
Bobbi-SMR <bobbi@bobbifox.net>
2020-01-14 11:46:28 -0500

    rubocopped

--- backend/app/converters/bulk_import_converter.rb
+++ backend/app/converters/bulk_import_converter.rb
@@ -1,33 +1,32 @@
-require_relative "converter"
-require_relative "lib/bulk_import/bulk_import_tracker"
-require "nokogiri"
-require "pp"
-require "rubyXL"
-require "asutils"
+# frozen_string_literal: true
 
-START_MARKER = /ArchivesSpace field code/
-DO_START_MARKER = /ArchivesSpace digital object import field codes/
-#set_access_control "update_resource_record" => [:new, :edit, :create, :update, :rde, :add_children, :publish, :accept_children, :load_ss, :load_dos]
+require_relative 'converter'
+require_relative 'lib/bulk_import/bulk_import_tracker'
+require 'nokogiri'
+require 'pp'
+require 'rubyXL'
+require 'asutils'
+
+START_MARKER = /ArchivesSpace field code/.freeze
+DO_START_MARKER = /ArchivesSpace digital object import field codes/.freeze
+# set_access_control "update_resource_record" => [:new, :edit, :create, :update, :rde, :add_children, :publish, :accept_children, :load_ss, :load_dos]
 
 class BulkImportConverter < Converter
-  def self.import_types(show_hidden = false)
+  def self.import_types(_show_hidden = false)
     [
       {
-        :name => "bulk_import_excel",
-        :description => "Import Import Archival and Digital Objects from an Excel Spreadsheet",
-      },
+        name: 'bulk_import_excel',
+        description: 'Import Import Archival and Digital Objects from an Excel Spreadsheet'
+      }
     ]
   end
+
   def self.instance_for(type, input_file, opts = {})
-    if type == "bulk_import_excel"
-      self.new(input_file, opts)
-    else
-      nil
-    end
+    new(input_file, opts) if type == 'bulk_import_excel'
   end
 
   def run
-    Log.error("RUN")
+    Log.error('RUN')
     initialize_info
   end
 
@@ -36,7 +35,7 @@ class BulkImportConverter < Converter
     @batch = ASpaceImport::RecordBatch.new
     @opts = opts
     Log.error("OPTS: #{@opts}")
-    #WAAY more initialization to come
+    # WAAY more initialization to come
   end
 
   private
@@ -47,7 +46,7 @@ class BulkImportConverter < Converter
     @report_out = []
     @report = BulkImportTracker.new
     @headers
-    @digital_load = @opts[:digital_load] == "true"
+    @digital_load = @opts[:digital_load] == 'true'
     @report.set_file_name(@orig_filename)
     # initialize_handler_enums
     @resource = Resource.get_or_die(@opts[:rid])
@@ -59,11 +58,11 @@ class BulkImportConverter < Converter
     # ingest archival objects needs this
     unless @digital_load
       @note_types = note_types_for(:archival_object)
-      tree = JSONModel(:resource_tree).find(nil, :resource_id => @opts[:rid]).to_hash
+      tree = JSONModel(:resource_tree).find(nil, resource_id: @opts[:rid]).to_hash
       @ao = nil
       aoid = @opts[:aoid] || nil
       @resource_level = aoid.nil?
-      @first_one = false  # to determine whether we need to worry about positioning
+      @first_one = false # to determine whether we need to worry about positioning
       if @resource_level
         @parents.set_uri(0, nil)
         @hier = 0
@@ -71,7 +70,7 @@ class BulkImportConverter < Converter
         @ao = JSONModel(:archival_object).find(aoid, find_opts)
         @start_position = @ao.position
         parent = @ao.parent # we need this for sibling/child disabiguation later on
-        @parents.set_uri(0, (parent ? ASUtils.jsonmodels_to_hashes(parent)["ref"] : nil))
+        @parents.set_uri(0, (parent ? ASUtils.jsonmodels_to_hashes(parent)['ref'] : nil))
         @parents.set_uri(1, @ao.uri)
         @first_one = true
       end
@@ -82,9 +81,9 @@ class BulkImportConverter < Converter
     @error_rows = 0
     Log.error("About to open #{@input_file}")
     workbook = RubyXL::Parser.parse(@input_file)
-    Log.error("Got workbook ")
+    Log.error('Got workbook ')
     sheet = workbook[0]
-    Log.error("Got sheet: ")
+    Log.error('Got sheet: ')
     rows = sheet.enum_for(:each)
   end
 end
