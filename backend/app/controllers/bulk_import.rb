5552d4704a9049c0a871357242067f1c7ad0a192
Bobbi-SMR <bobbi@bobbifox.net>
2020-01-14 11:50:28 -0500

    Rubocopped

--- backend/app/controllers/bulk_import.rb
+++ backend/app/controllers/bulk_import.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 class ArchivesSpaceService < Sinatra::Base
-  require "pp"
-  require_relative "../converters/bulk_import_converter"
-  #Supports bulk import of spreadsheets
-  Endpoint.post("/bulkimport/ssload")
-    .description("Bulk Import an Excel Spreadsheet")
-    .params(["repo_id", :repo_id],
-            ["rid", :id],
-            ["ref_id", String, "Ref ID"],
-            ["position", String, "Position in tree"],
-            ["type", String, "resource or archival_object"],
-            ["aoid", String, "archival object ID"],
-            ["filename", String, "the original file name"],
-            ["filepath", String, "the spreadsheet temp path"])
-    .permissions([:update_resource_record])
-    .returns([200, "HTML"],
-             [400, :error]) do
-    #right now, just return something!
+  require 'pp'
+  require_relative '../converters/bulk_import_converter'
+  # Supports bulk import of spreadsheets
+  Endpoint.post('/bulkimport/ssload')
+          .description('Bulk Import an Excel Spreadsheet')
+          .params(['repo_id', :repo_id],
+                  ['rid', :id],
+                  ['ref_id', String, 'Ref ID'],
+                  ['position', String, 'Position in tree'],
+                  ['type', String, 'resource or archival_object'],
+                  ['aoid', String, 'archival object ID'],
+                  ['filename', String, 'the original file name'],
+                  ['filepath', String, 'the spreadsheet temp path'])
+          .permissions([:update_resource_record])
+          .returns([200, 'HTML'],
+                   [400, :error]) do
+    # right now, just return something!
     importer = BulkImportConverter.new(params.fetch(:filepath), params)
     importer.run
     erb :'bulk/bulk_import_response'
