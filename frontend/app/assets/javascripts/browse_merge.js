a45017e2c49ce7703f907c78ef970f932f6670e9
Brian Zelip <bzelip@protonmail.com>
2020-01-15 09:51:34 -0500

    frontend/app/assets/javascripts/browse_merge.js: Clean up js
    Fix get_assets() definition,
    remove activatBtn() which does nothing

--- frontend/app/assets/javascripts/browse_merge.js
+++ frontend/app/assets/javascripts/browse_merge.js
@@ -1,6 +1,7 @@
-get_selection = function() {
+// fix function def init
+function get_selection() {
   var results = [];
-
+  
   $(document).find(".multiselect-column :input:checked").each(function(i, checkbox) {
     results.push({
       uri: checkbox.value,
@@ -12,17 +13,21 @@ get_selection = function() {
   return results;
 };
 
-function activateBtn(event) {
-  var merge_btn = document.getElementsByClassName("merge-button")[0];
-  if ($(":input:checked").length > 0) {
-    merge_btn.removeAttribute("disabled");
-  } else {
-    merge_btn.attr("disabled", "disabled");
-  };
-};
+
+
+// removed activatBtn function here, because it didn't do anything,
+// it was copied from somewhere else (looks like near merge top containers)
+// and wasn't actually called; i think the previous author thought
+// activateBtn was responsible for the un-disabling of the merge button
+// in container profiles view, but it's not, the `$(".multiselect-enabled")`
+// bit in frontend/app/assets/javascripts/search.js is what's responsible for it
+// this is why i removed that class from the template
 
 $(document).on("click", "#batchMerge", function() {
-  let modal_title = $(this).text() + " " + $("h2").text()
+  let modal_title = "Merge Container Profiles"
+  // should be i18n!
+  // changed to hardcoded since it's basically hard coded anyways, given
+  // that no other markup uses this js
   let dialog_content = AS.renderTemplate("batch_merge_modal_template", {
     selection: get_selection()
   });
