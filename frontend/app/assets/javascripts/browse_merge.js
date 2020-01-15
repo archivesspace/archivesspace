5a9b5b396ad50b61840ea021adacc8d7ed92bc3c
Brian Zelip <bzelip@protonmail.com>
2020-01-15 13:07:15 -0500

    frontend/app/assets/javascripts/browse_merge.js: Refactor
    Add activateBtn() back, as it *is* used in the template,
    copy in and adapt two modal workflow from ANW-462 work

--- frontend/app/assets/javascripts/browse_merge.js
+++ frontend/app/assets/javascripts/browse_merge.js
@@ -13,6 +13,14 @@ function get_selection() {
   return results;
 };
 
+function activateBtn(event) {
+  var merge_btn = document.getElementsByClassName("merge-button")[0];
+  if ($(":input:checked").length > 0) {
+    merge_btn.removeAttribute("disabled");
+  } else {
+    merge_btn.attr("disabled", "disabled");
+  };
+};
 
 
 // removed activatBtn function here, because it didn't do anything,
@@ -27,10 +35,52 @@ $(document).on("click", "#batchMerge", function() {
   let modal_title = "Merge Container Profiles"
   // should be i18n!
   // changed to hardcoded since it's basically hard coded anyways, given
-  // that no other markup uses this js
-  let dialog_content = AS.renderTemplate("batch_merge_modal_template", {
+  // that no other markup uses this js;
+  // should be made dynamically in the future, and should leverage i18n
+  
+  let dialog_content = AS.renderTemplate("merge_container_profiles_modal", {
     selection: get_selection()
   });
+  
   AS.openCustomModal("batchMergeModal", modal_title, dialog_content,
     'full');
+  
+  // Access modal1 DOM
+  const $selectTargetBtn = $("[data-js='selectTarget']");
+    
+  $selectTargetBtn.on("click", function(e) {
+    e.preventDefault();
+
+    // Set up data for form submission
+    const mergeList = get_selection()
+                      .map(function(profile) {
+                        return {
+                          uri: profile.uri,
+                          display_string: profile.display_string
+                        }
+                      });
+
+    const targetEl = document.querySelector('input[name="target[]"]:checked');
+
+    const target = {
+      display_string: targetEl.getAttribute('aria-label'),
+      uri: targetEl.getAttribute('value')
+    };
+    
+    // compute victims list for template rendering
+    const victims = mergeList.reduce(function(acc, profile) {
+      if (profile.display_string !== target.display_string) {
+        acc.push(profile.display_string);
+      }
+      return acc;
+    }, [])
+    
+    // Init modal2
+    AS.openCustomModal("bulkMergeConfirmModal", "Confirm Merge Container Profiles", AS.renderTemplate("merge_confirm_container_profiles_modal", {
+      mergeList,
+      target,
+      victims
+    }), false);
+  });
+
 });
