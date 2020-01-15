35f5c96b404776727642ca5aab7d402aaf5d089d
Laney McGlohon <lmcglohon@gmail.com>
2020-01-15 07:44:27 -0800

    Revert "Merge branch 'master' into master"
    This reverts commit bbcec98d2603cf7eb00d3e68793b4412b3148f66, reversing
    changes made to db60099f9cc010fb470a3171100a180d4e0e24f6.

--- plugins/digitization_work_order/backend/model/id_generators/default_generator.rb
+++
@@ -1,9 +0,0 @@
-class DefaultGenerator < GeneratorInterface
-
-  PREFIX = 'cuid'
-
-  def generate(record)
-    PREFIX + Sequence.get("/repositories/#{record.repo_id}/archival_objects/component_id_for_work_order").to_s
-  end
-
-end
--- plugins/digitization_work_order/frontend/plugin_init.rb
+++
@@ -1,2 +0,0 @@
-my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
-ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
