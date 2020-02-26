fb91d39351b43d3a8e5573f8d74bac5db7952e35
Dave Mayo <dave_mayo@harvard.edu>
2020-02-26 14:37:34 -0500

    Add extra_columns argument to SearchController#do_search
    In order to support columns being added to ajax-loaded tables,
    this commit adds an extra_columns argument to
    SearchController#do_search, changes
    SearchHelper#build_search_parameters to handle it, and a FORMATTERS
    constant containing procs that return procs usable by
    SearcHelper::ExtraColumn#new.

--- frontend/app/controllers/search_controller.rb
+++ frontend/app/controllers/search_controller.rb
@@ -1,9 +1,27 @@
 require 'advanced_query_builder'
 
 class SearchController < ApplicationController
+  # This class provides search functionality through the frontend.  Methods
+  # here generally perform a search against the backend, then render those search results
+  # as JSON or HTML fragments to be rendered into tables in the application,
+  # either directly in templates, or as responses to ajax calls.
+
+  # Formatters are procs that take a fieldname, and return a proc usable as the
+  # value_block argument in SearchHelper::ExtraColumn#new
+  #
+  # Usage:
+  #       FORMATTERS[formatter].call(field)
+  #
+  # FORMATTERS is customized to return a default formatter if passed a format it doesn't understand,
+  # which will produce the value as returned from the field without alteration.
+  FORMATTERS = Hash.new do |key| proc {|field| proc {|record| Rails.logger.debug('default formatter called');record[field] } } end
+
+  FORMATTERS.merge!({
+    'stringify' => proc {|field| proc {|record| record[field].to_s } }
+  })
 
   set_access_control  "view_repository" => [:do_search, :advanced_search]
-  
+
   include ExportHelper
 
   def advanced_search
@@ -37,24 +55,50 @@ class SearchController < ApplicationController
         @search_data = Search.all(session[:repo_id], criteria)
         render "search/do_search"
       }
-      format.csv { 
+      format.csv {
         uri = "/repositories/#{session[:repo_id]}/search"
         csv_response( uri, criteria )
-      }  
+      }
     end
   end
 
   def do_search
+    # Execute a backend search, rendering results as JSON/HTML fragment/HTML/CSS
+    #
+    # In addition to params handled by ApplicationController#params_for_backend_search, takes:
+    #   :extra_columns - hash with keys 'title', 'field', sort_options (hash with keys 'sortable', 'sort_by')
+    #   :display_identifier - whether to display the identifier column
+    #
+    # For example to add uri to the data-browse field of an AJAX-backed table:
+    #
+    #    data-browse-url="<%= url_for :controller => :search, :action => :do_search,
+    #                                 :extra_columns => [{
+    #                                    'title' => 'uri',
+    #                                    'formatter' =>'stringify',
+    #                                    'field'=> 'uri',
+    #                                    'sort_options' => {'sortable' => true, 'sort_by' => 'uri'}
+    #                                  }],
+    #                                  :format => :json, :facets => [], :sort => "title_sort asc" %>"
+    #
+    # The date-browse-url field would be identical, but with :format => :js
+    #
+    # Note: you will need to add an entry to frontend/config/locales under the search_sorting key for the title of any column you add
+
+    unless request.format.css?
+      @search_data = Search.all(session[:repo_id], params_for_backend_search.merge({"facet[]" => SearchResultData.BASE_FACETS.concat(params[:facets]||[]).uniq}))
+      if params[:extra_columns]
+        @extra_columns = params[:extra_columns].map do |opts|
+          SearchHelper::ExtraColumn.new(opts['title'], FORMATTERS[opts['formatter']].call(opts['field']), opts['sort_options'] || {}, @search_data)
+        end
+      end
+      @display_identifier = params[:display_identifier] ? params[:display_identifier] : false
+    end
 
     respond_to do |format|
       format.json {
-        @search_data = Search.all(session[:repo_id], params_for_backend_search.merge({"facet[]" => SearchResultData.BASE_FACETS.concat(params[:facets]||[]).uniq}))
-        @display_identifier = params[:display_identifier] ? params[:display_identifier] : false
         render :json => @search_data
       }
       format.js {
-        @search_data = Search.all(session[:repo_id], params_for_backend_search.merge({"facet[]" => SearchResultData.BASE_FACETS.concat(params[:facets]||[]).uniq}))
-        @display_identifier = params[:display_identifier] ? params[:display_identifier] : false
         if params[:listing_only]
           render_aspace_partial :partial => "search/listing"
         else
@@ -62,15 +106,15 @@ class SearchController < ApplicationController
         end
       }
       format.html {
-        @search_data = Search.all(session[:repo_id], params_for_backend_search.merge({"facet[]" => SearchResultData.BASE_FACETS.concat(params[:facets]||[]).uniq}))
-        @display_identifier = params[:display_identifier] ? params[:display_identifier] : false
+        # default render
       }
-      format.csv { 
+      format.csv {
         criteria = params_for_backend_search.merge({"facet[]" => SearchResultData.BASE_FACETS})
         uri = "/repositories/#{session[:repo_id]}/search"
         csv_response( uri, criteria )
-      }  
+      }
     end
   end
+
 
 end
