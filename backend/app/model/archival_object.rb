4649a497c0048b017a7864c93f1694a7b06035a6
Lora Woodford <lorawoodford@gmail.com>
2020-03-05 09:11:28 -0500

    ANW-172: Add all dates to ao and do records h2

--- backend/app/model/archival_object.rb
+++ backend/app/model/archival_object.rb
@@ -65,15 +65,15 @@ class ArchivalObject < Sequel::Model(:archival_object)
     display_string = json['title'] || ""
 
     date_label = json.has_key?('dates') && json['dates'].length > 0 ?
-                   lambda {|date|
+                   json['dates'].map do |date|
                      if date['expression']
-                       date['expression']
+                       date['date_type'] == 'bulk' ? "#{I18n.t("date_type_bulk.bulk")}: #{date['expression']}" : date['expression']
                      elsif date['begin'] and date['end']
-                       "#{date['begin']} - #{date['end']}"
+                       date['date_type'] == 'bulk' ? "#{I18n.t("date_type_bulk.bulk")}: #{date['begin']} - #{date['end']}" : "#{date['begin']} - #{date['end']}"
                      else
-                       date['begin']
+                       date['date_type'] == 'bulk' ? "#{I18n.t("date_type_bulk.bulk")}: #{date['begin']}" : date['begin']
                      end
-                   }.call(json['dates'].first) : false
+                   end.join(', ') : false
 
     display_string += ", " if json['title'] && date_label
     display_string += date_label if date_label
