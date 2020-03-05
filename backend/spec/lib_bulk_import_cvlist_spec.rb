a09f719887ae33136b9a8ad390d3b50ef2bb987b
Bobbi Fox <bobbi@bobbifox.net>
2020-03-05 15:28:26 -0500

    fixed first spec

--- backend/spec/lib_bulk_import_cvlist_spec.rb
+++ backend/spec/lib_bulk_import_cvlist_spec.rb
@@ -3,10 +3,8 @@ require_relative "../app/controllers/lib/bulk_import/cv_list"
 
 describe "Controlled Value List" do
   it "should instantiate as a Control Value list" do
-    as_test_user("admin") do
-      current_user = RequestContext.get(:current_user)
+      current_user = User.find(:username => 'admin')
       subject_sources = CvList.new("subject_source", current_user)
       expect(subject_sources.length).to be > 0
-    end
   end
 end
