16b6e3328bf14d3fd5d0f3280802a13185aecea1
Bobbi Fox <bobbi@bobbifox.net>
2020-03-19 14:29:41 -0400

    finished tests

--- backend/spec/lib_bulk_import_lang_handler_spec.rb
+++ backend/spec/lib_bulk_import_lang_handler_spec.rb
@@ -14,15 +14,15 @@ describe "Language Handler" do
   end
 
   def hash_it(obj)
-    ASUtils.jsonmodels_to_hashes(obj)[0]
+    ASUtils.jsonmodels_to_hashes(obj)
   end
 
   it "creates a language  (no script)" do
     lang = @lh.create_language("eng", nil, nil, true, @report)
     hsh = hash_it(lang)
-    expect(hsh["language_and_script"]["language"]).to eq("eng")
-    expect(hsh["notes"]).to eq([])
-    expect(hsh["language_and_script"]["script"]).to eq(nil)
+    expect(hsh[0]["language_and_script"]["language"]).to eq("eng")
+    expect(hsh[0]["notes"]).to eq([])
+    expect(hsh[0]["language_and_script"]["script"]).to eq(nil)
   end
 
   it "fails to create a non-existent language (dothraki)" do
@@ -40,22 +40,54 @@ describe "Language Handler" do
   it "creates a language material note" do
     lang = @lh.create_language(nil, nil, "<p>This is a note!</p>", true, @report)
     hsh = hash_it(lang)
-    note = hsh["notes"][0]
+    note = hsh[0]["notes"][0]
     expect(note["content"][0]).to eq("<p>This is a note!</p>")
   end
   it "creates a non-publish language material note" do
     lang = @lh.create_language(nil, nil, "<p>This is an unpublished note!</p>", false, @report)
     hsh = hash_it(lang)
-    note = hsh["notes"][0]
+    note = hsh[0]["notes"][0]
     expect(note["content"][0]).to eq("<p>This is an unpublished note!</p>")
     expect(note["publish"]).to eq(false)
   end
-  xit "handles a non-compliant lang material note" do
+  it "handles an ill-formed lang material note" do
+    lang = @lh.create_language(nil, nil, "<p>This is a <strong>bad unpublished note!</p>", false, @report)
+    expect(lang).to eq([])
+    expect(current_row.errors[0]).to start_with("langmaterial note is not wellformed:")
   end
 
-  xit "creates a language and a lang material note" do
+  it "creates a language and a lang material note" do
+    lang = @lh.create_language("ara", nil, "<p>This is an Arabic note!</p>", false, @report)
+    hsh = hash_it(lang)
+    expect(hsh[0]["language_and_script"]["language"]).to eq("ara")
+    expect(hsh[0]["language_and_script"]["script"]).to eq(nil)
+    note = hsh[1]["notes"]
+    expect(note[0]["publish"]).to eq(false)
+    expect(note[0]["type"]).to eq("langmaterial")
+    expect(note[0]["content"][0]).to eq("<p>This is an Arabic note!</p>")
   end
 
-  xit "creates a language, script, and lang material note" do
+  it "creates a language, BAD script, and lang material note" do
+    lang = @lh.create_language("ara", "arab", "<p>This is an Arabic note!</p>", false, @report)
+    hsh = hash_it(lang)
+    expect(hsh[0]["language_and_script"]["language"]).to eq("ara")
+    expect(current_row["errors"][0]).to start_with("Cannot validate a language script")
+    note = hsh[1]["notes"]
+    expect(note[0]["publish"]).to eq(false)
+    expect(note[0]["type"]).to eq("langmaterial")
+    expect(note[0]["content"][0]).to eq("<p>This is an Arabic note!</p>")
+  end
+  it "creates a language, and lang material note" do
+    lang = @lh.create_language("ara", "Arab", "<p>This is an Arabic note!</p>", false, @report)
+    hsh = hash_it(lang)
+    expect(hsh[0]["language_and_script"]["language"]).to eq("ara")
+    expect(hsh[0]["language_and_script"]["script"]).to eq("Arab")
+    puts "count"
+    puts hsh.count
+    puts hsh[1]
+    note = hsh[1]["notes"]
+    expect(note[0]["publish"]).to eq(false)
+    expect(note[0]["type"]).to eq("langmaterial")
+    expect(note[0]["content"][0]).to eq("<p>This is an Arabic note!</p>")
   end
 end
