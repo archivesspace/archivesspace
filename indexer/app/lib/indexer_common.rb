fe3ebce9cdc298d26be5f5bb05cbe4d8b44b3133
James Bullen <jambun@gmail.com>
2020-03-24 12:27:36 +1100

    When indexing enum values, don't hardcode the list of enums, dig them out of the schemas

--- indexer/app/lib/indexer_common.rb
+++ indexer/app/lib/indexer_common.rb
@@ -269,15 +269,22 @@ class IndexerCommon
     end
   end
 
-  # TODO: We should fix this to read from the JSON schemas
-  HARDCODED_ENUM_FIELDS = ["relator", "type", "role", "source", "rules", "acquisition_type", "resource_type", "processing_priority", "processing_status", "era", "calendar", "digital_object_type", "level", "processing_total_extent_type", "extent_type", "event_type", "type_1", "type_2", "type_3", "salutation", "outcome", "finding_aid_description_rules", "finding_aid_status", "instance_type", "use_statement", "checksum_method", "language", "date_type", "label", "certainty", "scope", "portion", "xlink_actuate_attribute", "xlink_show_attribute", "file_format_name", "temporary", "name_order", "country", "jurisdiction", "rights_type", "ip_status", "term_type", "enum_1", "enum_2", "enum_3", "enum_4", "relator_type", "job_type"]
+
+  def enum_fields
+    @enum_fields ||= JSONModel.models.map{|_,model|
+      model.schema['properties'].select{|_,hash| hash.has_key?('dynamic_enum')}.map{|prop,_| prop}
+    }.flatten.uniq
+
+    @enum_fields
+  end
+
 
   def configure_doc_rules
 
     add_document_prepare_hook {|doc, record|
       found_keys = Set.new
 
-      ASUtils.search_nested(record["record"], HARDCODED_ENUM_FIELDS, ['_resolved']) do |field, field_value|
+      ASUtils.search_nested(record["record"], enum_fields, ['_resolved']) do |field, field_value|
         key = "#{field}_enum_s"
 
         doc[key] ||= Set.new
