efdd319ac634249b636642dd4ddc30285c3f3f21
Bobbi Fox <bobbi@bobbifox.net>
2020-03-26 15:58:25 -0400

    formatting changes

--- backend/app/controllers/lib/bulk_import/lang_handler.rb
+++ backend/app/controllers/lib/bulk_import/lang_handler.rb
@@ -1,13 +1,12 @@
 class LangHandler < Handler
-  
   def initialize(current_user)
-    @language_types = CvList.new('language_iso639_2', current_user)
-    @script_types = CvList.new('script_iso15924', current_user)
+    @language_types = CvList.new("language_iso639_2", current_user)
+    @script_types = CvList.new("script_iso15924", current_user)
   end
 
   def renew
-      clear(@language_types)
-      clear(@script_types)
+    clear(@language_types)
+    clear(@script_types)
   end
 
   def create_language(lang_val, script, langmaterial, publish, report)
@@ -27,7 +26,7 @@ class LangHandler < Handler
         begin
           lang_code = @language_types.value(lang_val)
         rescue Exception => n
-          report.add_errors( I18n.t('bulk_import.error.lang_code', :lang =>lang_val))
+          report.add_errors(I18n.t("bulk_import.error.lang_code", :lang => lang_val))
         end
       end
       if lang_code
@@ -37,36 +36,39 @@ class LangHandler < Handler
           begin
             langscript.script = @script_types.value(script)
           rescue Exception => n
-            report.add_errors( I18n.t('bulk_import.error.script_code', :script => script))
+            report.add_errors(I18n.t("bulk_import.error.script_code", :script => script))
           end
         end
         lang = JSONModel(:lang_material).new
         lang.language_and_script = langscript
         langs.push lang
-      end        
+      end
       if have_note
         lang = JSONModel(:lang_material).new
         content = langmaterial
         begin
           wellformed(content)
           note = JSONModel(:note_langmaterial).new
+          if publish.class.name == "String"
+            publish = publish == "1"
+          end
           note.publish = publish
-          note.type = 'langmaterial'
+          note.type = "langmaterial"
           note.content.push content if !content.nil?
           lang.notes.push note
-          langs.push lang 
+          langs.push lang
         rescue Exception => e
-          report.add_errors(I18n.t('bulk_import.error.bad_note', :type => 'langmaterial' , :msg => CGI::escapeHTML( e.message)))
-        end                     
+          report.add_errors(I18n.t("bulk_import.error.bad_note", :type => "langmaterial", :msg => CGI::escapeHTML(e.message)))
+        end
       end
     end
-    langs 
+    langs
   end
 
   # currently a repeat from the controller
   def wellformed(note)
     if note.match("</?[a-zA-Z]+>")
-      frag = Nokogiri::XML("<root xmlns:xlink='https://www.w3.org/1999/xlink'>#{note}</root>") {|config| config.strict}
+      frag = Nokogiri::XML("<root xmlns:xlink='https://www.w3.org/1999/xlink'>#{note}</root>") { |config| config.strict }
     end
   end
 end
