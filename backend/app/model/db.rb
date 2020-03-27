74cacfd0d809d566d41542a78b4e3f540846c0e2
Mark Triggs <mark@dishevelled.net>
2020-03-27 11:40:43 +1100

    Fix handling of nested DB.open calls when retries are happening
    Previously it was possible for an inner call to DB.open to retry and
    commit a partial result, while the outer call(s) were rolled back.
    See comment added to `model/db.rb` for gory details!

--- backend/app/model/db.rb
+++ backend/app/model/db.rb
@@ -161,6 +161,36 @@ class DB
       Thread.current[:nesting_level] += 1
 
       begin
+        if Thread.current[:nesting_level] > 1
+          # We are already inside another DB.open that will handle all
+          # exceptions and retries for us.  We want to avoid a situation like
+          # this:
+          #
+          # transaction scope / DB.open do |db|
+          #                   |   db[:sometable].insert(foo)
+          #                   |
+          #                   |   DB.open do |db|                                         \
+          #                   |     db[:sometable].insert(something_that_depends_on_foo)  | retry scope
+          #                   |   end                                                     /
+          #                   \ end
+          #
+          # Despite the nested DB.open calls, Sequel's default behavior is to
+          # merge the inner call to DB.transaction with the already active
+          # transaction.
+          #
+          # If the inner "retry scope" hits an exception, the whole transaction
+          # is rolled back (all of "transaction scope", including the insert of
+          # `foo`), but only the inner "retry scope" is retried.  If that
+          # succeeds on the retry, we end up losing first insert and keeping the
+          # second.
+          #
+          # So the fix here is to let the outermost DB.open take responsibility
+          # for everything: make the retry scope and the transaction scope line
+          # up with each other.
+
+          return yield @pool
+        end
+
         last_err = false
         retries = opts[:retries] || 10
 
