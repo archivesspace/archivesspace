bd2e55431d92d8ef1a3fd4f862721aa3e624fd49
Payten Giles <payten.giles@gmail.com>
2020-04-14 14:53:28 +1000

    Ensure linker typehead dropdown is hidden when browsing/creating from a linker as sometimes blur leaves them visible

--- frontend/app/assets/javascripts/linker.js
+++ frontend/app/assets/javascripts/linker.js
@@ -92,6 +92,9 @@ $(function() {
 
 
       var showLinkerCreateModal = function() {
+        // Ensure all typeahead dropdowns are hidden (sometimes blur leaves them visible)
+        $('.token-input-dropdown').hide();
+
         AS.openCustomModal(config.modal_id, "Create "+ config.label, AS.renderTemplate("linker_createmodal_template", config), 'large', {}, this);
         if ($(this).hasClass("linker-create-btn")) {
           renderCreateFormForObject($(this).data("target"));
@@ -215,6 +218,9 @@ $(function() {
           $("#"+config.modal_id).modal('hide');
           $this.trigger("change");
         };
+
+        // Ensure all typeahead dropdowns are hidden (sometimes blur leaves them visible)
+        $('.token-input-dropdown').hide();
 
         AS.openCustomModal(config.modal_id, "Browse "+ config.label_plural, AS.renderTemplate("linker_browsemodal_template",config), 'large', {}, this);
         renderItemsInModal();
