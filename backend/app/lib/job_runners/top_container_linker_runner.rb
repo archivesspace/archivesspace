3874015822c32ba91c2ed621c7003e0a7c68d32e
vac765 <valdeva_crema@harvard.edu>
2020-04-25 14:42:39 -0400

    Made sure job failed and rolled back properly with exceptions

--- backend/app/lib/job_runners/top_container_linker_runner.rb
+++ backend/app/lib/job_runners/top_container_linker_runner.rb
@@ -9,9 +9,6 @@ class TopContainerLinkerRunner < JobRunner
     begin
       job_data = @json.job
      
-          
-      terminal_error = nil
-         
       DB.open(DB.supports_mvcc?,
               :retry_on_optimistic_locking_fail => true) do
 
@@ -30,43 +27,48 @@ class TopContainerLinkerRunner < JobRunner
               {:rid => @json.job['resource_id'], :repo_id => @job.repo_id}, current_user)
             
             begin 
-              tcl.run
+              result = tcl.run
               report = tcl.report
-              modified_uris = []
-              report.rows.each do |row|
-                modified_uris << row.archival_object_id
-                #Report out the collected data:
-                if !row.errors.empty?
-                  @job.write_output("Errors discovered during linker processing:")
-                  row.errors.each do |error|
-                    @job.write_output(error)
-                  end
-                end
-                if !row.info.empty?
-                  @job.write_output("\nData from linker processing:")
-                  row.info.each do |info|
-                    @job.write_output(info)
-                  end
-                end
-              end
+              write_out_errors(report)
               
-              if modified_uris.empty?
-                @job.write_output("No records modified.")
-              else
-                @job.write_output("Logging modified records.")
-              end
               self.success!
-              @job.record_created_uris(modified_uris.uniq)
+              
             rescue Exception => e
               report = tcl.report
+              write_out_errors(report)
               @job.write_output(e.message)
               @job.write_output(e.backtrace)
-              raise e
+              raise Sequel::Rollback
             end
          end
          end
        end
     end
-end
+  end
+  
+  private
+  def write_out_errors(report)
+    modified_uris = []
+    report.rows.each do |row|
+      modified_uris << row.archival_object_id
+      #Report out the collected data:
+      if !row.errors.empty?
+        row.errors.each do |error|
+          @job.write_output(error)
+        end
+      end
+      if !row.info.empty?
+        row.info.each do |info|
+          @job.write_output(info)
+        end
+      end
+    end
+    if modified_uris.empty?
+      @job.write_output("No records modified.")
+    else
+      @job.write_output("Logging modified records.")
+    end
+    @job.record_created_uris(modified_uris.uniq)
+  end
 
 end
