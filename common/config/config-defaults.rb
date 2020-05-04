ad249a8d12eb2b76228b41002a8b319ab777bc05
Hurlyburly2 <douglas_simon@harvard.edu>
2020-05-04 09:24:10 -0400

    removed appconfig option from subcontainer barcode

--- common/config/config-defaults.rb
+++ common/config/config-defaults.rb
@@ -665,6 +665,3 @@ AppConfig[:ark_url_prefix] = proc { AppConfig[:public_proxy_url] }
 
 # Specifies if the fields that show up in csv should be limited to those in search results
 AppConfig[:limit_csv_fields] = true
-
-# Turns fields for subcontainer barcodes on/off on the front end
-AppConfig[:show_subcontainer_barcode_fields] = false
