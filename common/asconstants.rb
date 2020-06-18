1cbae4b1196ff6266c4d489d282c6d948ac70358
vac765 <valdeva_crema@harvard.edu>
2020-06-18 12:30:10 -0400

    Latest version

--- common/asconstants.rb
+++ common/asconstants.rb
@@ -26,7 +26,7 @@ module ASConstants
       else # some servlet containers have a hard time finding the resource...
         # fallback_version variable gets changed in dist ant task . The a is
         # just a cue that we're using this..
-        fallback_version = "v2.7.1.a"
+        fallback_version = "hm-10"
         @VERSION = fallback_version
       end
     rescue
