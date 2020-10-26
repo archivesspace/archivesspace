2bf0f4ad09262d5ae1ade28acf5ecc4d4f1f5563
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2020-10-26 17:15:10 -0400

    make total processing hours calculation work for all types

--- frontend/app/assets/javascripts/collection_management_records.crud.js
+++ frontend/app/assets/javascripts/collection_management_records.crud.js
@@ -4,22 +4,22 @@
 
 var calculate_total_processing_hours = function(form) { 
     var $form = $(form);
-    var phe = parseFloat($("#resource_collection_management__processing_hours_per_foot_estimate_", $form).val(), 10);
-    var pte = parseFloat($("#resource_collection_management__processing_total_extent_", $form).val(), 10);
+    var phe = parseFloat($('input[id*="_collection_management__processing_hours_per_foot_estimate_"]', $form).val(), 10);
+    var pte = parseFloat($('input[id*="_collection_management__processing_total_extent_"]', $form).val(), 10);
 
     if ( $.isNumeric(phe) && $.isNumeric(pte) ) {
         var tph = (phe * pte).toFixed(2);
-        $("#resource_collection_management__processing_hours_total_", $form).val(tph);
+        $('input[id*="_collection_management__processing_hours_total_"]', $form).val(tph);
     }
 }
 
 
 $(document).bind("subrecordcreated.aspace", function(event, object_name, subform) {
 
-   $("#resource_collection_management__processing_hours_per_foot_estimate_", $(subform)).bind('change', function() { 
+   $('input[id*="_collection_management__processing_hours_per_foot_estimate_"]', $(subform)).bind('change', function() { 
         calculate_total_processing_hours( subform ); 
     });
-   $("#resource_collection_management__processing_total_extent_", $(subform)).bind('change', function() { 
+   $('input[id*="_collection_management__processing_total_extent_"]', $(subform)).bind('change', function() { 
         calculate_total_processing_hours( subform ); 
     });
 
