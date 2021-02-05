e95fd912b879171e382cb27d4d5ef5a308e611f7
Mark Cooper <mark.c.cooper@outlook.com>
2021-02-04 17:38:34 -0800

    ANW-429 Exports update 1

--- backend/app/exporters/models/marc_auth.rb
+++ backend/app/exporters/models/marc_auth.rb
@@ -1,25 +1,21 @@
+# frozen_string_literal: true
+
 class MARCAuthModel < ASpaceExport::ExportModel
   model_for :marc_auth
 
   attr_reader :related_records, :json
 
-  RESOLVE = ['subjects', 'places']
+  RESOLVE = ['subjects', 'places'].freeze
 
-  
   @marc_auth_event = Class.new do
-    
     def initialize(event)
       @event = event
     end
-    
+
     def method_missing(meth)
-      if @event.respond_to?(meth)
-        @event.send(meth)
-      else
-        nil
-      end
+      @event.send(meth) if @event.respond_to?(meth)
     end
-    
+
     def type
       if @event.event_type == 'cataloging'
         'created'
@@ -27,80 +23,64 @@ class MARCAuthModel < ASpaceExport::ExportModel
         @event.event_type
       end
     end
-    
+
     def date_time
       @date_time ||= nil
 
       if @date_time.nil?
-        if @event.date
-          @date_time = @event.date['begin']
-        else
-          @date_time = @event.timestamp.gsub(/Z.*/, '')
-        end
+        @date_time = if @event.date
+                       @event.date['begin']
+                     else
+                       @event.timestamp.gsub(/Z.*/, '')
+                     end
       end
-      
+
       @date_time
     end
-    
+
     def agents
-      
       agents = []
-      
-      @event.linked_agents.each do |a| 
+
+      @event.linked_agents.each do |a|
         case a['_resolved']['agent_type']
         when 'agent_person'
           agents << ['human', a['_resolved']['display_name']['sort_name']]
         when 'agent_software'
-          agents << ['machine', a['_resolved']['display_name']['sort_name']] 
+          agents << ['machine', a['_resolved']['display_name']['sort_name']]
         end
       end
-      
-      agents
-    end  
 
+      agents
+    end
   end
-   
 
   def initialize(obj, events, related_records, repo)
     @type = obj.jsonmodel_type
     @json = URIResolver.resolve_references(obj, RESOLVE)
-    @events = events.map {|e| self.class.instance_variable_get(:@marc_auth_event).new(e) }
+    @events = events.map { |e| self.class.instance_variable_get(:@marc_auth_event).new(e) }
     @related_records = related_records
     @repo = repo
   end
-   
-  
+
   def self.from_agent(obj, events, related_records, repo)
-    self.new(obj, events, related_records, repo)
-  end
-  
-  
-  def events
-    @events
+    new(obj, events, related_records, repo)
   end
 
+  attr_reader :events
 
   def maintenanceAgency
     if @ma.nil?
       @ma = OpenStruct.new
       @ma.agencyName = @repo.name
-      if @repo.org_code
-        @ma.agencyCode = @repo.org_code
-      end
+      @ma.agencyCode = @repo.org_code if @repo.org_code
     end
 
     @ma
   end
-  
-  
-  def method_missing(meth)
-    if @json.respond_to?(meth)
-      @json.send(meth)
-    else
-      nil
-    end
-  end
 
+  def method_missing(meth)
+    @json.send(meth) if @json.respond_to?(meth)
+  end
 
   def related_agents
     if @json.respond_to?(:related_agents)
@@ -110,50 +90,48 @@ class MARCAuthModel < ASpaceExport::ExportModel
     end
   end
 
-
   # maps name.{field} => EAC @localType attribute
   def name_part_fields
     case @type
     when 'agent_person'
       {
-        "primary_name" => "surname",
-        "title" => "title",
-        "prefix" => "prefix",
-        "rest_of_name" => "forename",
-        "suffix" => "suffix",
-        "fuller_form" => "fuller_form",
-        "number" => "numeration",
-        "qualifier" => "qualifier",
-        "dates" => "dates",
+        'primary_name' => 'surname',
+        'title' => 'title',
+        'prefix' => 'prefix',
+        'rest_of_name' => 'forename',
+        'suffix' => 'suffix',
+        'fuller_form' => 'fuller_form',
+        'number' => 'numeration',
+        'qualifier' => 'qualifier',
+        'dates' => 'dates'
       }
     when 'agent_family'
       {
-        "prefix" => "prefix",
-        "family_name" => 'surname',
-        "number" => "numeration",
-        "qualifier" => "qualifier",
-        "location" => "location",
-        "dates" => "dates",
-        "family_type" => "family_type",
+        'prefix' => 'prefix',
+        'family_name' => 'surname',
+        'number' => 'numeration',
+        'qualifier' => 'qualifier',
+        'location' => 'location',
+        'dates' => 'dates',
+        'family_type' => 'family_type'
       }
     # TODO: Remove software?
     when 'agent_software'
       {
-        "software_name" => nil,
-        "version" => nil,
-        "manufacturer" => nil
+        'software_name' => nil,
+        'version' => nil,
+        'manufacturer' => nil
       }
     when 'agent_corporate_entity'
       {
-        "primary_name" => "primary_name", 
-        "subordinate_name_1" => "subordinate_name_1", 
-        "subordinate_name_2" => "subordinate_name_2", 
-        "number" => "numeration",
-        "qualifier" => "qualifier",
-        "location" => "location",
-        "dates" => "dates",
+        'primary_name' => 'primary_name',
+        'subordinate_name_1' => 'subordinate_name_1',
+        'subordinate_name_2' => 'subordinate_name_2',
+        'number' => 'numeration',
+        'qualifier' => 'qualifier',
+        'location' => 'location',
+        'dates' => 'dates'
       }
     end
   end
-
 end
--- backend/spec/export_spec_helper.rb
+++ backend/spec/export_spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'i18n'
 require 'asutils'
@@ -10,25 +12,23 @@ if ENV['ASPACE_BACKEND_URL']
 
   include FactoryBot::Syntax::Methods
   I18n.enforce_available_locales = false # do not require locale to be in available_locales for export
-  I18n.load_path += ASUtils.find_locales_directories(File.join("enums", "#{AppConfig[:locale]}.yml"))
+  I18n.load_path += ASUtils.find_locales_directories(File.join('enums', "#{AppConfig[:locale]}.yml"))
 
-  JSONModel::init(:client_mode => true, :strict_mode => true,
-                  :url => ENV['ASPACE_BACKEND_URL'],
-                  :priority => :high)
+  JSONModel.init(client_mode: true, strict_mode: true,
+                 url: ENV['ASPACE_BACKEND_URL'],
+                 priority: :high)
 
   require_relative 'factories.rb'
 
-  auth = JSONModel::HTTP.post_form('/users/admin/login', {:password => 'admin'})
+  auth = JSONModel::HTTP.post_form('/users/admin/login', { password: 'admin' })
   JSONModel::HTTP.current_backend_session = JSON.parse(auth.body)['session']
 
   def get_xml(uri)
     uri = URI("#{ENV['ASPACE_BACKEND_URL']}#{uri}")
-    response = JSONModel::HTTP::get_response(uri)
+    response = JSONModel::HTTP.get_response(uri)
 
     if response.is_a?(Net::HTTPSuccess) || response.status == 200
       Nokogiri::XML::Document.parse(response.body)
-    else
-      nil
     end
   end
 
@@ -55,24 +55,23 @@ else
 
 end
 
-
-def get_mets(rec, dmd = "mods")
+def get_mets(rec, dmd = 'mods')
   get_xml("/repositories/#{$repo_id}/digital_objects/mets/#{rec.id}.xml?dmd=#{dmd}")
 end
-
 
 def get_marc(rec, include_unpublished = true)
   marc = get_xml("/repositories/#{$repo_id}/resources/marc21/#{rec.id}.xml?include_unpublished_marc=#{include_unpublished}")
   marc.instance_eval do
-    def df(tag, ind1=nil, ind2=nil)
-      selector ="@tag='#{tag}'"
+    def df(tag, ind1 = nil, ind2 = nil)
+      selector = "@tag='#{tag}'"
       selector += " and @ind1='#{ind1}'" if ind1
       selector += " and @ind2='#{ind2}'" if ind2
-      datafields = self.xpath("//xmlns:datafield[#{selector}]")
+      datafields = xpath("//xmlns:datafield[#{selector}]")
       datafields.instance_eval do
         def sf(code)
-          self.xpath("xmlns:subfield[@code='#{code}']")
+          xpath("xmlns:subfield[@code='#{code}']")
         end
+
         def sf_t(code)
           sf(code).inner_text
         end
@@ -86,35 +85,32 @@ def get_marc(rec, include_unpublished = true)
 end
 
 def get_marc_auth(rec, repo_id = $repo_id)
-  repo_record = JSONModel(:repository).find($repo_id)
+  xml = nil
 
   case rec.jsonmodel_type
   when 'agent_person'
-    get_xml("/repositories/#{repo_id}/marc_auth/people/#{rec.id}.xml")
+    xml = get_xml("/repositories/#{repo_id}/agents/people/marc21/#{rec.id}.xml")
   when 'agent_corporate_entity'
-    get_xml("/repositories/#{repo_id}/marc_auth/corporate_entities/#{rec.id}.xml")
+    xml = get_xml("/repositories/#{repo_id}/agents/corporate_entities/marc21/#{rec.id}.xml")
   when 'agent_family'
-    get_xml("/repositories/#{repo_id}/marc_auth/families/#{rec.id}.xml")
+    xml = get_xml("/repositories/#{repo_id}/agents/families/marc21/#{rec.id}.xml")
   end
+  xml.remove_namespaces!
 end
 
 def get_mods(rec)
   get_xml("/repositories/#{$repo_id}/digital_objects/mods/#{rec.id}.xml")
 end
 
-
 def get_dc(rec)
   get_xml("/repositories/#{$repo_id}/digital_objects/dublin_core/#{rec.id}.xml")
 end
 
 def get_labels(rec)
-  get_xml("/repositories/#{$repo_id}/resource_labels/#{rec.id}.tsv", true )
+  get_xml("/repositories/#{$repo_id}/resource_labels/#{rec.id}.tsv", true)
 end
 
-
 def get_eac(rec, repo_id = $repo_id)
-  repo_record = JSONModel(:repository).find($repo_id)
-
   case rec.jsonmodel_type
   when 'agent_person'
     get_xml("/repositories/#{repo_id}/archival_contexts/people/#{rec.id}.xml")
@@ -127,47 +123,43 @@ def get_eac(rec, repo_id = $repo_id)
   end
 end
 
-
 def multipart_note_set(publish = true)
-  ["accruals", "appraisal", "arrangement", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "altformavail", "originalsloc", "fileplan", "odd", "acqinfo", "legalstatus", "otherfindaid", "phystech", "prefercite", "processinfo", "relatedmaterial", "scopecontent", "separatedmaterial"].map do |type|
+  ['accruals', 'appraisal', 'arrangement', 'bioghist', 'accessrestrict', 'userestrict', 'custodhist', 'dimensions', 'altformavail', 'originalsloc', 'fileplan', 'odd', 'acqinfo', 'legalstatus', 'otherfindaid', 'phystech', 'prefercite', 'processinfo', 'relatedmaterial', 'scopecontent', 'separatedmaterial'].map do |type|
     build(:json_note_multipart, {
-            :publish => publish,
-            :type => type,
-            :subnotes =>  [ build(:json_note_text, :publish => publish) ]
+            publish: publish,
+            type: type,
+            subnotes: [build(:json_note_text, publish: publish)]
           })
   end
 end
-
 
 def singlepart_note_set(publish = true)
-  ["abstract", "physdesc", "physloc", "materialspec", "physfacet"].map do |type|
+  ['abstract', 'physdesc', 'physloc', 'materialspec', 'physfacet'].map do |type|
     build(:json_note_singlepart, {
-            :publish => publish,
-            :type => type
+            publish: publish,
+            type: type
           })
   end
 end
-
 
 def full_note_set(publish = true)
   multipart_note_set(publish) + singlepart_note_set(publish)
 end
 
-
 def digital_object_note_set
-  ["summary", "bioghist", "accessrestrict", "userestrict", "custodhist", "dimensions", "edition", "extent", "altformavail", "originalsloc", "note", "acqinfo", "inscription", "legalstatus", "physdesc", "prefercite", "processinfo", "relatedmaterial"].map do |type|
+  ['summary', 'bioghist', 'accessrestrict', 'userestrict', 'custodhist', 'dimensions', 'edition', 'extent', 'altformavail', 'originalsloc', 'note', 'acqinfo', 'inscription', 'legalstatus', 'physdesc', 'prefercite', 'processinfo', 'relatedmaterial'].map do |type|
     build(:json_note_digital_object, {
-            :publish => true,
-            :type => type
+            publish: true,
+            type: type
           })
   end
 end
 
 def unpublished_extent_note_set
-  ["dimensions", "physdesc"].map do |type|
+  ['dimensions', 'physdesc'].map do |type|
     build(:json_note_digital_object, {
-            :publish => false,
-            :type => type
+            publish: false,
+            type: type
           })
   end
 end
