43eed17717881a66315bcaca2b514166f7beca39
Emmanuel Rodriguez <manny@theavatar.org>
2021-03-25 10:00:54 -0600

    Using Google Translate API to automate translations of locale files for French, Spanish and Japanese

--- scripts/tasks/check.rb
+++ scripts/tasks/check.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'easy_translate'
+
+API_KEY = ENV['GOOGLE_API_KEY']
+
 module Check
   def self.run(checker)
     checker.check
@@ -10,9 +14,14 @@ module Check
     end
   end
 
-  def self.output_en(path_to_yml, values_file)
+  # used by check:translate_[es,fr,ja] rake tasks to look up values from the EN locale and translate them using the Google Translate API, and write the translations out into another YAML file.
+  def self.translate(path_to_yml, values_file, language)
+    EasyTranslate.api_key = API_KEY
+
     path_to_yml = File.join(File.dirname(__FILE__), "..", "..", path_to_yml)
     yaml = YAML.load(File.read(path_to_yml))
+
+    yaml_trans = {}
 
     File.readlines(values_file).each do |line|
       line.strip!
@@ -20,11 +29,37 @@ module Check
       value = yaml.dig('en', *key)
 
       if value
-        puts value.gsub(/\n+/, '')
+        value.gsub!(/\n+/, '')
       else
-        puts ""
+        value = ""
+      end
+
+      value_trans = EasyTranslate.translate(value, from: :en, to: language.to_sym, format: :html)
+
+      add_value!(yaml_trans, key.unshift(language), value_trans)
+    end
+
+    write_yaml_file(yaml_trans, language)
+  end
+
+  # Adds a translated value to the hash representing the YAML file
+  def self.add_value!(yaml_hash, key, value)
+    base = yaml_hash
+
+    while k = key.shift
+      if base[k].nil? && key.length > 0
+        base[k] = {}
+        base = base[k]
+      elsif key.length > 0
+        base = base[k]
+      else
+        base[k] = value
       end
     end
+  end
+
+  def self.write_yaml_file(yaml_hash, lang)
+    File.open("#{lang}.yml", 'w') { |file| file.write(yaml_hash.to_yaml) }
   end
 
   class Locales
