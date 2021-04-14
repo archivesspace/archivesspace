f9970c1c1cafbb2f3a4f5474f811a0bd3e1c3349
lorawoodford <lorawoodford@gmail.com>
2021-04-14 10:00:16 -0400

    Update migration output

--- common/db/migrations/146_tc_link_cleanup.rb
+++ common/db/migrations/146_tc_link_cleanup.rb
@@ -8,9 +8,15 @@ Sequel.migration do
 
       bad_rlshp = self[:top_container_link_rlshp].exclude(sub_container_id: nil).where(top_container_id: nil).all
       if bad_rlshp.count.zero?
-        $stderr.puts("Good news! No corrupt top container relationship records found in DB.")
+        $stderr.puts("Good news! No corrupt top container relationship records found.")
       else
-        $stderr.puts("#{bad_rlshp.count} corrupt records found in DB.")
+        $stderr.puts("\n")
+        $stderr.puts("==========================================")
+        $stderr.puts("\n")
+        $stderr.puts("#{bad_rlshp.count} corrupted top container links found due " +
+                     "to a previous failed merge of linked containers into an " + 
+                     "unlinked container.")
+        $stderr.puts("\n")
       end
 
       bad_rlshp.each do |r|
@@ -19,7 +25,7 @@ Sequel.migration do
           record = self[:instance].where(id: instance).get(:"#{type}_id")
           next if record.nil?
 
-          $stderr.puts("Bad #{type} record found: #{record.inspect}")
+          $stderr.puts("     Bad top container link found on #{type} #{record}")
           repo = self[:"#{type}"].where(id: record).get(:repo_id)
           new_tc = self[:top_container].where(indicator: 'Lost and found',
                                               repo_id: repo)
@@ -33,14 +39,21 @@ Sequel.migration do
                                                  system_mtime: Time.now,
                                                  user_mtime: Time.now)
 
-            $stderr.puts("Created new top container #{new_tc} for repository #{repo} corrupted records.")
+            $stderr.puts("     Creating top container #{new_tc} in repository #{repo}")
           end
 
           self[:top_container_link_rlshp].where(id: r[:id]).update(top_container_id: new_tc)
-          $stderr.puts("Updated top container releationship for #{type} record #{record} to point to top container #{new_tc} in repository #{repo}.")
+          $stderr.puts("     Linking #{type} #{record} to top container #{new_tc} in repository #{repo}")
         end
       end
     end
+    $stderr.puts("\n")
+    $stderr.puts("To identify the records that need to be corrected, do a keyword " + 
+                 "search for the 'Lost and Found' top container in the Manage " +
+                 "Top Containers area within the application.")
+    $stderr.puts("\n")
+    $stderr.puts("==========================================")
+    $stderr.puts("\n")
   end
 
 
