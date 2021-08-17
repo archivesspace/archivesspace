e593828bca33f10eb784d47caab070b1cbb9c127
Brian Zelip <bzelip@protonmail.com>
2021-08-17 07:33:33 -0400

    Remove Lodash.js, replace with js and jquery

--- frontend/app/assets/javascripts/preferences.js
+++ frontend/app/assets/javascripts/preferences.js
@@ -8,17 +8,17 @@ $(function() {
   sortable = Sortable.create(el, {});
 
   $('form#new_preference').submit(function(event) {
-
-   _.each(sortable.toArray(), function(note_type, i) {
-
-     $('<input>').attr({
-       type: 'hidden',
-       id: 'preference_defaults__note_order',
-       name: 'preference[defaults][note_order]['+i+']',
-       value: note_type
-     }).appendTo(event.target);
-   });
-
+    // .toArray() via Sortable
+    sortable.toArray().forEach(function (noteType, i) {
+      $('<input>')
+        .attr({
+          type: 'hidden',
+          id: 'preference_defaults__note_order',
+          name: 'preference[defaults][note_order][' + i + ']',
+          value: noteType
+        })
+        .appendTo(event.target);
+    });
   });
 
 });
