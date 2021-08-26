842c610e7e1c7b9a242678f8de078600ad342275
Mark Triggs <mark@dishevelled.net>
2021-08-26 16:21:31 +1000

    Drop the unique check on user_value for now

--- common/db/migrations/142_ark_uniqueness.rb
+++ common/db/migrations/142_ark_uniqueness.rb
@@ -7,9 +7,5 @@ Sequel.migration do
       String :record_uri, :null => false, :unique => true
       String :generated_value, :null => false, :unique => true
     end
-
-    alter_table(:ark_name) do
-      add_unique_constraint([:user_value], :name => "ark_user_value_uniq")
-    end
   end
 end
