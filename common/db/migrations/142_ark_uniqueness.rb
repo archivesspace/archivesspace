4ddacdde36a8fa4fc8474928a71e93c7ec1d4619
Mark Triggs <mark@dishevelled.net>
2021-08-26 15:38:17 +1000

    Ensure generated Ark values are unique to a record

---
+++ common/db/migrations/142_ark_uniqueness.rb
@@ -0,0 +1,15 @@
+require_relative 'utils'
+
+Sequel.migration do
+  up do
+    create_table(:ark_uniq_check) do
+      primary_key :id
+      String :record_uri, :null => false
+      String :generated_value, :null => false, :unique => true
+    end
+
+    alter_table(:ark_name) do
+      add_unique_constraint([:user_value], :name => "ark_user_value_uniq")
+    end
+  end
+end
