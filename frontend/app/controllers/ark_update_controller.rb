11cf7deb93ef12bb11ae48b8df06f70311b20060
Mark Triggs <mark@dishevelled.net>
2021-09-09 14:28:32 +1000

    Interface for editing ARKs as the admin user

---
+++ frontend/app/controllers/ark_update_controller.rb
@@ -0,0 +1,19 @@
+class ArkUpdateController < ApplicationController
+
+  set_access_control  "administer_system" => [:update]
+
+  def update
+    uri = "#{JSONModel::HTTP.backend_url}#{params[:uri]}/ark_name"
+
+    response = JSONModel::HTTP::post_json(URI(uri), params[:ark_name])
+
+    if response.code == '200'
+      render :plain => "OK"
+    else
+      err = ASUtils.json_parse(response.body)
+      render :plain => err.fetch('error'), :status => response.code
+    end
+  end
+
+end
+
