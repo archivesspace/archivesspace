c58544dff4f31762eb5480132796643369e6be3a
lorawoodford <lorawoodford@gmail.com>
2021-10-22 12:39:20 -0400

    ANW-1421 Don't allow bulk import to import bad date types

--- backend/app/lib/bulk_import/bulk_import_mixins.rb
+++ backend/app/lib/bulk_import/bulk_import_mixins.rb
@@ -220,18 +220,24 @@ module BulkImportMixins
   # The following methods assume @report is defined, and is a BulkImportReport object
   def create_date(dates_label, date_begin, date_end, date_type, expression, date_certainty)
     date_str = "(Date: type:#{date_type}, label: #{dates_label}, begin: #{date_begin}, end: #{date_end}, expression: #{expression})"
+    date = {}
+
     begin
-      date_type = @date_types.value(date_type || "inclusive")
+      date['date_type'] = @date_types.value(date_type || "inclusive")
     rescue Exception => e
-      @report.add_errors(I18n.t("bulk_import.error.date_type", :what => date_type, :date_str => date_str))
+      @report.add_errors(I18n.t("bulk_import.error.date_type",
+                                :what => date_type,
+                                :date_str => date_str))
+
+      return nil
     end
     begin
-      date = { "date_type" => date_type,
-               "label" => @date_labels.value(dates_label || "creation") }
+      date['label'] = @date_labels.value(dates_label || "creation")
     rescue Exception => e
       @report.add_errors(I18n.t("bulk_import.error.date_label",
-                                :what => dates_label, :date_str => date_str))
-      #don't bother processsing if the label mis-matches
+                                :what => dates_label,
+                                :date_str => date_str))
+
       return nil
     end
 
@@ -242,6 +248,7 @@ module BulkImportMixins
         @report.add_errors(I18n.t("bulk_import.error.certainty", :what => e.message, :date_str => date_str))
       end
     end
+
     date["begin"] = date_begin if date_begin
     date["end"] = date_end if date_end
     date["expression"] = expression if expression
