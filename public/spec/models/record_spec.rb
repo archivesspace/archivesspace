cb61cfec579735cc56d3a312d1feb6a551e43597
Brian Hoffman <brian.hoffman@lyrasis.org>
2021-11-16 13:44:00 -0500

    ensure untitled object display strings in PUI use date for display

--- public/spec/models/record_spec.rb
+++ public/spec/models/record_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Record model" do
 
-  it "builds a display string for an untitled record using its parent's title" do
+  it "builds a display string for an untitled record using its parent resource and its date" do
     solr_result = ASUtils.json_parse(File.read(File.join(FIXTURES_DIR, 'solr_response.json')))
     record = Record.new(solr_result)
-    expect(record.display_string).to eq "From the item: Resource with child inheriting title"
+    expect(record.display_string).to eq "Resource with child inheriting title, bulk: 1900s"
   end
 end
