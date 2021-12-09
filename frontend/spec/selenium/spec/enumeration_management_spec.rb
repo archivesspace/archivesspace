2ec7fdf5a4de385248c519039dd9dfe0b53d1f1f
Manny Rodriguez <manny@theavatar.org>
2021-12-09 10:53:46 -0700

    bugfix for not being able to set an enumeration default
    when there is a value in the enumeration that is suppressed

--- frontend/spec/selenium/spec/enumeration_management_spec.rb
+++ frontend/spec/selenium/spec/enumeration_management_spec.rb
@@ -257,4 +257,26 @@ describe 'Enumeration Management' do
 
     @driver.click_and_wait_until_gone(css: "form#delete_enumeration button[type='submit']")
   end
+
+  it 'lets you set a default value with another value suppressed' do
+    @driver.go_home
+    @driver.get($frontend)
+    @driver.find_element(:link, 'System').click
+    @driver.wait_for_dropdown
+    @driver.click_and_wait_until_gone(:link, 'Manage Controlled Value Lists')
+
+    enum_select = @driver.find_element(id: 'enum_selector')
+    enum_select.select_option_with_text('Subject Term Type (subject_term_type)')
+
+    # Wait for the table of enumerations to load
+    @driver.find_element(:css, '.enumeration-list')
+
+    topical = @driver.find_element_with_text('//tr', /topical/)
+    @driver.click_and_wait_until_element_gone(topical.find_element(:link, 'Suppress'))
+
+    temporal = @driver.find_element_with_text('//tr', /temporal/)
+    @driver.click_and_wait_until_element_gone(temporal.find_element(:link, 'Set as Default'))
+
+    temporal.find_element(:link, 'Unset Default')
+  end
 end
