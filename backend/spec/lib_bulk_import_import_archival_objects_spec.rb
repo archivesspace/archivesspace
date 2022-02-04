4483023b616ce9bfe24ef1534472c7bf2f2ff465
Brian Hoffman <brian.hoffman@lyrasis.org>
2022-02-04 06:05:36 -0500

    fix column key publish -> digital _object_publish
    (cherry picked from commit 844701778e2523678a19aa6e45020bae2c542259)

--- backend/spec/lib_bulk_import_import_archival_objects_spec.rb
+++ backend/spec/lib_bulk_import_import_archival_objects_spec.rb
@@ -4,6 +4,7 @@ require_relative "../app/lib/bulk_import/import_archival_objects.rb"
 
 describe "Import Archival Objects" do
   BULK_FIXTURES_DIR = File.join(File.dirname(__FILE__), "fixtures", "bulk_import")
+  TEMPLATES_DIR = File.join(File.dirname(__FILE__), "../", "../", "templates")
   before(:each) do
     @current_user = User.find(:username => "admin")
     # create the resource
@@ -33,6 +34,38 @@ describe "Import Archival Objects" do
     id = resource.save
     @resource = Resource.get_or_die(id)
   end
+
+  # we don't want to test fixtures with different keys than the real templates!
+  it "keeps the example fixtures up to date with the templates" do
+    xlsx_fixture = BULK_FIXTURES_DIR + "/bulk_import_VFIRST01_test01.xlsx"
+    xlsx_template = TEMPLATES_DIR + "/bulk_import_template.xlsx"
+    xlsx_fixture = RubyXL::Parser.parse(xlsx_fixture)
+    xlsx_template = RubyXL::Parser.parse(xlsx_template)
+
+    fixture_keys = xlsx_fixture.worksheets[0][3].cells.map { |c| c.value }
+    template_keys = xlsx_template.worksheets[0][3].cells.map { |c| c.value }
+    expect(template_keys - fixture_keys).to be_empty
+    expect(fixture_keys - template_keys).to be_empty
+
+    csv_fixture = BULK_FIXTURES_DIR + "/bulk_import_VFIRST01_test01.csv"
+    csv_template = TEMPLATES_DIR + "/bulk_import_template.csv"
+    csv_fixture = CSV.read(csv_fixture)
+    csv_template = CSV.read(csv_template)
+
+    fixture_keys = csv_fixture.to_a.first
+    template_keys = csv_template.to_a.first
+    expect(template_keys - fixture_keys).to be_empty
+    expect(fixture_keys - template_keys).to be_empty
+  end
+
+  it "doesn't have any duplicate keys in the template" do
+    xlsx_template = TEMPLATES_DIR + "/bulk_import_template.xlsx"
+    xlsx_template = RubyXL::Parser.parse(xlsx_template)
+
+    template_keys = xlsx_template.worksheets[0][3].cells.map { |c| c.value }
+    expect(template_keys.size).to eq template_keys.uniq.size
+  end
+
   it "reads in excel spreadsheet at the resource level" do
     opts = { :repo_id => @resource[:repo_id],
              :rid => @resource[:id],
@@ -52,12 +85,14 @@ describe "Import Archival Objects" do
     expect(report.rows[1].archival_object_display).to eq("A subseries, 2010 - 2011")
     tree = JSONModel(:resource_tree).find(nil, :resource_id => @resource.id).to_hash
     children = tree["children"]
-    #    aos = []
     children.each_with_index do |child, index|
       expect(child["record_uri"]).to eq(report.rows[index].archival_object_id)
-      #      ao = JSONModel(:archival_object).find_by_uri(child["record_uri"]).to_hash
-      #      aos << ao
     end
+    child = JSONModel(:archival_object).find(children[0]['id'])
+    dig_obj = JSONModel(:digital_object).find(
+      JSONModel(:digital_object).id_for(child.instances[0]['digital_object']['ref'])
+    )
+    expect(dig_obj.publish).to be false
   end
   it "reads in CSV spreadsheet at the resource level" do
     opts = { :repo_id => @resource[:repo_id],
