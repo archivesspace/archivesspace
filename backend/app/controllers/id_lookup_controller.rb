69b9358d1e61d3089c603046fa70ef9af0348880
crugas <cschmidtuga@gmail.com>
2022-04-08 13:56:21 -0400

    Added python and shell examples to endpoints except resources

--- backend/app/controllers/id_lookup_controller.rb
+++ backend/app/controllers/id_lookup_controller.rb
@@ -15,6 +15,50 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.get('/repositories/:repo_id/find_by_id/archival_objects')
     .description("Find Archival Objects by ref_id or component_id")
+    .example("shell") do
+      <<~SHELL
+        curl -s -F password="admin" "http://localhost:8089/users/admin/login"
+        # Replace "admin" with your password and "http://localhost:8089/users/admin/login" with your ASpace API URL
+        # followed by "/users/{your_username}/login"
+    
+        set SESSION="session_id"
+        # If using Git Bash, replace set with export
+    
+        curl -H "X-ArchivesSpace-Session: $SESSION" //
+        "http://localhost:8089/repositories/2/find_by_id/archival_objects?component_id[]=hello_im_a_component_id"
+        # Replace "http://localhost:8089" with your ASpace API URL, "2" with the repository ID, and 
+        # "hello_im_a_component_id" with the component ID you are searching for
+
+        curl -H "X-ArchivesSpace-Session: $SESSION" //
+        "http://localhost:8089/repositories/2/find_by_id/archival_objects?ref_id[]=hello_im_a_ref_id"
+        # Replace "http://localhost:8089" with your ASpace API URL, "2" with the repository ID, and 
+        # "hello_im_a_ref_id" with the ref ID you are searching for
+      SHELL
+    end
+    .example("python") do
+      <<~PYTHON
+        from asnake.client import ASnakeClient  # import the ArchivesSnake client
+        
+        client = ASnakeClient(baseurl="http://localhost:8089", username="admin", password="admin")
+        # Replace "http://localhost:8089" with your ArchivesSpace API URL and "admin" for your username and password
+        
+        client.authorize()  # authorizes the client
+        
+        find_ao_compid = client.get("repositories/2/find_by_id/archival_objects", params={"component_id[]": 
+                                                                                          "hello_im_a_component_id"})
+        # Replace "2" with the repository ID and "hello_im_a_component_id" with the component ID you are searching for
+
+        print(find_ao_compid.json())
+        # Output (dict): {'archival_objects': [{'ref': '/repositories/2/archival_objects/708425'}]}
+
+        find_ao_refid = client.get("repositories/2/find_by_id/archival_objects", params={"ref_id[]": 
+                                                                                         "hello_im_a_ref_id"})
+        # Replace "2" with the repository ID and "hello_im_a_ref_id" with the ref ID you are searching for
+        
+        print(find_ao_refid.json())
+        # Output (dict): {'archival_objects': [{'ref': '/repositories/2/archival_objects/708425'}]}
+      PYTHON
+    end
     .params(["repo_id", :repo_id],
             ["ref_id", [String], "An archival object's Ref ID (param may be repeated)", :optional => true],
             ["component_id", [String], "An archival object's component ID (param may be repeated)", :optional => true],
@@ -29,6 +73,38 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.get('/repositories/:repo_id/find_by_id/digital_object_components')
     .description("Find Digital Object Components by component_id")
+    .example("shell") do
+      <<~SHELL
+        curl -s -F password="admin" "http://localhost:8089/users/admin/login"
+        # Replace "admin" with your password and "http://localhost:8089/users/admin/login" with your ASpace API URL
+        # followed by "/users/{your_username}/login"
+    
+        set SESSION="session_id"
+        # If using Git Bash, replace set with export
+    
+        curl -H "X-ArchivesSpace-Session: $SESSION" //
+        "http://localhost:8089/repositories/2/find_by_id/digital_object_components?component_id[]=im_a_do_component_id"
+        # Replace "http://localhost:8089" with your ASpace API URL, "2" with the repository ID, and 
+        # "im_a_do_component_id" with the component ID you are searching for
+      SHELL
+    end
+    .example("python") do
+      <<~PYTHON
+        from asnake.client import ASnakeClient  # import the ArchivesSnake client
+        
+        client = ASnakeClient(baseurl="http://localhost:8089", username="admin", password="admin")
+        # Replace "http://localhost:8089" with your ArchivesSpace API URL and "admin" for your username and password
+        
+        client.authorize()  # authorizes the client
+        
+        find_do_comp = client.get("repositories/2/find_by_id/digital_object_components", 
+                                  params={"component_id[]": "im_a_do_component_id"})
+        # Replace "2" with the repository ID and "im_a_do_component_id" with the component ID you are searching for
+  
+        print(find_do_comp.json())
+        # Output (dict): {'digital_object_components': [{'ref': '/repositories/2/digital_object_components/1'}]}
+      PYTHON
+    end
     .params(["repo_id", :repo_id],
             ["component_id", [String], "A digital object component's component ID (param may be repeated)", :optional => true],
             ["resolve", :resolve])
