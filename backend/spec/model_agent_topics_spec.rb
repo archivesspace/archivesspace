59759fd6602cf5f410c2a88a92e8ccd769757a48
Manny Rodriguez <manny@theavatar.org>
2022-05-19 10:00:41 -0600

    test fixes

--- backend/spec/model_agent_topics_spec.rb
+++ backend/spec/model_agent_topics_spec.rb
@@ -14,17 +14,17 @@ describe 'AgentTopic model' do
     }.to raise_error(JSONModel::ValidationException)
   end
 
-  it "won't let you delete a subject that has a role in a topic" do
+  it "will let you delete a subject that has a role in a topic" do
     subject = create(:json_subject)
     subject_place = create(:json_subject)
     agent_topic = AgentTopic.create_from_json(build(:json_agent_topic, subjects: [{'ref' => subject.uri}], places: [{'ref' => subject_place.uri}]))
 
     expect {
       Subject[subject.id].delete
-    }.to raise_error ConflictException
+    }.to_not raise_error
 
     expect {
       Subject[subject_place.id].delete
-    }.to raise_error ConflictException
+    }.to_not raise_error
   end
 end
