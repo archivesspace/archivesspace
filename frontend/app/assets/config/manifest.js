fd55243c94a1b0bf3d3b92125ca6c17704389cd1
summer-cook <summercook@bellsouth.net>
2022-07-20 20:15:41 -0500

    fix a couple more scss issues

--- frontend/app/assets/config/manifest.js
+++ frontend/app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link application.css
 //= link_directory ../../../vendor/assets/javascripts
