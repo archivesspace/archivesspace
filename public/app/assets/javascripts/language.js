2aa6f701cc7a7ef7a532c12a0ff0516d9673c42e
Manny Rodriguez <manny@theavatar.org>
2022-08-10 18:40:51 -0600

    prettier fixes

--- public/app/assets/javascripts/language.js
+++ public/app/assets/javascripts/language.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('#language_select_dropdown').on('change', function() {
+$(document).ready(function () {
+  $('#language_select_dropdown').on('change', function () {
     $('#language_select_form').trigger('submit');
-	});
+  });
 });
