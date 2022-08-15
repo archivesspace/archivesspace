49dd37d5d3a5efe4734679b726ac30677d4bb43f
Donald Smith <dsmith@alum.rit.edu>
2022-08-15 11:18:09 -0400

    fix indentation

--- frontend/spec/features/context_sensitive_help_spec.rb
+++ frontend/spec/features/context_sensitive_help_spec.rb
@@ -4,18 +4,18 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe 'Context Sensitive Help', js: true do
-    before(:each) do
-        login_admin
-    end
+  before(:each) do
+    login_admin
+  end
     
-    it 'displays a clickable tooltip for a field label' do
-        visit('/accessions/new')
+  it 'displays a clickable tooltip for a field label' do
+    visit('/accessions/new')
 
-        find('label[for="accession_title_"]').hover
-        expect(page).to have_content('The title assigned')
+    find('label[for="accession_title_"]').hover
+    expect(page).to have_content('The title assigned')
 
-        # hover somewhere else to make it disappear
-        find('h2', text: 'New Accession').hover
-        expect(page).not_to have_content('The title assigned')
-    end
+    # hover somewhere else to make it disappear
+    find('h2', text: 'New Accession').hover
+    expect(page).not_to have_content('The title assigned')
+  end
 end
