2533367844aa69375b1899627595b12eb4ad39df
summer-cook <summercook@bellsouth.net>
2022-09-07 16:01:04 -0700

    fix JS error in console

--- frontend/app/assets/javascripts/bootstrap_overrides.js
+++ frontend/app/assets/javascripts/bootstrap_overrides.js
@@ -1,6 +1,8 @@
 // Fix focus issue when two modals are on the page
 // see http://stackoverflow.com/questions/13649459/twitter-bootstrap-multiple-modal-error
-$.fn.modal.Constructor.prototype.enforceFocus = function () {};
+
+// commenting this out for now since it doesn't work with BS5 & causes a console error
+// $.fn.modal.Constructor.prototype.enforceFocus = function () {};
 
 // console.log("BS OVERRIDE");
 
