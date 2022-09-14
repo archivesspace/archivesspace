934f6666749bc48e73a0b179c62f963c0f2218bf
Brian Hoffman <brian.hoffman@lyrasis.org>
2022-09-14 16:01:16 -0400

    monkey patch warbler for ostruct bug

--- public/config/environments/production.rb
+++ public/config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
