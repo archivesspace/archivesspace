f08695cdece4fd855ae1d0794521aced1a6a8080
Brian Zelip <brian.zelip@lyrasis.org>
2023-02-17 06:46:46 -0500

    ANW-1533: Show rep file version in PUI search results (#2932)
    Shows thumbnail for dobj, dobjc, resources, aobj, and accessions records.

---
+++ public/spec/features/representative_file_version_spec.rb
@@ -0,0 +1,153 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Representative File Version', js: true do
+  describe 'search result thumbnail' do
+    before(:all) do
+      @img_1 = 'https://www.archivesspace.org/demos/Congreave%20E-4/ms292_008.jpg'
+      @img_2 = 'https://www.archivesspace.org/demos/Congreave%20E-1/ms292_001.jpg'
+      @caption_1 = 'This is caption 1'
+      @caption_2 = 'This is caption 2'
+      @long_caption = 'This is the long caption whose character count exceeds 56'
+      @max_caption_chars = 42
+      @dobj_with_repfv = create(
+        :digital_object,
+        publish: true,
+        title: 'Digital object with representative file version',
+        file_versions: [
+          {
+            publish: true,
+            is_representative: true,
+            file_uri: @img_1,
+            use_statement: 'image-thumbnail',
+            caption: @caption_1
+          }
+        ]
+      )
+      @dobjc_with_repfv = create(:digital_object_component,
+        publish: true,
+        digital_object: { ref: @dobj_with_repfv.uri },
+        title: 'Digital object component with representative file version',
+        file_versions: [
+          {
+            publish: true,
+            is_representative: true,
+            file_uri: @img_2,
+            use_statement: 'image-thumbnail',
+            caption: @caption_2
+          }
+        ]
+      )
+      @resource_with_repfv = create(:resource,
+        publish: true,
+        title: 'Resource with representative file version',
+        instances: [build(:instance_digital,
+          digital_object: { ref: @dobj_with_repfv.uri },
+          is_representative: true
+        )]
+      )
+      @aobj_with_repfv = create(:archival_object,
+        publish: true,
+        title: 'Archival Object with representative file version',
+        resource: {'ref' => @resource_with_repfv.uri},
+        instances: [build(:instance_digital,
+          digital_object: { ref: @dobj_with_repfv.uri },
+          is_representative: true
+        )]
+      )
+      @accession_with_repfv = create(:accession,
+        publish: true,
+        title: 'Accession with representative file version',
+        instances: [build(:instance_digital,
+          digital_object: { ref: @dobj_with_repfv.uri },
+          is_representative: true
+        )]
+      )
+
+      @dobj_with_repfv_long_caption = create(
+        :digital_object,
+        publish: true,
+        title: 'Digital object with representative file version and long caption',
+        file_versions: [
+          {
+            publish: true,
+            is_representative: true,
+            file_uri: @img_1,
+            use_statement: 'image-thumbnail',
+            caption: @long_caption
+          }
+        ]
+      )
+
+      run_indexers
+    end
+
+    it 'is shown for digital objects' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @dobj_with_repfv.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@dobj_with_repfv.uri}']" do
+        expect(page.find("img.result-repfv__img", visible: :all)[:src]).to eq @img_1
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @caption_1
+      end
+    end
+
+    it 'is shown for digital object components' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @dobjc_with_repfv.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@dobjc_with_repfv.uri}']" do
+        expect(page.find("img.result-repfv__img", visible: :all)[:src]).to eq @img_2
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @caption_2
+      end
+    end
+
+    it 'is shown for resources' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @resource_with_repfv.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@resource_with_repfv.uri}']" do
+        expect(page.find("img.result-repfv__img", visible: :all)[:src]).to eq @img_1
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @caption_1
+      end
+    end
+
+    it 'is shown for archival objects' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @aobj_with_repfv.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@aobj_with_repfv.uri}']" do
+        expect(page.find("img.result-repfv__img", visible: :all)[:src]).to eq @img_1
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @caption_1
+      end
+    end
+
+    it 'is shown for accessions' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @accession_with_repfv.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@accession_with_repfv.uri}']" do
+        expect(page.find("img.result-repfv__img", visible: :all)[:src]).to eq @img_1
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @caption_1
+      end
+    end
+
+    it 'truncates captions longer than 42 characters with an appended ellipsis' do
+      visit('/')
+      page.fill_in 'Enter your search terms', with: @dobj_with_repfv_long_caption.title
+      click_button 'Search'
+
+      within ".recordrow[data-uri='#{@dobj_with_repfv_long_caption.uri}']" do
+        expect(page.find('figcaption.result-repfv__figcaption')).to have_content @long_caption
+          .split('')
+          .first(@max_caption_chars)
+          .join('') + '...'
+      end
+    end
+  end
+end
