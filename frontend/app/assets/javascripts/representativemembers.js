f3fad7b1982c9b6d7e6f096865e5995efa637761
Manny Rodriguez <manny@theavatar.org>
2023-05-04 11:10:38 -0600

    prettier and lint fixes

--- frontend/app/assets/javascripts/representativemembers.js
+++ frontend/app/assets/javascripts/representativemembers.js
@@ -6,7 +6,7 @@ $(function () {
       $subform.removeClass('is-representative');
     }
 
-    $(':input[name$="[' + field_name +']"]', $subform).val(
+    $(':input[name$="[' + field_name + ']"]', $subform).val(
       isRepresentative ? 1 : 0
     );
 
@@ -23,20 +23,18 @@ $(function () {
         object_name === 'agent_contact' ||
         object_name === 'linked_agent'
       ) {
-
         // ANW-504: for linked agents, the field that stores the switch denoting a representative number is 'is_primary'
-        if ( object_name === 'linked_agent' ) {
-          var field_name = "is_primary"
-        } 
-        else {
-          var field_name = "is_representative"
+        var rep_field_name;
+        if (object_name === 'linked_agent') {
+          rep_field_name = 'is_primary';
+        } else {
+          rep_field_name = 'is_representative';
         }
-
 
         const $subform = $(subform);
         const $section = $subform.closest('section.subrecord-form');
         const isRepresentative =
-          $(':input[name$="[' + field_name + ']"]', $subform).val() === '1';
+          $(':input[name$="[' + rep_field_name + ']"]', $subform).val() === '1';
         const $labelBtn = $subform.find('.is-representative-label');
         const $repBtn = $subform.find('.is-representative-toggle');
         const eventName =
@@ -56,7 +54,7 @@ $(function () {
               $subform.hasClass('is-representative') &&
               $(this).prop('checked', true)
             ) {
-              handleRepresentativeChange($subform, false, field_name);
+              handleRepresentativeChange($subform, false, rep_field_name);
               $(this).prop('checked', false);
             }
 
@@ -81,14 +79,14 @@ $(function () {
         $labelBtn.click(function (e) {
           e.preventDefault();
           $(this).parent().off('click');
-          handleRepresentativeChange($subform, false, field_name);
+          handleRepresentativeChange($subform, false, rep_field_name);
         });
 
         $section.on(eventName, function (e, representative_subform) {
           handleRepresentativeChange(
             $subform,
             representative_subform == $subform,
-            field_name
+            rep_field_name
           );
           $('.tooltip').tooltip('hide');
         });
