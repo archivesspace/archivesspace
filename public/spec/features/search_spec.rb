4ac51d1325530ad86a0d406ac2a6db2ef20b0159
Manny Rodriguez <manny@theavatar.org>
2023-05-17 17:08:08 -0600

    Adding option to sort search results by identifier in PUI

--- public/spec/features/search_spec.rb
+++ public/spec/features/search_spec.rb
@@ -24,4 +24,37 @@ describe 'Search', js: true do
     find('#q1').native.send_keys(:return)
     expect(page).to have_content('Showing Results')
   end
+
+  it "should sort by identifier on results page" do
+    visit('/search')
+    click_on('Add a search row')
+    find('#q1').native.send_keys(:return)
+
+    expect(page).to have_content('Showing Results')
+    expect(page).to have_content('Published Accession')
+    expect(page).to have_content('Published Accession with Deaccession')
+    expect(page).to have_content('Accession for Phrase Search')
+    expect(page).to have_content('Accession with Relationship')
+    expect(page).to have_content('Accession with Deaccession')
+    expect(page).to have_content('Accession with Lang/Script')
+    expect(page).to have_content('Accession with Lang Material Note')
+    expect(page).to have_content('Accession without Lang Material Note')
+    expect(page).to have_content('Published Resource')
+    expect(page).to have_content('Resource with Deaccession')
+
+    find('#sort').select("Identifier (descending)")
+
+    click_on('Sort')
+
+    expect(page).to have_content('Digital Object 5')
+    expect(page).to have_content('Digital Object 4')
+    expect(page).to have_content('Digital Object 3')
+    expect(page).to have_content('Digital Object 2')
+    expect(page).to have_content('Digital Object With Classification')
+    expect(page).to have_content('Digital Object 1')
+    expect(page).to have_content('Born digital')
+    expect(page).to have_content('Resource with digital instance')
+    expect(page).to have_content('Resource with scope note')
+    expect(page).to have_content('Resource with Subject')
+  end
 end
