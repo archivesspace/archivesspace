52d045761a012be37fad9247c5966640a7f3f546
Mark Cooper <mark.c.cooper@outlook.com>
2023-08-29 17:22:00 +0000

    Return display string in compact resource respresentations
    Not all record types have (or require) titles. However the
    PUI breadcrumbs for search were expecting a title, which
    when absent results in an error.
    
    This is resolved by including the display string in compact
    results and using that as a fallback when title is not
    present.

--- backend/app/model/search_resolver_compact_resource.rb
+++ backend/app/model/search_resolver_compact_resource.rb
@@ -1,6 +1,6 @@
 class SearchResolverCompactResource
 
-  FIELDS_TO_KEEP = ['id_0', 'id_1', 'id_2', 'id_3', 'level', 'other_level', 'title', 'uri', 'publish']
+  FIELDS_TO_KEEP = ['id_0', 'id_1', 'id_2', 'id_3', 'level', 'other_level', 'title', 'display_string', 'uri', 'publish']
 
   # Really just including this as a demo.  Let's parse the JSON and extract a few fields.
   def resolve(record)
