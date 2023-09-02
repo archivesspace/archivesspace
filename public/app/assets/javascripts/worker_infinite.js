0aa1a6a97a01f74550901d63d2b99a44200c6cb2
Brian Zelip <brian.zelip@lyrasis.org>
2023-09-02 16:37:34 -0400

    Use a Worker to fetch all remaining empty waypoints
    Have to look into Chrome routinely erring out on 400+
    fetch() requests; FF and Safari both work as expected

---
+++ public/app/assets/javascripts/worker_infinite.js
@@ -0,0 +1,55 @@
+onmessage = async e => postMessage(await fetchWaypoints(e.data));
+
+/**
+ * fetchWaypoints
+ * @description Fetch one or more waypoints of records
+ * @param {object} data - The data the worker receives
+ * @param {string} data.resourceUri - The uri of the collection resource,
+ * ie: /repositories/18/resources/11861
+ * @param {[wpNum, uris]} data.waypointTuples - Array of tuples of waypoint metadata
+ * @typedef {number} wpNum - The waypoint number
+ * @typedef {string[]} uris - Array of record uris belonging to the waypoint
+ * @returns {Promise} - A Promise that resolves to an array of waypoint
+ * objects, each with the signature: `{ wpNum, records }`
+ */
+async function fetchWaypoints(data) {
+  const { waypointTuples, resourceUri } = data;
+
+  const promises = waypointTuples.map(tuple =>
+    fetchWaypoint(...tuple, resourceUri)
+  );
+
+  return await Promise.all(promises).catch(err => {
+    console.error(err);
+  });
+}
+
+/**
+ * fetchWaypoint
+ * @description Fetch a waypoint of records
+ * @param {number} wpNum - the waypoint number to fetch
+ * @param {string[]} uris - Array of record uris belonging to the waypoint
+ * @param {string} resourceUri - The uri of the collection resource,
+ * ie: /repositories/18/resources/11861
+ * @returns {Promise} - Promise that resolves with the waypoint object made up of
+ * keys of record uris and values of record markup
+ */
+async function fetchWaypoint(wpNum, uris, resourceUri) {
+  const origin = self.location.origin;
+  const query = new URLSearchParams();
+
+  uris.forEach(uri => {
+    query.append('urls[]', uri);
+  });
+
+  const url = `${origin}${resourceUri}/infinite/waypoints?${query}`;
+
+  try {
+    const response = await fetch(url);
+    const records = await response.json();
+
+    return { wpNum, records };
+  } catch (err) {
+    console.error(err);
+  }
+}
