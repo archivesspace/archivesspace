a43ecb4b64301926857ddbb8df193f496a4a061d
Brian Zelip <brian.zelip@lyrasis.org>
2023-09-14 10:46:01 -0400

    Use async generator for better batch fetching from worker
    Update the data model between worker and main
    Refactor the content tree event listener into the tree code
    Allow updateShowingCurrent() to close the modal
    Clean up code a bit

--- public/app/assets/javascripts/worker_infinite.js
+++ public/app/assets/javascripts/worker_infinite.js
@@ -1,20 +1,44 @@
-onmessage = async e => postMessage(await fetchWaypoints(e.data));
+// Chrome seems to have a max of 1350 fetches per process, anything more and
+// it throws a `net::ERR_INSUFFICIENT_RESOURCES` error. Here we use an
+// async generator to fetch waypoints in batches. Don't go over 1350 batch size,
+// 1000 takes a while, use a smaller number for more frequent feedback to the
+// user about loading progress (lower number probably helps with memory usage too
+// as there is less to append to the DOM each iteration).
+const MAX_FETCHES_PER_PROCESS = 300;
+
+onmessage = async e => {
+  const { waypointTuples, resourceUri } = e.data;
+  const chunks = chunkGenerator(waypointTuples, MAX_FETCHES_PER_PROCESS);
+
+  let done = false;
+
+  for await (let chunk of chunks) {
+    postMessage({ data: chunk, done });
+  }
+
+  done = true;
+
+  postMessage({ done });
+
+  async function* chunkGenerator(arr, size) {
+    for (let i = 0; i < arr.length; i += size) {
+      yield await fetchWaypoints(arr.slice(i, i + size), resourceUri);
+    }
+  }
+};
 
 /**
  * fetchWaypoints
  * @description Fetch one or more waypoints of records
- * @param {object} data - The data the worker receives
- * @param {string} data.resourceUri - The uri of the collection resource,
+ * @param {string} resourceUri - The uri of the collection resource,
  * ie: /repositories/18/resources/11861
- * @param {[wpNum, uris]} data.waypointTuples - Array of tuples of waypoint metadata
+ * @param {[wpNum, uris]} waypointTuples - Array of tuples of waypoint metadata
  * @typedef {number} wpNum - The waypoint number
  * @typedef {string[]} uris - Array of record uris belonging to the waypoint
  * @returns {Promise} - A Promise that resolves to an array of waypoint
  * objects, each with the signature: `{ wpNum, records }`
  */
-async function fetchWaypoints(data) {
-  const { waypointTuples, resourceUri } = data;
-
+async function fetchWaypoints(waypointTuples, resourceUri) {
   const promises = waypointTuples.map(tuple =>
     fetchWaypoint(...tuple, resourceUri)
   );
