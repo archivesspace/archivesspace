2a83459cf196ef8e2eb194835bb92f8123e9d1c5
Brian Zelip <brian.zelip@lyrasis.org>
2024-02-01 13:04:59 -0500

    ANW-1884 Rewrite frontend spawn tests as controller tests
    This rewrites two spawn tests from feature tests to
    controller tests. They only verify that the title
    carries over when spawning.

--- frontend/spec/controllers/digital_objects_controller_spec.rb
+++ frontend/spec/controllers/digital_objects_controller_spec.rb
@@ -29,4 +29,45 @@ describe DigitalObjectsController, type: :controller do
       expect(response.body).to have_css('#digital_object_title_.form-control.mixed-content')
     end
   end
+
+  describe 'spawning' do
+    before(:each) do
+      session = User.login('admin', 'admin')
+      User.establish_session(controller, session, 'admin')
+      controller.session[:repo_id] = JSONModel.repository
+
+      allow(AppConfig).to receive(:[]).and_call_original
+      allow(controller).to receive(:user_prefs).and_return('digital_object_spawn' => true)
+    end
+
+    title_input_selector = 'textarea#digital_object_title_'
+
+    it 'can create a digital object from a resource' do
+      resource = create(:json_resource)
+      fixture_dir = File.join(File.dirname(__FILE__), '..', '..', '..', 'backend', 'spec', 'fixtures', 'oai')
+      resource.update(ASUtils.json_parse(File.read(File.join(fixture_dir, 'resource.json'))))
+      resource.id_0 = "resource_#{Time.now}"
+      resource.ead_id = "ead_#{Time.now}"
+      resource.subjects = []
+      resource.linked_events = []
+      resource.linked_agents = []
+      resource.save
+
+      get :new, params: {spawn_from_resource_id: resource.id, inline: true}
+      result = Capybara.string(response.body)
+      result.find(:css, title_input_selector) do |input|
+        expect(input.value).to eq('Resource 1')
+      end
+    end
+
+    it 'can create a digital object from an accession' do
+      acc_title = 'Accession title for spawn test'
+      accession = create(:json_accession, :title => acc_title)
+      get :new, params: {spawn_from_accession_id: accession.id, inline: true}
+      result = Capybara.string(response.body)
+      result.find(:css, title_input_selector) do |input|
+        expect(input.value).to eq(acc_title)
+      end
+    end
+  end
 end
