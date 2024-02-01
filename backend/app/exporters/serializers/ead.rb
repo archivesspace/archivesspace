234d94ba77d54e0afdf4ca45b65872b579d32331
Manny Rodriguez <manny@theavatar.org>
2024-02-01 10:17:38 -0700

    tweaking generated EADs so the SUI PDF picks up digital object links
    moving <dao> tags inside of the ead:archdesc:did

--- backend/app/exporters/serializers/ead.rb
+++ backend/app/exporters/serializers/ead.rb
@@ -210,14 +210,16 @@ class EADSerializer < ASpaceExport::Serializer
             end
 
             EADSerializer.run_serialize_step(data, xml, @fragments, :did)
+            
+            if @include_daos
+              data.instances_with_digital_objects.each do |instance|
+                serialize_digital_object(instance['digital_object']['_resolved'], xml, @fragments)
+              end
+          end
 
           }# </did>
 
-          if @include_daos
-            data.instances_with_digital_objects.each do |instance|
-              serialize_digital_object(instance['digital_object']['_resolved'], xml, @fragments)
-            end
-          end
+
 
           serialize_nondid_notes(data, xml, @fragments)
 
