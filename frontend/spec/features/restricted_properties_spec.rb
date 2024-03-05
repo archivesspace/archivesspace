d7e28b9537a6b70d538cc845e4d5c2a522e24cfd
Thimios Dimopulos <thimios@wantedpixel.com>
2024-03-05 13:42:16 +0200

    ANW-1941 port restricted properties spec from selenium to capybara

---
+++ frontend/spec/features/restricted_properties_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Restricted properties', js: true do
+  let!(:repository) { create(:repo, repo_code: "agents_test_#{Time.now.to_i}") }
+  let(:user) { create_user(repository => ['repository-advanced-data-entry']) }
+  let!(:agent) { create(:agent_person) }
+  let!(:agent_victim) { create(:agent_person) }
+  let(:restricted_property) { 'agent_contacts' }
+  let(:admin) { BackendClientMethods::ASpaceUser.new('admin', 'admin') }
+
+  it 'updates a record with restricted properties retains the restricted data' do
+    login_user(user)
+    select_repository(repository)
+
+    expect(agent[restricted_property].count).to eq 1
+
+    visit "/agents/#{agent['jsonmodel_type']}/#{agent.id}/edit"
+
+    find('#agent_restricted_properties___', visible: false)
+
+    # Click on save
+    find('button', text: 'Save Person', match: :first).click
+
+    expect(page).to have_text 'Agent Saved'
+
+    visit 'logout'
+
+    login_user(admin)
+
+    visit "/agents/#{agent['jsonmodel_type']}/#{agent.id}/edit"
+
+    element = find('#agent_agent_contacts__0__name_')
+    expect(element.value).to_not be_nil
+
+    element = find('#agent_agent_contacts__0__telephones__0__number_')
+    expect(element.value).to_not be_nil
+
+    element = find('#agent_agent_contacts__0__notes__0__date_of_contact_')
+    expect(element.value).to_not be_nil
+  end
+
+  it 'prevents merging of agents containing restricted data' do
+    login_user(admin)
+    run_index_round
+    select_repository(repository)
+
+    find('.repo-container .btn.dropdown-toggle').click
+    click_on 'Manage Groups'
+
+    row = find(:xpath, "//tr[contains(., 'repository-advanced-data-entry')]")
+    within row do
+      click_on 'Edit'
+    end
+
+    expect(page).to have_text("Advanced Data Entry users of the #{repository.repo_code} repository")
+
+    find('#merge_agents_and_subjects').click
+
+    # Click on save
+    find('button', text: 'Save Group', match: :first).click
+
+    visit 'logout'
+
+    login_user(user)
+
+    visit "/agents/#{agent['jsonmodel_type']}/#{agent.id}/edit"
+
+    click_on 'Merge'
+
+    element = find('#token-input-merge_ref_')
+    element.fill_in with: agent_victim['names'][0]['primary_name']
+    dropdown_items = all('li.token-input-dropdown-item2')
+    dropdown_items.first.click
+
+    within '#form_merge' do
+      click_on 'Merge'
+    end
+
+    within '#confirmChangesModal' do
+      click_on 'Compare Agents'
+    end
+
+    expect(page).to have_text 'The merge cannot be completed because one or more of the agents has contact details you do not have permission to access.'
+  end
+end
