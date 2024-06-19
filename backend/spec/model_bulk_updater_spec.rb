78f531ab860ac189297bfa36dfa81cbda4b9acbf
Laney McGlohon <113810706+stitching-coder@users.noreply.github.com>
2024-06-19 11:03:37 -0700

    Create missing top containers tests

--- backend/spec/model_bulk_updater_spec.rb
+++ backend/spec/model_bulk_updater_spec.rb
@@ -4,6 +4,8 @@ describe 'Bulk Updater model' do
   BULK_FIXTURES_DIR = File.join(File.dirname(__FILE__), "fixtures", "bulk_updater")
   let(:test_file) { BULK_FIXTURES_DIR + "/test_sheet.xlsx" }
   let(:test_file_with_errors) { BULK_FIXTURES_DIR + "/test_sheet_with_errors.xlsx" }
+  let(:test_file_with_top_containers) { BULK_FIXTURES_DIR + "/test_file_with_top_containers.xlsx" }
+  let(:test_file_with_new_top_containers) { BULK_FIXTURES_DIR + "/test_file_with_new_top_containers.xlsx" }
 
   let(:dates) { [build(:json_date)] }
   let(:notes) { [build(:json_note_singlepart), build(:json_note_multipart)] }
@@ -16,7 +18,17 @@ describe 'Bulk Updater model' do
                           :notes => notes,
                           :lang_materials => lang_materials,
                           :related_accessions => [{'ref' => accession.uri}]) }
-  let(:ao) { create(:json_archival_object, :resource => {:ref => resource.uri}, :dates => dates, :notes => notes) }
+  let(:location) { create(:json_location, :temporary => generate(:temporary_location_type)) }
+  let(:top_container) { create(:json_top_container,
+                              :container_locations => [{'ref' => location.uri,
+                              'status' => 'current',
+                              'start_date' => generate(:yyyy_mm_dd),
+                              'end_date' => generate(:yyyy_mm_dd)}]) }
+  let(:instances) { [build(:json_instance,
+                          :sub_container => build(:json_sub_container,
+                          :top_container => {:ref => top_container.uri}))] }
+
+  let(:ao) { create(:json_archival_object, :resource => {:ref => resource.uri}, :dates => dates, :notes => notes, :instances => instances) }
   let(:min_subrecords) { 4 }
   let(:extra_subrecords) { 3 }
   let(:min_notes) { 2 }
@@ -126,6 +138,42 @@ describe 'Bulk Updater model' do
         allow(AppConfig).to receive(:has_key?).with(:bulk_updater_create_missing_top_containers).and_return(false)
         allow(bulk_updater.job.job).to receive(:has_key?).with('create_missing_top_containers').and_return(false)
         expect(bulk_updater.create_missing_top_containers?).to be false
+      end
+    end
+  end
+
+  describe "#create_missing_top_containers" do
+    let(:job) { build(:json_job, job_type: 'bulk_update_job') }
+    let(:bulk_updater) { BulkUpdater.new(test_file_with_new_top_containers, job) }
+    let(:in_sheet) { { BulkUpdater::TopContainerCandidate.new("box", "1", "123") => nil } }
+
+    context "when create_missing_top_containers? is true" do
+      before do
+        allow(bulk_updater).to receive(:create_missing_top_containers?).and_return(true)
+        allow(job).to receive(:write_output).and_return("")
+      end
+
+      it "creates missing top containers" do
+        bulk_updater.instance_variable_set(:@top_containers_in_resource, { BulkUpdater::TopContainerCandidate.new("box", "3", "345") => "/repositories/2/top_containers/2" })
+
+        bulk_updater.create_missing_top_containers(in_sheet, job)
+
+        expect(bulk_updater.instance_variable_get(:@top_containers_in_resource)).to eq({ BulkUpdater::TopContainerCandidate.new("box", "3", "345") => "/repositories/2/top_containers/2", BulkUpdater::TopContainerCandidate.new("box", "1", "123") => "/repositories/2/top_containers/3"})
+      end
+    end
+
+    context "when create_missing_top_containers? is false" do
+      before do
+        allow(bulk_updater).to receive(:create_missing_top_containers?).and_return(false)
+      end
+
+      it "does not create missing top containers" do
+        expect(bulk_updater).not_to receive(:extract_top_containers_from_sheet)
+        expect(TopContainer).not_to receive(:create_from_json)
+
+        expect(bulk_updater).not_to receive(:create_missing_top_containers)
+
+        expect(bulk_updater.instance_variable_get(:@top_containers_in_resource)).to be_nil
       end
     end
   end
