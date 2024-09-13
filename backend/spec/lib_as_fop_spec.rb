2f503a9ca68fa4b39904f2ac8091cbfc482ee9ef
Thimios Dimopulos <thimios@wantedpixel.com>
2024-09-13 20:14:29 +0300

    ANW-2044 Staff PDF archival object component unique identifier

--- backend/spec/lib_as_fop_spec.rb
+++ backend/spec/lib_as_fop_spec.rb
@@ -13,31 +13,63 @@ describe 'AS_fop' do
     }
   end
 
+  def generate_pdf(resource)
+    resource = Resource.get_or_die(resource.id)
+
+    resource_jsonmodel = Resource.to_jsonmodel(resource)
+
+    object = URIResolver.resolve_references(resource_jsonmodel, [
+      "repository",
+      "linked_agents",
+      "subjects",
+      "digital_object",
+      "top_container",
+      "top_container::container_profile"
+    ])
+
+    record = JSONModel(:resource).new(object)
+    ead = ASpaceExport.model(:ead).from_resource(record, resource.tree(:all, mode = :sparse), options)
+    xml = ""
+    ASpaceExport.stream(ead).each { |x| xml << x }
+
+    pdf = ASFop.new(xml, pdf_image).to_pdf
+
+    File.read(pdf.path)
+  end
+
   context 'when converting ead xml to pdf' do
     it 'successfully generates a pdf file' do
       resource = create(:json_resource, title: "Resource Title")
       resource = Resource.get_or_die(resource.id)
-      resource_jsonmodel = Resource.to_jsonmodel(resource)
 
-      object = URIResolver.resolve_references(resource_jsonmodel, [
-        "repository",
-        "linked_agents",
-        "subjects",
-        "digital_object",
-        "top_container",
-        "top_container::container_profile"
-      ])
-
-      record = JSONModel(:resource).new(object)
-      ead = ASpaceExport.model(:ead).from_resource(record, resource.tree(:all, mode = :sparse), options)
-      xml = ""
-      ASpaceExport.stream(ead).each { |x| xml << x }
-
-      pdf = ASFop.new(xml, pdf_image).to_pdf
-      pdf_file = File.read(pdf.path)
+      pdf_file = generate_pdf(resource)
 
       expect(pdf_file).to include "%PDF-1.4"
       expect(pdf_file).to include "Creator (Apache FOP Version 2.5)"
+    end
+  end
+
+  context 'when the resource has archival objects' do
+    let(:uuid) { SecureRandom.uuid }
+
+    before do
+      stub_const("MimeConstants::MIME_PDF", MimeConstants::MIME_PLAIN_TEXT)
+    end
+
+    it 'successfully generates a pdf file with Component Unique Identifier' do
+      resource = create(:json_resource, title: "Resource Title #{uuid}", publish: true)
+      archival_object = create(:json_archival_object,
+        title: "Archival Object #{uuid}",
+        publish: true,
+        resource: { :ref => resource.uri },
+        component_id: "archival-object-component-id-#{uuid}",
+        dates: []
+      )
+
+      pdf_file = generate_pdf(resource)
+
+      # require 'pry'; binding.pry
+      expect(pdf_file.gsub("\n", '').gsub("\r", '')).to include "ID: archival-object-component-id-#{uuid}"
     end
   end
 
@@ -89,27 +121,10 @@ describe 'AS_fop' do
         )
 
         resource.save
-        resource = Resource.get_or_die(resource.id)
-        resource_jsonmodel = Resource.to_jsonmodel(resource)
 
-        object = URIResolver.resolve_references(resource_jsonmodel, [
-          "repository",
-          "linked_agents",
-          "subjects",
-          "digital_object",
-          "top_container",
-          "top_container::container_profile"
-        ])
+        pdf_file = generate_pdf(resource)
 
-        record = JSONModel(:resource).new(object)
-        ead = ASpaceExport.model(:ead).from_resource(record, resource.tree(:all, mode = :sparse), options)
-        xml = ""
-        ASpaceExport.stream(ead).each { |x| xml << x }
-
-        pdf = ASFop.new(xml, pdf_image).to_pdf
-        file = File.read(pdf.path)
-
-        expect(file).to include "Language of the    English, Australian languages, Estonian\r\n                   Material:\r\n\r\n\r\n"
+        expect(pdf_file).to include "Language of the    English, Australian languages, Estonian\r\n                   Material:\r\n\r\n\r\n"
       end
     end
 
@@ -171,27 +186,10 @@ describe 'AS_fop' do
         )
 
         resource.save
-        resource = Resource.get_or_die(resource.id)
-        resource_jsonmodel = Resource.to_jsonmodel(resource)
 
-        object = URIResolver.resolve_references(resource_jsonmodel, [
-          "repository",
-          "linked_agents",
-          "subjects",
-          "digital_object",
-          "top_container",
-          "top_container::container_profile"
-        ])
+        pdf_file = generate_pdf(resource)
 
-        record = JSONModel(:resource).new(object)
-        ead = ASpaceExport.model(:ead).from_resource(record, resource.tree(:all, mode = :sparse), options)
-        xml = ""
-        ASpaceExport.stream(ead).each { |x| xml << x }
-
-        pdf = ASFop.new(xml, pdf_image).to_pdf
-        file = File.read(pdf.path)
-
-        expect(file).to include "Language of the    #{language_material_note_a}\r\n                   Material:\r\n\r\n\r\n\r\n              Language of the    #{language_material_note_b}\r\n        ^ Return to Table of Contents\r\n\r\n\r\n              Language of the    #{language_material_note_c}\r\n                   Material:\r\n        ────────────────────────────────────────────────────────────────────────────────────────\r\n"
+        expect(pdf_file).to include "Language of the    #{language_material_note_a}\r\n                   Material:\r\n\r\n\r\n\r\n              Language of the    #{language_material_note_b}\r\n        ^ Return to Table of Contents\r\n\r\n\r\n              Language of the    #{language_material_note_c}\r\n                   Material:\r\n        ────────────────────────────────────────────────────────────────────────────────────────\r\n"
       end
     end
   end
