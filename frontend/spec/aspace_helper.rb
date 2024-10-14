df037b4e4e218c384d4d3b418405e2c1e908820e
Thimios Dimopulos <thimios@wantedpixel.com>
2024-10-14 12:16:27 -0500

    ensure jQuery.active is defined before checking its return value

--- frontend/spec/aspace_helper.rb
+++ frontend/spec/aspace_helper.rb
@@ -54,6 +54,6 @@ module ASpaceHelpers
   end
 
   def finished_all_ajax_requests?
-    page.evaluate_script('jQuery.active').zero?
+    page.evaluate_script('window.jQuery != undefined') && page.evaluate_script('jQuery.active != undefined') && page.evaluate_script('jQuery.active').zero?
   end
 end
