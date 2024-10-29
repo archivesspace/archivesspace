057d16e31fd0ce779c851279aae2c758f3d4d978
Lora Woodford <lorawoodford@gmail.com>
2024-10-29 13:02:13 -0400

    ANW-2107 Allow owner repo to be imported via CSV (#3275)
    ANW-2107 allow owner repo to be imported via CSV when importing locations

--- backend/spec/lib_location_converter_spec.rb
+++ backend/spec/lib_location_converter_spec.rb
@@ -9,17 +9,38 @@ describe 'Location converter' do
     LocationConverter
   end
 
-  before(:all) do
+  let(:test_file) do
     test_file = File.expand_path(
       '../../backend/spec/examples/aspace_location_import_template.csv',
       File.dirname(__FILE__)
     )
-    @records = convert(test_file)
-    @locations = @records.select {|r| r['jsonmodel_type'] == 'location' }
   end
 
-  it 'created one Location record for each row in the CSV file' do
-    expect(@locations.count).to eq(3)
+  it 'creates one location record for each row in the CSV file' do
+    locations = convert(test_file).select {|r| r['jsonmodel_type'] == 'location' }
+    expect(locations.count).to eq(3)
+  end
+
+  context 'when import repository is not checked' do
+    before do
+      my_converter.instance_variable_set(:@import_options, {})
+    end
+
+    it 'creates location records without owner repo' do
+      locations = convert(test_file).select {|r| r['jsonmodel_type'] == 'location' }
+      expect(locations.select { |l| l['owner_repo'] }).to be_empty
+    end
+  end
+
+  context 'when import repository is checked' do
+    before do
+      my_converter.instance_variable_set(:@import_options, {:import_repository => true})
+    end
+
+    it 'creates location records with owner repo' do
+      locations = convert(test_file).select {|r| r['jsonmodel_type'] == 'location' }
+      expect(locations.select { |l| l['owner_repo'] }).not_to be_empty
+    end
   end
 
 end
