641fd7a892b745a5a26682eec8a0c5edaf02d37c
Donald Smith <donald.smith@lyrasis.org>
2025-03-06 16:53:44 -0500

    title selection rules fleshed out

--- common/multiple_titles_helper.rb
+++ common/multiple_titles_helper.rb
@@ -3,10 +3,19 @@ module MultipleTitlesHelper
   @@locale_map = {'en' => 'eng', 'es' => 'spa', 'fr' => 'fre', 'de' => 'ger', 'ja' => 'jpn'}
 
   # titles is an array of Title in json form
-  def self.determine_display_title(titles, locale)
-    pref_lang = @@locale_map[locale.to_s]
-    title = titles.find{|t| t['language'] == pref_lang} || titles[0]
-    title['title']
+  def self.determine_display_title(titles, current_locale, default_language = 'eng')
+    # formal titles take precedence over all others
+    titles.each { |t| return t['title'] if t['type'] == 'formal' }
+
+    # in absence of formal titles, try to find a title with the preferred language (the language of the UI)
+    pref_lang = @@locale_map[current_locale.to_s]
+    titles.each { |t| return t['title'] if t['language'] == pref_lang }
+
+    # fallback to the default language
+    titles.each { |t| return t['title'] if t['language'] == default_language }
+
+    # if no preferred title can be determined, return the first title in the list
+    titles[0]['title']
   end
 
 end
