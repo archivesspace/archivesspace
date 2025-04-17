2e88ea6ab785388327d7c4bf6cef9053de0b51b8
Thimios Dimopulos <thimios.dimopulos@lyrasis.com>
2025-04-17 15:11:44 +0300

    re-enable frontend model and controller specs

--- frontend/spec/controllers/preferences_controller_spec.rb
+++ frontend/spec/controllers/preferences_controller_spec.rb
@@ -4,31 +4,39 @@ require 'rails_helper'
 describe PreferencesController, type: :controller do
   render_views
 
-  before(:each) do
-    set_repo($repo)
-    session = User.login('admin', 'admin')
-    User.establish_session(controller, session, 'admin')
-    controller.session[:repo_id] = JSONModel.repository
-  end
-
   describe 'GET edit' do
-    it 'redirects to welcome page when no repositories exist' do
-      # Ensure no repositories exist
-      allow(JSONModel::HTTP).to receive(:get_json).with("/repositories").and_return([])
+    context 'when no repositories exist' do
+      before(:each) do
+        session = User.login('admin', 'admin')
+        User.establish_session(controller, session, 'admin')
 
-      get :edit, params: { id: 0 }
-      expect(response).to redirect_to(controller: :welcome, action: :index)
-      expect(flash[:error]).to eq(I18n.t("preference._frontend.messages.no_access_to_preferences"))
+        allow(JSONModel::HTTP).to receive(:get_json).and_call_original
+        allow(JSONModel::HTTP).to receive(:get_json).with("/repositories").and_return([])
+        allow(MemoryLeak::Resources).to receive(:get).and_call_original
+        allow(MemoryLeak::Resources).to receive(:get).with(:repository).and_return(double(find_all: []))
+      end
+
+      it 'redirects to welcome page when no repositories exist' do
+        get :edit, params: { id: 0 }
+        expect(response).to redirect_to(controller: :welcome, action: :index)
+        expect(flash[:error]).to eq(I18n.t("preference._frontend.messages.no_access_to_preferences"))
+      end
     end
 
-    it 'shows preferences page when repositories exist' do
-      # Mock the existence of at least one repository
-      allow(JSONModel::HTTP).to receive(:get_json).with("/repositories").and_return([{"uri" => "/repositories/1"}])
+    context 'when repositories exist' do
+      before(:each) do
+        set_repo($repo)
+        session = User.login('admin', 'admin')
+        User.establish_session(controller, session, 'admin')
+        controller.session[:repo_id] = JSONModel.repository
+      end
 
-      get :edit, params: { id: 0 }
-      expect(response).not_to redirect_to(controller: :welcome, action: :index)
-      expect(response.status).to eq(200)
-      expect(flash[:error]).to be_nil
+      it 'shows preferences page' do
+        get :edit, params: { id: $repo.id }
+        expect(response).not_to redirect_to(controller: :welcome, action: :index)
+        expect(response.status).to eq(200)
+        expect(flash[:error]).to be_nil
+      end
     end
   end
 end
