786f8b3a4037320444b880a720789df46b802240
Thimios Dimopulos <thimios.dimopulos@lyrasis.com>
2025-05-12 19:48:03 +0300

    clean up formatting of indexer file

--- indexer/app/lib/fake_solr_timeout_response.rb
+++ indexer/app/lib/fake_solr_timeout_response.rb
@@ -5,23 +5,21 @@ require 'net/http'
 # crash in the index round. Instead we will rescue the timeout error and return
 # this faked response.
 class FakeSolrTimeoutResponse < Net::HTTPRequestTimeOut
+  def initialize(req)
+    @req = req
+    super('1.0', '408', 'SOLR TIMEOUT ERROR')
+  end
 
-    def initialize(req)
-      @req = req 
-      super('1.0', '408', 'SOLR TIMEOUT ERROR') 
-    end
-    
-    
-    # This needs to be added so Net::HTTP stream check passes.
-    def read_body(*args, &block)
-      @body = "
+  # This needs to be added so Net::HTTP stream check passes.
+  def read_body(*args, &block)
+    @body = "
 Timeout error with #{@req.uri} #{@req.method} #{@req.body}.
-Please check your :indexer_solr_timeout_seconds, :indexer_thread_count, and :indexer_records_per_thread settings in 
+Please check your :indexer_solr_timeout_seconds, :indexer_thread_count, and :indexer_records_per_thread settings in
 your config.rb file.
 Also check https://wiki.apache.org/solr/SolrPerformanceProblems for possible performance issues.
-      " 
-      yield @body if block_given?
-      @body
-    end
+      "
+    yield @body if block_given?
 
+    @body
+  end
 end
