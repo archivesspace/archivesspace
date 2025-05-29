731ef84098abc60e6cec64437214f37384716b92
Thimios Dimopulos <thimios.dimopulos@lyrasis.com>
2025-05-29 10:25:59 +0300

    ANW-2401 remove config overrides from docker env to avoid confusion

--- build/scripts/export_config.rb
+++ build/scripts/export_config.rb
@@ -1,15 +1,26 @@
 require 'config/config-distribution'
 
 def main
-  if ARGV.length != 1
+  if ARGV.length < 1
     raise "No output file given"
   end
 
   output_file = ARGV[0]
+  apply_env_overrides = ARGV[1]
 
-  File.write(output_file, "# Configuration defaults are shown below\n\n" + AppConfig.read_defaults.gsub(/^/, "#"))
+  config = AppConfig.read_defaults.gsub(/^/, "#")
+
+  if apply_env_overrides
+    ENV.select { |key, _value| key =~ /^APPCONFIG_/ }.each do |envvar, value|
+      property = envvar.partition('_').last.downcase.to_sym
+      property_value = AppConfig.parse_value(value)
+      property_value = "'#{property_value}'" if property_value.is_a?(String)
+
+      config = config.gsub(/^#AppConfig\[:#{property}\].*/, "AppConfig[:#{property}] = #{property_value}")
+    end
+  end
+
+  File.write(output_file, "# Configuration defaults are shown below\n\n" + config )
 end
 
-
 main
-
