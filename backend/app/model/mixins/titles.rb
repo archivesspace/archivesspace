a4eb3a390898ecad961e956629157dc800bb4c55
Donald Smith <donald.smith@lyrasis.org>
2025-06-23 11:23:42 -0400

    provide primary title method in titles mixin

--- backend/app/model/mixins/titles.rb
+++ backend/app/model/mixins/titles.rb
@@ -1,3 +1,5 @@
+require 'multiple_titles_helper'
+
 module Titles
 
   def self.included(base)
@@ -5,6 +7,10 @@ module Titles
     base.def_nested_record(:the_property => :titles,
                            :contains_records_of_type => :title,
                            :corresponding_to_association  => :title)
+  end
+
+  def primary_title
+    MultipleTitlesHelper.determine_primary_title(json['titles'], Preference.get_user_global_preference('locale'))
   end
 
 end
