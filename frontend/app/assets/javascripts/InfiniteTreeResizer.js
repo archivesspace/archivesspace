b8a09e73727a96f5911ab7e5d7743b6b36d2fe9b
Brian Zelip <brian.zelip@lyrasis.org>
2025-07-18 07:41:22 -0600

    Persist user updated tree height

--- frontend/app/assets/javascripts/InfiniteTreeResizer.js
+++ frontend/app/assets/javascripts/InfiniteTreeResizer.js
@@ -1,3 +1,5 @@
+//= require LocalStorage
+
 (function (exports) {
   class InfiniteTreeResizer {
     static #MIN_HEIGHT = 60;
@@ -9,12 +11,16 @@
     constructor(container) {
       this.container = container;
       this.isResizing = false;
-      this.startY = 0;
-      this.startHeight = 0;
+      this.startY = undefined;
+      this.startHeight = undefined;
       this.minHeight = InfiniteTreeResizer.#MIN_HEIGHT;
       this.maximizedMarginBottom = InfiniteTreeResizer.#MAXIMIZED_MARGIN_BOTTOM;
       this.handle = document.querySelector('[data-resize-handle]');
       this.toggleBtn = document.querySelector('[data-resize-toggle]');
+
+      this.storage = new LocalStorage();
+      const storedHeight = this.getStoredHeight();
+      if (storedHeight !== null) this.setHeight(storedHeight);
 
       this.handle.addEventListener('mousedown', this.onMouseDown.bind(this));
       this.handle.addEventListener('keydown', this.onHandleKeyDown.bind(this));
@@ -29,24 +35,57 @@
       this.updateHandleAriaAttrs();
     }
 
+    /**
+     * @returns {number | null} stored height in pixels or null if not found
+     */
+    getStoredHeight() {
+      const value = this.storage.get('AS_Tree_Height');
+      if (!value) return null;
+      const parsed = parseInt(value, 10);
+
+      return isNaN(parsed) ? null : parsed;
+    }
+
+    /**
+     * @param {number} height in pixels
+     */
+    setHeight(height) {
+      this.container.style.height = `${height}px`;
+      this.storeHeight(height);
+    }
+
+    /**
+     * @param {number} height in pixels
+     */
+    storeHeight(height) {
+      this.storage.set('AS_Tree_Height', String(height));
+    }
+
+    /**
+     * @param {MouseEvent} e
+     */
     onMouseDown(e) {
       this.isResizing = true;
       this.startY = e.clientY;
       this.startHeight = this.container.offsetHeight;
       document.body.style.userSelect = 'none';
 
-      this.onMouseMoveHandler = this.onMouseMove.bind(this); // needed for removing the listener
-      this.onMouseUpHandler = this.onMouseUp.bind(this); // needed for removing the listener
+      this.onMouseMoveHandler = this.onMouseMove.bind(this); // needed for listener removal
+      this.onMouseUpHandler = this.onMouseUp.bind(this);
 
       document.addEventListener('mousemove', this.onMouseMoveHandler);
       document.addEventListener('mouseup', this.onMouseUpHandler);
     }
 
+    /**
+     * @param {MouseEvent} e
+     */
     onMouseMove(e) {
       if (!this.isResizing) return;
       const deltaY = e.clientY - this.startY;
       const newHeight = Math.max(this.minHeight, this.startHeight + deltaY);
-      this.container.style.height = `${newHeight}px`;
+
+      this.setHeight(newHeight);
       this.updateHandleAriaAttrs(newHeight);
     }
 
@@ -57,8 +96,11 @@
       document.removeEventListener('mouseup', this.onMouseUpHandler);
     }
 
+    /**
+     * @param {TouchEvent} e
+     */
     onTouchStart(e) {
-      if (e.touches.length !== 1) return; // ignore multi-touch
+      if (e.touches.length !== 1) return; // Ignore multi-touch
 
       this.isResizing = true;
       this.startY = e.touches[0].clientY;
@@ -74,13 +116,16 @@
       document.addEventListener('touchend', this.onTouchEndHandler);
     }
 
+    /**
+     * @param {TouchEvent} e
+     */
     onTouchMove(e) {
       if (!this.isResizing) return;
 
       const deltaY = e.touches[0].clientY - this.startY;
       const newHeight = Math.max(this.minHeight, this.startHeight + deltaY);
 
-      this.container.style.height = `${newHeight}px`;
+      this.setHeight(newHeight);
       this.updateHandleAriaAttrs(newHeight);
 
       e.preventDefault(); // Prevent scrolling while resizing
@@ -93,6 +138,9 @@
       document.removeEventListener('touchend', this.onTouchEndHandler);
     }
 
+    /**
+     * @param {KeyboardEvent} e
+     */
     onHandleKeyDown(e) {
       const step = 10;
       const largeStep = 50;
@@ -129,10 +177,10 @@
           return;
       }
 
-      this.container.style.height = `${newHeight}px`;
-      this.updateHandleAriaAttrs(newHeight);
-
       e.preventDefault();
+
+      this.setHeight(newHeight);
+      this.updateHandleAriaAttrs(newHeight);
     }
 
     toggleMaximized() {
@@ -141,11 +189,11 @@
       this.isResizing = true;
 
       if (!this.isMaximized) {
-        this.container.style.height = `${this.availableHeight}px`;
+        this.setHeight(this.availableHeight);
         this.handle.classList.add('maximized');
         this.toggleBtn.setAttribute('aria-expanded', true);
       } else {
-        this.container.style.height = `${this.minHeight}px`;
+        this.setHeight(this.minHeight);
         this.handle.classList.remove('maximized');
         this.toggleBtn.setAttribute('aria-expanded', false);
       }
