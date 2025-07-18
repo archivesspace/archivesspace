96614e08fddce10470f9d0bb78b03b016764cc6a
Brian Zelip <brian.zelip@lyrasis.org>
2025-07-18 06:43:51 -0600

    Fix localStorage deprecated event property calls

--- frontend/app/assets/javascripts/LocalStorage.js
+++ frontend/app/assets/javascripts/LocalStorage.js
@@ -42,16 +42,8 @@
       } catch (e) {
         return (
           e instanceof DOMException &&
-          // everything except Firefox
-          (e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
+          (e.name === 'QuotaExceededError' ||
             e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-          // acknowledge QuotaExceededError only if there's something already stored
           window.localStorage &&
           window.localStorage.length !== 0
         );
