a2b42cda8db5fb54dff411d837532f1aeaef1fad
Brian Zelip <brian.zelip@lyrasis.org>
2025-08-05 12:27:24 -0400

    ANW-2415 Load the Tree and RecordPane based on the document uri fragment (#3670)
    - Adapt the URI fragment support and bi-directional infinite scrolling from the PUI ANW-2309 (#3490)
    - Port the readonly parts of the ajaxtree.js.erb to InfiniteTreeRecordPane.js
    - Port and extend the TreeIds utility class to InfiniteTreeIds.js
    - Communicate with the broader, jquery-based form initialization event system via vanilla js

---
+++ frontend/app/assets/javascripts/InfiniteTreeIds.js
@@ -0,0 +1,77 @@
+(function (exports) {
+  class InfiniteTreeIds {
+    static #uriPattern = /\/repositories\/([0-9]+)\/([a-z_]+)\/([0-9]+)/;
+
+    static #childTypeMap = {
+      resource: 'archival_object',
+      digital_object: 'digital_object_component',
+      classification: 'classification_term',
+    };
+
+    static uriToParts(uri) {
+      const match = uri.match(this.#uriPattern);
+      if (!match) return null;
+
+      const [, , typePlural, id] = match;
+      const type = typePlural.replace(/s$/, '');
+
+      return { type, id };
+    }
+
+    static rootUriToParts(rootUri) {
+      const match = rootUri.match(this.#uriPattern);
+      if (!match) return null;
+
+      const [, repoId, typePlural, id] = match;
+      const type = typePlural.replace(/s$/, '');
+      const childType = this.#childTypeMap[type];
+
+      return { repoId, type, id, childType };
+    }
+
+    static uriToTreeId(uri) {
+      const parts = this.uriToParts(uri);
+
+      return `${parts.type}_${parts.id}`;
+    }
+
+    static parseTreeId(treeId) {
+      const match = treeId.match(/([a-z_]+)([0-9]+)/);
+      if (!match) return null;
+
+      const [, rowType, rowId] = match;
+
+      return { type: rowType.replace(/_$/, ''), id: rowId };
+    }
+
+    static uriToLocationHash(uri) {
+      return `tree::${this.uriToTreeId(uri)}`;
+    }
+
+    /**
+     *
+     * @param {string} hash the URI fragment returned from window.location.hash
+     * @returns {string} the HTML id of the node
+     */
+    static locationHashToHtmlId(hash) {
+      return hash.replace('#tree::', '');
+    }
+
+    static treeLinkUrl(uri) {
+      return `#${this.uriToLocationHash(uri)}`;
+    }
+
+    static backendUriToFrontendUri(uri) {
+      return AS.app_prefix(uri.replace(/\/repositories\/[0-9]+\//, ''));
+    }
+
+    static locationHashToFrontendUri(hash) {
+      const treeId = hash.replace('tree::', '');
+      const parts = this.parseTreeId(treeId);
+
+      return `/${parts.type}s/${parts.id}`;
+    }
+  }
+
+  exports.InfiniteTreeIds = InfiniteTreeIds;
+})(window);
