47f0269593eb5e06bbd782373c52ea33468a21c4
cpapazoglou <hrrsppzgl@gmail.com>
2025-10-03 14:41:14 +0300

    ANW-1023: adds container profile defaults.

--- backend/spec/controller_default_values_spec.rb
+++ backend/spec/controller_default_values_spec.rb
@@ -80,6 +80,16 @@ describe 'Default Values' do
       expect(defaults['defaults']['languages']['script']).to eq("Latn")
     end
 
+  let(:container_profile_defaults) {
+    {
+      "record_type" => "container_profile",
+      "defaults" => {
+        "name" => "DEFAULT BOX",
+        "extent_dimension" => "width"
+      }
+    }
+  }
+
 
     it "can set a default finding aid language and script and retrieve them" do
       uri = "/repositories/#{JSONModel.repository}/default_values/resource"
@@ -145,4 +155,114 @@ describe 'Default Values' do
       expect(defaults['defaults']['script']).to eq("Latn")
     end
   end
+
+
+  it "can set a default language and script for a resource and retrieve them" do
+    uri = "/repositories/#{JSONModel.repository}/default_values/resource"
+    url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(resource_defaults))
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    defaults['defaults']['languages'] = {:language => "eng", :script => "Latn"}
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(defaults))
+
+    expect(response.status).to eq(200)
+
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    expect(defaults['defaults']['languages']['language']).to eq("eng")
+    expect(defaults['defaults']['languages']['script']).to eq("Latn")
+  end
+
+
+  it "can set a default language and script for an accession and retrieve them" do
+		uri = "/repositories/#{JSONModel.repository}/default_values/accession"
+		url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
+
+		response = JSONModel::HTTP.post_json(url, ASUtils.to_json(accession_defaults))
+		defaults = JSONModel::HTTP.get_json(uri)
+
+		defaults['defaults']['languages'] = {:language => "eng", :script => "Latn"}
+
+		response = JSONModel::HTTP.post_json(url, ASUtils.to_json(defaults))
+
+		expect(response.status).to eq(200)
+
+		defaults = JSONModel::HTTP.get_json(uri)
+
+		expect(defaults['defaults']['languages']['language']).to eq("eng")
+		expect(defaults['defaults']['languages']['script']).to eq("Latn")
+	end
+
+
+  it "can set a default finding aid language and script and retrieve them" do
+    uri = "/repositories/#{JSONModel.repository}/default_values/resource"
+    url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(resource_defaults))
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    defaults['defaults']['finding_aid_language'] = "eng"
+    defaults['defaults']['finding_aid_script'] = "Latn"
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(defaults))
+
+    expect(response.status).to eq(200)
+
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    expect(defaults['defaults']['finding_aid_language']).to eq("eng")
+    expect(defaults['defaults']['finding_aid_script']).to eq("Latn")
+  end
+
+
+  it "can set a default accession language and script of description and retrieve them" do
+    uri = "/repositories/#{JSONModel.repository}/default_values/accession"
+    url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(accession_defaults))
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    defaults['defaults']['language'] = "eng"
+    defaults['defaults']['script'] = "Latn"
+
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(defaults))
+
+    expect(response.status).to eq(200)
+
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    expect(defaults['defaults']['language']).to eq("eng")
+    expect(defaults['defaults']['script']).to eq("Latn")
+  end
+
+  it "can create and update default values for container profiles with different extent dimensions" do
+    uri = "/repositories/#{JSONModel.repository}/default_values/container_profile"
+    url = URI("#{JSONModel::HTTP.backend_url}#{uri}")
+
+    # Test setting width as default (addressing Jira issue requirement)
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(container_profile_defaults))
+
+    expect(response.status).to eq(200)
+
+    defaults = JSONModel::HTTP.get_json(uri)
+
+    expect(defaults['defaults']['name']).to eq('DEFAULT BOX')
+    expect(defaults['defaults']['extent_dimension']).to eq('width')
+
+    # Test changing default to height
+    defaults['defaults']['extent_dimension'] = 'height'
+    
+    response = JSONModel::HTTP.post_json(url, ASUtils.to_json(defaults))
+
+    expect(response.status).to eq(200)
+
+    updated_defaults = JSONModel::HTTP.get_json(uri)
+
+    expect(updated_defaults['defaults']['name']).to eq('DEFAULT BOX')
+    expect(updated_defaults['defaults']['extent_dimension']).to eq('height')
+  end
+
 end
--- frontend/spec/features/default_values_spec.rb
+++ frontend/spec/features/default_values_spec.rb
@@ -51,4 +51,58 @@ describe 'Default Form Values', js: true do
     visit '/accessions'
     expect(page).not_to have_content('Edit Default Values')
   end
+
+  it 'verifies container profile extent dimension defaults behavior' do
+    visit('/preferences/0/edit?global=true')
+    check('preference[defaults][default_values]')
+    click_button('Save')
+    expect(page).to have_checked_field('preference[defaults][default_values]')
+    expect(page).to have_content('Preferences updated')
+
+    # First verify that without any defaults set, new container profiles default to "Height"
+    visit('/container_profiles/new')
+    expect(page).to have_select('container_profile_extent_dimension_', selected: 'Height')
+
+    # Now set defaults to "Width" (addressing the Jira issue requirement)
+    visit '/container_profiles'
+    click_link('Edit Default Values')
+    wait_for_ajax
+    
+    fill_in 'container_profile_name_', with: 'DEFAULT BOX'
+    select 'Width', from: 'container_profile_extent_dimension_'
+    fill_in 'container_profile_depth_', with: '12'
+    fill_in 'container_profile_height_', with: '15' 
+    fill_in 'container_profile_width_', with: '10'
+    select 'Inches', from: 'container_profile_dimension_units_'
+    
+    click_on('Save Container Profile')
+    expect(page).to have_content('Defaults Updated')
+
+    # Verify defaults are applied with "Width" selected
+    visit('/container_profiles/new')
+    expect(page).to have_field('container_profile_name_', with: 'DEFAULT BOX')
+    expect(page).to have_select('container_profile_extent_dimension_', selected: 'Width')
+    expect(page).to have_field('container_profile_depth_', with: '12')
+    expect(page).to have_field('container_profile_height_', with: '15')
+    expect(page).to have_field('container_profile_width_', with: '10')
+    expect(page).to have_select('container_profile_dimension_units_', selected: 'Inches')
+
+    # Now test changing defaults to "Height" 
+    visit '/container_profiles'
+    click_link('Edit Default Values')
+    wait_for_ajax
+    
+    select 'Height', from: 'container_profile_extent_dimension_'
+    click_on('Save Container Profile')
+    expect(page).to have_content('Defaults Updated')
+
+    # Verify defaults now use "Height"
+    visit('/container_profiles/new')
+    expect(page).to have_field('container_profile_name_', with: 'DEFAULT BOX')
+    expect(page).to have_select('container_profile_extent_dimension_', selected: 'Height')
+    expect(page).to have_field('container_profile_depth_', with: '12')
+    expect(page).to have_field('container_profile_height_', with: '15')
+    expect(page).to have_field('container_profile_width_', with: '10')
+    expect(page).to have_select('container_profile_dimension_units_', selected: 'Inches')
+  end
 end
