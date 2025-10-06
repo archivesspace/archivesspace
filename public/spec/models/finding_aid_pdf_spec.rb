72d739bb7f5941c664ed49a024d32c099275e176
Harris Papazoglou <hrrsppzgl@gmail.com>
2025-10-06 16:38:44 +0300

    ANW-2217: Fix top container title-with-semicolon bug for PUI pdf export (#3729)
    ---------
    
    Co-authored-by: Brian Zelip <brian.zelip@lyrasis.org>

--- public/spec/models/finding_aid_pdf_spec.rb
+++ public/spec/models/finding_aid_pdf_spec.rb
@@ -80,4 +80,39 @@ describe "FindingAidPDF" do
     expect(html).to include "Archival Object <b>Title</b> #{now} title&amp;title"
     expect(html).to include "Archival Object <b>Title</b> &amp;; &amp;&amp;; &amp;&amp;&amp;; #{now} &amp;title;"
   end
+
+  it "generates html for pdf export with container indicators containing semicolons" do
+    now = Time.now.to_i
+
+    repository = create(:repo, :repo_code => "finding_aid_pdf_#{now}", publish: true)
+    set_repo repository
+
+    resource = create(:resource, title: "Resource Title #{now}", publish: true)
+
+    top_container = create(:json_top_container,
+      indicator: "Series 1: accession 2",
+      type: "box")
+
+    archival_object = create(:archival_object,
+      title: "Archival Object with Semicolon Container #{now}",
+      resource: { 'ref' => resource.uri },
+      publish: true,
+      instances: [build(:json_instance, {
+        instance_type: 'text',
+        sub_container: build(:json_sub_container, {
+          top_container: { ref: top_container.uri },
+          type_2: 'folder',
+          indicator_2: '1'
+        })
+      })]
+    )
+
+    run_indexers
+
+    pdf = FindingAidPDF.new(repository.id, resource.id, ArchivesSpaceClient.instance, nil)
+    source_file = pdf.source_file
+    html = File.read(source_file)
+
+    expect(html).to include "box series 1: accession 2"
+  end
 end
