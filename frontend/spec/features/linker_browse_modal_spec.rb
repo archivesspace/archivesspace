a016a9a109e992bcbb53808d7e490e9075825784
Brian Zelip <brian.zelip@lyrasis.org>
2025-10-23 11:04:43 -0400

    ANW-2026 Fix table sorting on all linker browse modals (#3748)
    ---------
    
    Co-authored-by: Thimios Dimopulos <thimios.dimopulos@lyrasis.com>

---
+++ frontend/spec/features/linker_browse_modal_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Linker Browse Modal', js: true do
+  describe 'results table sorting' do
+    let!(:subject1) { create(:subject) }
+    let!(:subject2) { create(:subject) }
+
+    before :each do
+      run_index_round
+      login_admin
+    end
+
+    RSpec::Matchers.define :have_first_row_title do |expected|
+      match do |page|
+        page.has_css?('.modal-dialog #tabledSearchResults tbody tr:first-child > td.title', text: expected)
+      end
+    end
+
+    it 'works by sort buttons and column headers' do
+      visit 'accessions/new'
+
+      aggregate_failures 'initial ascending sort' do
+        expect(page).to have_css('#accession_subjects_')
+        click_on 'Add Subject'
+        within '#accession_subjects_' do
+          find('.linker-wrapper .dropdown-toggle', match: :first).click
+          find('.dropdown-menu .linker-browse-btn', match: :first).click
+        end
+
+        expect(page).to have_first_row_title(subject1.title)
+      end
+
+      aggregate_failures 'descending sort' do
+        within '#pagination-summary-primary-sort-opts' do
+          click_on 'Terms Ascending'
+          find('a', text: 'Terms').hover
+          click_on 'Descending'
+        end
+        expect(page).to have_first_row_title(subject2.title)
+      end
+
+      aggregate_failures 'final ascending sort' do
+        within '.modal-dialog #tabledSearchResults' do
+          click_on 'Terms'
+        end
+        expect(page).to have_first_row_title(subject1.title)
+      end
+    end
+  end
+end
