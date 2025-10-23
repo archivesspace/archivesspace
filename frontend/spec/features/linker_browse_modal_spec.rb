db59921652c9e7c020154195e39c6a3db265aabc
Thimios Dimopulos <thimios.dimopulos@lyrasis.com>
2025-10-23 13:05:20 +0300

    use a custom matcher instead of a method to follow the rspec paradigm

--- frontend/spec/features/linker_browse_modal_spec.rb
+++ frontend/spec/features/linker_browse_modal_spec.rb
@@ -11,8 +11,10 @@ describe 'Linker Browse Modal', js: true do
       login_admin
     end
 
-    def expect_first_row_title(expected_text)
-      expect(page).to have_css('.modal-dialog #tabledSearchResults tbody tr:first-child > td.title', text: expected_text)
+    RSpec::Matchers.define :have_first_row_title do |expected|
+      match do |page|
+        page.has_css?('.modal-dialog #tabledSearchResults tbody tr:first-child > td.title', text: expected)
+      end
     end
 
     it 'works by sort buttons and column headers' do
@@ -25,7 +27,8 @@ describe 'Linker Browse Modal', js: true do
           find('.linker-wrapper .dropdown-toggle', match: :first).click
           find('.dropdown-menu .linker-browse-btn', match: :first).click
         end
-        expect_first_row_title(subject1.title)
+
+        expect(page).to have_first_row_title(subject1.title)
       end
 
       aggregate_failures 'descending sort' do
@@ -34,14 +37,14 @@ describe 'Linker Browse Modal', js: true do
           find('a', text: 'Terms').hover
           click_on 'Descending'
         end
-        expect_first_row_title(subject2.title)
+        expect(page).to have_first_row_title(subject2.title)
       end
 
       aggregate_failures 'final ascending sort' do
         within '.modal-dialog #tabledSearchResults' do
           click_on 'Terms'
         end
-        expect_first_row_title(subject1.title)
+        expect(page).to have_first_row_title(subject1.title)
       end
     end
   end
