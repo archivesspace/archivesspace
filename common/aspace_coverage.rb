1ee16bef3888d131599288dce08329fd330b38e8
Thimios <thimios.dimopulos@lyrasis.org>
2025-10-31 21:01:39 +0200

    ANW-2276 add coverage from running the e2e tests to our test coverage meter

--- common/aspace_coverage.rb
+++ common/aspace_coverage.rb
@@ -1,20 +1,19 @@
 module ASpaceCoverage
-
   require 'asutils'
   require 'tmpdir'
   require 'pp'
-  require 'simplecov'
 
-  def self.start(test_name, env = nil)
+  def self.start(group_name, env = nil)
+    require 'simplecov'
     SimpleCov.root(ASUtils.find_base_directory)
-    SimpleCov.coverage_dir("coverage/#{ENV.fetch('COVERAGE_REPORT_DIR', '')}/#{test_name}")
-    SimpleCov.command_name test_name + ":#{Time.now.to_i}:#{$$}"
+    SimpleCov.coverage_dir("coverage/#{ENV.fetch(' COVERAGE_REPORT_DIR', '')}/#{group_name}")
+    SimpleCov.command_name group_name + ":#{Time.now.to_i}:#{$$}"
     SimpleCov.merge_timeout 3600
 
-    puts "Starting coverage reporting for: #{test_name}"
+    puts "Starting coverage reporting for: #{group_name}"
 
     SimpleCov.start(env) do
-      add_group test_name, "."
+      add_group group_name, "."
       add_filter "config/"
       add_filter "build/gems"
       add_filter "common/spec"
