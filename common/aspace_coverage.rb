d8602683db76920ac53b68a46dc37949c947a295
Thimios <thimios.dimopulos@lyrasis.org>
2025-11-01 23:32:30 +0200

    save coverage reports to data dir

--- common/aspace_coverage.rb
+++ common/aspace_coverage.rb
@@ -5,12 +5,20 @@ module ASpaceCoverage
 
   def self.start(group_name, env = nil)
     require 'simplecov'
+    require 'fileutils'
+
     SimpleCov.root(ASUtils.find_base_directory)
-    SimpleCov.coverage_dir("coverage/#{ENV.fetch(' COVERAGE_REPORT_DIR', '')}/#{group_name}")
+
+    # Always save coverage reports to the data directory
+    base_dir = ENV.fetch('APPCONFIG_DATA_DIR', File.join(Dir.home, 'ArchivesSpace'))
+    coverage_dir = File.join(base_dir, 'coverage', ENV.fetch('COVERAGE_REPORT_DIR', ''), group_name)
+    SimpleCov.coverage_dir(coverage_dir)
+
     SimpleCov.command_name group_name + ":#{Time.now.to_i}:#{$$}"
     SimpleCov.merge_timeout 3600
 
     puts "Starting coverage reporting for: #{group_name}"
+    puts "Coverage directory: #{coverage_dir}"
 
     SimpleCov.start(env) do
       add_group group_name, "."
