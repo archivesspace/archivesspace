6da1b35b69380939eaf1f831c13d0529b7aea02e
Lora Woodford <lorawoodford@gmail.com>
2025-11-18 06:27:45 -0500

    ANW-2546 Add apple-touch-icon to frontend and public (#3789)

--- frontend/spec/features/favicons_spec.rb
+++ frontend/spec/features/favicons_spec.rb
@@ -4,11 +4,13 @@ require 'rails_helper.rb'
 describe 'Favicons', js: true do
   let(:png) { 'link[rel="icon"][type="image/png"][href="/favicon-AS.png"]' }
   let(:svg) { 'link[rel="icon"][type="image/svg+xml"][href="/favicon-AS.svg"]' }
+  let(:apple) { 'link[rel="apple-touch-icon"][type="image/png"][href="/favicon-AS.png"]' }
 
   it 'are present by default as png and svg' do
     visit '/'
     expect(page).to have_css(png, visible: false)
     expect(page).to have_css(svg, visible: false)
+    expect(page).to have_css(apple, visible: false)
     visit '/favicon-AS.png'
     visit '/favicon-AS.svg'
   end
@@ -19,5 +21,6 @@ describe 'Favicons', js: true do
     visit '/'
     expect(page).to_not have_css(png, visible: false)
     expect(page).to_not have_css(svg, visible: false)
+    expect(page).to_not have_css(apple, visible: false)
   end
 end
