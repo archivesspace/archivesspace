99b08cc9af75264634d63a87db0ca691a2143200
Thimios <thimios.dimopulos@lyrasis.org>
2025-12-16 15:18:33 +0200

    ANW-2256 fix display of system info and logs on staff UI

---
+++ frontend/spec/controllers/system_info_controller_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_helper'
+
+describe SystemInfoController, type: :controller do
+  render_views
+
+  before(:each) do
+    session = User.login('admin', 'admin')
+    User.establish_session(controller, session, 'admin')
+  end
+
+  describe 'show system info action' do
+    before(:each) do
+      allow(JSONModel::HTTP).to receive(:get_json).and_call_original
+    end
+
+    context "with app_context: 'backend_info' param" do
+      it 'fetches the system info from the backend' do
+        get :show, params: { app_context: 'backend_info' }
+
+        expect(JSONModel::HTTP).to have_received(:get_json).with('/system/info')
+        expect(response.status).to eq(200)
+        expect(response.body).to include 'APPCONFIG'
+        expect(response.body).to include 'Backend System Information'
+      end
+    end
+
+    context "with app_context: 'frontend_info' param" do
+      it 'fetches the system info from the frontend' do
+        get :show, params: { app_context: 'frontend_info' }
+
+        expect(JSONModel::HTTP).not_to have_received(:get_json).with('/system/info')
+        expect(response.status).to eq(200)
+        expect(response.body).to include 'APPCONFIG'
+        expect(response.body).to include 'Frontend System Information'
+      end
+    end
+  end
+
+  describe 'stream log action' do
+    before(:each) do
+      allow(JSONModel::HTTP).to receive(:get_response).and_call_original
+    end
+
+    context "with app_context: 'backend_log' param" do
+      it 'fetches the system log from the backend' do
+        get :stream_log, params: { app_context: 'backend_log' }
+
+        expect(response.status).to eq(200)
+        expect(JSONModel::HTTP).to have_received(:get_response).with(URI.parse(AppConfig[:backend_url] + "/system/log"))
+      end
+    end
+
+    context "with app_context: 'frontend_log' param" do
+      it 'fetches the system log from the frontend' do
+        get :stream_log, params: { app_context: 'frontend_log' }
+
+        expect(response.status).to eq(200)
+        expect(JSONModel::HTTP).not_to have_received(:get_response).with(URI.parse(AppConfig[:backend_url] + "/system/log"))
+      end
+    end
+  end
+end
