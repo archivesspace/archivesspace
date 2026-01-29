47855f1314fcac4ff303ffeb44a5d464c6339bbc
Thimios <thimios.dimopulos@lyrasis.org>
2026-01-29 21:35:11 +0200

    ANW-2519 fix flakey default values e2e specs

--- e2e-tests/staff_features/locations/step_definitions/location_edit_default_values.rb
+++ e2e-tests/staff_features/locations/step_definitions/location_edit_default_values.rb
@@ -7,9 +7,12 @@ end
 Then 'the new Location form has the following default values' do |form_values_table|
   visit "#{STAFF_URL}/locations/new"
 
+  expect(page).to have_selector('h2', visible: true, text: 'Location')
   wait_for_ajax
 
-  form_values_table.hashes.each do |row|
-    expect(page).to have_field(row['form_field'], with: /#{Regexp.quote(row['form_value'])}/i)
+  aggregate_failures do
+    form_values_table.hashes.each do |row|
+      expect(page).to have_field(row['form_field'], with: /#{Regexp.quote(row['form_value'])}/i)
+    end
   end
 end
