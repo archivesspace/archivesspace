520e653386c8267ab2ceb31107298dccb28c6496
Brian Zelip <brian.zelip@lyrasis.org>
2026-02-26 11:40:52 -0500

    ANW-2632 Make favicon customization consistent with logo customization

--- public/spec/features/favicons_spec.rb
+++ public/spec/features/favicons_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper.rb'
 require 'rails_helper.rb'
 
 describe 'Favicons', js: true do
-  let(:png) { 'link[rel="icon"][type="image/png"][href="/favicon-AS.png"]' }
-  let(:svg) { 'link[rel="icon"][type="image/svg+xml"][href="/favicon-AS.svg"]' }
-  let(:apple) { 'link[rel="apple-touch-icon"][type="image/png"][href="/favicon-AS.png"]' }
+  let(:png) { 'link[rel="icon"][type="image/png"][href*="favicon-AS"]' }
+  let(:svg) { 'link[rel="icon"][type="image/svg+xml"][href*="favicon-AS"]' }
+  let(:apple) { 'link[rel="apple-touch-icon"][type="image/png"][href*="favicon-AS"]' }
 
   it 'are present by default as png and svg' do
     visit '/'
     expect(page).to have_css(png, visible: false)
     expect(page).to have_css(svg, visible: false)
     expect(page).to have_css(apple, visible: false)
-    visit '/favicon-AS.png'
-    visit '/favicon-AS.svg'
+
+    png_href = page.find(png, visible: false)[:href]
+    svg_href = page.find(svg, visible: false)[:href]
+    visit png_href
+    visit svg_href
   end
 
   it 'are not present when configured as such' do
