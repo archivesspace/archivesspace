07924442c43aa8e497ac5ce1fc349c4a506a95b3
Thimios <thimios.dimopulos@lyrasis.org>
2026-03-02 13:27:54 -0500

    Remove unnecessary waits and improve filter reliability
    - Eliminate redundant wait_for_ajax in checkbox selection# *Crack*... Parsing shards of logic...
    
    ANW-2519 digital object deletion flakinesss fix

--- e2e-tests/staff_features/digital_objects/step_definitions/digital_object_shared.rb
+++ e2e-tests/staff_features/digital_objects/step_definitions/digital_object_shared.rb
@@ -52,23 +52,8 @@ end
 
 When 'the user filters by text with the Digital Object title' do
   fill_in 'Filter by text', with: "Digital Object Identifier #{@uuid}"
-
-  find('#filter-text').send_keys(:enter)
-
-  rows = []
-  checks = 0
-
-  while checks < 5
-    checks += 1
-
-    begin
-      rows = all('tr', text: @uuid)
-    rescue Selenium::WebDriver::Error::JavascriptError
-      sleep 1
-    end
-
-    break if rows.length == 1
-  end
+  click_button 'Filter by text'
+  expect(page).to have_css('#tabledSearchResults tbody tr', count: 1)
 end
 
 Given 'the user is on the Digital Object view page' do
