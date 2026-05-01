e0d1a5d621b812b53c97293b04ecee6dd9120035
Lora Woodford <lora.woodford@lyrasis.org>
2026-05-01 15:39:08 -0400

    ANW-2723 mlc data model (#4056)

---
+++ backend/spec/model_language_and_script_of_description_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'LanguageAndScriptOfDescription model' do
+  it "defaults is_primary to false" do
+    record = LanguageAndScriptOfDescription.create_from_json(
+      JSONModel(:language_and_script_of_description).from_hash(
+        "language" => "fre",
+        "script"   => "Latn"
+      )
+    )
+
+    expect(LanguageAndScriptOfDescription[record.id].is_primary).to eq(0)
+  end
+
+  it "requires language to be present" do
+    expect {
+      LanguageAndScriptOfDescription.create_from_json(
+        JSONModel(:language_and_script_of_description).from_hash(
+          "script" => "Latn"
+        )
+      )
+    }.to raise_error(JSONModel::ValidationException)
+  end
+
+  it "requires script to be present" do
+    expect {
+      LanguageAndScriptOfDescription.create_from_json(
+        JSONModel(:language_and_script_of_description).from_hash(
+          "language" => "eng"
+        )
+      )
+    }.to raise_error(JSONModel::ValidationException)
+  end
+end
---
+++ common/schemas/language_and_script_of_description.rb
@@ -0,0 +1,13 @@
+{
+  :schema => {
+    "$schema" => "http://www.archivesspace.org/archivesspace.json",
+    "version" => 1,
+    "type" => "object",
+
+    "properties" => {
+      "language"   => {"type" => "string", "dynamic_enum" => "language_iso639_2", "ifmissing" => "error"},
+      "script"     => {"type" => "string", "dynamic_enum" => "script_iso15924", "ifmissing" => "error"},
+      "is_primary" => {"type" => "boolean", "default" => false},
+    },
+  },
+}
