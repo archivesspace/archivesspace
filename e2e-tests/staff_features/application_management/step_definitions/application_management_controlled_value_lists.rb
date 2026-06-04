9e17308feb483b2c2c06724af1304c1cec4a1411
marthatenney <martha.tenney@lyrasis.com>
2026-06-04 16:02:37 +0000

    fixed style per rubocop

--- e2e-tests/staff_features/application_management/step_definitions/application_management_controlled_value_lists.rb
+++ e2e-tests/staff_features/application_management/step_definitions/application_management_controlled_value_lists.rb
@@ -2,10 +2,9 @@
 
 Given 'the user selects {string} in the List Name dropdown menu' do |string|
   select string, from: 'enum_selector' do
-  wait_for_ajax
+    wait_for_ajax
   end
 end
-
 
 Given 'the value {string} is added to the list' do |string|
   expect(page).to have_css('tbody tr td', text: string)
