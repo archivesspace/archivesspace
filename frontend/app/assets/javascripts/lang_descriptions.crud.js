02be4076c29a797a53e5f06d38a88a445ca8e205
Lora Woodford <lora.woodford@lyrasis.org>
2026-06-04 15:50:50 -0400

    ANW-2790 Change detection bug (#4108)

--- frontend/app/assets/javascripts/lang_descriptions.crud.js
+++ frontend/app/assets/javascripts/lang_descriptions.crud.js
@@ -4,11 +4,22 @@ $(function () {
   $(document).bind('subrecordcreated.aspace', function (event, object_name) {
     if (object_name === 'lang_description') {
       // If this is the first lang_description subrecord, then make sure it's set as primary
-      const $langDescriptions = $(
+      const langDescriptions = document.querySelector(
         '#resource_lang_descriptions_, #accession_lang_descriptions_, #digital_object_lang_descriptions_'
       );
-      if ($langDescriptions.find('ul').children().length == 1) {
-        $langDescriptions.find('.is-representative-toggle').click();
+      const ul = langDescriptions.querySelector('ul');
+      if (ul.children.length === 1) {
+        const toggle = langDescriptions.querySelector(
+          '.is-representative-toggle'
+        );
+        const isPrimary = langDescriptions.querySelector(
+          'input[id$="_is_primary_"]'
+        );
+        const alreadyPrimary = isPrimary?.value === '1';
+
+        if (toggle && !alreadyPrimary) {
+          toggle.click();
+        }
       }
     }
   });
