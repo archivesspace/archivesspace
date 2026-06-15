f0f051de48373fa192d33b49126084b15e9271e4
David K Uspal <dku@null.net~>
2026-06-15 16:17:41 -0400

    ANW-2659: Update resources index view for Bootstrap 5, fix modals and button styles

--- public/app/assets/javascripts/cite.js
+++ public/app/assets/javascripts/cite.js
@@ -1,7 +1,23 @@
+var citeModal;
+
 function setupCite() {
-  $('#cite_sub').submit(function () {
+  $('#cite_sub').on('submit', function (e) {
+    e.preventDefault();
     new Clipboard('.clip-btn');
-    $('#cite_modal').modal('show');
+    citeModal = new bootstrap.Modal(document.getElementById('cite_modal'));
+    citeModal.show();
     return false;
   });
+
+  document.getElementById('cite_modal').addEventListener('hidden.bs.modal', function() {
+    document.body.classList.remove('modal-open');
+    var backdrops = document.querySelectorAll('.modal-backdrop');
+    backdrops.forEach(function(backdrop) {
+      backdrop.remove();
+    });
+  });
 }
+
+$(document).ready(function() {
+  setupCite();
+});
