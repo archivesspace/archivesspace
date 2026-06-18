96dcaf04023ba174a1687a11911519a63d7ac04b
David Uspal <david.uspal@villanova.edu>
2026-06-18 13:00:06 -0400

    PUI Bootstrap v5: ANW-2672, ANW-2658 Update repositories index and show views (#4130)
    ---------
    
    Co-authored-by: David K Uspal <david.uspal@villanova.edu>
    Co-authored-by: Brian Zelip <brian.zelip@lyrasis.org>

--- public/spec/features/subjects_spec.rb
+++ public/spec/features/subjects_spec.rb
@@ -34,7 +34,7 @@ describe 'Subjects', js: true do
 
       click_on 'Repositories'
       click_on 'Test Repo 1'
-      find('#whats-in-container form .btn.btn-default.subject').click
+      find('#whats-in-container form .btn.subject').click
 
       expect(page).to_not have_text Pathname.new(current_path).parent.to_s
     end
