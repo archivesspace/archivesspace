54cfc7c1a254c2d8d4d8d7a62f5693035813558c
Thimios <thimios.dimopulos@lyrasis.org>
2026-06-19 16:56:49 +0300

    try to replace the wait loop with a wait_for_ajax call

--- e2e-tests/staff_features/assessments/step_definitions/assessment_edit.rb
+++ e2e-tests/staff_features/assessments/step_definitions/assessment_edit.rb
@@ -5,18 +5,5 @@ When 'the user filters by the text {string}' do |string|
 
   find('#filter-text').send_keys(:enter)
 
-  rows = []
-  checks = 0
-
-  while checks < 5
-    checks += 1
-
-    begin
-      rows = all('tr', text: @uuid)
-    rescue Selenium::WebDriver::Error::JavascriptError
-      sleep 1
-    end
-
-    break if rows.length == 1
-  end
+  wait_for_ajax
 end
