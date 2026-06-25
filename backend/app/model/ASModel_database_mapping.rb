3c16798b3a6d2de2840702af266534ad0e10fcb4
Thimios <thimios.dimopulos@lyrasis.org>
2026-06-25 14:03:20 +0300

    ANW-2717 fix linking agents to resources on derby embedded demo db

--- backend/app/model/ASModel_database_mapping.rb
+++ backend/app/model/ASModel_database_mapping.rb
@@ -2,30 +2,34 @@ module ASModel
   # Some low-level details of mapping certain Ruby types to database types.
   module DatabaseMapping
 
+    JSON_TO_DB_MAPPINGS = {
+      'boolean' => {
+        :description => "JSON booleans become DB integers",
+        :json_to_db => ->(bool) { bool ? 1 : 0 },
+        :db_to_json => ->(int) { int === 1 }
+      },
+      'date' => {
+        :description => "Date strings become dates",
+        :json_to_db => ->(s) { s.nil? ? s : Date.parse(s) },
+        :db_to_json => ->(date) { date.nil? ? date : date.strftime('%Y-%m-%d') }
+      }
+    }
+
+    def self.prepare_booleans_for_db(hash)
+      bool_to_int = JSON_TO_DB_MAPPINGS['boolean'][:json_to_db]
+      hash.transform_values { |v| [true, false].include?(v) ? bool_to_int.call(v) : v }
+    end
+
     def self.included(base)
       base.extend(ClassMethods)
     end
 
     module ClassMethods
-      JSON_TO_DB_MAPPINGS = {
-        'boolean' => {
-          :description => "JSON booleans become DB integers",
-          :json_to_db => ->(bool) { bool ? 1 : 0 },
-          :db_to_json => ->(int) { int === 1 }
-        },
-        'date' => {
-          :description => "Date strings become dates",
-          :json_to_db => ->(s) { s.nil? ? s : Date.parse(s) },
-          :db_to_json => ->(date) { date.nil? ? date : date.strftime('%Y-%m-%d') }
-        }
-      }
-
-
       def prepare_for_db(jsonmodel_class, hash)
         schema = jsonmodel_class.schema
         hash = hash.clone
         schema['properties'].each do |property, definition|
-          mapping = JSON_TO_DB_MAPPINGS[definition['type']]
+          mapping = ASModel::DatabaseMapping::JSON_TO_DB_MAPPINGS[definition['type']]
           if mapping && hash.has_key?(property)
             hash[property] = mapping[:json_to_db].call(hash[property])
           end
@@ -45,7 +49,7 @@ module ASModel
       def map_db_types_to_json(schema, hash)
         hash = hash.clone
         schema['properties'].each do |property, definition|
-          mapping = JSON_TO_DB_MAPPINGS[definition['type']]
+          mapping = ASModel::DatabaseMapping::JSON_TO_DB_MAPPINGS[definition['type']]
 
           property = property.intern
           if mapping && hash.has_key?(property)
