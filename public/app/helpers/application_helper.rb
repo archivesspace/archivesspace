fb00a5c4146c2738fe53c682d4c09dea4748b422
Brian Zelip <brian.zelip@lyrasis.org>
2026-08-05 08:33:15 -0400

    PUI Bootstrap v5: ANW-2915 Fix flash message markup

--- public/app/helpers/application_helper.rb
+++ public/app/helpers/application_helper.rb
@@ -8,12 +8,9 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
-        concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
-   concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
-   concat content_tag(:span, 'Close', class: 'visually-hidden')
- end)
-        concat (message.is_a?(Array) ? message.join('<br/>').html_safe : message.html_safe)
+      concat(content_tag(:div, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show", role: 'alert') do
+        concat(message.is_a?(Array) ? message.join('<br/>').html_safe : message.html_safe)
+        concat(content_tag(:button, '', class: 'btn-close', type: 'button', data: { 'bs-dismiss' => 'alert' }, 'aria-label' => 'Close'))
       end)
     end
     nil
