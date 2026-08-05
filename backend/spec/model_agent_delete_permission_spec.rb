27a84e0212db112a3ce3d26869c54fb776796632
lorawoodford <lorawoodford@gmail.com>
2026-08-05 15:55:30 -0400

    ANW-2829 Deleting cross-repo agents

---
+++ backend/spec/model_agent_delete_permission_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'delete_agent_record_linked_elsewhere permission' do
+
+  it "is defined at the global level" do
+    permission = Permission[:permission_code => "delete_agent_record_linked_elsewhere"]
+
+    expect(permission).not_to be_nil
+    expect(permission.level).to eq("global")
+  end
+
+
+  it "is granted to the administrators group automatically" do
+    admins = Group.any_repo[:group_code => Group.ADMIN_GROUP_CODE]
+
+    expect(admins.permission.map {|permission| permission[:permission_code]}).
+      to include("delete_agent_record_linked_elsewhere")
+  end
+
+
+  it "is not granted to an ordinary repository manager via manage_agent_record" do
+    repo = create(:repo)
+
+    group = Group.create_from_json(build(:json_group), :repo_id => repo.id)
+    group.grant("manage_agent_record")
+
+    user = make_test_user("repo_manager_agent_test")
+    group.add_user(user)
+
+    RequestContext.put(:repo_id, repo.id)
+
+    expect(User[:username => user.username].can?("delete_agent_record")).to be_truthy
+    expect(User[:username => user.username].can?("delete_agent_record_linked_elsewhere")).to be_falsey
+  end
+
+end
