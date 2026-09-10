7ffca748118d8076eb7c4de923332036bef9cba8
Thimios <thimios.dimopulos@lyrasis.org>
2026-09-10 20:43:15 +0300

    ANW-2707 multiple repository and sponsor oai sets

--- backend/app/controllers/oai_config.rb
+++ backend/app/controllers/oai_config.rb
@@ -41,16 +41,7 @@ class ArchivesSpaceService < Sinatra::Base
     oc = OAIConfig.first
     json = params[:oai_config]
 
-    oc.update(:oai_repository_name     => json["oai_repository_name"],
-              :oai_admin_email         => json["oai_admin_email"],
-              :oai_record_prefix       => json["oai_record_prefix"],
-              :repo_set_codes          => json["repo_set_codes"],
-              :sponsor_set_names       => json["sponsor_set_names"],
-              :repo_set_description    => json["repo_set_description"],
-              :sponsor_set_description => json["sponsor_set_description"],
-              :repo_set_name           => json["repo_set_name"],
-              :sponsor_set_name        => json["sponsor_set_name"]
-              )
+    oc.update_from_json(json)
 
     updated_response(oc, json)
   end
